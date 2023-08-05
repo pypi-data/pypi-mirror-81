f8718a3c27103543f7233a179070205442e0bc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package psy-simple/1.3.0/psy-simple-1.3.0.tar.gz

---
+++ packages/psy-simple/psy-simple-1.3.0.tar.gz/psy-simple-1.3.0/psy_simple/__init__.py
@@ -0,0 +1,17 @@
+"""psy-simple: The psyplot plugin for simple visualizations
+
+This package contains the basic plotters for simple interactive visualization
+tasks with the psyplot visualization framework.
+"""
+from psy_simple.version import __version__
+
+
+__author__ = "Philipp S. Sommer"
+__copyright__ = "Copyright 2015 - 2020, Philipp S. Sommer"
+__credits__ = ["Philipp S. Sommer"]
+__license__ = "GPL-2.0-only"
+
+__maintainer__ = "Philipp S. Sommer"
+__email__ = "philipp.sommer@hzg.de"
+
+__status__ = "Production"
---
+++ packages/psy-simple/psy-simple-1.3.0.tar.gz/psy-simple-1.3.0/psy_simple/version.py
@@ -0,0 +1,3 @@
+# version of psy-simple
+
+__version__ = "1.3.0"

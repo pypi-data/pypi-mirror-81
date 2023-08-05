753cbf7698e24ed7c2836d04a8677de6e1b9ab70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:49 +0000

    Add package pyinspect/0.0.1/pyinspect-0.0.1.tar.gz

---
+++ packages/pyinspect/pyinspect-0.0.1.tar.gz/pyinspect-0.0.1/pyinspect/__init__.py
@@ -0,0 +1,6 @@
+from pyinspect.exceptions import install, print_exception
+
+install()
+
+from pyinspect.utils import print_function
+from pyinspect.find import find
---
+++ packages/pyinspect/pyinspect-0.0.1.tar.gz/pyinspect-0.0.1/pyinspect/exceptions.py
@@ -0,0 +1,93 @@
+import sys
+import inspect
+import rich
+from rich.scope import render_scope
+from rich.traceback import Traceback
+from rich import print
+from rich.console import Console
+from rich.style import Style
+from rich.theme import Theme
+
+from pyinspect.utils import timestamp
+
+# Override some of rich's default parameters
+rich.default_styles.DEFAULT_STYLES["scope.border"] = Style(color="green")
+
+
+def inspect_traceback(tb):
+    # Get the whole traceback stack
+    while 1:
+        if not tb.tb_next:
+            break
+        tb = tb.tb_next
+    stack = []
+    f = tb.tb_frame
+    while f:
+        stack.append(f)
+        f = f.f_back
+    stack.reverse()
+    return stack
+
+
+def get_locals():
+    """
+        Returns a rich rendering of the variables in the local scope
+    """
+    caller = inspect.stack()[1]
+
+    locals_map = {
+        key: value
+        for key, value in caller.frame.f_locals.items()
+        if not key.startswith("__")
+    }
+    return render_scope(locals_map, title="[i]locals")
+
+
+def print_exception(message=None, traceback=None, **kwargs):
+    """
+        It prints a nicely formatted traceback for an exception, 
+        including the variables in the local scope.
+
+        :param message: str, optional. A message to add to the start of the traceback.
+                If none is passed a default message is used
+    """
+    # Get message
+    if message is None:
+        message = f":x:  [bold]Error -- [/bold][grey]{timestamp()} -- :x:\n"
+
+    # Get traceback if not passed
+    traceback = Traceback(**kwargs)
+
+    # print
+    print(message, traceback, "\n", get_locals(), sep="\n")
+
+
+def install():
+    """
+        Install an improved rich traceback handler (it includes a view of the local variables).
+        Once installed, any tracebacks will be printed with syntax highlighting and rich formatting.
+    """
+    traceback_console = Console(
+        file=sys.stderr, theme=Theme(rich.default_styles.DEFAULT_STYLES)
+    )
+
+    def excepthook(
+        type_, value, traceback,
+    ):
+        stack = inspect_traceback(traceback)
+
+        locals_panels = []
+        for n, frame in enumerate(stack):
+            locals_panels.append(
+                render_scope(frame.f_locals, title=f"[i]locals frame {n}")
+            )
+
+        traceback_console.print(
+            Traceback.from_exception(type_, value, traceback,),
+            *locals_panels,
+            sep="\n",
+        )
+
+    old_excepthook = sys.excepthook
+    sys.excepthook = excepthook
+    return old_excepthook

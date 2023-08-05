470b3b7d6db160dc259d8fca6b4a0520a801a12c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:42 +0000

    Add package steamspypi/1.1.0/steamspypi-1.1.0.tar.gz

---
+++ packages/steamspypi/steamspypi-1.1.0.tar.gz/steamspypi-1.1.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='steamspypi',
+    version='1.1.0',
+    author='Wok',
+    author_email='wok@tuta.io',
+    description='SteamSpy API on PyPI',
+    keywords=['steam', 'steamspy', 'api'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/woctezuma/steamspypi',
+    download_url='https://github.com/woctezuma/steamspypi/archive/1.1.0.tar.gz',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Games/Entertainment',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)

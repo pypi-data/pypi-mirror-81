746a8f1fdf4ba3f82a1345cb5f3760a6ebded229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:59 +0000

    Add package lyncs-cppyy/0.1.1/lyncs_cppyy-0.1.1.tar.gz

---
+++ packages/lyncs-cppyy/lyncs_cppyy-0.1.1.tar.gz/lyncs_cppyy-0.1.1/setup.py
@@ -0,0 +1,15 @@
+import glob
+from lyncs_setuptools import setup
+
+setup(
+    "lyncs_cppyy",
+    install_requires=["cppyy>=1.8.1", "numpy", "deprecated"],
+    keywords=[
+        "Lyncs",
+        "cppyy",
+    ],
+    extras_require={
+        "test": ["pytest", "pytest-cov", "meson", "ninja"],
+    },
+    data_files=[("test/cnumbers", glob.glob("test/cnumbers/*"))],
+)

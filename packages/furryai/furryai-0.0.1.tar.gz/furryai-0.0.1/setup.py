22dc9271fed89fa0e033a089fe7a3d355d2602cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:34 +0000

    Add package furryai/0.0.1/furryai-0.0.1.tar.gz

---
+++ packages/furryai/furryai-0.0.1.tar.gz/furryai-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name="furryai",
+    version="0.0.1",
+    description="Tries to *replicate* a furry (note: not actually an AI)",
+    url="",
+    author="BotNC",
+    author_email="botnc1234567890@gmail.com",
+    license="MIT",
+    packages=["furryai"],
+    zip_safe=False)
+    

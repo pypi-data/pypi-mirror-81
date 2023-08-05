de2d3bb81fdbaca1f73718a2dcea3008ea060bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:37 +0000

    Add package nbapis/0.1.20/nbapis-0.1.20.tar.gz

---
+++ packages/nbapis/nbapis-0.1.20.tar.gz/nbapis-0.1.20/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nbapis",  # Replace with your own username
+    version="0.1.20",
+    author="zhchangAuthor",
+    author_email="chang@nextbillion.ai",
+    description="nb api package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'fastapi>=0.53.2'
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)

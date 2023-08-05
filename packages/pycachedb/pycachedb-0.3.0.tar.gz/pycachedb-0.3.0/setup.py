6b136f8f41c1ca36eea0d64c535ac26cc18c97a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:01 +0000

    Add package pycachedb/0.3.0/pycachedb-0.3.0.tar.gz

---
+++ packages/pycachedb/pycachedb-0.3.0.tar.gz/pycachedb-0.3.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pycachedb',
+    version='0.3.0',
+    author="Savage Lasa",
+    author_email="superoutput@gmail.com",
+    description="PyCacheDB is a smallest embedded kay-value caching library.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/superoutput/pycachedb",
+    license='Savage',
+    install_requires=[
+        'ZODB >= 5.6.0',
+        'ZEO >= 5.2.2'
+    ],
+    scripts=['pycachedb'],
+    keywords='python cache caching cached embedded database db',
+    packages=['pycachedb'],
+    package_dir={'pycachedb':'src/main/pycachedb'},
+    package_data={},
+    include_package_data=True
+)

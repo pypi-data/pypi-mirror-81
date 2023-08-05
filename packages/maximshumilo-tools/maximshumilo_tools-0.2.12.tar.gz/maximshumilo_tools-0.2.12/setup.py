a97188b7e0f87613647727f402bea4292260e16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:18 +0000

    Add package maximshumilo-tools/0.2.12/maximshumilo_tools-0.2.12.tar.gz

---
+++ packages/maximshumilo-tools/maximshumilo_tools-0.2.12.tar.gz/maximshumilo_tools-0.2.12/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='maximshumilo_tools',
+      version='0.2.12',
+      description='Gaussian and Binomial distributions',
+      packages=['ms_tools', 'ms_tools.flask', 'ms_tools.yandex_cloud', 'ms_tools.flask.test_case'],
+      author='Shumilo Maksim',
+      author_email='shumilo.mk@gmail.com',
+      zip_safe=False)

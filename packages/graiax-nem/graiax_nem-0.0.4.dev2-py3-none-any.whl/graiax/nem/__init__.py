4b5c7fd10ad61e89eb19363d9161a0939da7d838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:41 +0000

    Add package graiax-nem/0.0.4.dev2/graiax_nem-0.0.4.dev2-py3-none-any.whl

---
+++ packages/graiax-nem/graiax_nem-0.0.4.dev2-py3-none-any.whl/graiax/nem/__init__.py
@@ -0,0 +1,28 @@
+'''
+ _   _   _____   __  __ 
+| \ | | | ____| |  \/  |
+|  \| | |  _|   | |\/| |
+| |\  | | |___  | |  | |
+|_| \_| |_____| |_|  |_|
+
+  Not Enough Messages
+    更好的消息解析器
+需配合 Graia Framework 使用
+
+
+Usage:
+``` python
+from graiaX.nem import NEM
+
+...
+
+@bcc.receiver(GroupMessage)
+async def group_message_listener(app: GraiaMiraiApplication, group: Group, _gm: GroupMessage):
+    nem = NEM(_gm)
+    await app.sendGroupMessage(group, MessageChain.create([Plain(nem.plain_message)]))
+
+...
+```
+'''
+
+from .nem import NEM

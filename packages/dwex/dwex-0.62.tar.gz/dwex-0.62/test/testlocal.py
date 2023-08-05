f5d6532ff2c882b4da57d2220da383548d366ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package dwex/0.62/dwex-0.62.tar.gz

---
+++ packages/dwex/dwex-0.62.tar.gz/dwex-0.62/test/testlocal.py
@@ -0,0 +1,7 @@
+from testall import test_file, test_tree
+
+test_file("H:\\dev\\dwex\\samples\\a.exe")
+test_file("H:\\dev\\dwex\\samples\\YarxiMin.app.dSYM")
+test_file("H:\\dev\\dwex\\samples\\An-3.30.35-y-ARM64-libyarxi.so")
+test_file("H:\\dev\\dwex\\samples\\An-2.60.3-j-ARMv7-libyarxi.so")
+test_tree("H:\\dev\\pyelftools2\\test\\testfiles_for_dwarf")

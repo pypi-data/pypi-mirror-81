405b404eabad5c044ce5cb78958d0fdbc67a6a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:24 +0000

    Add package CaChannel/3.1.3/CaChannel-3.1.3-cp27-cp27m-macosx_10_9_x86_64.whl

---
+++ packages/CaChannel/CaChannel-3.1.3-cp27-cp27m-macosx_10_9_x86_64.whl/CaChannel/__init__.py
@@ -0,0 +1,5 @@
+# retrieve numeric waveforms as numpy arrays, default No
+USE_NUMPY = False
+from .CaChannel import CaChannel, CaChannelException
+from . import ca
+from ._version import __version__, version_info

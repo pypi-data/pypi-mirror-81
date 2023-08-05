dbbdde228b5620cace7187374d86722b9cd7b67f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:20 +0000

    Add package graviti/0.2.3/graviti-0.2.3.tar.gz

---
+++ packages/graviti/graviti-0.2.3.tar.gz/graviti-0.2.3/python/graviti/label/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#
+# Copyright 2020 Graviti. All Rights Reserved.
+#
+
+""" label and labeled shape classes """
+
+from .label import (
+    Classification,
+    Label,
+    LabeledBox2D,
+    LabeledBox3D,
+    LabeledPolygon2D,
+    LabeledPolyline2D,
+    LabelType,
+)
+from .label_table import AttributeType, LabelTable, LabelTables
+
+__all__ = [
+    "Classification",
+    "Label",
+    "LabelTable",
+    "LabelTables",
+    "LabelType",
+    "LabeledBox2D",
+    "LabeledBox3D",
+    "LabeledPolygon2D",
+    "LabeledPolyline2D",
+    "AttributeType",
+]

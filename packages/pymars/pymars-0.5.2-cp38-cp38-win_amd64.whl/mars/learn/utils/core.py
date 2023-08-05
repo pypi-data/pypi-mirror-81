0f5f64ef67649ad0a76b4ac7233d46e1052f7a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:10 +0000

    Add package pymars/0.5.2/pymars-0.5.2-cp38-cp38-win_amd64.whl

---
+++ packages/pymars/pymars-0.5.2-cp38-cp38-win_amd64.whl/mars/learn/utils/core.py
@@ -0,0 +1,34 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pandas as pd
+
+from ...tensor import tensor as astensor
+from ...dataframe import DataFrame, Series
+from ...dataframe.core import DATAFRAME_TYPE, SERIES_TYPE
+
+
+def convert_to_tensor_or_dataframe(item):
+    if isinstance(item, (DATAFRAME_TYPE, pd.DataFrame)):
+        item = DataFrame(item)
+    elif isinstance(item, (SERIES_TYPE, pd.Series)):
+        item = Series(item)
+    else:
+        item = astensor(item)
+    return item
+
+
+def concat_chunks(chunks):
+    tileable = chunks[0].op.create_tileable_from_chunks(chunks)
+    return tileable.op.concat_tileable_chunks(tileable).chunks[0]

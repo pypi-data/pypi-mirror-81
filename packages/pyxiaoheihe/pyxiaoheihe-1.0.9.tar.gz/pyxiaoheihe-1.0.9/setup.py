3a0ff1cf1ab50680dd4acbfc6048adedc5181a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:41 +0000

    Add package pyxiaoheihe/1.0.9/pyxiaoheihe-1.0.9.tar.gz

---
+++ packages/pyxiaoheihe/pyxiaoheihe-1.0.9.tar.gz/pyxiaoheihe-1.0.9/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from pyxiaoheihe.static import PYXIAOHEIHE_VERSION
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyxiaoheihe",
+    version=PYXIAOHEIHE_VERSION,
+    author="Chr_",
+    author_email="chr@chrxw.com",
+    description="用Python实现的小黑盒客户端",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chr233/pyxiaoheihe",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'pyDes',
+        'rsa'
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ),
+)

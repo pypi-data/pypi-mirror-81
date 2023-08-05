198d300b1ab2a20822dc738ae43c5b5b070344dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:32 +0000

    Add package datastand/1.3/DataStand-1.3.tar.gz

---
+++ packages/datastand/DataStand-1.3.tar.gz/DataStand-1.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='DataStand',
+    packages=['DataStand'],
+    version='1.3',
+    license='MIT  ',
+    author="Vincent Njonge",
+    author_email="njongevincent@gmail.com",
+    description="A python package that helps Data Scientists, Machine Learning Engineers and Analysts to quickly explore and understand a dataset.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lyraxvincent/DataStand",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+ )

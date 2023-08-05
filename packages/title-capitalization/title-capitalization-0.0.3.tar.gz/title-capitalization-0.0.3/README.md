02485b5480ce460d0d0eec0870240eb13a15bbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:59 +0000

    Add package title-capitalization/0.0.3/title-capitalization-0.0.3.tar.gz

---
+++ packages/title-capitalization/title-capitalization-0.0.3.tar.gz/title-capitalization-0.0.3/README.md
@@ -0,0 +1,16 @@
+# title-capitalization
+
+A simple package to capitalize text according to title capitalization conventions.
+
+Written for CSC630 in fall 2020 by Sarah, Gaia, Nikol, and Ali
+
+Install using 
+
+```pip install title-capitalization``` 
+
+Import and use in Python using 
+
+```
+from titlecap import title
+title = title.use_caps('demo title text')
+```
---
+++ packages/title-capitalization/title-capitalization-0.0.3.tar.gz/title-capitalization-0.0.3/titlecap/title.py
@@ -0,0 +1,9 @@
+short_words = ["the", "in", "a", "as", "to", "be", "an", "for", "nor", "but", "yet", "so", "at", "around", "by", "after", "along", "for", "from", "of", "on", "to", "with", "without"]
+
+def add_caps(s):
+    words = s.lower().split()
+    for i in range(0, len(words)):
+        if words[i] not in short_words and len(words[i])>3:
+            words[i] = words[i].capitalize()
+    words[0] = words[0].capitalize()
+    return " ".join(words)

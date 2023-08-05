c3f0594fd2e2f027e354d434393fa98ae86784f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:11 +0000

    Add package restart/2.6.4/restart-2.6.4.tar.gz

---
+++ packages/restart/restart-2.6.4.tar.gz/restart-2.6.4/restart/__init__.py
@@ -0,0 +1,8 @@
+"""Initialization script for restart package."""
+
+# Version
+__version__ = "2.6.4"
+
+# Public interface for the model
+from restart.data import Data
+from restart.restart import RestartModel

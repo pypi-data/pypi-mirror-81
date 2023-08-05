28723e30006d0289b1c4ebc8decce3f82df9e34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:44 +0000

    Add package tplinkrouter/1.0.0/tplinkrouter-1.0.0.tar.gz

---
+++ packages/tplinkrouter/tplinkrouter-1.0.0.tar.gz/tplinkrouter-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tplinkrouter",
+    version="1.0.0",
+    author="Menahi Shayan",
+    author_email="menahi.shayan@gmail.com",
+    description="TP-Link Archer C50 API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/menahishayan/TP-Link-Archer-C50-API",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

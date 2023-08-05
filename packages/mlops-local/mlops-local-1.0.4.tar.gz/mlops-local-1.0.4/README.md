3cb76218c7d843dd901fbc12b9aa93b4f9c05ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:34 +0000

    Add package mlops-local/1.0.4/mlops-local-1.0.4.tar.gz

---
+++ packages/mlops-local/mlops-local-1.0.4.tar.gz/mlops-local-1.0.4/README.md
@@ -0,0 +1,4 @@
+# MLOps local test environment
+This package is a support role when using the MLOps platform.
+With it, you can test preprocessing jobs and machine learning algorithms locally, using 
+the in-browser interpreter and debugger, before committing your work to the platform.
---
+++ packages/mlops-local/mlops-local-1.0.4.tar.gz/mlops-local-1.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlops-local",
+    version="1.0.4",
+    author="Petter Hultin Gustafsson",
+    author_email="petter@mlops.cloud",
+    description="Package to test preprocessing and ML scripts locally before pushing to the MLOps platform",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_modules=['mlops_local'],
+    install_requires=["Click", "docker"],
+    python_requires='>=3.6',
+    entry_points={"console_scripts": ["mlops=mlops_local.cli:cli"]},
+)

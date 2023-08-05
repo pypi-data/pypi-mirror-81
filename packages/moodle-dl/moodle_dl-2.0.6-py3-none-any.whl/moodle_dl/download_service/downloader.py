1045278bedb1618479c7347e7bbabb0cb4f791a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:57 +0000

    Add package moodle-dl/2.0.6/moodle_dl-2.0.6-py3-none-any.whl

---
+++ packages/moodle-dl/moodle_dl-2.0.6-py3-none-any.whl/moodle_dl/download_service/downloader.py
@@ -0,0 +1,60 @@
+import threading
+
+from queue import Queue, Empty
+
+from moodle_dl.state_recorder.state_recorder import StateRecorder
+
+
+class Downloader(threading.Thread):
+    """
+    Downloader processes the queue and puts an
+    URL target back into the queue if an error occurs.
+    """
+
+    def __init__(
+        self,
+        queue: Queue,
+        report: [],
+        state_recorder: StateRecorder,
+        thread_id: int,
+        db_lock: threading.Lock,
+    ):
+        """
+        Initiates a downloader thread.
+        """
+        threading.Thread.__init__(self)
+        self.daemon = True
+
+        self.queue = queue
+        self.report = report
+        self.thread_id = thread_id
+        self.db_lock = db_lock
+        self.state_recorder = state_recorder
+
+    def run(self):
+        """
+        Work the queue until it is empty.
+        """
+        while self.queue.empty() is False:
+            try:
+                # raise condition
+                url_target = self.queue.get(False)
+            except Empty:
+                break
+
+            response = url_target.download(self.thread_id)
+
+            # All information is still saved in url_target
+
+            # If a download fails, add it to the error report.
+            if response is False:
+                self.report['failure'].append(url_target)
+
+            # If a download was successful, store it in the database.
+            elif response is True:
+                self.db_lock.acquire()
+                self.state_recorder.save_file(url_target.file, url_target.course.id, url_target.course.fullname)
+                self.db_lock.release()
+                self.report['success'].append(url_target)
+
+            self.queue.task_done()

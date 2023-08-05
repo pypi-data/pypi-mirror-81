ea97b677310f2aba6ddf5793bf1a6760b92b4c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:56 +0000

    Add package myMathPackage/0.0.3/myMathPackage-0.0.3-py3-none-any.whl

---
+++ packages/myMathPackage/myMathPackage-0.0.3-py3-none-any.whl/myMathPackage/__init__.py
@@ -0,0 +1,15 @@
+ 
+####################################################################################################################
+# __init__.py
+# topic: __init__.py for myMathPackage
+# author: Michael Heser, hesermi71995, 3172761
+# date: 2020-09-29
+# recent changes: first implementation
+# build info: operating system: Linux Fedora 32, editor: idle3, python version: 3.8.5
+####################################################################################################################
+
+from .chooseYourOption import calculateSquares
+from .chooseYourOption import calculateSquareRoots
+from .chooseYourOption import calculateFibo
+from .chooseYourOption import calculatePows
+

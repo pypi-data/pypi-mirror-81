c3e1a3178a4b983934ddcd2f33046552fc05898f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:02 +0000

    Add package financepy/0.182/financepy-0.182-py3-none-any.whl

---
+++ packages/financepy/financepy-0.182-py3-none-any.whl/financepy/finutils/FinOptionTypes.py
@@ -0,0 +1,39 @@
+##############################################################################
+# Copyright (C) 2018, 2019, 2020 Dominic O'Kane
+##############################################################################
+
+# This is an exhaustive list of all option types
+
+from enum import Enum
+
+###############################################################################
+
+
+class FinOptionTypes(Enum):
+    EUROPEAN_CALL = 1
+    EUROPEAN_PUT = 2
+    AMERICAN_CALL = 3
+    AMERICAN_PUT = 4
+    DIGITAL_CALL = 5
+    DIGITAL_PUT = 6
+    ASIAN_CALL = 7
+    ASIAN_PUT = 8
+    COMPOUND_CALL = 9
+    COMPOUND_PUT = 10
+
+###############################################################################
+
+
+class FinLiborSwapTypes(Enum):
+    PAYER = 1
+    RECEIVER = 2
+
+###############################################################################
+
+
+class FinOptionExerciseTypes(Enum):
+    EUROPEAN = 1
+    BERMUDAN = 2
+    AMERICAN = 3
+
+###############################################################################

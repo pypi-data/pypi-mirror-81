6f58150fc99dffbc5cf230cce6ab3839e8d9cbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:24 +0000

    Add package sharedmem/0.3.8/sharedmem-0.3.8.tar.gz

---
+++ packages/sharedmem/sharedmem-0.3.8.tar.gz/sharedmem-0.3.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(name="sharedmem", version="0.3.8",
+      author="Yu Feng",
+      author_email="rainwoodman@gmail.com",
+      description="Dispatch your trivially parallizable jobs with sharedmem. ",
+      url="http://github.com/rainwoodman/sharedmem",
+      zip_safe=False,
+      package_dir = {'sharedmem': 'sharedmem'},
+      packages = [
+        'sharedmem', 'sharedmem.tests'
+      ],
+      license="GPLv3",
+      install_requires=['numpy'],
+)
+

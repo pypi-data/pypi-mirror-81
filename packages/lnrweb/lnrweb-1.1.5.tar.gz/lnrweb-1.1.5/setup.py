472e6116d2aad7d30c0c7e0efbd7408d29af34d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:42 +0000

    Add package lnrweb/1.1.5/lnrweb-1.1.5.tar.gz

---
+++ packages/lnrweb/lnrweb-1.1.5.tar.gz/lnrweb-1.1.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lnrweb",
+    version="1.1.5",
+    description='A project for solving linear programming problem',
+    author='Ashish Kumar',
+    long_description=long_description,
+    url='https://github.com/Ashish2000L/linear_programing',
+    packages=setuptools.find_packages(),
+    )

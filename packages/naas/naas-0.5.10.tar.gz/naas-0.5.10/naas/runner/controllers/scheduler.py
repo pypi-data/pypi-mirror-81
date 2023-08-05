4061639c8ecf3aeaf7f9d822bb771e6b3b43af84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:37 +0000

    Add package naas/0.5.10/naas-0.5.10.tar.gz

---
+++ packages/naas/naas-0.5.10.tar.gz/naas-0.5.10/naas/runner/controllers/scheduler.py
@@ -0,0 +1,19 @@
+from sanic.views import HTTPMethodView
+from sanic.response import json
+
+
+class SchedulerController(HTTPMethodView):
+    __logger = None
+    __scheduler = None
+
+    def __init__(self, logger, scheduler, *args, **kwargs):
+        super(SchedulerController, self).__init__(*args, **kwargs)
+        self.__logger = logger
+        self.__scheduler = scheduler
+
+    async def get(self, request, mode):
+        if mode == "pause":
+            self.__scheduler.pause()
+        elif mode == "resume":
+            self.__scheduler.resume()
+        return json({"status": self.__scheduler.status()})

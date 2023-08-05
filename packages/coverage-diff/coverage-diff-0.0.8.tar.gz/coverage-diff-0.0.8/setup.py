49a128f1c01934fb99589d5f015cfdb987cee4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:45 +0000

    Add package coverage-diff/0.0.8/coverage-diff-0.0.8.tar.gz

---
+++ packages/coverage-diff/coverage-diff-0.0.8.tar.gz/coverage-diff-0.0.8/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='coverage-diff',
+    version='0.0.8',
+    description='Show/check coverage only for changed files (between any git branches)',
+    long_description=readme,
+    author='strayge',
+    author_email='strayge@gmail.com',
+    long_description_content_type="text/markdown",
+    url="https://github.com/strayge/coverage-diff",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'coverage-diff = coverage_diff.main:main',
+        ],
+    },
+    python_requires='>=3.6',
+    install_requires=['coverage>=4'],
+    data_files=[('', ['LICENSE'])],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+    ],
+)

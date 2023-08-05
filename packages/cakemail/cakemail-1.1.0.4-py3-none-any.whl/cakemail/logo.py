0ec3c73e33d5e7437cc09353076d695b0ded07ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:57 +0000

    Add package cakemail/1.1.0.4/cakemail-1.1.0.4-py3-none-any.whl

---
+++ packages/cakemail/cakemail-1.1.0.4-py3-none-any.whl/cakemail/logo.py
@@ -0,0 +1,13 @@
+from cakemail_openapi import LogoApi
+from cakemail.wrapper import WrappedApi
+
+class Logo(WrappedApi):
+    upload_default: LogoApi.upload_default_logo
+
+    def __init__(self, superclass):
+        super().__init__(
+            superclass=superclass,
+            namemap={
+                'upload_default': 'upload_default_logo',
+            }
+        )

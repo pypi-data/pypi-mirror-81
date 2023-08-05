32b6be5fb9af0a2112a6805dc559cc9074e4042c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:49 +0000

    Add package boonnano/3.1.2/boonnano-3.1.2.tar.gz

---
+++ packages/boonnano/boonnano-3.1.2.tar.gz/boonnano-3.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='boonnano',
+    version='3.1.2',
+    author="BoonLogic",
+    author_email="elise@boonlogic.com",
+    packages=['boonnano'],
+    install_requires=['urllib3','numpy'],
+    description="A SDK package for utilizing the BoonLogic nano API",
+    long_description=long_description,
+    license='MIT',
+    long_description_content_type="text/markdown",
+    url="https://github.com/boonlogic/Python_API",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)

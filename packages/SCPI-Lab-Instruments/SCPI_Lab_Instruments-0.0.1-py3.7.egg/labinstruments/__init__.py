d0d4af3aac6b0aa15975f4a9bacbe01cca70df6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:06 +0000

    Add package SCPI-Lab-Instruments/0.0.1/SCPI_Lab_Instruments-0.0.1-py3.7.egg

---
+++ packages/SCPI-Lab-Instruments/SCPI_Lab_Instruments-0.0.1-py3.7.egg/labinstruments/__init__.py
@@ -0,0 +1,14 @@
+"""SCPI-Lab-Instruments
+
+Control instruments in the receiver lab over Ethernet using SCPI commands.
+
+"""
+
+from labinstruments.hittite import Hittite, SignalGenerator
+# from labinstruments.keithley import Keithley2280
+# from labinstruments.microlambda import YigFilter
+
+__author__ = "John Garrett"
+__version__ = "0.0.1"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 Smithsonian Astrophysical Observatory"

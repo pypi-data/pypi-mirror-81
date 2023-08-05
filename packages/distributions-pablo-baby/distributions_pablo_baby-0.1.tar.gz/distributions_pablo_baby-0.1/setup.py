18fab9d81db3ec78406e056084a1bfbc19847174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:08 +0000

    Add package distributions-pablo-baby/0.1/distributions_pablo_baby-0.1.tar.gz

---
+++ packages/distributions-pablo-baby/distributions_pablo_baby-0.1.tar.gz/distributions_pablo_baby-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_pablo_baby',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_pablo_baby'],
+      zip_safe=False)

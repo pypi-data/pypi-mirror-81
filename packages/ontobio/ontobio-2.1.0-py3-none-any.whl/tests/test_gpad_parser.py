a529d859074507346bc59fc27ad8bfaec4749da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:13 +0000

    Add package ontobio/2.1.0/ontobio-2.1.0-py3-none-any.whl

---
+++ packages/ontobio/ontobio-2.1.0-py3-none-any.whl/tests/test_gpad_parser.py
@@ -0,0 +1,18 @@
+from ontobio.io.gpadparser import GpadParser
+from ontobio.io import assocparser
+
+import yaml
+
+POMBASE = "tests/resources/truncated-pombase.gpad"
+
+def test_skim():
+    p = GpadParser()
+    results = p.skim(open(POMBASE,"r"))
+    print(str(results))
+
+
+def test_parse():
+    p = GpadParser(config=assocparser.AssocParserConfig(group_metadata=yaml.load(open("tests/resources/mgi.dataset.yaml"), Loader=yaml.FullLoader)))
+    test_gpad_file = "tests/resources/mgi.test.gpad"
+    results = p.parse(open(test_gpad_file, "r"))
+    print(p.report.to_markdown())

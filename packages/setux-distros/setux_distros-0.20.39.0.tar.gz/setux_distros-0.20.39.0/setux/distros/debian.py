a3f5518a20f78da03f6bfbc933c7953588932be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:50 +0000

    Add package setux-distros/0.20.39.0/setux_distros-0.20.39.0.tar.gz

---
+++ packages/setux-distros/setux_distros-0.20.39.0.tar.gz/setux_distros-0.20.39.0/setux/distros/debian.py
@@ -0,0 +1,31 @@
+from setux.core.distro import Distro
+
+
+class Debian(Distro):
+    Package = 'apt'
+    Service = 'SystemD'
+    pkgmap = dict(
+        pip        = 'python3-pip',
+        setuptools = 'python3-setuptools',
+        sqlite     = 'sqlite3',
+        p7zip      = 'p7zip-full',
+        xz         = 'xz-utils',
+        ctag       = 'exuberant-ctags',
+        unrar      = 'unrar-free',
+    )
+    svcmap = dict(
+    )
+
+    @classmethod
+    def release_name(cls, infos):
+        did = infos['ID'].strip()
+        ver = infos['VERSION_ID'].strip('\r"')
+        return f'{did}_{ver}'
+
+
+class debian_9(Debian):
+    pass
+
+
+class debian_10(Debian):
+    pass

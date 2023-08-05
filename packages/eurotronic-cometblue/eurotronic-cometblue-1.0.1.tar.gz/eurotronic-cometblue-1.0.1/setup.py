e4c95b3989bafd7085cf0a3fe3a095bdbdfb3bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:25 +0000

    Add package eurotronic-cometblue/1.0.1/eurotronic-cometblue-1.0.1.tar.gz

---
+++ packages/eurotronic-cometblue/eurotronic-cometblue-1.0.1.tar.gz/eurotronic-cometblue-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read()
+
+setuptools.setup(
+    name="eurotronic-cometblue",
+    version="1.0.1",
+    author="Johanens Rottler",
+    author_email="johannes@rottler.me",
+    description="Allows you to access Eurotronics GmbH BLE Comet Blue Radiator Controller",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zero-udo/eurotronic-cometblue",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    install_requires=requirements,
+    python_requires='>=3.6',
+)

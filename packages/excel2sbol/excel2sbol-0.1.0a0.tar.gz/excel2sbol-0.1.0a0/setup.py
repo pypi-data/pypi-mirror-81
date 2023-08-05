e95e866f2504c03da9e283869c4d5f0645ae1500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:28 +0000

    Add package excel2sbol/0.1.0a0/excel2sbol-0.1.0a0.tar.gz

---
+++ packages/excel2sbol/excel2sbol-0.1.0a0.tar.gz/excel2sbol-0.1.0a0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 16:41:00 2020
+
+@author: isapoetzsch
+"""
+
+from setuptools import setup, find_packages
+ 
+setup(name='excel2sbol',
+      version='0.1.0-alpha',
+      url='https://github.com/SynBioDex/Excel-to-SBOL/tree/master/excel2sbol',
+      license='BSD 3-clause',
+      maintainer='Tramy Nguyen',
+      maintainer_email='tramy.nguy@gmail.com',
+      description='convert excel templates into sbol',
+      packages=find_packages(include=['excel2sbol']),
+      long_description=open('README.md').read(),
+      install_requires=[
+        'sbol2==1.0b8',
+        'pandas==1.0.1',
+        'numpy==1.18.1'
+      ],
+      zip_safe=False)

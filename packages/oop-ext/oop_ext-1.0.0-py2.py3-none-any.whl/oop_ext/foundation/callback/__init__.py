bc308c289b7f11b244aea2716d514771e04128c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:22 +0000

    Add package oop-ext/1.0.0/oop_ext-1.0.0-py2.py3-none-any.whl

---
+++ packages/oop-ext/oop_ext-1.0.0-py2.py3-none-any.whl/oop_ext/foundation/callback/__init__.py
@@ -0,0 +1,14 @@
+from ._callbacks import Callbacks
+from ._fast_callback import Callback
+from ._priority_callback import PriorityCallback
+from ._shortcuts import After, Before, Remove, WrapForCallback
+
+__all__ = [
+    "Callbacks",
+    "Callback",
+    "PriorityCallback",
+    "After",
+    "Before",
+    "Remove",
+    "WrapForCallback",
+]

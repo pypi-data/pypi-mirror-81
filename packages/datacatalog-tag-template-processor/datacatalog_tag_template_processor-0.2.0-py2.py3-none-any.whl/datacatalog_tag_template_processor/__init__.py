614f151e408ebf809228fdca1517be934756555f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:34 +0000

    Add package datacatalog-tag-template-processor/0.2.0/datacatalog_tag_template_processor-0.2.0-py2.py3-none-any.whl

---
+++ packages/datacatalog-tag-template-processor/datacatalog_tag_template_processor-0.2.0-py2.py3-none-any.whl/datacatalog_tag_template_processor/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for datacatalog_tag_template_processor."""
+
+from .datacatalog_tag_template_processor_cli import main  # noqa: F401
+
+__author__ = """Marcelo Miranda"""
+__email__ = 'mesmacosta@gmail.com'
+__version__ = '0.1.2'
+
+__all__ = ('main')

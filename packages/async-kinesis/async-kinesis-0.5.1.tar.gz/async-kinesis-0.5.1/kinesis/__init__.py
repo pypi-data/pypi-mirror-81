a22e926ec448650e04015894714c7852aa9faf31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:23 +0000

    Add package async-kinesis/0.5.1/async-kinesis-0.5.1.tar.gz

---
+++ packages/async-kinesis/async-kinesis-0.5.1.tar.gz/async-kinesis-0.5.1/kinesis/__init__.py
@@ -0,0 +1,11 @@
+from .producer import Producer
+from .consumer import Consumer
+from .processors import (
+    StringProcessor,
+    JsonProcessor,
+    JsonLineProcessor,
+    MsgpackProcessor,
+)
+from .serializers import StringSerializer, JsonSerializer, MsgpackSerializer
+from .checkpointers import MemoryCheckPointer, RedisCheckPointer
+from .aggregators import SimpleAggregator, NewlineAggregator, NetstringAggregator

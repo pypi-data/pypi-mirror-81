b9d42e59749ac01f80d125fb60f1d292733bfcb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:43 +0000

    Add package torch-semiring-einsum/1.0.0/torch_semiring_einsum-1.0.0-py3-none-any.whl

---
+++ packages/torch-semiring-einsum/torch_semiring_einsum-1.0.0-py3-none-any.whl/torch_semiring_einsum/__init__.py
@@ -0,0 +1,35 @@
+from .equation import compile_equation
+from .extend import semiring_einsum_forward
+from .function import combine
+from .real_forward import real_einsum_forward
+from .real_backward import real_einsum_backward
+from .log_forward import log_einsum_forward
+from .log_backward import log_einsum_backward
+from .log_viterbi_forward import log_viterbi_einsum_forward
+
+einsum = combine(real_einsum_forward, real_einsum_backward)
+r"""Differentiable version of ordinary (real) einsum.
+
+This combines :py:func:`real_einsum_forward` and
+:py:func:`real_einsum_backward` into one auto-differentiable function.
+"""
+
+log_einsum = combine(log_einsum_forward, log_einsum_backward)
+r"""Differentiable version of log-space einsum.
+
+This combines :py:func:`log_einsum_forward` and
+:py:func:`log_einsum_backward` into one auto-differentiable function.
+"""
+
+__all__ = [
+    'compile_equation',
+    'real_einsum_forward',
+    'real_einsum_backward',
+    'einsum',
+    'log_einsum_forward',
+    'log_einsum_backward',
+    'log_einsum',
+    'log_viterbi_einsum_forward',
+    'semiring_einsum_forward',
+    'combine'
+]
---
+++ packages/torch-semiring-einsum/torch_semiring_einsum-1.0.0-py3-none-any.whl/torch_semiring_einsum/equation.py
@@ -0,0 +1,199 @@
+import itertools
+
+class Equation:
+    r"""An einsum equation that has been pre-compiled into some useful data
+    structures."""
+
+    def __init__(self, source, variable_locations, input_variables,
+            output_variables, num_variables):
+        self.source = source
+        self.variable_locations = variable_locations
+        self.input_variables = input_variables
+        self.output_variables = output_variables
+        self.num_variables = num_variables
+        self.reduce_input_to_output = None
+        self.reduce_others_to_input = None
+        self.reduce_all_to_input = None
+
+    def validate_sizes(self, args):
+        for loc_list in self.variable_locations:
+            i, j = loc_list[0]
+            size = args[i].size(j)
+            for i, j in loc_list[1:]:
+                if args[i].size(j) != size:
+                    raise ValueError(
+                        'dimension {} of argument {} does not match '
+                        'dimension {} of argument {}'.format(
+                            j, i, loc_list[0][1], loc_list[0][0]))
+
+    def get_sizes(self, args, variables):
+        result = []
+        for variable in variables:
+            i, j = self.variable_locations[variable][0]
+            result.append(args[i].size(j))
+        return result
+
+    def all_variables(self):
+        return range(self.num_variables)
+
+    def prepare_for_forward(self):
+        if self.reduce_input_to_output is None:
+            self.reduce_input_to_output = create_reduce_info(
+                self.input_variables,
+                self.output_variables)
+
+    def prepare_for_backward(self):
+        if self.reduce_others_to_input is None:
+            self.reduce_others_to_input = []
+            for i, input_var_list in enumerate(self.input_variables):
+                other_vars = list(self.input_variables)
+                other_vars[i] = self.output_variables
+                self.reduce_others_to_input.append(create_reduce_info(
+                    other_vars, input_var_list))
+
+    def prepare_for_log_backward(self):
+        if self.reduce_all_to_input is None:
+            self.reduce_all_to_input = []
+            inputs = self.input_variables + [self.output_variables]
+            for input_var_list in self.input_variables:
+                reduce_info = create_reduce_info(inputs, input_var_list)
+                output_lookup_info = reduce_info.lookup_info.pop()
+                self.reduce_all_to_input.append((reduce_info, output_lookup_info))
+
+def compile_equation(equation: str) -> Equation:
+    r"""Pre-compile an einsum equation for use with the einsum functions in
+    this package.
+
+    :param equation: An equation in einsum syntax.
+    :return: A pre-compiled equation.
+    """
+    args_str, output_vars = equation.split('->', 1)
+    arg_strs = args_str.split(',')
+    char_to_int = {}
+    int_to_arg_dims = []
+    args_dims = []
+    for arg_no, arg_str in enumerate(arg_strs):
+        arg_dims = []
+        for dim_no, dim_char in enumerate(arg_str):
+            dim_int = char_to_int.get(dim_char)
+            if dim_int is None:
+                dim_int = char_to_int[dim_char] = len(char_to_int)
+                int_to_arg_dims.append([])
+            int_to_arg_dims[dim_int].append((arg_no, dim_no))
+            arg_dims.append(dim_int)
+        args_dims.append(arg_dims)
+    output_dims = [char_to_int[c] for c in output_vars]
+    num_variables = len(char_to_int)
+    return Equation(
+        equation,
+        int_to_arg_dims,
+        args_dims,
+        output_dims,
+        num_variables)
+
+def get_variables_not_in(variables, excluded):
+    added = set()
+    result = []
+    for variable in variables:
+        if variable not in excluded and variable not in added:
+            added.add(variable)
+            result.append(variable)
+    return result
+
+class ReduceInfo:
+    r"""Holds data structures that facilitate the basic einsum operation of
+    multiplying terms together while summing over multiple dimensions."""
+
+    def __init__(self, lookup_info, output_variables, reduced_variables,
+            reduced_dims):
+        self.lookup_info = lookup_info
+        self.output_variables = output_variables
+        self.reduced_variables = reduced_variables
+        self.reduced_dims = reduced_dims
+
+    def get_ranges(self, equation, args, block_size):
+        return get_ranges(equation, args, self.reduced_variables,
+            block_size)
+
+    def get_term_size(self, equation, args, var_values):
+        # Compute the size of each of the terms in an einsum. Each term is a
+        # slice over ranges of the summed variables.
+        # Note that var_values is a list of slices, not a list of ints.
+        return (
+            # The output dimensions come first, and their sizes match the
+            # inputs.
+            equation.get_sizes(args, self.output_variables) +
+            # The summed dimensions come last, and their sizes correspond to
+            # the sizes of the slices.
+            [s.stop - s.start for s in var_values]
+        )
+
+def get_ranges(equation, args, variables, block_size):
+    sizes = equation.get_sizes(args, variables)
+    return [list(generate_slices(s, block_size)) for s in sizes]
+
+def generate_slices(total_size, block_size):
+    lo = 0
+    while lo < total_size:
+        hi = min(lo + block_size, total_size)
+        yield slice(lo, hi)
+        lo = hi
+
+_COLON = slice(None)
+
+class LookupInfo:
+    r"""Holds data structures for slicing, unsqueezing, and permuting a tensor
+    so that all of its dimensions line up in a predictable way."""
+
+    def __init__(self, index_map, num_extra_vars, permutation):
+        self.index_map = index_map
+        self.num_extra_vars = num_extra_vars
+        self.permutation = permutation
+
+    def lookup(self, arg, var_values):
+        index = [_COLON] * arg.dim()
+        for source_index, dest_index in self.index_map:
+            index[dest_index] = var_values[source_index]
+        for i in range(self.num_extra_vars):
+            index.append(None)
+        return arg[index].permute(self.permutation)
+
+def create_reduce_info(input_vars, output_vars):
+    r"""Pre-compile a data structure that will help reduce the variables
+    given in ``input_vars`` to the variables in ``output_vars``."""
+    reduced_vars = get_variables_not_in(
+        (v for var_list in input_vars for v in var_list),
+        set(output_vars))
+    # The shape of the final, reshaped tensor will correspond to
+    # output_vars + reduced_vars.
+    lookup_info = []
+    reduced_vars_dict = { v : i for i, v in enumerate(reduced_vars) }
+    for input_var_list in input_vars:
+        index_map = []
+        # Loop over all variables in this input tensor.
+        for dest_index, input_var in enumerate(input_var_list):
+            source_index = reduced_vars_dict.get(input_var)
+            if source_index is not None:
+                # The variable is a reduced variable.
+                # This maps an index of a list of reduced variable values
+                # (source_index) to an index of a tuple used to slice the
+                # input tensor (dest_index).
+                index_map.append((source_index, dest_index))
+        input_var_dict = { v : i for i, v in enumerate(input_var_list) }
+        num_extra_vars = 0
+        permutation = []
+        # Loop over all variables in the output.
+        for output_var in itertools.chain(output_vars, reduced_vars):
+            perm_index = input_var_dict.get(output_var)
+            if perm_index is None:
+                # The variable does not appear in the input after it has been
+                # indexed.
+                perm_index = len(input_var_dict) + num_extra_vars
+                num_extra_vars += 1
+            permutation.append(perm_index)
+        lookup_info.append(LookupInfo(index_map, num_extra_vars, permutation))
+    reduced_dims = tuple(range(
+        len(output_vars),
+        len(output_vars) + len(reduced_vars)
+    ))
+    return ReduceInfo(lookup_info, output_vars, reduced_vars, reduced_dims)

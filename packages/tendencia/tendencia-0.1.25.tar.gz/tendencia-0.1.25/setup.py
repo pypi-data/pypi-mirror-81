2b54574ff9472cfc2a2f86ba70893ea9a120d2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:04 +0000

    Add package tendencia/0.1.25/tendencia-0.1.25.tar.gz

---
+++ packages/tendencia/tendencia-0.1.25.tar.gz/tendencia-0.1.25/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tendencia']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['tendencia = tendencia.cli:cli']}
+
+setup_kwargs = {
+    'name': 'tendencia',
+    'version': '0.1.25',
+    'description': '',
+    'long_description': None,
+    'author': 'chenn',
+    'author_email': 'chenn@gisuni.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

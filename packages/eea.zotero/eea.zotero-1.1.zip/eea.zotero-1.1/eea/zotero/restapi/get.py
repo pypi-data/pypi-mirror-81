a1a7eda998106a8b35cabd3770be5509db3dde81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:16 +0000

    Add package eea.zotero/1.1/eea.zotero-1.1.zip

---
+++ packages/eea.zotero/eea.zotero-1.1.zip/eea.zotero-1.1/eea/zotero/restapi/get.py
@@ -0,0 +1,24 @@
+""" RestAPI enpoint @zotero GET
+"""
+from eea.zotero.interfaces import IZoteroClientSettings
+from plone import api
+from plone.restapi.services import Service
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
+
+
+@implementer(IPublishTraverse)
+class ZoteroGet(Service):
+    """ Zotero GET
+    """
+    def reply(self):
+        """ Reply
+        """
+        return {
+            "server": api.portal.get_registry_record(
+                "server", interface=IZoteroClientSettings, default=""),
+            "password": api.portal.get_registry_record(
+                "password", interface=IZoteroClientSettings, default=""),
+            "style": api.portal.get_registry_record(
+                "style", interface=IZoteroClientSettings, default=""),
+        }

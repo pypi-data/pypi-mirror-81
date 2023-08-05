ff84dfed639bf017a54d18a6914a1bb2b1e42445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:36 +0000

    Add package tdub/0.0.58/tdub-0.0.58.tar.gz

---
+++ packages/tdub/tdub-0.0.58.tar.gz/tdub-0.0.58/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    entry_points={"console_scripts": ["tdub = tdub.__main__:run_cli"]},
+    package_data={"": ["*.typed"]},
+)

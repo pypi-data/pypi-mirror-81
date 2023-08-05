c24659ebc981dfe2f7e9b4233e98228093b6c378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:51 +0000

    Add package fldataparser/0.0.1/fldataparser-0.0.1.tar.gz

---
+++ packages/fldataparser/fldataparser-0.0.1.tar.gz/fldataparser-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='fldataparser',
+    version='0.0.1',
+    description='parser tool for object send through rabbitmq in FLExplorer',
+    packages=find_packages()
+)

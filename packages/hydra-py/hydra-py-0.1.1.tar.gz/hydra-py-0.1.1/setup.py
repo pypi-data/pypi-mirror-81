9d35484ec72b317757e8670d587bd7f94711e52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:11 +0000

    Add package hydra-py/0.1.1/hydra-py-0.1.1.tar.gz

---
+++ packages/hydra-py/hydra-py-0.1.1.tar.gz/hydra-py-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+setuptools.setup(
+    name="hydra-py",
+    version="0.1.1",
+    description="Hydra for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pnxtech/HydraPy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=install_requires
+)

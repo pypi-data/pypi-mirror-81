d2a2c2b97b80b58f7c96e8635a5bc0f0bf5cab04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:23 +0000

    Add package bob.ip.optflow.hornschunck/2.0.19/bob.ip.optflow.hornschunck-2.0.19.zip

---
+++ packages/bob.ip.optflow.hornschunck/bob.ip.optflow.hornschunck-2.0.19.zip/bob.ip.optflow.hornschunck-2.0.19/README.rst
@@ -0,0 +1,51 @@
+.. vim: set fileencoding=utf-8 :
+.. Mon 15 Aug 2016 18:33:46 CEST
+
+.. image:: https://img.shields.io/badge/docs-available-orange.svg
+   :target: https://www.idiap.ch/software/bob/docs/bob/bob.ip.optflow.hornschunck/master/index.html
+.. image:: https://gitlab.idiap.ch/bob/bob.ip.optflow.hornschunck/badges/v2.0.19/pipeline.svg
+   :target: https://gitlab.idiap.ch/bob/bob.ip.optflow.hornschunck/commits/v2.0.19
+.. image:: https://gitlab.idiap.ch/bob/bob.ip.optflow.hornschunck/badges/v2.0.19/coverage.svg
+   :target: https://gitlab.idiap.ch/bob/bob.ip.optflow.hornschunck/commits/v2.0.19
+.. image:: https://img.shields.io/badge/gitlab-project-0000c0.svg
+   :target: https://gitlab.idiap.ch/bob/bob.ip.optflow.hornschunck
+
+
+==================================================================
+ Implementation of Horn & Schunck's Optical Flow Framework for Bob
+==================================================================
+
+This package is part of the signal-processing and machine learning toolbox
+Bob_. It contains a simple Python wrapper to an open-source Optical Flow
+estimator based on the works by Horn & Schunck::
+
+  @article{Horn_Schunck_1981,
+    author = {Horn, B. K. P. and Schunck, B. G.},
+    title = {Determining optical flow},
+    year = {1981},
+    booktitle = {Artificial Intelligence},
+    volume = {17},
+    pages = {185--203},
+  }
+
+
+Installation
+------------
+
+Complete Bob's `installation`_ instructions. Then, to install this package,
+run::
+
+  $ conda install bob.ip.optflow.hornschunck
+
+
+Contact
+-------
+
+For questions or reporting issues to this software package, contact our
+development `mailing list`_.
+
+
+.. Place your references here:
+.. _bob: https://www.idiap.ch/software/bob
+.. _installation: https://www.idiap.ch/software/bob/install
+.. _mailing list: https://www.idiap.ch/software/bob/discuss
---
+++ packages/bob.ip.optflow.hornschunck/bob.ip.optflow.hornschunck-2.0.19.zip/bob.ip.optflow.hornschunck-2.0.19/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Andre Anjos <andre.anjos@idiap.ch>
+# Thu 20 Sep 2012 14:43:19 CEST
+
+"""Bindings for optical flow from Horn & Schunck
+"""
+
+bob_packages = ['bob.core', 'bob.sp']
+
+from setuptools import setup, find_packages, dist
+dist.Distribution(dict(setup_requires=['bob.extension', 'bob.blitz'] + bob_packages))
+from bob.blitz.extension import Extension, build_ext
+
+from bob.extension.utils import load_requirements
+build_requires = load_requirements()
+
+# Define package version
+version = open("version.txt").read().rstrip()
+
+setup(
+
+    name="bob.ip.optflow.hornschunck",
+    version=version,
+    description="Optical Flow framework based on Horn & Schunck",
+    license="BSD",
+    author='Andre Anjos',
+    author_email='andre.anjos@idiap.ch',
+    long_description=open('README.rst').read(),
+    url='https://gitlab.idiap.ch/bob/bob.ip.optflow.hornschunck',
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    setup_requires = build_requires,
+    install_requires = build_requires,
+
+    ext_modules = [
+      Extension("bob.ip.optflow.hornschunck.version",
+        [
+          "bob/ip/optflow/hornschunck/version.cpp",
+        ],
+        bob_packages = bob_packages,
+        version = version,
+      ),
+
+      Extension("bob.ip.optflow.hornschunck._library",
+        [
+          "bob/ip/optflow/hornschunck/SpatioTemporalGradient.cpp",
+          "bob/ip/optflow/hornschunck/HornAndSchunckFlow.cpp",
+          "bob/ip/optflow/hornschunck/forward.cpp",
+          "bob/ip/optflow/hornschunck/central.cpp",
+          "bob/ip/optflow/hornschunck/vanilla.cpp",
+          "bob/ip/optflow/hornschunck/flow.cpp",
+          "bob/ip/optflow/hornschunck/main.cpp",
+        ],
+        bob_packages = bob_packages,
+        version = version,
+      ),
+    ],
+
+    cmdclass = {
+      'build_ext': build_ext
+    },
+
+    classifiers = [
+      'Framework :: Bob',
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Scientific/Engineering :: Image Recognition',
+    ],
+
+)

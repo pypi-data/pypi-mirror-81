1ee8a4a9fb56fced677ab4f9a94a6a21f9505611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:26 +0000

    Add package install/1.3.4/install-1.3.4.tar.gz

---
+++ packages/install/install-1.3.4.tar.gz/install-1.3.4/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "install",
+    version = "1.3.4",
+    author = "Eugene Kolo",
+    author_email = "eugene@kolobyte.com",
+    description = "Install packages from within code",
+    license = "MIT",
+    keywords = "install packages from within code",
+    url = "https://github.com/eugenekolo/pip-install",
+    packages=['install'],
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7, >=3.5',
+)

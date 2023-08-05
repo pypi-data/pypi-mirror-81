09c346382c3afed01b16e2c04aaae8c6be8a8f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:36 +0000

    Add package pygate-grpc/0.0.12/pygate_grpc-0.0.12.tar.gz

---
+++ packages/pygate-grpc/pygate_grpc-0.0.12.tar.gz/pygate_grpc-0.0.12/pygate_grpc/client.py
@@ -0,0 +1,12 @@
+from proto import ffs_rpc_pb2
+from pygate_grpc import health, faults, buildinfo, ffs, wallet, net
+
+
+class PowerGateClient(object):
+    def __init__(self, host_name, is_secure=False):
+        self.health = health.HealthClient(host_name, is_secure)
+        self.faults = faults.FaultsClient(host_name, is_secure)
+        self.buildinfo = buildinfo.BuildinfoClient(host_name, is_secure)
+        self.ffs = ffs.FfsClient(host_name, is_secure)
+        self.wallet = wallet.WalletClient(host_name, is_secure)
+        self.net = net.NetClient(host_name, is_secure)
---
+++ packages/pygate-grpc/pygate_grpc-0.0.12.tar.gz/pygate_grpc-0.0.12/pygate_grpc/health.py
@@ -0,0 +1,22 @@
+import grpc
+import logging
+
+from proto import health_rpc_pb2
+from proto import health_rpc_pb2_grpc
+from pygate_grpc.errors import ErrorHandlerMeta
+
+logger = logging.getLogger(__name__)
+
+
+class HealthClient(object, metaclass=ErrorHandlerMeta):
+    def __init__(self, host_name, is_secure):
+        channel = (
+            grpc.secure_channel(host_name, grpc.ssl_channel_credentials())
+            if is_secure
+            else grpc.insecure_channel(host_name)
+        )
+        self.client = health_rpc_pb2_grpc.RPCServiceStub(channel)
+
+    def check(self):
+        req = health_rpc_pb2.CheckRequest()
+        return self.client.Check(req)

f919029c40f2209d3d0982a501e7dddc3348a938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:43 +0000

    Add package eudplib/0.63.3/eudplib-0.63.3.tar.gz

---
+++ packages/eudplib/eudplib-0.63.3.tar.gz/eudplib-0.63.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+
+    def cythonize(x):
+        return None
+
+
+__version__ = "0.63.3"
+
+
+setup(
+    name="eudplib",
+    version=__version__,
+    packages=find_packages(),
+    package_data={"": ["*.c", "*.pyx", "*.dll", "*.dylib", "*.lst", "*.mo"]},
+    include_package_data=True,
+    ext_modules=cythonize(["eudplib/**/*.pyx"]),
+    python_requires=">=3",
+    # metadata for upload to PyPI
+    author="Trgk",
+    author_email="whyask37@naver.com",
+    description="EUD Trigger generator",
+    license="MIT license",
+    keywords="starcraft rawtrigger eud",
+    url="https://github.com/armoha/eudplib",  # project home page, if any
+)

403ac9d45035fc944c7478adf98050d1e92bb0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:54 +0000

    Add package SoundsLike/0.0.1/SoundsLike-0.0.1.tar.gz

---
+++ packages/SoundsLike/SoundsLike-0.0.1.tar.gz/SoundsLike-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='SoundsLike',
+   version='0.0.1',
+   author='Tal Zaken',
+   author_email='talzaken@gmail.com',
+   packages=find_packages(),
+   #scripts=['bin/script1','bin/script2'],
+   url='http://github.com/tal-z/SoundsLike/',
+   license='LICENSE',
+   description='SoundsLike is a python package that helps find words that sound like other words.',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=[
+       "cmudict >= 0.4.4",
+       "g2p-en >= 2.1.0",
+   ],
+)

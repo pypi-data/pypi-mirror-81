c556d4ea53e92facd58a212ccbfdf2b80502905e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:11 +0000

    Add package strava-offline/1.0.0b2/strava_offline-1.0.0b2-py3-none-any.whl

---
+++ packages/strava-offline/strava_offline-1.0.0b2-py3-none-any.whl/strava_offline/redirect_server.py
@@ -0,0 +1,58 @@
+from multiprocessing import Process, Queue
+from time import sleep
+from types import SimpleNamespace
+import bottle  # type: ignore [import]
+import webbrowser
+
+from . import config
+
+
+shared = SimpleNamespace()
+
+
+@bottle.route("/authorize")
+def authorize():
+    global shared
+    bottle.redirect(shared.authorization_url)
+
+
+@bottle.route("/code")
+def code():
+    global shared
+    shared.queue.put(bottle.request.query.code)
+
+    return "OK"
+
+
+def server(_shared: SimpleNamespace) -> None:
+    global shared
+    shared = _shared
+
+    bottle.run(host=shared.config.http_host, port=shared.config.http_port)
+
+
+def get_code(config: config.StravaApiConfig, authorization_url: str) -> str:
+    queue: Queue[str] = Queue()
+
+    shared = SimpleNamespace()
+    shared.config = config
+    shared.queue = queue
+    shared.authorization_url = authorization_url
+
+    process = Process(target=server, args=(shared,))
+    process.start()
+
+    sleep(0.2)
+    webbrowser.open_new(f"http://{config.http_host}:{config.http_port}/authorize")
+
+    code = queue.get()
+
+    sleep(0.2)
+    process.terminate()
+    process.join()
+
+    return code
+
+
+def redirect_uri(config: config.StravaApiConfig) -> str:
+    return f"http://{config.http_host}:{config.http_port}/code"

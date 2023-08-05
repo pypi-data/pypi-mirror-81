2f12f84e710f6968b90fdbdf09a8cfee8653716e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:01 +0000

    Add package urigui/2.0.0/urigui-2.0.0.tar.gz

---
+++ packages/urigui/urigui-2.0.0.tar.gz/urigui-2.0.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""setup.py: setuptools control."""
+
+import re
+from setuptools import setup
+
+version = re.search(
+    r'^__version__\s*=\s*"(.*)"',
+    open('urigui/uricli.py').read(),
+    re.M
+).group(1)
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+setup(
+    name="urigui",
+    packages=["urigui"],
+    entry_points={
+        "console_scripts": ['uri = urigui.uricli:main']
+    },
+    version=version,
+    description="Utility CLI for submitting solutions to URI Online Judge.",
+    long_description=long_descr,
+    long_description_content_type='text/markdown',
+    author="Marcus Vinicius",
+    author_email="viniips@hotmail.com",
+    install_requires=[
+        "docopt==0.6.2",
+        "PyInquirer==1.0.2",
+        "requests==2.20.0",
+        "mechanicalSoup==0.11.0",
+        "demjson==2.2.4",
+        "websocket-client==0.53.0"
+    ],
+    url="https://github.com/vininjr/urigui",
+    license='Apache 2.0'
+)

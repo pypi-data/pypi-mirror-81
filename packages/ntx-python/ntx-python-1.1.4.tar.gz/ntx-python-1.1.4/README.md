fa305fb2ee9a027bae62d30cf0f22a7488b47ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:13 +0000

    Add package ntx-python/1.1.4/ntx-python-1.1.4.tar.gz

---
+++ packages/ntx-python/ntx-python-1.1.4.tar.gz/ntx-python-1.1.4/README.md
@@ -0,0 +1,25 @@
+# Configuration for examples
+
+Create `ntx_python/__config__.py` in the root directory and fill it like this:
+
+```
+AUDIENCE='https://example.com' # Without the root slash in the path part
+USERNAME='your-username'
+PASSWORD='your-password'
+
+ID='id-of-service'
+LABEL='label-for-service'
+DOMAIN='example.com'
+
+TOKEN='static-token-here' # The trascription example is setup to use the static token
+```
+
+# Running the examples
+
+## Transcription
+
+`python -m ntx_python speech-8000-mono.wav`
+
+## Token
+
+`python -m ntx_python.__main_token__`
---
+++ packages/ntx-python/ntx-python-1.1.4.tar.gz/ntx-python-1.1.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ntx-python",
+    version="1.1.4",
+    author="Goheeca",
+    author_email="goheeca@gmail.com",
+    description="Python library for speech to text using NanoTrix cloud",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/nanogrid-libs/ntx-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'grpcio~=1.30.0',
+        'aiohttp~=3.6.2'
+    ]
+)

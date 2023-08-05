760694b38493c4de7ed7e59baa26e05c326016b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:50 +0000

    Add package bigflow/1.0.dev56/bigflow-1.0.dev56.tar.gz

---
+++ packages/bigflow/bigflow-1.0.dev56.tar.gz/bigflow-1.0.dev56/bigflow/logger.py
@@ -0,0 +1,61 @@
+import logging
+
+from google.cloud import logging_v2
+from urllib.parse import quote_plus
+
+
+def create_logging_client():
+    return logging_v2.LoggingServiceV2Client()
+
+
+class GCPLoggerHandler(logging.StreamHandler):
+    def __init__(self, project_id, workflow_id):
+        logging.StreamHandler.__init__(self)
+        self.client = create_logging_client()
+        self.project_id = project_id
+        self.workflow_id = workflow_id
+
+    def emit(self, record):
+        if record.levelname == 'INFO':
+            self.info(record.getMessage())
+        elif record.levelname == 'WARNING':
+            self.warning(record.getMessage())
+        else:
+            self.error(record.getMessage())
+
+    def get_resource(self):
+        return {
+            "type": "global",
+            "labels": {
+                "project_id": f"{self.project_id}"
+            }
+        }
+
+    def warning(self, message):
+        self.write_log_entries(message, 'WARNING')
+
+    def error(self, message):
+        self.write_log_entries(message, 'ERROR')
+
+    def info(self, message):
+        self.write_log_entries(message, 'INFO')
+
+    def write_log_entries(self, message, severity):
+        entry = logging_v2.types.LogEntry(
+            log_name=f"projects/{self.project_id}/logs/{self.workflow_id}",
+            resource=self.get_resource(),
+            text_payload=message,
+            severity=severity,
+            labels={"workflow": f"{self.workflow_id}"}
+        )
+        self.client.write_log_entries([entry])
+
+    def get_gcp_logs_message(self):
+        query = quote_plus(f'''logName="projects/{self.project_id}/logs/{self.workflow_id}"
+labels.workflow="{self.workflow_id}"''')
+        return f'*************************LOGS LINK************************* \n ' \
+               f'You can find logs for this workflow here: https://console.cloud.google.com/logs/query;query={query}' \
+               f'***********************************************************'
+
+
+
---
+++ packages/bigflow/bigflow-1.0.dev56.tar.gz/bigflow-1.0.dev56/test/test_logger.py
@@ -0,0 +1,67 @@
+from unittest import TestCase, mock
+
+from google.cloud import logging_v2
+
+from bigflow.logger import GCPLoggerHandler
+
+
+class MockedGCPLoggerHandler(TestCase):
+
+    @mock.patch('bigflow.logger.create_logging_client')
+    def setUp(self, create_logging_client_mock) -> None:
+        create_logging_client_mock.return_value = mock.create_autospec(logging_v2.LoggingServiceV2Client)
+        self.test_logger = GCPLoggerHandler('project-id', 'workflow-id')
+
+
+class LoggerTestCase(MockedGCPLoggerHandler):
+
+    def test_should_create_correct_logging_link(self):
+        message = self.test_logger.get_gcp_logs_message()
+        self.assertEqual(message, '''*************************LOGS LINK************************* 
+ You can find logs for this workflow here: https://console.cloud.google.com/logs/query;query=logName%3D%22projects%2Fproject-id%2Flogs%2Fworkflow-id%22%0Alabels.workflow%3D%22workflow-id%22 
+ ***********************************************************''')
+
+    def test_should_send_warning(self):
+        self.test_logger.warning("warning message")
+        self.test_logger.client.write_log_entries.assert_called_with([logging_v2.types.LogEntry(
+            log_name="projects/project-id/logs/workflow-id",
+            resource={
+                "type": "global",
+                "labels": {
+                    "project_id": "project-id"
+                }
+            },
+            text_payload="warning message",
+            severity='WARNING',
+            labels={"workflow": "workflow-id"}
+        )])
+
+    def test_should_send_info(self):
+        self.test_logger.info("info message")
+        self.test_logger.client.write_log_entries.assert_called_with([logging_v2.types.LogEntry(
+            log_name="projects/project-id/logs/workflow-id",
+            resource={
+                "type": "global",
+                "labels": {
+                    "project_id": "project-id"
+                }
+            },
+            text_payload="info message",
+            severity='INFO',
+            labels={"workflow": "workflow-id"}
+        )])
+
+    def test_should_send_error(self):
+        self.test_logger.error("error message")
+        self.test_logger.client.write_log_entries.assert_called_with([logging_v2.types.LogEntry(
+            log_name="projects/project-id/logs/workflow-id",
+            resource={
+                "type": "global",
+                "labels": {
+                    "project_id": "project-id"
+                }
+            },
+            text_payload="error message",
+            severity='ERROR',
+            labels={"workflow": "workflow-id"}
+        )])

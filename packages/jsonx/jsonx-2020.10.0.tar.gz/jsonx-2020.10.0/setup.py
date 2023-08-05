c1b4b93ee4615e5ec37dc742c04df9650a3a195f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:16 +0000

    Add package jsonx/2020.10.0/jsonx-2020.10.0.tar.gz

---
+++ packages/jsonx/jsonx-2020.10.0.tar.gz/jsonx-2020.10.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jsonx',
+    description="JSONx, an XML representation of the JSON data model",
+    version='2020.10.0',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    py_modules=['jsonx'],
+)

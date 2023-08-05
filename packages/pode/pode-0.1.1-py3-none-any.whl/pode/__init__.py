cce02234fc5cdd9468914732b64acddb3d93b7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:23 +0000

    Add package pode/0.1.1/pode-0.1.1-py3-none-any.whl

---
+++ packages/pode/pode-0.1.1-py3-none-any.whl/pode/__init__.py
@@ -0,0 +1,10 @@
+"""Polygon decomposition"""
+
+__version__ = '0.1.1'
+
+from .pode import (divide,
+                   Contour,
+                   Point,
+                   Polygon,
+                   Requirement)
+from .utils import joined_constrained_delaunay_triangles

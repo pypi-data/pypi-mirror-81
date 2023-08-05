69ada8db18a3c035919edc19bcd190ba1032cb4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:46 +0000

    Add package wolvr/0.0.6/wolvr-0.0.6.tar.gz

---
+++ packages/wolvr/wolvr-0.0.6.tar.gz/wolvr-0.0.6/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Author: Tom Bresee <tbresee@umich.edu>
+#
+# License: BSD 3 clause
+
+
+import setuptools
+
+import wolvr
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as fh:
+    required = fh.read().splitlines()
+
+
+setuptools.setup(
+    name="wolvr",
+    version="0.0.6",
+    author="Tom Bresee",
+    author_email="tbresee@umich.edu",
+    description="Utility Belt for Machine Learning",
+    license="BSD 3-Clause",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/tombresee/wolvr",
+    packages=setuptools.find_packages(),
+    classifiers=[  # https://pypi.org/classifiers/
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    python_requires='>=3.6',
+    include_package_data=True,
+)
---
+++ packages/wolvr/wolvr-0.0.6.tar.gz/wolvr-0.0.6/wolvr/datasets/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Author: Tom Bresee <tbresee@umich.edu>
+#
+# License: BSD 3 clause
+
+from ._datasets import public_dataset
+
+# this is for "from <package_name>.datasets import *"
+__all__ = ["public_dataset"]

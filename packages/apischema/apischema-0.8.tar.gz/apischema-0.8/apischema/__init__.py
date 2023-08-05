569ffe2928487db4f6f6db05a426feef5f4da401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:31 +0000

    Add package apischema/0.8/apischema-0.8.tar.gz

---
+++ packages/apischema/apischema-0.8.tar.gz/apischema-0.8/apischema/__init__.py
@@ -0,0 +1,56 @@
+__all__ = [
+    "NotNull",
+    "Skip",
+    "Unsupported",
+    "ValidationError",
+    "alias",
+    "check_types",
+    "conversions",
+    "deserialization",
+    "deserialize",
+    "deserializer",
+    "fields",
+    "json_schema",
+    "metadata",
+    "properties",
+    "schema",
+    "schema_ref",
+    "serialization",
+    "serialize",
+    "serializer",
+    "settings",
+    "validation",
+    "validator",
+]
+
+
+from . import (
+    conversions,
+    deserialization,
+    fields,
+    json_schema,
+    metadata,
+    serialization,
+    settings,
+    validation,
+)
+from .aliases import alias
+from .conversions import deserializer, serializer
+from .deserialization import deserialize
+from .json_schema.refs import schema_ref
+from .json_schema.schema import schema
+from .metadata import properties
+from .serialization import serialize
+from .type_checker import check_types
+from .types import NotNull, Skip
+from .validation import (
+    ValidationError,
+    validator,
+)
+from .visitor import Unsupported
+
+# Handle standard library + internal types
+serializer(ValidationError.format, ValidationError)
+from . import std_types  # noqa: E402
+
+del std_types  # clean namespace

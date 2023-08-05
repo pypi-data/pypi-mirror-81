46fcccb0dc511a2bc3053641c1ff0424c010200f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:26 +0000

    Add package Hypyxel/0.0.14/Hypyxel-0.0.14.tar.gz

---
+++ packages/Hypyxel/Hypyxel-0.0.14.tar.gz/Hypyxel-0.0.14/hypyxel/errors.py
@@ -0,0 +1,7 @@
+class ApiKeyError(Exception):
+    """The Hypixel API key is invalid/hasn\'t been set. Set it with `set_api_key()`"""
+    pass
+
+class UUIDNotFoundError(Exception):
+    """A UUID could not be found for the passed user name"""
+    pass

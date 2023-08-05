37696a29d4990b1b56ed1b605886aff675a760ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package bitsbehumble/0.6/bitsbehumble-0.6.tar.gz

---
+++ packages/bitsbehumble/bitsbehumble-0.6.tar.gz/bitsbehumble-0.6/setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import *
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+my_packages=find_packages()
+
+
+setup(
+  name = 'bitsbehumble',      
+  packages = my_packages,  
+  version = '0.6',      
+  long_description=long_description,
+
+  long_description_content_type="text/markdown", 
+  author = 'Alya Gomaa',  
+  url = 'https://github.com/AlyaGomaa/bitsbehumble', 
+  download_url  = 'https://github.com/AlyaGomaa/bitsbehumble/releases/tag/v-2.0.01',
+
+  keywords = ['CTF', 'Converter'],  
+  classifiers=[
+    'Development Status :: 4 - Beta',    
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',  
+    'Programming Language :: Python :: 3',   
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    
+  ],
+)

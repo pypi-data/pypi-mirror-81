72b177d2503bd60e55f002bf624a44c6257f8252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:51 +0000

    Add package rk-udacity-distributions-package/1.0/rk_udacity_distributions_package-1.0.tar.gz

---
+++ packages/rk-udacity-distributions-package/rk_udacity_distributions_package-1.0.tar.gz/rk_udacity_distributions_package-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='rk_udacity_distributions_package',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['rk_udacity_distributions_package'],
+      author = 'Rohan Krishnakumar',
+      author_email = 'rohankrishnakumar3@gmail.com',
+      zip_safe=False)

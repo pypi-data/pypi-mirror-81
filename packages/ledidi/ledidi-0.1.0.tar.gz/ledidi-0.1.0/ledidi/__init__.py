413802bb406bb2316616960d64a04631cbc60d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:39 +0000

    Add package ledidi/0.1.0/ledidi-0.1.0.tar.gz

---
+++ packages/ledidi/ledidi-0.1.0.tar.gz/ledidi-0.1.0/ledidi/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+# Authors: Yang Lu <ylu465@uw.edu> and Jacob Schreiber <jmschreiber91@gmail.com>
+
+from .ledidi import Ledidi
+from .ledidi import TensorFlowRegressor
+
+__version__ = '0.1.0'

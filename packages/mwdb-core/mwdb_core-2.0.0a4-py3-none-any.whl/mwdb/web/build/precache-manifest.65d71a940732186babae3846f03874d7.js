25efea31545afb0f5c1cd9995ef21d328db9f7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:56 +0000

    Add package mwdb-core/2.0.0a4/mwdb_core-2.0.0a4-py3-none-any.whl

---
+++ packages/mwdb-core/mwdb_core-2.0.0a4-py3-none-any.whl/mwdb/web/build/precache-manifest.65d71a940732186babae3846f03874d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbb93be97b5d9f9622f483a9e3101a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa5c690e3fdf31263f6",
+    "url": "/static/css/2.90562ad3.chunk.css"
+  },
+  {
+    "revision": "83ab879e6c9072ed9e2b",
+    "url": "/static/css/main.b6123f66.chunk.css"
+  },
+  {
+    "revision": "baa5c690e3fdf31263f6",
+    "url": "/static/js/2.b50cfce0.chunk.js"
+  },
+  {
+    "revision": "09ebc81dbfc28cf93962ae7782c9c947",
+    "url": "/static/js/2.b50cfce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ab879e6c9072ed9e2b",
+    "url": "/static/js/main.833eca5e.chunk.js"
+  },
+  {
+    "revision": "f89b46cc4b0d8b273726",
+    "url": "/static/js/runtime-main.27f1ea9c.js"
+  },
+  {
+    "revision": "b21df568d6dd40d16a1f30ac4eb81763",
+    "url": "/static/media/logo.b21df568.png"
+  }
+]);
---
+++ packages/mwdb-core/mwdb_core-2.0.0a4-py3-none-any.whl/mwdb/web/src/components/UserProfile.js
@@ -0,0 +1,182 @@
+import React, {Component} from 'react';
+import { connect } from "react-redux";
+import { Link } from "react-router-dom"
+import {capabilitiesList} from "./Capabilities";
+
+import api from "@mwdb-web/commons/api";
+import { View, DateString, ErrorBoundary } from "@mwdb-web/commons/ui";
+import { makeSearchLink } from "../commons/helpers";
+
+import ManageAPIKeys from './ManageAPIKeys';
+
+class UserProfile extends Component {
+    state = {
+        pressedRequestPassword: false,
+        error: false
+    }
+
+    get capabilities() {
+        return [...this.state.profile.groups.reduce((p, c) => new Set([...p, ...c.capabilities]), new Set())]
+    }
+
+    inheritedFrom(cap) {
+        return this.state.profile.groups.filter(c => c.capabilities.includes(cap)).map(c => c.name)
+    }
+
+    get groups() {
+        return this.state.profile.groups.filter(g => g.name !== "public" && !g.private)
+    }
+
+    handleUpdate = async () => {
+        try {
+            let response = await api.getUserProfile(this.props.match.params.login)
+            this.setState({
+                profile: response.data,
+            })
+
+        } catch(error) {
+            this.setState({error})
+        }
+    }
+
+    requestPasswordChange = async () => {
+        try {
+            await api.authRequestPasswordChange();
+            this.setState({
+                pressedRequestPassword: true,
+                success: "Password reset link has been sent to your e-mail address."
+            });
+        } catch(error) {
+            this.setState({error})
+        }
+    }
+
+    componentDidMount() {
+        this.handleUpdate();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.match.params.login !== prevProps.match.params.login) {
+            this.handleUpdate();
+        }
+    }
+
+    render() {
+        if (!this.state.profile && !this.state.error) {
+            return <div>Loading...</div>;
+        } else if(!this.state.profile && this.state.error) {
+            return <ErrorBoundary error={this.state.error}/>
+        }
+        
+        return (
+            <View ident="userProfile" error={this.state.error} success={this.state.success}>
+                <table className="table table-striped table-bordered wrap-table">
+                    <thead>
+                        <tr>
+                            <th key="key" className="col-2">Attribute</th>
+                            <th key="value">Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Login</td>
+                            <td>
+                                {this.state.profile.login}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>E-mail</td>
+                            <td>
+                                {this.state.profile.email}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Registered</td>
+                            <td><DateString date={this.state.profile.registered_on}/></td>
+                        </tr>
+                        <tr>
+                            <td>Last login</td>
+                            <td><DateString date={this.state.profile.logged_on}/></td>
+                        </tr>
+                        {
+                            this.props.userLogin === this.state.profile.login &&
+                            <tr>
+                                <td>Last password set</td>
+                                <td><DateString date={this.state.profile.set_password_on}/></td>
+                            </tr>
+                        }
+                        {
+                            (this.state.profile.capabilities && this.capabilities.length > 0) &&
+                            <tr>
+                                <td>Capabilities</td>
+                                <td>
+                                    <ul className="table-ul">
+                                        {
+                                            this.capabilities.map(c => (
+                                                <li>
+                                                    {capabilitiesList[c]} 
+                                                    (inherited from: {this.inheritedFrom(c).join(", ")})
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
+                                </td>
+                            </tr>
+                        }
+                        {
+                            (this.state.profile.groups && this.groups.length > 0) &&
+                            <tr>
+                                <td>Groups</td>
+                                <td>
+                                    <ul className="table-ul">
+                                        {
+                                            this.groups.map(g => <li>{g.name}</li>)
+                                        }
+                                    </ul>
+                                </td>
+                            </tr>
+                        }
+                        <tr>
+                            <td style={{textAlign: 'left'}} colspan="3">
+                                {
+                                    this.props.userLogin === this.state.profile.login ? (
+                                        <button type="button" className="btn btn-success"
+                                                onClick={this.requestPasswordChange} disabled={this.state.pressedRequestPassword}>
+                                            Request new password
+                                        </button>
+                                    ) : (
+                                        <Link to={makeSearchLink("uploader", this.state.profile.login)}>
+                                            <button type="button" className="btn btn-success">
+                                                Search {this.state.profile.login}'s uploads
+                                            </button>
+                                        </Link>
+                                    )
+                                }
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                {
+                    this.props.userLogin === this.state.profile.login &&
+                    <div>
+                        <h4>API keys</h4>
+                        <ManageAPIKeys items={this.state.profile.api_keys}
+                                       userLogin={this.props.userLogin}
+                                       onSuccess={(success) => { this.handleUpdate(); this.setState({success}); }}
+                                       onError={(error) => this.setState({error})} />
+                    </div>
+                }
+            </View>
+        )
+    }
+}
+
+function mapStateToProps(state, ownProps)
+{
+    return {
+        ...ownProps,
+        userLogin: state.auth.loggedUser && state.auth.loggedUser.login
+    }
+}
+
+export default connect(mapStateToProps)(UserProfile);

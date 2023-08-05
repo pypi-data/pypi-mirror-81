196fc17f6e28792d67e0885b6e4ab568459d9fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:21 +0000

    Add package Hypyxel/0.0.13/Hypyxel-0.0.13-py3-none-any.whl

---
+++ packages/Hypyxel/Hypyxel-0.0.13-py3-none-any.whl/hypyxel/utils.py
@@ -0,0 +1,25 @@
+import requests
+
+from hypyxel.errors import UUIDNotFoundError, ApiKeyError
+
+key = ""
+
+def _get_uuid(username):
+    try:
+        return requests.request("GET", f"https://playerdb.co/api/player/minecraft/{username}")['player']['meta']['raw_id']
+    except:
+        raise UUIDNotFoundError(f"A UUID could not be found for {username}")
+
+def set_api_key(user_key):
+    """Set the API key"""
+    global key
+    key = user_key
+
+def _key_check():
+    """An internal function to check if the key exists"""
+    if not key:
+        raise ApiKeyError("You need to set the key with set_api_key()")
+
+def _get_key():
+    global key
+    return key

300897d789af0de287c4f33561f42f29fcc31306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:26 +0000

    Add package pybarycuda/1.0.0/pybarycuda-1.0.0-py3.6-linux-x86_64.egg

---
+++ packages/pybarycuda/pybarycuda-1.0.0-py3.6-linux-x86_64.egg/pybarycuda/api.py
@@ -0,0 +1,7 @@
+import pybarycuda.core as bary
+
+def point_in_simplex(pts, n, dim, verts):
+  return list(map(bool, bary.point_in_simplex(pts, n, dim, verts)))
+
+def bary_simplex(pts, n, dim, verts):
+  return bary.bary_simplex(pts, n, dim, verts)

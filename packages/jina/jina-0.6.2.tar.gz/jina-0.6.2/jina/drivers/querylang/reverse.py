e56c41144a5de5036acd5b0cbaa2587462b05161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:38 +0000

    Add package jina/0.6.2/jina-0.6.2.tar.gz

---
+++ packages/jina/jina-0.6.2.tar.gz/jina-0.6.2/jina/drivers/querylang/reverse.py
@@ -0,0 +1,32 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+from typing import Iterable
+
+from .. import QuerySetReader, BaseRecursiveDriver
+
+if False:
+    from ...proto import jina_pb2
+
+
+class ReverseQL(QuerySetReader, BaseRecursiveDriver):
+    """Reverses the order of the provided ``docs``.
+
+        This is often useful when the proceeding Pods require only a signal, not the full message.
+
+        Example ::
+        - !Chunk2DocRankerDriver {}
+        - !ReverseQL {}
+
+        will reverse the order of the documents returned by the `Chunk2DocRankerDriver` before sending them to the next `Pod`
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.is_apply = False
+
+    def _apply_all(self, docs: Iterable['jina_pb2.Document'], *args, **kwargs) -> None:
+        prev_len = len(docs)
+        for d in reversed(docs):
+            dd = docs.add()
+            dd.CopyFrom(d)
+        del docs[:prev_len]

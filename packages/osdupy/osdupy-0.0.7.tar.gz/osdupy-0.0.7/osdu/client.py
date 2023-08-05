570b2523bf2dc3412239bc41b0497a1e00831011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:21 +0000

    Add package osdupy/0.0.7/osdupy-0.0.7.tar.gz

---
+++ packages/osdupy/osdupy-0.0.7.tar.gz/osdupy-0.0.7/osdu/client.py
@@ -0,0 +1,85 @@
+""" Handles the authentication and token management for interacting with the OSDU platform.
+"""
+
+import logging
+import os
+
+import boto3
+
+from osdu.delivery import DeliveryService
+from osdu.search import SearchService
+from osdu.storage import StorageService
+
+
+class BaseOsduClient:
+
+    @property
+    def token(self):
+        return self._token
+
+    @property
+    def api_url(self):
+        return self._api_url
+
+    @property
+    def search(self):
+        return self._search
+
+    @property
+    def storage(self):
+        return self._storage
+
+    @property
+    def delivery(self):
+        return self._delivery
+
+    @property
+    def data_partition_id(self):
+        return self._data_partition_id
+
+    @data_partition_id.setter
+    def data_partition_id(self, val):
+        self._data_partition_id = val
+
+
+    def __init__(self, data_partition_id, api_url:str=None, client_id:str=None, user:str=None, password:str=None):
+        """Authenticate and instantiate a new OSDU client.
+        
+        'api_url' must be only the base URL, e.g. https://myapi.myregion.mydomain.com
+        """
+        # Environment variables.
+        # TODO: Validate api_url against URL regex pattern.        
+        self._api_url = (api_url or os.environ.get('OSDU_API_URL')).rstrip('/')
+        self._client_id = client_id or os.environ.get('OSDU_CLIENT_ID')
+        self._user = user or os.environ.get('OSDU_USER')
+        p = password or os.environ.get('OSDU_PASSWORD')
+        self._token = self.get_access_token(p)
+        p = None # Don't leave password lying around.
+
+        self._data_partition_id = data_partition_id
+
+        # Instantiate services.
+        self._search = SearchService(self)
+        self._storage = StorageService(self)
+        self._delivery = DeliveryService(self)
+
+        # TODO: Implement these services.
+        # self.__legal = LegaService(self)
+        # self.__entitlements = EntitlementsService(self)
+
+
+    # Abstract Method
+    def get_access_token(self, password):
+        raise NotImplementedError('This method must be implemented by a subclass')
+
+
+class AwsOsduClient(BaseOsduClient):
+
+    def get_access_token(self, password):
+        client = boto3.client('cognito-idp')
+        response = client.initiate_auth(
+            AuthFlow='USER_PASSWORD_AUTH',
+            ClientId=self._client_id,
+            AuthParameters={ 'USERNAME': self._user, 'PASSWORD': password }
+        )
+        return "Bearer " + response['AuthenticationResult']['AccessToken']   
---
+++ packages/osdupy/osdupy-0.0.7.tar.gz/osdupy-0.0.7/osdu/search.py
@@ -0,0 +1,65 @@
+""" Provides a simple Python interface to the OSDU Search API.
+"""
+import requests
+from .base import BaseService
+import logging
+
+
+class SearchService(BaseService):
+
+    def __init__(self, client):
+        super().__init__(client, 'search')
+
+
+    def query(self, query: dict) -> dict:
+        """Executes a query against the OSDU search service.
+
+        :param query:   dict representing the JSON-style query to be sent to the search API. Must adhere to
+                        the Lucene syntax suported by OSDU. For more details, see: 
+                        https://community.opengroup.org/osdu/documentation/-/wikis/Releases/R2.0/OSDU-Query-Syntax
+
+        :returns:       dict containing 3 items: aggregations, results, totalCount
+                        - aggregations: dict:   returned only if 'aggregateBy' specified in query
+                        - results:      list:   of records resutling from search query  
+                        - totalCount:   int:    the total number of results despite any 'limit' specified in the
+                                                query or the 1,000 record limit of the API
+        """
+        url = f'{self._service_url}/query'
+        response = requests.post(url=url, headers=self._headers(), json=query)
+        response.raise_for_status()
+
+        return response.json()
+
+
+    def query_with_paging(self, query: dict):
+        """Executes a query with cursor against the OSDU search service. Returns a generator, which can than be
+        iterated over to retrieve each page in the result set without having to deal with any cursor.
+
+        :param query:   dict representing the JSON-style query to be sent to the search API. Must adhere to
+                        the Lucene syntax suported by OSDU. For more details, see: 
+                        https://community.opengroup.org/osdu/documentation/-/wikis/Releases/R2.0/OSDU-Query-Syntax
+
+        :returns:       iterator of tuple containing 2 items: (results, totalCount)
+                        - results:      list:   one page of records resutling from search query. Default page size
+                                                is 10. This can be modified by passing the 'limit' parameter in
+                                                query with the maximum allowed being 1000.
+                        - totalCount:   int:    the total number of results despite any 'limit' specified in the
+                                                query or the 1,000 record limit of the API
+        """
+        url = f'{self._service_url}/query_with_cursor'
+        cursor=''
+
+        while cursor:
+            # Add cursor to request body for subsequent requests.
+            if cursor != '':
+                query['cursor'] = cursor
+            
+            response = requests.post(url=url, headers=self._headers(), json=query)
+            response.raise_for_status()
+
+            cursor, results, total_count  = response.json().values()
+            if not cursor:  # Effetive do-while condition
+                break
+            else:
+                yield results, total_count
+                

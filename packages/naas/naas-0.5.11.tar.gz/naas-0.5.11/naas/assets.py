7d949d06c371a75a3749826a5cb41cc8a4947c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:38 +0000

    Add package naas/0.5.11/naas-0.5.11.tar.gz

---
+++ packages/naas/naas-0.5.11.tar.gz/naas-0.5.11/naas/assets.py
@@ -0,0 +1,115 @@
+from .types import t_asset
+from .manager import Manager
+import os
+
+
+class Assets:
+    naas = None
+    role = t_asset
+
+    def __init__(self):
+        self.manager = Manager()
+
+    def current_raw(self):
+        json_data = self.manager.get_naas()
+        for item in json_data:
+            if item["type"] == self.role:
+                print(item)
+
+    def currents(self, raw=False):
+        json_data = self.manager.get_naas()
+        if raw:
+            for item in json_data:
+                if item["type"] == self.role:
+                    print(item)
+        else:
+            for item in json_data:
+                kind = None
+                if item["type"] == self.role:
+                    kind = f"gettable with this url {self.manager.proxy_url('assets', item['value'])}"
+                    print(f"File ==> {item['path']} is {kind}")
+
+    def get(self, path=None):
+        if not self.manager.notebook_path():
+            print("No get done you are in already in naas folder\n")
+            return
+        current_file = self.manager.get_path(path)
+        self.manager.get_prod(current_file)
+
+    def get_history(self, histo, path=None):
+        if not self.manager.notebook_path():
+            print("No get history done you are in already in naas folder\n")
+            return
+        current_file = self.manager.get_path(path)
+        self.manager.get_history(current_file, histo)
+
+    def list_history(self, path=None):
+        current_file = self.manager.get_path(path)
+        self.manager.list_history(current_file)
+
+    def clear_history(self, path=None, histo=None):
+        if not self.manager.notebook_path():
+            print("No clear history done you are in already in naas folder\n")
+            return
+        current_file = self.manager.get_path(path)
+        self.manager.clear_history(current_file, histo)
+        print("🕣 Your Assets history has been remove from production folder.\n")
+
+    def add(self, path=None, params={}, debug=False, Force=False):
+        current_file = self.manager.get_path(path)
+        if current_file is None:
+            print("Missing file path in prod mode")
+            return
+        prod_path = self.manager.get_prod_path(current_file)
+        token = self.manager.get_value(prod_path, self.role)
+        if token is None or Force is True:
+            token = os.urandom(30).hex()
+        url = self.manager.proxy_url(self.role, token)
+        if not self.manager.notebook_path() and Force is False:
+            print("No add done you are in already in naas folder\n")
+            return url
+        print("👌 Well done! Your Assets has been sent to production folder.\n")
+        print(f"🔗 You can access this assets remotely with: {url} \n")
+        self.manager.copy_url(url)
+        print('PS: to remove the "Assets" feature, just replace .add by .delete')
+        self.manager.add_prod(
+            {"type": self.role, "path": current_file, "params": params, "value": token},
+            not debug,
+        )
+        return url
+
+    def delete(self, path=None, all=False, debug=False):
+        if not self.manager.notebook_path():
+            print("No delete done you are in already in naas folder\n")
+            return
+        current_file = self.manager.get_path(path)
+        self.manager.del_prod({"type": self.role, "path": current_file}, not debug)
+        print("🗑 Done! Your Assets has been remove from production folder.\n")
+        if all is True:
+            self.manager.clear_history(current_file)
+
+    def help(self):
+        print(f"=== {type(self).__name__} === \n")
+        print(
+            f".add(path, params) => add path to the prod {type(self).__name__} server\n"
+        )
+        print(
+            f".delete(path) => delete path to the prod {type(self).__name__} server\n"
+        )
+        print(
+            ".clear_history(path, histonumber) => clear history, history number and path are optionel, \
+                if you don't provide them it will erase full history of current file \n"
+        )
+        print(
+            ".list_history(path) => list history, of a path or if not provided the current file \n"
+        )
+        print(
+            ".get_history(path, histonumber) => get history file, of a path or if not provided the current file \n"
+        )
+        print(
+            ".get(path) => get current prod file of a path, or if not provided the current file \n"
+        )
+        print(f".currents() => get current list of {type(self).__name__} prod file\n")
+        print(
+            f".current(raw=True) => get json current list of {type(self).__name__} prod file\n"
+        )
---
+++ packages/naas/naas-0.5.11.tar.gz/naas-0.5.11/naas/runner/runner.py
@@ -0,0 +1,186 @@
+from naas.types import t_main, t_notebook, t_scheduler, t_asset, t_job
+from sentry_sdk.integrations.sanic import SanicIntegration
+from .controllers.scheduler import SchedulerController
+from .controllers.assets import AssetsController
+from .controllers.notebooks import NbController
+from .controllers.jobs import JobsController
+from .controllers.logs import LogsController
+from .controllers.env import EnvController
+from .notifications import Notifications
+from .scheduler import Scheduler
+from .notebooks import Notebooks
+from sanic_openapi import swagger_blueprint
+from .logger import Logger
+from .proxy import escape_kubernet
+from .jobs import Jobs
+
+# from naas.__version__ import __version__
+from sanic import Sanic
+import sentry_sdk
+import asyncio
+import getpass
+import uuid
+import os
+import json
+import sys
+import errno
+import nest_asyncio
+
+# TODO remove this fix when papermill support uvloop of Sanic support option to don't use uvloop
+asyncio.set_event_loop_policy(None)
+nest_asyncio.apply()
+
+__version__ = "0.5.11"
+
+
+class Runner:
+    # Declare semaphore variable
+    __name = "naas_runner"
+    __naas_folder = ".naas"
+    __tasks_sem = None
+    # Declare path variable
+    __path_lib_files = os.path.dirname(os.path.abspath(__file__))
+    __path_user_files = None
+    __port = 5000
+    __single_user_api_path = None
+    __html_files = "html"
+    __assets_files = "assets"
+    __manager_index = "manager.html"
+    __log_filename = "logs.csv"
+    __tasks_files = "jobs.json"
+    __info_file = "info.json"
+    __app = None
+    __nb = None
+    __http_server = None
+    __notif = None
+    __jobs = None
+    __scheduler = None
+    __daemon = None
+    __sentry = None
+    __logger = None
+    __testing = False
+    tz = None
+    user = None
+    __shell_user = None
+    public_url = None
+    proxy_url = None
+
+    def __init__(self):
+        self.__path_user_files = os.environ.get(
+            "JUPYTER_SERVER_ROOT", f'/home/{os.environ.get("NB_USER", "ftp")}'
+        )
+        self.__port = int(os.environ.get("NAAS_RUNNER_PORT", 5000))
+        self.__user = os.environ.get("JUPYTERHUB_USER", "joyvan@naas.com")
+        self.__shell_user = os.environ.get("NB_USER", None)
+        self.__public_url = os.environ.get("JUPYTERHUB_URL", "")
+        self.__proxy_url = os.environ.get("PUBLIC_PROXY_API", "http://localhost:3002")
+        self.__notifications_url = os.environ.get(
+            "NOTIFICATIONS_API", "http://localhost:3003"
+        )
+        self.__tz = os.environ.get("TZ", "Europe/Paris")
+        self.__path_naas_files = os.path.join(
+            self.__path_user_files, self.__naas_folder
+        )
+        self.__path_html_files = os.path.join(self.__path_lib_files, self.__html_files)
+        self.__path_manager_index = os.path.join(
+            self.__path_html_files, self.__manager_index
+        )
+        self.__path_pidfile = os.path.join(self.__path_naas_files, f"{self.__name}.pid")
+
+    def __main(self, debug=True):
+        self.init_app()
+        uid = str(uuid.uuid4())
+        self.__logger.info({"id": uid, "type": t_main, "status": "start API"})
+        self.__app.run(host="0.0.0.0", port=self.__port, debug=debug, access_log=debug)
+
+    def __version(self):
+        version = None
+        try:
+            with open(
+                os.path.join(self.__path_lib_files, self.__info_file), "r"
+            ) as json_file:
+                version = json.load(json_file)
+        except IOError:
+            version = {"error": "cannot get info.json"}
+        return version
+
+    async def initialize_before_start(self, app, loop):
+        if self.__jobs is None:
+            self.__jobs = Jobs(self.__logger)
+            self.__nb = Notebooks(self.__logger, self.__notif)
+            self.__app.add_route(
+                NbController.as_view(self.__logger, self.__jobs, self.__nb),
+                f"/{t_notebook}/<token>",
+            )
+            self.__scheduler = Scheduler(self.__logger, self.__jobs, self.__nb, loop)
+            self.__app.add_route(
+                SchedulerController.as_view(self.__logger, self.__scheduler),
+                f"/{t_scheduler}/<mode>",
+            )
+            self.__app.add_route(
+                AssetsController.as_view(
+                    self.__logger, self.__jobs, self.__path_lib_files
+                ),
+                f"/{t_asset}/<token>",
+            )
+            self.__app.add_route(
+                JobsController.as_view(self.__logger, self.__jobs), f"/{t_job}"
+            )
+            await self.__scheduler.start()
+
+    def init_app(self):
+        if not os.path.exists(self.__path_naas_files):
+            try:
+                print("Init Naas folder Jobs")
+                os.makedirs(self.__path_naas_files)
+            except OSError as exc:  # Guard against race condition
+                print("__path_naas_files", self.__path_naas_files)
+                if exc.errno != errno.EEXIST:
+                    raise
+            except Exception as e:
+                print("Exception", e)
+        self.__app = Sanic(__name__)
+        self.__logger = Logger()
+        self.__notif = Notifications(self.__logger)
+        self.__app.register_listener(
+            self.initialize_before_start, "before_server_start"
+        )
+        self.__app.add_route(
+            EnvController.as_view(
+                self.__logger,
+                self.__user,
+                self.__public_url,
+                self.__proxy_url,
+                self.__notifications_url,
+                self.__tz,
+            ),
+            "/env",
+        )
+        self.__app.add_route(LogsController.as_view(self.__logger), "/log")
+        self.__app.static("/", self.__path_manager_index, name="manager.html")
+        self.__app.blueprint(swagger_blueprint)
+        uid = str(uuid.uuid4())
+        self.__logger.info({"id": uid, "type": t_main, "status": "init API"})
+        return self.__app
+
+    def start(self, deamon=True, port=None, debug=False):
+        user = getpass.getuser()
+        if user != self.__shell_user:
+            raise Exception(f"{user} not autorized, use {self.__shell_user} instead")
+        if port:
+            self.__port = port
+        print("Start Runner", __version__)
+        try:
+            if os.environ.get("NAAS_SENTRY_DSN"):
+                self.__sentry = sentry_sdk.init(
+                    dsn=os.environ.get("NAAS_SENTRY_DSN"),
+                    traces_sample_rate=1.0,
+                    environment=escape_kubernet(self.__user),
+                    integrations=[SanicIntegration()],
+                )
+                with sentry_sdk.configure_scope() as scope:
+                    scope.set_context("Naas", {"version": __version__})
+            self.__main(debug)
+        except KeyboardInterrupt:
+            print("Shutdown server")
+            sys.exit()

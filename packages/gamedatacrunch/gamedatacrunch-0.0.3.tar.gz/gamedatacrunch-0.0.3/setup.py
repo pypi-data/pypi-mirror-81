b0117ee096b769e0c2f369289c82a0b04b07798f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:19 +0000

    Add package gamedatacrunch/0.0.3/gamedatacrunch-0.0.3.tar.gz

---
+++ packages/gamedatacrunch/gamedatacrunch-0.0.3.tar.gz/gamedatacrunch-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='gamedatacrunch',
+    version='0.0.3',
+    author='Wok',
+    author_email='wok@tuta.io',
+    description='GameDataCrunch API on PyPI',
+    keywords=['steam', 'gamedatacrunch', 'api'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/woctezuma/gamedatacrunch',
+    download_url='https://github.com/woctezuma/gamedatacrunch/archive/0.0.3.tar.gz',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Games/Entertainment',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)

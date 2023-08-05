8f7b049e3298caebc12067727bef308ac44471e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:08 +0000

    Add package geringoso/0.0.1/geringoso-0.0.1.tar.gz

---
+++ packages/geringoso/geringoso-0.0.1.tar.gz/geringoso-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="geringoso",
+    version="0.0.1",
+    author="John Lenton",
+    author_email="jlenton@gmail.com",
+    description="tool for geringoso",
+    long_description="TBD",
+    license="MIT",
+    packages=["geringoso"],
+    entry_points={
+        "console_scripts": ["geringoso = geringoso.__main__:main"],
+        },
+    python_requires=">=3",
+)

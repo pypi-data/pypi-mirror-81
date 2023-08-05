6b3674d03c11232e8d8f2ca51f94a90c7ab4cbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:46 +0000

    Add package kaiko/0.0.1/kaiko-0.0.1.tar.gz

---
+++ packages/kaiko/kaiko-0.0.1.tar.gz/kaiko-0.0.1/kaiko/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+# Version number
+__version__ = "0.1"
+
+from kaiko.kaiko import Candles, KaikoData, KaikoClient, OrderBookSnapshots, OrderBookAggregations, TickTrades
+import kaiko.utils as utils

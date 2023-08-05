8461d0e7e2b156aa6a14abc9595ff0e4a6312a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:23 +0000

    Add package sparkpipelineframework/0.1.0/sparkpipelineframework-0.1.0-py3-none-any.whl

---
+++ packages/sparkpipelineframework/sparkpipelineframework-0.1.0-py3-none-any.whl/spark_pipeline_framework/utilities/spark_data_frame_helpers.py
@@ -0,0 +1,25 @@
+from typing import Dict, Any
+
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import StructType
+
+
+class SparkDataFrameHelpers:
+    @staticmethod
+    def create_view_from_dictionary(view: str,
+                                    data: [Dict[str, Any]],
+                                    spark_session: SparkSession,
+                                    schema=None
+                                    ) -> DataFrame:
+        df = spark_session.createDataFrame(data=data, schema=schema)
+        df.createOrReplaceTempView(name=view)
+        return df
+
+    @staticmethod
+    def create_empty_dataframe(spark_session: SparkSession) -> DataFrame:
+        schema = StructType([])
+
+        df: DataFrame = spark_session.createDataFrame(
+            spark_session.sparkContext.emptyRDD(), schema)
+
+        return df

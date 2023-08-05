d9852fef550f73287968c932d840a267a1319663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:44 +0000

    Add package aruntestpackageair/0.0.3.3/aruntestpackageair-0.0.3.3.tar.gz

---
+++ packages/aruntestpackageair/aruntestpackageair-0.0.3.3.tar.gz/aruntestpackageair-0.0.3.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aruntestpackageair", # Replace with your own username
+    version="0.0.3.3",
+    author="Zana",
+    author_email="aarunkaashyap@gmail.com",
+    description="A small example package to test backend functionality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcarun123/air_backend_test",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

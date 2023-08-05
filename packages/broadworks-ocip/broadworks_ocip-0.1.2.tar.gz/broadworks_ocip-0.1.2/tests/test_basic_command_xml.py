c18144825d4a9a6339226bdfa980c050d80bd3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:46 +0000

    Add package broadworks-ocip/0.1.2/broadworks_ocip-0.1.2.tar.gz

---
+++ packages/broadworks-ocip/broadworks_ocip-0.1.2.tar.gz/broadworks_ocip-0.1.2/tests/test_basic_command_xml.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+"""Tests for `broadworks_ocip` package."""
+import pytest  # noqa: F401
+from lxml import etree
+
+from broadworks_ocip import BroadworksAPI
+
+BASIC_API_PARAMS = {
+    "host": "localhost",
+    "username": "username@example.com",
+    "password": "password",
+    "session": "00000000-1111-2222-3333-444444444444",
+}
+
+
+def canon_xml(inxml):
+    """
+    we XML canonicalise/pretty print this to prevent whitespace/quote ordering
+    issues and to make any diffs easier to read.
+    Returned XML is as a decoded string - prettier for diffs
+    """
+    return etree.tostring(etree.fromstring(inxml), pretty_print=True).decode()
+
+
+def compare_command_xml(wanted, command, **kwargs):
+    """Create a Broadworks XML command framgment from the argumenta"""
+    api = BroadworksAPI(**BASIC_API_PARAMS)
+    generated = api.get_command_xml(command, **kwargs)
+    assert canon_xml(generated) == canon_xml(wanted)
+
+
+def test_authentication_request_xml():
+    compare_command_xml(
+        (
+            b'<?xml version="1.0" encoding="ISO-8859-1"?>\n'
+            b'<BroadsoftDocument protocol="OCI" xmlns="C" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+            b'<sessionId xmlns="">00000000-1111-2222-3333-444444444444</sessionId>'
+            b'<command xmlns="" xsi:type="AuthenticationRequest">'
+            b"<userId>username@example.com</userId></command></BroadsoftDocument>"
+        ),
+        "AuthenticationRequest",
+        user_id="username@example.com",
+    )
+
+
+def test_error_response_xml():
+    compare_command_xml(
+        (
+            b'<?xml version="1.0" encoding="ISO-8859-1"?>\n'
+            b'<BroadsoftDocument protocol="OCI" xmlns="C" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
+            b'<sessionId xmlns="">00000000-1111-2222-3333-444444444444</sessionId>'
+            b'<command type="Error" echo="" xsi:type="c:ErrorResponse" xmlns:c="C" xmlns="">'
+            b"<summary>[Error 4962] Invalid password</summary>"
+            b"<summaryEnglish>[Error 4962] Invalid password</summaryEnglish>"
+            b"</command></BroadsoftDocument>"
+        ),
+        "ErrorResponse",
+        summary="[Error 4962] Invalid password",
+        summary_english="[Error 4962] Invalid password",
+    )
+
+
+# end

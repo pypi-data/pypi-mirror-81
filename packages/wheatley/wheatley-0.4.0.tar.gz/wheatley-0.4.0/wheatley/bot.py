146e8a17d22ae7abf74e6bb4baec67515425500f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:00 +0000

    Add package wheatley/0.4.0/wheatley-0.4.0.tar.gz

---
+++ packages/wheatley/wheatley-0.4.0.tar.gz/wheatley-0.4.0/wheatley/bot.py
@@ -0,0 +1,247 @@
+"""
+Module to contain the Bot class, which acts as 'glue' to combine the rhythm, row generation and
+socket-io communication provided by the Rhythm, RowGenerator and Tower objects into a useful
+program.
+"""
+
+import time
+import logging
+from typing import Optional
+
+from wheatley import calls
+from wheatley.row_generation import RowGenerator
+from wheatley.bell import Bell
+from wheatley.rhythm import Rhythm
+from wheatley.tower import RingingRoomTower
+
+
+class Bot:
+    """
+    A class to hold all the information that the bot will use to glue together the rhythm,
+    row_gen and socket-io parts together into a useful program.
+    """
+
+    logger_name = "BOT"
+
+    def __init__(self, tower: RingingRoomTower, row_generator: RowGenerator, do_up_down_in: bool,
+                 stop_at_rounds: bool, rhythm: Rhythm, user_name: Optional[str] = None):
+        """ Initialise a Bot with all the parts it needs to run. """
+        self._rhythm = rhythm
+
+        self._do_up_down_in = do_up_down_in
+        self._stop_at_rounds = stop_at_rounds
+        self._user_name = user_name
+
+        self.row_generator = row_generator
+
+        self._tower = tower
+
+        self._tower.invoke_on_call[calls.LOOK_TO].append(self._on_look_to)
+        self._tower.invoke_on_call[calls.GO].append(self._on_go)
+        self._tower.invoke_on_call[calls.BOB].append(self._on_bob)
+        self._tower.invoke_on_call[calls.SINGLE].append(self._on_single)
+        self._tower.invoke_on_call[calls.THATS_ALL].append(self._on_thats_all)
+        self._tower.invoke_on_call[calls.STAND].append(self._on_stand_next)
+
+        self._tower.invoke_on_bell_rung.append(self._on_bell_ring)
+        self._tower.invoke_on_reset.append(self._on_size_change)
+
+        self._is_ringing = False
+        self._is_ringing_rounds = True
+
+        self._should_start_method = False
+        self._should_start_ringing_rounds = False
+        self._should_stand = False
+
+        self._row_number = 0
+        self._place = 0
+        self._row = None
+
+        self.logger = logging.getLogger(self.logger_name)
+
+    # Convenient properties that are frequently used
+    @property
+    def is_handstroke(self):
+        """ Returns true if the current row (determined by self._row_number) represents a handstroke. """
+        return self._row_number % 2 == 0
+
+    @property
+    def stage(self):
+        """ Convenient property to find the number of bells in the current tower. """
+        return self._tower.number_of_bells
+
+    # Callbacks
+    def _on_size_change(self):
+        if not self.row_generator.is_tower_size_valid(self._tower.number_of_bells):
+            self.logger.warning(f"Row generation requires {self.row_generator.number_of_bells} \
+bells, but the current tower has {self._tower.number_of_bells}.  Wheatley will crash when you go \
+into changes unless something is done!")
+
+    def _on_look_to(self):
+        """ Callback called when a user calls 'Look To'. """
+
+        treble = Bell.from_number(1)
+
+        # Count number of user controlled bells
+        number_of_user_controlled_bells = 0
+        for i in range(self.stage):
+            if self._user_assigned_bell(Bell.from_index(i)):
+                number_of_user_controlled_bells += 1
+
+        self._rhythm.initialise_line(self.stage, self._user_assigned_bell(treble),
+                                     time.time() + 3, number_of_user_controlled_bells)
+
+        # Clear all the flags
+        self._should_stand = False
+        self._should_start_method = False
+        self._should_start_ringing_rounds = False
+
+        # Reset the state, so that the bot starts by ringing rounds
+        self._is_ringing = True
+        self._is_ringing_rounds = True
+
+        # Start at the first place of the first row
+        self._row_number = 0
+        self._place = 0
+
+        self.start_next_row()
+
+    def _on_go(self):
+        """ Callback called when a user calls 'Go'. """
+        if self._is_ringing_rounds:
+            self._should_start_method = True
+
+    def _on_bob(self):
+        """ Callback called when a user calls 'Bob'. """
+        self.row_generator.set_bob()
+
+    def _on_single(self):
+        """ Callback called when a user calls 'Single'. """
+        self.row_generator.set_single()
+
+    def _on_thats_all(self):
+        """ Callback called when a user calls 'That`s All'. """
+        self._should_start_ringing_rounds = True
+
+    def _on_stand_next(self):
+        """ Callback called when a user calls 'Stand Next'. """
+        self._should_stand = True
+
+    def _on_bell_ring(self, bell, stroke):
+        """ Callback called when the Tower receives a signal that a bell has been rung. """
+        if self._user_assigned_bell(bell):
+            # This will give us the stroke _after_ the bell rings, we have to invert it, because
+            # otherwise this will always expect the bells on the wrong stroke and no ringing will
+            # ever happen
+            self._rhythm.on_bell_ring(bell, not stroke, time.time())
+
+    # Mainloop and helper methods
+    def expect_bell(self, index, bell):
+        """ Called to let the rhythm expect a user-controlled bell at a certain time and stroke. """
+        if self._user_assigned_bell(bell):
+            self._rhythm.expect_bell(
+                bell,
+                self._row_number,
+                index,
+                self.is_handstroke
+            )
+
+    def start_next_row(self):
+        """
+        Creates a new row from the row generator and tells the rhythm to expect the new bells.
+        """
+        if self._is_ringing_rounds:
+            for index in range(self.stage):
+                self.expect_bell(index, Bell.from_index(index))
+        else:
+            self._row = self.row_generator.next_row(self.is_handstroke)
+
+            for (index, bell) in enumerate(self._row):
+                self.expect_bell(index, bell)
+
+    def start_method(self):
+        """
+        Called when the ringing is about to go into changes.
+        Resets the row_generator and starts the next row.
+        """
+        assert self.row_generator.number_of_bells == self._tower.number_of_bells, \
+            f"{self.row_generator.number_of_bells} != {self._tower.number_of_bells}"
+
+        self.row_generator.reset()
+        self.start_next_row()
+
+    def tick(self):
+        """ Called every time the main loop is executed when the bot is ringing. """
+        bell = Bell.from_index(self._place) if self._is_ringing_rounds else self._row[self._place]
+        user_controlled = self._user_assigned_bell(bell)
+
+        self._rhythm.wait_for_bell_time(time.time(), bell, self._row_number, self._place,
+                                        user_controlled, self.is_handstroke)
+
+        if not user_controlled:
+            self._tower.ring_bell(bell, self.is_handstroke)
+
+        self._place += 1
+
+        if self._place == self.stage:
+            # Determine if we're finishing a handstroke
+            has_just_rung_rounds = True
+
+            if self._row is None:
+                has_just_rung_rounds = False
+            else:
+                for i, bell in enumerate(self._row):
+                    if bell.index != i:
+                        has_just_rung_rounds = False
+
+            # Generate the next row and update row indices
+            self._row_number += 1
+            self._place = 0
+            self.start_next_row()
+
+            # Implement handbell-style 'up down in'
+            if self._do_up_down_in:
+                if self._is_ringing_rounds and self._row_number == 2:
+                    self._should_start_method = True
+
+            # Implement handbell-style stopping at rounds
+            if self._stop_at_rounds:
+                if has_just_rung_rounds:
+                    self._should_stand = False
+                    self._is_ringing = False
+
+            # If we're starting a handstroke, we should convert all the flags into actions
+            if self._row_number % 2 == 0:
+                if self._should_stand:
+                    self._should_stand = False
+                    self._is_ringing = False
+
+                if self._should_start_method and self._is_ringing_rounds:
+                    self._should_start_method = False
+                    self._is_ringing_rounds = False
+
+                    self.start_method()
+
+                if self._should_start_ringing_rounds and not self._is_ringing_rounds:
+                    self._should_start_ringing_rounds = False
+                    self._is_ringing_rounds = True
+
+    def main_loop(self):
+        """
+        The main_loop of the bot.
+        The main thread will get stuck forever in this function whilst the bot rings.
+        """
+        while True:
+            if self._is_ringing:
+                self.tick()
+
+            time.sleep(0.01)
+
+    def _user_assigned_bell(self, bell: Bell):
+        """ True when the bell is assigned to a different user name than given to the bot """
+        return not self._bot_assigned_bell(bell)
+
+    def _bot_assigned_bell(self, bell: Bell):
+        """ True when the bell is assigned to the user name given to the bot
+        or bell is unassigned when user name is not set"""
+        return self._tower.is_bell_assigned_to(bell, self._user_name)
---
+++ packages/wheatley/wheatley-0.4.0.tar.gz/wheatley-0.4.0/wheatley/main.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python3
+
+"""
+The file containing the main function for the bot, as well as all the command line argument parsing
+required to make the bot easily configurable.
+"""
+
+import argparse
+import logging
+import os
+import sys
+
+from wheatley.rhythm import RegressionRhythm, WaitForUserRhythm
+from wheatley.tower import RingingRoomTower
+from wheatley.bot import Bot
+from wheatley.page_parser import get_load_balancing_url, TowerNotFoundError, InvalidURLError
+from wheatley.arg_parsing import parse_peal_speed, PealSpeedParseError, parse_call
+
+from wheatley.row_generation import RowGenerator, ComplibCompositionGenerator
+from wheatley.row_generation import MethodPlaceNotationGenerator
+from wheatley.row_generation.complib_composition_generator import PrivateCompError, InvalidCompError
+from wheatley.row_generation.method_place_notation_generator import MethodNotFoundError, \
+                                                                    generator_from_special_title
+
+
+def create_row_generator(args):
+    """ Generates a row generator according to the given CLI arguments. """
+    if "comp" in args and args.comp is not None:
+        try:
+            row_gen = ComplibCompositionGenerator(args.comp)
+        except (PrivateCompError, InvalidCompError) as e:
+            sys.exit(f"Bad value for '--comp': {e}")
+    elif "method" in args:
+        try:
+            row_gen = generator_from_special_title(args.method) or \
+                      MethodPlaceNotationGenerator(
+                          args.method,
+                          parse_call(args.bob),
+                          parse_call(args.single)
+                      )
+        except MethodNotFoundError as e:
+            sys.exit(f"Bad value for '--method': {e}")
+    else:
+        assert False, \
+            "This shouldn't be possible because one of --method and --comp should always be defined"
+
+    # row_gen = PlainHuntGenerator(8)
+    # row_gen = PlaceNotationGenerator(8, "x1", bob={1: "6"})
+    # row_gen = DixonoidsGenerator(6, DixonoidsGenerator.DixonsRules)
+    # row_gen = PlaceNotationGenerator.stedman(11)
+
+    return row_gen
+
+
+def create_rhythm(args):
+    """ Generates a rhythm object according to the given CLI arguments. """
+
+    try:
+        peal_speed = parse_peal_speed(args.peal_speed)
+    except PealSpeedParseError as e:
+        sys.exit(str(e))
+
+    # Sets the minimum number of bells that Wheatley will use in order to deduce a rhythm.  Setting this to
+    # larger numbers will make Wheatley more stable during the pull-off, whereas smaller numbers will make
+    # Wheatley more sensitive to user's pull-off speed.  If this is larger than `--max_bells_in_dataset`,
+    # this will be clamped to fit.
+    min_bells_in_dataset = 4
+
+    # Clamp min_bells_in_dataset to not be bigger than max_bells_in_dataset
+    min_bells_in_dataset = min(min_bells_in_dataset, args.max_bells_in_dataset)
+
+    regression = RegressionRhythm(
+        args.inertia,
+        handstroke_gap=args.handstroke_gap,
+        peal_speed=peal_speed,
+        min_bells_in_dataset=min_bells_in_dataset,
+        max_bells_in_dataset=args.max_bells_in_dataset
+    )
+
+    if not args.keep_going:
+        return WaitForUserRhythm(regression)
+
+    return regression
+
+
+def configure_logging():
+    """ Sets up the logging for the bot. """
+
+    logging.basicConfig(level=logging.WARNING)
+
+    logging.getLogger(RingingRoomTower.logger_name).setLevel(logging.INFO)
+    logging.getLogger(RowGenerator.logger_name).setLevel(logging.INFO)
+    logging.getLogger(RegressionRhythm.logger_name).setLevel(logging.INFO)
+    logging.getLogger(WaitForUserRhythm.logger_name).setLevel(logging.INFO)
+
+
+def main(override_args=None, stop_on_join_tower=False):
+    """
+    The main function of the bot.
+    This parses the CLI arguments, creates the Rhythm, RowGenerator and Bot objects, then starts
+    the bot's mainloop.
+
+    The two optional arguments are used by the integration tester to give Wheatley artificial argument values
+    (override_args) and make Wheatley exit with error code 0 on joining a tower so that hanging forever
+    can be differentiated from Wheatley's normal behaviour of sitting in an infinite loop waiting for input.
+    """
+
+    # Try to read the file with the version number, if not print an error and set a poison value
+    # as the version
+    try:
+        version_file_path = os.path.join(os.path.split(__file__)[0], "version.txt")
+
+        with open(version_file_path) as f:
+            __version__ = f.read()
+    except IOError:
+        __version__ = "<NO VERSION FILE FOUND>"
+
+    # PARSE THE ARGUMENTS
+
+    parser = argparse.ArgumentParser(
+        description="A bot to fill in bells during ringingroom.com practices"
+    )
+
+    # Tower arguments
+    tower_group = parser.add_argument_group("Tower arguments")
+    tower_group.add_argument(
+        "room_id",
+        type=int,
+        help="The numerical ID of the tower to join, represented as a row on 9 bells, \
+              e.g. 763451928."
+    )
+    tower_group.add_argument(
+        "--url",
+        default="https://ringingroom.com",
+        type=str,
+        help="The URL of the server to join (defaults to 'https://ringingroom.com')"
+    )
+    tower_group.add_argument(
+        "--name",
+        default=None,
+        type=str,
+        help="If set, then the bot will ring bells assigned to the given name. \
+             When not set, the bot rings unassigned bells."
+    )
+
+    # Row generation arguments
+    row_gen_group = parser.add_argument_group("Row generation arguments")
+
+    # An mutual exclusion group to disallow specifying both a method *and* a CompLib comp
+    comp_method_group = row_gen_group.add_mutually_exclusive_group(required=True)
+    comp_method_group.add_argument(
+        "-c", "--comp",
+        type=int,
+        help="The ID of the complib composition you want to ring"
+    )
+    comp_method_group.add_argument(
+        "-m", "--method",
+        type=str,
+        help="The title of the method you want to ring"
+    )
+
+    row_gen_group.add_argument(
+        "-b",
+        "--bob",
+        default="14",
+        help='An override for what place notation(s) should be made when a `Bob` is called in \
+              Ringing Room.  These will by default happen at the lead end.  Examples: "16" or \
+              "0:16" => 6ths place lead end bob.  "-1:3" or "-1:3.1" => a Grandsire Bob.  "20: 70" \
+              => a 70 bob taking effect 20 changes into a lead (the Half Lead for Surprise Royal). \
+              "20:7/0:4" => a 70 bob 20 changes into a lead and a 14 bob at the lead end. \
+              "3: 5/9: 5" => bobs in Stedman Triples.  Defaults to "14".'
+    )
+    row_gen_group.add_argument(
+        "-n",
+        "--single",
+        default="1234",
+        help='An override for what place notation(s) should be made when a `Single` is called in \
+              Ringing Room.  These will by default happen at the lead end.  Examples: "1678" or \
+              "0:1678" => 6ths place lead end single.  "-1:3.123" => a Grandsire Single. \
+              "20: 7890" => a 7890 single taking effect 20 changes into a lead (the Half Lead for \
+              Surprise Royal). "3: 567/9: 567" => singles in Stedman Triples.  Defaults to "1234".'
+    )
+    row_gen_group.add_argument(
+        "-u", "--use-up-down-in",
+        action="store_true",
+        help="If set, then the Wheatley will automatically go into changes after two rounds have been \
+              rung."
+    )
+    row_gen_group.add_argument(
+        "-s", "--stop-at-rounds",
+        action="store_true",
+        help="If set, then Wheatley will stand his bells the first time rounds is reached."
+    )
+    row_gen_group.add_argument(
+        "-H", "--handbell-style",
+        action="store_true",
+        help="If set, then Wheatley will ring 'handbell style', i.e. ringing two strokes of \
+              rounds then straight into changes, and stopping at the first set of rounds. By \
+              default, he will ring 'towerbell style', i.e. only taking instructions from the \
+              ringing-room calls. This is equivalent to using the '-us' flags."
+    )
+
+    # Rhythm arguments
+    rhythm_group = parser.add_argument_group("Rhythm arguments")
+    rhythm_group.add_argument(
+        "-k", "--keep-going",
+        action="store_true",
+        help="If set, Wheatley will not wait for users to ring - instead, he will push on with the \
+              rhythm."
+    )
+    rhythm_group.add_argument(
+        "-w", "--wait",
+        action="store_true",
+        help="Legacy parameter, which is now set by default. The previous default behaviour of not waiting \
+              can be set with '-k'/'--keep-going'."
+    )
+    rhythm_group.add_argument(
+        "-I", "--inertia",
+        type=float,
+        default=0.5,
+        help="Overrides Wheatley's 'inertia' - now much Wheatley will take other ringers' positions \
+              into account when deciding when to ring.  0.0 means he will cling as closely as \
+              possible to the current rhythm, 1.0 means that he will completely ignore the other \
+              ringers."
+    )
+    rhythm_group.add_argument(
+        "-S", "--peal-speed",
+        default="2h58",
+        help="Sets the default speed that Wheatley will ring (assuming a peal of 5040 changes), \
+              though this will usually be adjusted by Wheatley whilst ringing to keep with other \
+              ringers.  Example formatting: '3h4' = '3h4m' = '3h04m' = '3h04' = '184m' = '184'. \
+              Defaults to '2h58'."
+    )
+    rhythm_group.add_argument(
+        "-G", "--handstroke-gap",
+        type=float,
+        default=1.0,
+        help="Sets the handstroke gap as a factor of the space between two bells.  Defaults to \
+              '1.0'."
+    )
+    rhythm_group.add_argument(
+        "-X", "--max-bells-in-dataset",
+        type=int,
+        default=15,
+        help="Sets the maximum number of bells that Wheatley will store to determine the current \
+              ringing speed.  If you make this larger, then he will be more consistent but less \
+              quick to respond to changes in rhythm.  Setting both this and \
+              --inertia to a very small values could result in Wheatley ringing ridiculously \
+              quickly.  Defaults to '15'."
+    )
+
+    # Misc arguments
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"Wheatley v{__version__}"
+    )
+
+    # Parse arguments
+    # `[1:]` is apparently needed, because sys.argv[0] is the working file of the Python interpreter
+    # which `parser.parse_args` does not want to see as an argument
+    args = parser.parse_args(sys.argv[1:] if override_args is None else override_args)
+
+    # Deprecation warnings
+    if args.wait:
+        print("Deprecation warning: `--wait` has been replaced with `--keep-going`!")
+
+    # Run the program
+    configure_logging()
+
+    try:
+        tower_url = get_load_balancing_url(args.room_id, args.url)
+    except TowerNotFoundError as e:
+        sys.exit(f"Bad value for 'room_id': {e}")
+    except InvalidURLError as e:
+        sys.exit(f"Bad value for '--url': {e}")
+
+    tower = RingingRoomTower(args.room_id, tower_url)
+    row_generator = create_row_generator(args)
+    rhythm = create_rhythm(args)
+    bot = Bot(tower, row_generator, args.use_up_down_in or args.handbell_style,
+              args.stop_at_rounds or args.handbell_style, rhythm, user_name=args.name)
+
+    with tower:
+        tower.wait_loaded()
+
+        print("=== LOADED ===")
+
+        if not stop_on_join_tower:
+            bot.main_loop()
+
+
+if __name__ == "__main__":
+    main()

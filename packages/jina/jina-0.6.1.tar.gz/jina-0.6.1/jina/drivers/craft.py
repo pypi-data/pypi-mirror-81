3dcb22d5f8e5f8884cff686042e46070befa8215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:02 +0000

    Add package jina/0.6.1/jina-0.6.1.tar.gz

---
+++ packages/jina/jina-0.6.1.tar.gz/jina-0.6.1/jina/drivers/craft.py
@@ -0,0 +1,83 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+from typing import Dict, Set, List
+
+from . import BaseExecutableDriver
+from .helper import array2pb, pb_obj2dict
+from ..counter import RandomUintCounter, SimpleCounter
+from ..proto import jina_pb2
+
+
+class CraftDriver(BaseExecutableDriver):
+    """Drivers inherited from this Driver will bind :meth:`craft` by default """
+
+    def __init__(self, executor: str = None, method: str = 'craft', *args, **kwargs):
+        super().__init__(executor, method, *args, **kwargs)
+        self._is_apply_all = False
+
+    def _apply(self, doc: 'jina_pb2.Document', *args, **kwargs):
+        ret = self.exec_fn(**pb_obj2dict(doc, self.exec.required_keys))
+        if ret:
+            self.set_doc_attr(doc, ret)
+
+    def set_doc_attr(self, doc: 'jina_pb2.Document', doc_info: Dict, protected_keys: Set = None):
+        for k, v in doc_info.items():
+            if k == 'blob':
+                if isinstance(v, jina_pb2.NdArray):
+                    doc.blob.CopyFrom(v)
+                else:
+                    doc.blob.CopyFrom(array2pb(v))
+            elif isinstance(protected_keys, dict) and k in protected_keys:
+                self.logger.warning(f'you are assigning a {k} in {self.exec.__class__}, '
+                                    f'is it intentional? {k} will be overwritten by {self.__class__} '
+                                    f'anyway.')
+            elif isinstance(v, list) or isinstance(v, tuple):
+                doc.ClearField(k)
+                getattr(doc, k).extend(v)
+            else:
+                setattr(doc, k, v)
+
+
+class SegmentDriver(CraftDriver):
+    """Segment document into chunks using the executor
+    """
+
+    def __init__(
+        self,
+        first_chunk_id: int = 0,
+        random_chunk_id: bool = True,
+        level_names: List[str] = None,
+        traversal_paths: List[str] = ['r', 'c'],
+        *args,
+        **kwargs
+    ):
+        super().__init__(traversal_paths=traversal_paths, *args, **kwargs)
+        if isinstance(level_names, list) and (self._granularity_end - self._granularity_start + 1) != len(self.level_names):
+            self.level_names = level_names
+        elif level_names is None:
+            pass
+        else:
+            raise ValueError(f'bad level names: {level_names}, the length of it should match the recursive depth + 1')
+
+        # for adding new chunks, preorder is safer
+        self.recursion_order = 'pre'
+        self._counter = RandomUintCounter() if random_chunk_id else SimpleCounter(first_chunk_id)
+        self._protected_fields = {'length', 'id', 'parent_id', 'granularity'}
+
+    def _apply(self, doc: 'jina_pb2.Document', *args, **kwargs):
+        _args_dict = pb_obj2dict(doc, self.exec.required_keys)
+        ret = self.exec_fn(**_args_dict)
+        if ret:
+            for r in ret:
+                c = doc.chunks.add()
+                self.set_doc_attr(c, r, self._protected_fields)
+                c.length = len(ret)
+                c.id = next(self._counter)
+                c.parent_id = doc.id
+                c.granularity = doc.granularity + 1
+                if not c.mime_type:
+                    c.mime_type = doc.mime_type
+
+        else:
+            self.logger.warning(f'doc {doc.id} at level {doc.granularity} gives no chunk')
---
+++ packages/jina/jina-0.6.1.tar.gz/jina-0.6.1/jina/drivers/querylang/sort.py
@@ -0,0 +1,49 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+from typing import Iterable, List
+
+from .. import QuerySetReader, BaseRecursiveDriver
+from ...helper import rgetattr
+
+if False:
+    from ...proto import jina_pb2
+
+
+class SortQL(QuerySetReader, BaseRecursiveDriver):
+    """Sorts the incoming of the documents by the value of a given field.
+     It can also work in reverse mode
+
+        Example::
+        - !ReduceAllDriver
+            with:
+                traversal_paths: ['m']
+        - !SortQL
+            with:
+                reverse: true
+                field: 'score.value'
+                traversal_paths: ['m']
+        - !SliceQL
+            with:
+                start: 0
+                end: 50
+                granularity_range: [0, 0]
+                adjacency_range: [0, 1]
+
+        `SortQL` will ensure that only the documents are sorted by the score value before slicing the first top 50 documents
+    """
+
+    def __init__(self, field: str, reverse: bool = False, traversal_paths: List[str] = ['c'], *args, **kwargs):
+        """
+        :param field: the value of the field drives the sort of the iterable docs
+        :param reverse: sort the value from big to small
+        """
+
+        super().__init__(use_tree_traversal=True, traversal_paths=traversal_paths, *args, **kwargs)
+        self._reverse = reverse
+        self._field = field
+        self.is_apply = False
+
+    def _apply_all(self, docs: Iterable['jina_pb2.Document'], *args, **kwargs) -> None:
+        docs.sort(key=lambda x: rgetattr(x, self.field), reverse=self.reverse)
+

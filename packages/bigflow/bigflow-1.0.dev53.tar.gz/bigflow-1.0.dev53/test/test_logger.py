da2975619932a4f0205145c7394f1844c5a8cdfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:08 +0000

    Add package bigflow/1.0.dev53/bigflow-1.0.dev53.tar.gz

---
+++ packages/bigflow/bigflow-1.0.dev53.tar.gz/bigflow-1.0.dev53/test/test_logger.py
@@ -0,0 +1,66 @@
+from unittest import TestCase, mock
+from unittest.mock import patch
+
+from google.cloud import logging_v2
+
+from bigflow.logger import GCPLogger
+
+
+class MockedGCPLogger(TestCase):
+
+    @mock.patch('bigflow.logger.create_logging_client')
+    def setUp(self, create_logging_client_mock) -> None:
+        create_logging_client_mock.return_value = mock.create_autospec(logging_v2.LoggingServiceV2Client)
+        self.test_logger = GCPLogger('project-id', 'workflow-id')
+
+
+class LoggerTestCase(MockedGCPLogger):
+
+    def test_should_create_correct_logging_link(self):
+        message = self.test_logger.get_gcp_logs_message()
+        self.assertEqual(message, 'You can find logs for this workflow here: https://console.cloud.google.com/logs/query;query=logName%3D%22projects%2Fproject-id%2Flogs%2Fworkflow-id%22%0Alabels.workflow%3D%22workflow-id%22')
+
+    def test_should_send_warning(self):
+        self.test_logger.warning("warning message")
+        self.test_logger.client.write_log_entries.assert_called_with([logging_v2.types.LogEntry(
+            log_name="projects/project-id/logs/workflow-id",
+            resource={
+                "type": "global",
+                "labels": {
+                    "project_id": "project-id"
+                }
+            },
+            text_payload="warning message",
+            severity='WARNING',
+            labels={"workflow": "workflow-id"}
+        )])
+
+    def test_should_send_info(self):
+        self.test_logger.info("info message")
+        self.test_logger.client.write_log_entries.assert_called_with([logging_v2.types.LogEntry(
+            log_name="projects/project-id/logs/workflow-id",
+            resource={
+                "type": "global",
+                "labels": {
+                    "project_id": "project-id"
+                }
+            },
+            text_payload="info message",
+            severity='INFO',
+            labels={"workflow": "workflow-id"}
+        )])
+
+    def test_should_send_error(self):
+        self.test_logger.error("error message")
+        self.test_logger.client.write_log_entries.assert_called_with([logging_v2.types.LogEntry(
+            log_name="projects/project-id/logs/workflow-id",
+            resource={
+                "type": "global",
+                "labels": {
+                    "project_id": "project-id"
+                }
+            },
+            text_payload="error message",
+            severity='ERROR',
+            labels={"workflow": "workflow-id"}
+        )])
---
+++ packages/bigflow/bigflow-1.0.dev53.tar.gz/bigflow-1.0.dev53/test/test_workflow.py
@@ -0,0 +1,171 @@
+from collections import OrderedDict
+from unittest import TestCase, mock
+
+from google.cloud import logging_v2
+
+from bigflow.workflow import Workflow, Definition, InvalidJobGraph, WorkflowJob
+from test.test_monitoring import FailingJob
+
+
+class WorkflowTestCase(TestCase):
+    def test_should_run_jobs(self):
+        # given
+        definition = [mock.Mock() for i in range(100)]
+        workflow = Workflow(workflow_id='test_workflow', definition=definition)
+
+        # when
+        workflow.run('2019-01-01')
+
+        # then
+        for step in definition:
+            step.assert_has_calls([mock.call.run(runtime='2019-01-01')])
+
+    def test_should_run_single_job(self):
+        # given
+        first_job = mock.Mock()
+        setattr(first_job, 'id', 'first job')
+
+        definition = [first_job] + [mock.Mock() for i in range(100)]
+        workflow = Workflow(workflow_id='test_workflow', definition=definition)
+
+        # when
+        workflow.run_job('first job', '2020-01-01')
+
+        # then
+        for step in definition[1:]:
+            step.assert_not_called()
+        # and
+        first_job.assert_has_calls([mock.call.run('2020-01-01')])
+
+    def test_should_have_id_and_schedule_interval(self):
+        # given
+        workflow = Workflow(
+            workflow_id='test_workflow',
+            definition=[],
+            schedule_interval='@hourly')
+
+        # expected
+        self.assertEqual(workflow.schedule_interval, '@hourly')
+
+    def test_should_throw_exception_when_circular_dependency_is_found(self):
+        # given
+        original_job = mock.Mock()
+        job1, job2, job3, job4 = [WorkflowJob(original_job, i) for i in range(4)]
+
+        # job1 --- job2
+        #   |        |
+        #    \       |
+        #     \      |
+        #      \     |
+        #       \    |
+        #        \   |
+        #         \  |
+        #         job3
+
+        job_graph = {
+            job1: (job2,),
+            job2: (job3,),
+            job3: (job1,)
+        }
+
+        # expected
+        with self.assertRaises(InvalidJobGraph):
+            Definition(job_graph)
+
+        # given
+
+        # job1        job4
+        #  |          | |
+        #  |          | |
+        #  |          | |
+        # job2 ------ job3
+
+        job_graph = {
+            job1: (job2,),
+            job2: (job3,),
+            job3: (job4,),
+            job4: (job3,)
+        }
+
+        # expected
+        with self.assertRaises(InvalidJobGraph):
+            Definition(job_graph)
+
+    def test_should_run_jobs_in_order_accordingly_to_graph_schema(self):
+        # given
+        original_job = mock.Mock()
+        job1, job2, job3, job4, job5, job6, job7, job8, job9 = [WorkflowJob(original_job, i) for i in range(9)]
+        job_graph = OrderedDict([
+            (job1, (job5, job6)),
+            (job2, (job6,)),
+            (job3, (job6,)),
+            (job4, (job7,)),
+            (job6, (job8,)),
+            (job7, (job8,)),
+            (job5, (job9,))
+        ])
+
+        #  job1     job2  job3  job4
+        #    |  \    |    /      |
+        #    |   \   |   /       |
+        #    |    \  |  /        |
+        #    |     \ | /         |
+        #  job5    job6        job7
+        #    |        \         /
+        #    |         \       /
+        #    |          \     /
+        #    |           \   /
+        #    |            \ /
+        #   job9         job8
+
+        definition = Definition(job_graph)
+        workflow = Workflow(workflow_id='test_workflow', definition=definition, schedule_interval='@hourly')
+
+        # expected
+        self.assertEqual(list(workflow.build_sequential_order()), [job1, job5, job9, job2, job3, job6, job4, job7, job8])
+
+        # given
+        job_graph = OrderedDict([
+            (job1, (job5, job6, job7)),
+            (job2, (job6,)),
+            (job3, (job6,)),
+            (job4, (job7,)),
+            (job6, (job8,)),
+            (job7, (job8,)),
+            (job5, (job9,)),
+            (job6, (job9,))
+        ])
+
+        #  job1     job2  job3  job4
+        #    |  \    |    /      |
+        #    |   \   |   /       |
+        #    |    \  |  /        |
+        #    |     \ | /         |
+        #  job5    job6        job7
+        #    |      / \         /
+        #    |     /   \       /
+        #    |    /     \     /
+        #    |   /       \   /
+        #    |  /         \ /
+        #   job9         job8
+
+        definition = Definition(job_graph)
+        workflow = Workflow(workflow_id='test_workflow', definition=definition, schedule_interval='@hourly')
+
+        # expected
+        self.assertEqual(workflow.build_sequential_order(), [job1, job5, job2, job3, job6, job9, job4, job7, job8])
+
+    @mock.patch('bigflow.logger.create_logging_client')
+    def test_should_log_job_exception(self, create_logging_client_mock):
+        # given
+        create_logging_client_mock.return_value = mock.create_autospec(logging_v2.LoggingServiceV2Client)
+        definition = [mock.Mock(), FailingJob("id")]
+        workflow = Workflow(workflow_id='test_workflow', definition=definition, logging_project_id="some_project_id")
+
+        # when
+        with self.assertLogs('test_workflow', level='INFO') as logs:
+            with self.assertRaises(Exception):
+                workflow.run('2019-09-01')
+        # then
+            self.assertEqual(logs.output, ['INFO:test_workflow:You can find logs for this workflow here: https://console.cloud.google.com/logs/query;query=logName%3D%22projects%2Fsome_project_id%2Flogs%2Ftest_workflow%22%0Alabels.workflow%3D%22test_workflow%22',
+                                           'ERROR:test_workflow:Panic!'])

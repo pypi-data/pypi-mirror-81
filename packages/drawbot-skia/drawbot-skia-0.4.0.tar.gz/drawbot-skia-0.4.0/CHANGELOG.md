dfbbd6d647d5e75bdfb45fc69e6ecb063ac2efa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:36 +0000

    Add package drawbot-skia/0.4.0/drawbot-skia-0.4.0.tar.gz

---
+++ packages/drawbot-skia/drawbot-skia-0.4.0.tar.gz/drawbot-skia-0.4.0/CHANGELOG.md
@@ -0,0 +1,46 @@
+# Changelog for drawbot-skia
+
+## [0.3.1] - 2020-09-27
+
+- Added `path.removeOverlap()`
+- Added `path.union(other)` and `path.__or__(other)`
+- Added `path.intersection(other)` and `path.__and__(other)`
+- Added `path.difference(other)` and `path.__mod__(other)`
+- Added `path.xor(other)` and `path.__xor__(other)`
+
+## [0.3.0] - 2020-09-27
+
+- Added `clipPath(path)`
+- Added `path.pointInside(point)`
+- Added `path.bounds()`
+- Added `path.controlPointBounds()`
+- Added `path.reverse()`
+- Added `path.appendPath(other)`
+- Added `path.copy()`
+- Added `path.translate(x, y)`
+- Added `path.scale(x, y=None, center=(0, 0))`
+- Added `path.rotate(angle, center=(0, 0))`
+- Added `path.skew(x, y=0, center=(0, 0))`
+- Added `path.transform(transform, center=(0, 0))`
+- Added `path.arc(center, radius, startAngle, endAngle, clockwise)`
+- Added `path.arcTo(point1, point2, radius)`
+- Added `path.drawToPen(pen)`
+- Added `path.drawToPointPen(pen)`
+- Added `path.text(txt, ...)`
+- Fixed bug when `font()` was not set
+
+## [0.2.0] - 2020-09-26
+
+- Added `image(imagePath, position, alpha=1.0)`
+- Added `language(language)`
+- Fixed signature of `transform(matrix, center=(0, 0))`
+- Added `blendMode(blendMode)`
+- Added `lineDash(firstValue, *values)`
+
+## [0.1.1] - 2020-09-22
+
+Second first release on PyPI
+
+## [0.1.0] - 2020-09-22
+
+First first release on PyPI
---
+++ packages/drawbot-skia/drawbot-skia-0.4.0.tar.gz/drawbot-skia-0.4.0/tests/test_api.py
@@ -0,0 +1,95 @@
+from drawbot_skia.runner import makeDrawbotNamespace, runScript, runScriptSource
+from drawbot_skia.drawing import Drawing
+import pathlib
+import pytest
+from PIL import Image
+import numpy as np
+
+
+testDir = pathlib.Path(__file__).resolve().parent
+apiTestsDir = testDir / "apitests"
+apiTestsOutputDir = testDir / "apitests_output"
+apiTestsExpectedOutputDir = testDir / "apitests_expected_output"
+
+
+apiScripts = apiTestsDir.glob("*.py")
+
+
+@pytest.mark.parametrize("apiTestPath", apiScripts)
+def test_apitest(apiTestPath):
+    db = Drawing()
+    namespace = makeDrawbotNamespace(db)
+    runScript(apiTestPath, namespace)
+    if not apiTestsOutputDir.exists():
+        apiTestsOutputDir.mkdir()
+    outputPath = apiTestsOutputDir / (apiTestPath.stem + ".png")
+    expectedOutputPath = apiTestsExpectedOutputDir / (apiTestPath.stem + ".png")
+    db.saveImage(outputPath)
+    same, reason = compareImages(outputPath, expectedOutputPath)
+    assert same, reason
+
+
+multipageSource = """
+for i in range(3):
+    newPage(200, 200)
+    rect(50, 50, 100, 100)
+"""
+
+
+def test_saveImage_multipage(tmpdir):
+    tmpdir = pathlib.Path(tmpdir)
+    db = Drawing()
+    namespace = makeDrawbotNamespace(db)
+    runScriptSource(multipageSource, "<string>", namespace)
+    assert [] == sorted(tmpdir.glob("*.png"))
+    outputPath = tmpdir / "test.png"
+    db.saveImage(outputPath)
+    expected_filenames = ['test_0.png', 'test_1.png', 'test_2.png']
+    assert expected_filenames == [p.name for p in sorted(tmpdir.glob("*.png"))]
+
+
+def test_saveImage_mp4(tmpdir):
+    from drawbot_skia import ffmpeg
+    ffmpeg.FFMPEG_PATH = ffmpeg.getPyFFmpegPath()  # Force ffmpeg from pyffmpeg
+    tmpdir = pathlib.Path(tmpdir)
+    db = Drawing()
+    namespace = makeDrawbotNamespace(db)
+    runScriptSource(multipageSource, "<string>", namespace)
+    assert [] == sorted(tmpdir.glob("*.png"))
+    db.saveImage(tmpdir / "test.mp4")
+    db.saveImage(tmpdir / "test2.mp4", codec="mpeg4")
+    expected_filenames = ['test.mp4', 'test2.mp4']
+    paths = sorted(tmpdir.glob("*.mp4"))
+    assert paths[0].stat().st_size < paths[1].stat().st_size
+    assert expected_filenames == [p.name for p in paths]
+
+
+def test_noFont(tmpdir):
+    db = Drawing()
+    # Ensure we don't get an error when font is not set
+    db.text("Hallo", (0, 0))
+
+
+def readbytes(path):
+    with open(path, "rb") as f:
+        return f.read()
+
+
+def compareImages(path1, path2):
+    data1 = readbytes(path1)
+    data2 = readbytes(path2)
+    if data1 == data2:
+        return True, "data identical"
+    im1 = Image.open(path1)
+    im2 = Image.open(path2)
+    if im1 == im2:
+        return True, "images identical"
+    if im1.size != im2.size:
+        return False, "sizes differ"
+    a1 = np.array(im1).astype(int)
+    a2 = np.array(im2).astype(int)
+    diff = a1 - a2
+    maxDiff = max(abs(np.max(diff)), abs(np.min(diff)))
+    if maxDiff < 128:
+        return True, "images similar enough"
+    return False, f"images differ too much, maxDiff: {maxDiff}"

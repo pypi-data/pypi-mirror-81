5663518e1b4d7be42196e0859c39526314b08b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:34 +0000

    Add package perlin-noise/1.1/perlin_noise-1.1.tar.gz

---
+++ packages/perlin-noise/perlin_noise-1.1.tar.gz/perlin_noise-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("Readme_pypi.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='perlin_noise',
+      version='1.1',
+      description='Python implementation for Perlin Noise with unlimited coordinates space',
+      author="salaxieb",
+      author_email='salaxieb.ildar@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      zip_safe=False)

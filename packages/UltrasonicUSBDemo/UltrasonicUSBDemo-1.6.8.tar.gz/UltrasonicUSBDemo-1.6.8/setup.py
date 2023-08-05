e8894793ab2b777ad89964d50ab0aba3c053208f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:55 +0000

    Add package UltrasonicUSBDemo/1.6.8/UltrasonicUSBDemo-1.6.8.tar.gz

---
+++ packages/UltrasonicUSBDemo/UltrasonicUSBDemo-1.6.8.tar.gz/UltrasonicUSBDemo-1.6.8/setup.py
@@ -0,0 +1,30 @@
+#setup.py
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="UltrasonicUSBDemo", # Replace with your own username
+    version="1.6.8",
+    author="Roanoke Electronic Controls, Inc.",
+    author_email="info@roanokecontrols.com",
+    description="Demonstrates communication with different USB ultrasonic devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RoanokeControls/PkgUltrasonicUSBDemo",
+    packages=setuptools.find_packages(),
+    package_data={
+        "UltrasonicUSBDemo": ["image/*","fs000x.xml"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'PyQt5',
+        'pyserial',
+     ],
+    python_requires='>=3.6',
+)

d048a3e38638723bc7d7b5c2ad397f02a2d775f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:29 +0000

    Add package nle/0.5.2/nle-0.5.2.tar.gz

---
+++ packages/nle/nle-0.5.2.tar.gz/nle-0.5.2/include/nleobs.h
@@ -0,0 +1,36 @@
+
+#ifndef NLEOBS_H
+#define NLEOBS_H
+
+#define NLE_MESSAGE_SIZE 256
+#define NLE_BLSTATS_SIZE 25
+#define NLE_PROGRAM_STATE_SIZE 6
+#define NLE_INTERNAL_SIZE 7
+#define NLE_INVENTORY_SIZE 55
+#define NLE_INVENTORY_STR_LENGTH 80
+
+typedef struct nle_observation {
+    int action;
+    int done;
+    char in_normal_game;     /* Bool indicating if other obs are set. */
+    short *glyphs;           /* Size ROWNO * (COLNO - 1) */
+    unsigned char *chars;    /* Size ROWNO * (COLNO - 1) */
+    unsigned char *colors;   /* Size ROWNO * (COLNO - 1) */
+    unsigned char *specials; /* Size ROWNO * (COLNO - 1) */
+    unsigned char *message;  /* Size NLE_MESSAGE_SIZE */
+    long *blstats;           /* NLE_BLSTATS_SIZE */
+    int *program_state;      /* NLE_PROGRAM_STATE_SIZE */
+    int *internal;           /* NLE_INTERNAL_SIZE */
+    short *inv_glyphs;       /* NLE_INVENTORY_SIZE */
+    unsigned char
+        *inv_strs; /* NLE_INVENTORY_SIZE * NLE_INVENTORY_STR_LENGTH */
+    unsigned char *inv_letters;  /* NLE_INVENTORY_SIZE */
+    unsigned char *inv_oclasses; /* NLE_INVENTORY_SIZE */
+} nle_obs;
+
+typedef struct {
+    unsigned long seeds[2]; /* core, disp */
+    char reseed; /* boolean: use NetHack's anti-TAS reseed mechanism? */
+} nle_seeds_init_t;
+
+#endif /* NLEOBS_H */
---
+++ packages/nle/nle-0.5.2.tar.gz/nle-0.5.2/nle/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.5.2'
+git_version = '04050fc461e7ebb4422a987b26c5ff9b4da33f8e'

c28252cb2812254fee0fcae4851f7a8c8ed31c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:23 +0000

    Add package pycparser-fake-libc/2.20/pycparser-fake-libc-2.20.tar.gz

---
+++ packages/pycparser-fake-libc/pycparser-fake-libc-2.20.tar.gz/pycparser-fake-libc-2.20/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys
+
+# Version should patch the pycparser submodule version
+VERSION = "v2.20"
+
+with open("README.md", "r") as f:
+    long_description = f.read().replace("\r", "")
+
+setup(
+    name="pycparser-fake-libc",
+    version=VERSION.lstrip("v"),  # Remove the leading v, pip doesn't like that
+    description="Pip-installable package with pycparser's fake libc headers",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/ThomasGerstenberg/pycparser-fake-libc",
+    author="Thomas Gerstenberg",
+    author_email="tgerst6@gmail.com",
+    keywords="pycparser",
+    packages=find_packages(exclude=["test", "test.*"]),
+    include_package_data=True,
+)

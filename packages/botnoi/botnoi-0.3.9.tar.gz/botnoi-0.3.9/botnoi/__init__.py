bf1d07b615f210ca1a53ba70d1ec1a31af21b64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package botnoi/0.3.9/botnoi-0.3.9.tar.gz

---
+++ packages/botnoi/botnoi-0.3.9.tar.gz/botnoi-0.3.9/botnoi/__init__.py
@@ -0,0 +1,13 @@
+from . import openapi
+from . import scrape
+from . import cv
+from . import nlp
+from . import line
+
+__all__ = [
+    'openapi',
+    'scrape',
+    'cv',
+    'nlp',
+    'line'
+]

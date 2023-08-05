f38f8324aab11465a39fa9e35f84b3237e31f0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:55 +0000

    Add package bazze-json-logger/0.0.2/bazze_json_logger-0.0.2-py3-none-any.whl

---
+++ packages/bazze-json-logger/bazze_json_logger-0.0.2-py3-none-any.whl/bazze_json_logger/__init__.py
@@ -0,0 +1,38 @@
+import logging
+from datetime import datetime
+
+from pythonjsonlogger import jsonlogger
+
+__version__ = '0.0.2'
+
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+    def __init__(self, *args, **kwargs):
+        # Add any AWS Lambda events or context objects here
+        self.aws_request_id = kwargs.pop('aws_request_id', '')
+        super().__init__(*args, **kwargs)
+
+    def add_fields(self, log_record, record, message_dict):
+        super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('timestamp'):
+            # this doesn't use record.created, so it is slightly off
+            now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            log_record['timestamp'] = now
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
+        log_record['schema_version'] = __version__
+        if self.aws_request_id:
+            log_record['aws_request_id'] = self.aws_request_id
+
+
+def setup(*args, **kwargs):
+    logger = logging.getLogger()
+    # Remove Lambda Handler
+    if len(logger.handlers) == 1:
+        logger.removeHandler(logger.handlers[0])
+    jsonHandler = logging.StreamHandler()
+    formatter = CustomJsonFormatter(fmt='%(timestamp)s %(level)s %(name)s %(message)s', **kwargs)
+    jsonHandler.setFormatter(formatter)
+    logger.addHandler(jsonHandler)

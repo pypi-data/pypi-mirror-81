77a891af6c337f995bce22cf4ded003d5ddf7651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:44 +0000

    Add package tccli-intl-en/3.0.96.1/tccli-intl-en-3.0.96.1.tar.gz

---
+++ packages/tccli-intl-en/tccli-intl-en-3.0.96.1.tar.gz/tccli-intl-en-3.0.96.1/tccli/options_define.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+SecretId = "secretId"
+SecretKey = "secretKey"
+Region = "region"
+Endpoint = "endpoint"
+Version = "version"
+Filter = "filter"
+Profile = "profile"
+Timeout = "timeout"
+Output = "output"
+
+
+Help = "help"
+CliVersion = "version"
+
+ENV_SECRET_ID = "TENCENTCLOUD_SECRET_ID"
+ENV_SECRET_KEY = "TENCENTCLOUD_SECRET_KEY"
+ENV_REGION = "TENCENTCLOUD_REGION"
+
+
+ACTION_GLOBAL_OPT = [SecretId, SecretKey, Region, Endpoint, Version, Filter, Profile, Timeout, Output]
+
+
+

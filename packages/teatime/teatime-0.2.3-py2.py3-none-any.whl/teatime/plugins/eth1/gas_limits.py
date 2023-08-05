f1922803afa8cbb65faf4ef691a2ec9d379463a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:23 +0000

    Add package teatime/0.2.3/teatime-0.2.3-py2.py3-none-any.whl

---
+++ packages/teatime/teatime-0.2.3-py2.py3-none-any.whl/teatime/plugins/eth1/gas_limits.py
@@ -0,0 +1,66 @@
+"""This module contains plugins around the gas-setting RPC endpoints."""
+
+from teatime.plugins import Context, NodeType, Plugin
+from teatime.reporting import Issue, Severity
+
+
+class ParityGasCeiling(Plugin):
+    """Try to set a new gas ceiling target for mined blocks.
+
+    Severity: Critical
+
+    Parity/OpenEthereum: https://openethereum.github.io/wiki/JSONRPC-parity_set-module#parity_setgasceiltarget
+    """
+
+    INTRUSIVE = True
+
+    def __init__(self, gas_target: str):
+        self.gas_target = gas_target
+
+    def _check(self, context: Context) -> None:
+        if context.node_type != NodeType.PARITY:
+            return
+
+        payload = self.get_rpc_json(
+            context.target,
+            method="parity_setGasCeilTarget",
+            params=[self.gas_target],
+        )
+        context.report.add_issue(
+            Issue(
+                title="Gas ceiling target can be changed",
+                description="Anyone can change the gas ceiling value using the parity_setGasCeilTarget RPC call.",
+                raw_data=payload,
+                severity=Severity.CRITICAL,
+            )
+        )
+
+
+class ParityGasFloor(Plugin):
+    """Try to set a new gas floor target for mined blocks.
+
+    Severity: Critical
+
+    Parity/OpenEthereum: https://openethereum.github.io/wiki/JSONRPC-parity_set-module#parity_setgasfloortarget
+    """
+
+    INTRUSIVE = True
+
+    def __init__(self, gas_floor: str):
+        self.gas_floor = gas_floor
+
+    def _check(self, context: Context) -> None:
+        if context.node_type != NodeType.PARITY:
+            return
+
+        payload = self.get_rpc_json(
+            context.target, method="parity_setGasFloorTarget", params=[self.gas_floor]
+        )
+        context.report.add_issue(
+            Issue(
+                title="Gas floor target can be changed",
+                description="Anyone can change the gas floor value using the parity_setGasFloorTarget RPC call.",
+                raw_data=payload,
+                severity=Severity.CRITICAL,
+            )
+        )

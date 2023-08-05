61b7505b98b1947863a565dd01bde82d3da767f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:23 +0000

    Add package uplink-httpx/1.5/uplink-httpx-1.5.tar.gz

---
+++ packages/uplink-httpx/uplink-httpx-1.5.tar.gz/uplink-httpx-1.5/uplink_httpx/__init__.py
@@ -0,0 +1,68 @@
+import asyncio
+import logging
+
+from uplink.clients import exceptions, interfaces, io, register
+
+try:
+    import httpx
+
+    httpx.AsyncClient.__del__ = lambda x: x
+except ImportError:
+    httpx = None
+
+log = logging.getLogger("uplink.httpx")
+
+
+def threaded_callback(callback):
+    coroutine_callback = asyncio.coroutine(callback)
+
+    @asyncio.coroutine
+    def new_callback(response):
+        response = yield from coroutine_callback(response)
+        return response
+
+    return new_callback
+
+
+class HttpxClient(interfaces.HttpClientAdapter):
+    exceptions = exceptions.Exceptions()
+
+    def __init__(self, session=None, **kwargs):
+        if httpx is None:
+            raise NotImplementedError("httpx is not installed.")
+        self._auto_created_session = False
+        if session is None:
+            session = httpx.AsyncClient(**kwargs)
+            self._auto_created_session = True
+        self._session = session
+        self._sync_callback_adapter = threaded_callback
+
+    def __del__(self):
+        if self._auto_created_session:
+            asyncio.get_event_loop().create_task(self._session.aclose())
+
+    @staticmethod
+    @register.handler
+    def with_session(session, *args, **kwargs):
+        if isinstance(session, httpx.AsyncClient):
+            return HttpxClient(session, *args, **kwargs)
+
+    async def send(self, request):
+        method, url, extras = request
+        response = await self._session.request(method=method, url=url, **extras)
+        return response
+
+    def wrap_callback(self, callback):
+        if not asyncio.iscoroutinefunction(callback):
+            callback = self._sync_callback_adapter(callback)
+        return callback
+
+    def apply_callback(self, callback, response):
+        return self.wrap_callback(callback)(response)
+
+    @staticmethod
+    def io():
+        return io.AsyncioStrategy()
+
+
+HttpxClient.exceptions.ServerTimeout = httpx.ReadTimeout

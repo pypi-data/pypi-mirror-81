b1f930d41680837a092c647c4212eb12456bf9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package SigProfilerExtractor/1.0.18/SigProfilerExtractor-1.0.18-py3-none-any.whl

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.0.18-py3-none-any.whl/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.0.18'
+version = '1.0.18'
+Update = 'Heirarchy option deleted, clustering deleted and signatures orders by the mutation burden'
+    
+    

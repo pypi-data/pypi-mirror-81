e7233cbd0030f33f086f49986d55617b9cbe9cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:25 +0000

    Add package CaChannel/3.1.3/CaChannel-3.1.3-py3.7-linux-x86_64.egg

---
+++ packages/CaChannel/CaChannel-3.1.3-py3.7-linux-x86_64.egg/CaChannel/ca.py
@@ -0,0 +1,17 @@
+import readline
+import os
+import warnings
+
+if os.environ.get('CACHANNEL_BACKEND') == 'caffi':
+    from caffi.ca import *
+    from caffi.macros import *
+    from caffi.constants import *
+else:
+    try:
+        from ._ca import *
+    except:
+        warnings.warn("c extension is not available, trying caffi as fallback", RuntimeWarning)
+        from caffi.ca import *
+        from caffi.macros import *
+        from caffi.constants import *
+

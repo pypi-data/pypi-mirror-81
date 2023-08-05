f0b012b12a17d0093633736a85d17512857f0146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:40 +0000

    Add package rest-swagger/1.0.0/rest_swagger-1.0.0-py2.py3-none-any.whl

---
+++ packages/rest-swagger/rest_swagger-1.0.0-py2.py3-none-any.whl/rest_swagger/__init__.py
@@ -0,0 +1,11 @@
+"""
+rest-framework.
+
+An Implementation of the Swagger UI for Django Rest Framework.
+"""
+
+
+__version__ = "0.1.0"
+__author__ = 'Samuel Ajibade'
+__credits__ = 'Rest Swagger'
+__email__ = 'samuelajibade22@gmail.com'

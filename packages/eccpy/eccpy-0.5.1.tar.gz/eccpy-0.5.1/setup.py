a54bb2b8ff163ab6165896afa7229bca6a076646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:36 +0000

    Add package eccpy/0.5.1/eccpy-0.5.1.tar.gz

---
+++ packages/eccpy/eccpy-0.5.1.tar.gz/eccpy-0.5.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'readme.rst')) as f:
+    long_description = f.read()
+
+classifiers = """\
+Intended Audience :: Science/Research
+License :: OSI Approved :: MIT License
+Programming Language :: Python :: 3
+Topic :: Scientific/Engineering :: Bio-Informatics
+Topic :: Scientific/Engineering :: Medical Science Apps.
+Topic :: Scientific/Engineering :: Chemistry
+"""
+
+setup(name='eccpy',
+      description="High-throughput calculation of EC50 values.",
+      author="Mark Teese",
+      author_email="mark.teese@checkthereadme.com",
+      url="https://github.com/teese/eccpy",
+      download_url='https://github.com/teese/eccpy/archive/0.5.1.tar.gz',
+      project_urls={'Wiki': 'https://github.com/teese/eccpy/wiki',
+                    'LangoschLab': 'http://cbp.wzw.tum.de/index.php?id=9',
+                    "TU_Muenchen": "https://www.tum.de",
+                    "TNG Technology Consulting GmbH": "https://www.tngtech.com/en/index.html"},
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      license='MIT',
+      packages=find_packages(),
+      classifiers=classifiers.splitlines(),
+      install_requires=["pandas", "numpy", "matplotlib", "pytest", "scipy", "xlrd", "openpyxl", "pytest"],
+      keywords="EC50 LD50 IC50 doseresponse concentration dose inhibitor sigmoidal curve",
+      # obtains package data from MANIFEST.in
+      include_package_data=True,
+      version="0.5.1")

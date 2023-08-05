814c18ffb663ddf4ef49742edf7dbaf9869383dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:52 +0000

    Add package ledidi/0.2.0/ledidi-0.2.0.tar.gz

---
+++ packages/ledidi/ledidi-0.2.0.tar.gz/ledidi-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ledidi',
+    version='0.2.0',
+    author='Yang Lu and Jacob Schreiber',
+    author_email='jmschreiber91@gmail.com',
+    packages=['ledidi'],
+    url='http://pypi.python.org/pypi/ledidi/',
+    license='LICENSE.txt',
+    description='Ledidi is an optimization approach for designing edits to biological sequences.'
+)

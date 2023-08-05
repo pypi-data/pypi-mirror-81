1cbff7a519feef9fe5dee634a7b287f00519f62b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:59 +0000

    Add package yann/0.0.39/yann-0.0.39.tar.gz

---
+++ packages/yann/yann-0.0.39.tar.gz/yann-0.0.39/yann/callbacks/checkpoint.py
@@ -0,0 +1,18 @@
+from .base import Callback
+
+
+class Checkpoint(Callback):
+  def __init__(self, freq=1, load_latest=False):
+    self.freq = freq
+
+    self.load_latest = load_latest
+
+  def on_train_start(self, trainer=None):
+    if self.load_latest:
+      # trainer.load_checkpoint()
+      pass
+
+  def on_epoch_end(self, epoch, loss=None, metrics=None, trainer=None):
+    if epoch % self.freq == 0 :
+      trainer.checkpoint()
+

4971de0b58f7937d1536319b30b5a4d0fc153c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:42 +0000

    Add package pyimgren/0.9.2/pyimgren-0.9.2-py3-none-any.whl

---
+++ packages/pyimgren/pyimgren-0.9.2-py3-none-any.whl/pyimgren/__init__.py
@@ -0,0 +1,25 @@
+# MIT License
+# Copyright (c) 2018 s-ball
+
+from .pyimgren import Renamer
+from .version import version as __version__
+# import .pyimgren
+
+__all__ = ["Renamer", "__version__", "__name__"]
+
+import gettext
+import locale
+import os.path
+
+def nls_init(reset=False):
+    if reset:
+        pyimgren._ = lambda x: x
+    else:
+        loc = locale.getlocale()[0]
+        if loc is None: loc = locale.getdefaultlocale()[0]
+
+        pyimgren._ = gettext.translation("pyimgren",
+                                os.path.join(os.path.dirname(__file__),
+                                             "locale"),
+                                loc,
+                                fallback=True).gettext

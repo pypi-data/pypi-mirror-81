2c16931abf65f64fe3fb3e4a7e7f9500d31a26b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:05 +0000

    Add package fog/0.8.0/fog-0.8.0-cp36-cp36m-macosx_10_13_x86_64.whl

---
+++ packages/fog/fog-0.8.0-cp36-cp36m-macosx_10_13_x86_64.whl/fog/metrics/__init__.py
@@ -0,0 +1,10 @@
+from fog.metrics.cosine import (
+    cosine_similarity,
+    sparse_cosine_similarity,
+    sparse_dot_product
+)
+from fog.metrics.jaccard import (
+    jaccard_similarity,
+    weighted_jaccard_similarity
+)
+from fog.metrics.overlap import overlap_coefficient

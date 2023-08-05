8305d62fe8421e15fb6eb8dec83c696f2a9db2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:44 +0000

    Add package tutor-ecommerce/10.1.6/tutor-ecommerce-10.1.6.tar.gz

---
+++ packages/tutor-ecommerce/tutor-ecommerce-10.1.6.tar.gz/tutor-ecommerce-10.1.6/setup.py
@@ -0,0 +1,45 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, "README.rst"), "rt", encoding="utf8") as f:
+    readme = f.read()
+
+about = {}
+with io.open(os.path.join(here, "tutorecommerce", "__about__.py"), "rt", encoding="utf-8") as f:
+    exec(f.read(), about)
+
+setup(
+    name="tutor-ecommerce",
+    version=about["__version__"],
+    url="https://docs.tutor.overhang.io/",
+    project_urls={
+        "Documentation": "https://docs.tutor.overhang.io/",
+        "Code": "https://github.com/overhangio/tutor-ecommerce",
+        "Issue tracker": "https://github.com/overhangio/tutor-ecommerce/issues",
+        "Community": "https://discuss.overhang.io",
+    },
+    license="AGPLv3",
+    author="Overhang.io",
+    author_email="contact@overhang.io",
+    description="A Tutor plugin for Open edX Ecommerce",
+    long_description=readme,
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.5",
+    install_requires=["tutor-openedx>=10.0.0,<11.0.0", "tutor-discovery"],
+    entry_points={"tutor.plugin.v0": ["ecommerce = tutorecommerce.plugin"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)

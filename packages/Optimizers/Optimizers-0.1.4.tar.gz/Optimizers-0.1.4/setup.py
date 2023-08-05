99c25329ff6a7dc7be92b509f27096fc270e4502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:10 +0000

    Add package Optimizers/0.1.4/Optimizers-0.1.4.tar.gz

---
+++ packages/Optimizers/Optimizers-0.1.4.tar.gz/Optimizers-0.1.4/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'Optimizers',         # How you named your package folder (MyLib)
+  packages = ['Optimizers'],   # Chose the same as "name"
+  version = '0.1.4',      # Start with a small number and increase it with every change you make
+  license='GNU General Public License v3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A collection of examples for learning mathematical modelling',   # Give a short description about your library
+  author = 'David S.W. Lai',                   # Type in your name
+  author_email = 'davidswlai@outlook.com',      # Type in your E-Mail
+  url = 'https://github.com/davidswlai/',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/davidswlai/Optimizers/archive/v0.1-beta.tar.gz',    # I explain this later on
+  keywords = ['Optimizers', 'Logistics', 'Routing', 'Scheduling'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+        #   'validators',
+        #   'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)

0976308adf6103583c8376212563af14de6319af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:15 +0000

    Add package b-aws-testing-framework/0.0.6/b_aws_testing_framework-0.0.6.tar.gz

---
+++ packages/b-aws-testing-framework/b_aws_testing_framework-0.0.6.tar.gz/b_aws_testing_framework-0.0.6/HISTORY.md
@@ -0,0 +1,23 @@
+# Release history
+
+### 0.0.6
+* Update pytest dependency version.
+
+### 0.0.5
+* Rethink how this framework should be used.
+
+### 0.0.4
+* Fix imports.
+* Update examples.
+
+### 0.0.3
+* Rename project.
+* Update gitignore and ignore idea files.
+* Update MANIFEST.
+* Move examples to a documentation folder.
+
+### 0.0.2
+* Add hyperlinks for examples.
+
+### 0.0.1
+* Initial build.
---
+++ packages/b-aws-testing-framework/b_aws_testing_framework-0.0.6.tar.gz/b_aws_testing_framework-0.0.6/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup(
+    name='b_aws_testing_framework',
+    version='0.0.6',
+    license='Apache License 2.0',
+    packages=find_packages(exclude=[
+        # Exclude virtual environment.
+        'venv',
+        # Exclude test source files.
+        'b_aws_testing_framework_test'
+    ]),
+    description=(
+        'AWS infrastructure testing framework that supports multiple IAC tools.'
+    ),
+    long_description=README + '\n\n' + HISTORY,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=[
+        "pytest>=5.4.3,<7.0.0",
+        "biomapas-continuous-subprocess>=1.0.0,<2.0.0"
+    ],
+    author='Laimonas Sutkus',
+    author_email='laimonas.sutkus@biomapas.com',
+    keywords='AWS Test IAC',
+    url='https://github.com/biomapas/B.AwsTestingFramework.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)

a76161dd5d075d2c8ba8adb89eb40a935472c3d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package yamlpath/2.4.0/yamlpath-2.4.0.tar.gz

---
+++ packages/yamlpath/yamlpath-2.4.0.tar.gz/yamlpath-2.4.0/yamlpath/enums/pathsegmenttypes.py
@@ -0,0 +1,37 @@
+"""
+Implements the PathSegmentTypes enumeration.
+
+Copyright 2019, 2020 William W. Kimball, Jr. MBA MSIS
+"""
+from enum import Enum, auto
+
+
+class PathSegmentTypes(Enum):
+    """
+    Supported YAML Path segment types.
+
+    These include:
+
+    `ANCHOR`
+        A named YAML Anchor.
+
+    `COLLECTOR`
+        A sub YAML Path for which the result will be returned as a list.  The
+        data pointer is left where it was before the Collector expression is
+        resolved.
+
+    `INDEX`
+        A list element index.
+
+    `KEY`
+        A dictionary key name.
+
+    `SEARCH`
+        A search operation for which results are returned as they are matched.
+    """
+
+    ANCHOR = auto()
+    COLLECTOR = auto()
+    INDEX = auto()
+    KEY = auto()
+    SEARCH = auto()

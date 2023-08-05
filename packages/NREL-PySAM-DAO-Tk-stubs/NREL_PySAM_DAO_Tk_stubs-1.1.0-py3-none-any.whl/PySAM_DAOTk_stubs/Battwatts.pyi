b8df33df1a90a2d16d1ac945c46804282b5d068b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:53 +0000

    Add package NREL-PySAM-DAO-Tk-stubs/1.1.0/NREL_PySAM_DAO_Tk_stubs-1.1.0-py3-none-any.whl

---
+++ packages/NREL-PySAM-DAO-Tk-stubs/NREL_PySAM_DAO_Tk_stubs-1.1.0-py3-none-any.whl/PySAM_DAOTk_stubs/Battwatts.pyi
@@ -0,0 +1,161 @@
+class Lifetime(object):
+	def assign(self): 
+		pass
+
+	def export(self) -> Dict[Dict]:
+		pass
+
+	def __init__(self, *args, **kwargs): 
+		pass
+
+
+	analysis_period = float
+	system_use_lifetime_output = float
+
+
+class Battery(object):
+	def assign(self): 
+		pass
+
+	def export(self) -> Dict[Dict]:
+		pass
+
+	def __init__(self, *args, **kwargs): 
+		pass
+
+
+	ac = tuple
+	batt_custom_dispatch = tuple
+	batt_simple_chemistry = float
+	batt_simple_dispatch = float
+	batt_simple_enable = float
+	batt_simple_kw = float
+	batt_simple_kwh = float
+	batt_simple_meter_position = float
+	crit_load = tuple
+	dc = tuple
+	inverter_efficiency = float
+	load = tuple
+
+
+class Outputs(object):
+	def assign(self): 
+		pass
+
+	def export(self) -> Dict[Dict]:
+		pass
+
+	def __init__(self, *args, **kwargs): 
+		pass
+
+
+	annual_export_to_grid_energy = tuple
+	annual_import_to_grid_energy = tuple
+	average_battery_conversion_efficiency = float
+	average_battery_roundtrip_efficiency = float
+	avg_critical_load = float
+	batt_DOD = tuple
+	batt_DOD_cycle_average = tuple
+	batt_I = tuple
+	batt_SOC = tuple
+	batt_annual_charge_energy = tuple
+	batt_annual_charge_from_grid = tuple
+	batt_annual_charge_from_pv = tuple
+	batt_annual_discharge_energy = tuple
+	batt_annual_energy_loss = tuple
+	batt_annual_energy_system_loss = tuple
+	batt_bank_installed_capacity = float
+	batt_bank_replacement = tuple
+	batt_capacity_percent = tuple
+	batt_capacity_percent_calendar = tuple
+	batt_capacity_percent_cycle = tuple
+	batt_capacity_thermal_percent = tuple
+	batt_conversion_loss = tuple
+	batt_cost_to_cycle = tuple
+	batt_cycles = tuple
+	batt_dispatch_sched = tuple
+	batt_power = tuple
+	batt_power_target = tuple
+	batt_pv_charge_percent = float
+	batt_q0 = tuple
+	batt_q1 = tuple
+	batt_q2 = tuple
+	batt_qmax = tuple
+	batt_qmaxI = tuple
+	batt_qmax_thermal = tuple
+	batt_revenue_charge = tuple
+	batt_revenue_clipcharge = tuple
+	batt_revenue_discharge = tuple
+	batt_revenue_gridcharge = tuple
+	batt_system_loss = tuple
+	batt_temperature = tuple
+	batt_to_grid = tuple
+	batt_to_load = tuple
+	batt_voltage = tuple
+	batt_voltage_cell = tuple
+	cdf_of_surviving = tuple
+	fuelcell_to_batt = tuple
+	gen = tuple
+	grid_power = tuple
+	grid_power_target = tuple
+	grid_to_batt = tuple
+	grid_to_load = tuple
+	market_sell_rate_series_yr1 = tuple
+	monthly_batt_to_grid = tuple
+	monthly_batt_to_load = tuple
+	monthly_grid_to_batt = tuple
+	monthly_grid_to_load = tuple
+	monthly_pv_to_batt = tuple
+	monthly_pv_to_grid = tuple
+	monthly_pv_to_load = tuple
+	outage_durations = tuple
+	pdf_of_surviving = tuple
+	pv_to_batt = tuple
+	pv_to_grid = tuple
+	pv_to_load = tuple
+	resilience_hrs = tuple
+	resilience_hrs_avg = float
+	resilience_hrs_max = float
+	resilience_hrs_min = float
+	survival_function = tuple
+
+
+class Battwatts(object):
+	def assign(self, dict):
+		pass
+
+	def value(self, name, value=None):
+		pass
+
+	def execute(self, int_verbosity):
+		pass
+
+	def export(self):
+		pass
+
+	def __getattribute__(self, *args, **kwargs):
+		pass
+
+	def __init__(self, *args, **kwargs):
+		pass
+
+	Lifetime = Lifetime
+	Battery = Battery
+	Outputs = Outputs
+
+
+def default(config) -> Battwatts:
+	pass
+
+def new() -> Battwatts:
+	pass
+
+def wrap(ssc_data_t) -> Battwatts:
+	pass
+
+def from_existing(model, config="") -> Battwatts:
+	pass
+
+__loader__ = None 
+
+__spec__ = None

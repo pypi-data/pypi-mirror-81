7c0f163d6df6a6f4ad2cb43f0d58f187c60847be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:24 +0000

    Add package your/0.5.5/your-0.5.5.tar.gz

---
+++ packages/your/your-0.5.5.tar.gz/your-0.5.5/README.md
@@ -0,0 +1,54 @@
+# your
+
+  
+[![GitHub issues](https://img.shields.io/github/issues/thepetabyteproject/your?style=flat-square)](https://github.com/thepetabyteproject/your/issues)
+[![GitHub forks](https://img.shields.io/github/forks/thepetabyteproject/your?style=flat-square)](https://github.com/thepetabyteproject/your/network)
+[![GitHub stars](https://img.shields.io/github/stars/thepetabyteproject/your?style=flat-square)](https://github.com/thepetabyteproject/your/stargazers)
+[![GitHub license](https://img.shields.io/github/license/thepetabyteproject/your?style=flat-square)](https://github.com/thepetabyteproject/your/blob/master/LICENSE)
+[![HitCount](http://hits.dwyl.com/devanshkv/your.svg)](http://hits.dwyl.com/devanshkv/your)
+![Python package](https://github.com/thepetabyteproject/your/workflows/Python%20package/badge.svg?style=flat-square)
+[![codecov](https://codecov.io/gh/thepetabyteproject/your/branch/master/graph/badge.svg?style=flat-square)](https://codecov.io/gh/thepetabyteproject/your)
+
+
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/your?style=flat-square)
+![PyPI](https://img.shields.io/pypi/v/your?style=flat-square)
+![PyPI - Downloads](https://img.shields.io/pypi/dm/your?style=flat-square)
+  
+
+
+`your` stands for Your Unified Reader. This library can read data in sigproc filterbanks, psrfits, and psrdada formats in a unified way and can go from one format to another.
+
+| Format        | Read                     | Write               |
+| ------------- |:-------------:           | -----:              |
+| filterbank    | :white_check_mark:       | :white_check_mark:  |
+| psrfits       | :white_check_mark:       | :white_check_mark:  |
+| psrdada       | :x:                      | :white_check_mark:  |
+
+The inspiration for the name comes from the introduction of every [Daily Dose of Internet](https://www.youtube.com/channel/UCdC0An4ZPNr_YiFiYoVbwaw) video.
+
+# Installation
+You can install `your` directly using `pip`
+
+```bash
+pip install your
+```
+Or if you want to try out the lastest stuff,
+```bash
+pip install git+https://github.com/thepetabyteproject/your.git
+```
+
+
+
+**Note**:
+    To use the `psrdada` format, you would need to install [psrdada-python](https://github.com/AA-ALERT/psrdada-python). [`your_heimdall.py`](https://thepetabyteproject.github.io/your/bin/your_heimdall/) requires [Heimdall](https://sourceforge.net/projects/heimdall-astro/) and [psrdada-python](https://github.com/AA-ALERT/psrdada-python). 
+
+
+# Documentation
+Have a look at our [docs](https://thepetabyteproject.github.io/your/) for the documentation.
+
+# Tutorials
+Here are some [tutorial notebooks](https://github.com/devanshkv/your/tree/master/examples) to get you started.
+
+# Code Contributions
+We welcome all types of code contribution. Please have a look at our [guideline](CONTRIBUTING.md) and [code of conduct](CODE_OF_CONDUCT.md).
+
---
+++ packages/your/your-0.5.5.tar.gz/your-0.5.5/your/formats/filwriter.py
@@ -0,0 +1,109 @@
+import logging
+import os
+
+import numpy as np
+
+from your.formats.pysigproc import SigprocFile
+
+logger = logging.getLogger(__name__)
+
+
+def make_sigproc_obj(filfile, your_object, nchans, chan_freq, nstart):
+    """
+    Use Your class to make Sigproc class object with relevant parameters
+
+    Args:
+        filfile (str) : Name of the Filterbank file
+        your_object : Your object for the PSRFITS files
+        nchans (int) : No. of channels in the frequency range
+        chan_freq (np.ndarray) : An array of required frequency channel range
+        nstart (int): Start sample
+
+    Returns:
+        Object of class SigprocFile
+
+    """
+    logger.debug(f'Generating Sigproc object')
+    fil_obj = SigprocFile()
+
+    logger.debug(f'Setting attributes of Sigproc object from Your object.')
+    fil_obj.rawdatafile = filfile
+    fil_obj.source_name = your_object.your_header.source_name
+
+    # Verify the following parameters
+    fil_obj.machine_id = 0  # use "Fake" for now
+    fil_obj.barycentric = 0  # by default the data isn't barycentered
+    fil_obj.pulsarcentric = 0
+    fil_obj.telescope_id = 6  # use only GBT for now
+    fil_obj.data_type = 0
+
+    fil_obj.nchans = nchans
+    fil_obj.foff = your_object.your_header.foff
+    fil_obj.fch1 = chan_freq[0]
+    fil_obj.nbeams = 1
+    fil_obj.ibeam = 0
+    fil_obj.nbits = your_object.your_header.nbits
+    fil_obj.tsamp = your_object.your_header.tsamp
+    if not nstart:
+        nstart = 0
+    fil_obj.tstart = your_object.your_header.tstart + nstart * your_object.your_header.tsamp / (60 * 60 * 24)
+    fil_obj.nifs = 1  # Only use Intensity values
+
+    if your_object.your_header.ra_deg and your_object.your_header.dec_deg:
+        ra = your_object.your_header.ra_deg
+        dec = your_object.your_header.dec_deg
+    else:
+        ra = 0
+        dec = 0
+
+    from astropy.coordinates import SkyCoord
+    loc = SkyCoord(ra, dec, unit='deg')
+    ra_hms = loc.ra.hms
+    dec_dms = loc.dec.dms
+
+    fil_obj.src_raj = float(f'{int(ra_hms[0]):02d}{np.abs(int(ra_hms[1])):02d}{np.abs(ra_hms[2]):07.4f}')
+    fil_obj.src_dej = float(f'{int(dec_dms[0]):02d}{np.abs(int(dec_dms[1])):02d}{np.abs(dec_dms[2]):07.4f}')
+
+    fil_obj.az_start = -1
+    fil_obj.za_start = -1
+    return fil_obj
+
+
+def write_fil(data, your_object, nchans=None, chan_freq=None, filename=None, outdir=None, nstart=None):
+    """
+    Write Filterbank file given the Your object
+
+    Args:
+        data (np.ndarray): Data to write to the Filterbank file
+        your_object: Your object for the PSRFITS files
+        nchans (int) : No. of channels in the frequency range
+        chan_freq (np.ndarray) : Required frequency channel range
+        filename (str) : Output name of the Filterbank file
+        outdir (str): Output directory for the Filterbank file
+        nstart (int): Start sample number
+
+    """
+
+    filfile = outdir + '/' + filename
+
+    # Add checks for an existing fil file
+    logger.info(f'Trying to write data to filterbank file: {filfile}')
+    try:
+        if os.stat(filfile).st_size > 8192:  # check and replace with the size of header
+            logger.warning(f'Filterbank file already exists at {filfile}. Appending spectra to it.')
+            logger.info(f'Writing {data.shape[0]} spectra to file: {filfile}')
+            SigprocFile.append_spectra(data, filfile)
+        else:
+            logger.warning(f'Filterbank file already exists at {filfile}')
+            logger.info('Size of the Filerbank file is too small. Overwriting it.')
+            fil_obj = make_sigproc_obj(filfile, your_object, nchans, chan_freq, nstart)
+            fil_obj.write_header(filfile)
+            logger.info(f'Writing {data.shape[0]} spectra to file: {filfile}')
+            fil_obj.append_spectra(data, filfile)
+    except FileNotFoundError:
+        logger.info(f'Output file does not already exist. Creating a new Filterbank file.')
+        fil_obj = make_sigproc_obj(filfile, your_object, nchans, chan_freq, nstart)
+        fil_obj.write_header(filfile)
+        logger.info(f'Writing {data.shape[0]} spectra to file: {filfile}')
+        fil_obj.append_spectra(data, filfile)
+    logger.info(f'Successfully written data to Filterbank file: {filfile}')

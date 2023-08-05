40f3d38410bf0c22cd6860358894bf4ccc9dece3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:52 +0000

    Add package kora/0.6.22/kora-0.6.22.tar.gz

---
+++ packages/kora/kora-0.6.22.tar.gz/kora-0.6.22/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kora", 
+    version="0.6.22",
+    author="Korakot Chaovavanich",
+    author_email="korakot@gmail.com",
+    description="Convenient tools for Colab",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/airesearch-in-th/kora",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

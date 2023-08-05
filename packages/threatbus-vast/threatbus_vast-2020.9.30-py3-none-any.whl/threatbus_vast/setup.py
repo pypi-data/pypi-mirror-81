b8ef6c916dd68c396d698d97a9480a7cc74d1a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:22 +0000

    Add package threatbus-vast/2020.9.30/threatbus_vast-2020.9.30-py3-none-any.whl

---
+++ packages/threatbus-vast/threatbus_vast-2020.9.30-py3-none-any.whl/threatbus_vast/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+import pathlib
+
+plugin_dir = pathlib.Path(__file__).parent.absolute()
+
+with open(f"{plugin_dir}/README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author="Tenzir",
+    author_email="engineering@tenzir.com",
+    classifiers=[
+        # https://pypi.org/classifiers/
+        "Development Status :: 3 - Alpha",
+        "Environment :: Plugins",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        "Topic :: Security",
+        "Topic :: Software Development :: Object Brokering",
+        "Topic :: System :: Distributed Computing",
+    ],
+    description="A plugin to enable threatbus communication with VAST.",
+    entry_points={"threatbus.app": ["vast = threatbus_vast.plugin"]},
+    install_requires=[
+        "threatbus>=2020.09.30",
+        "pyzmq>=19",
+        "python-dateutil>=2.8.1",
+    ],
+    keywords=[
+        "threatbus",
+        "vast",
+        "threat intelligence",
+        "zeromq",
+    ],
+    license="BSD 3-clause",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="threatbus-vast",
+    package_dir={"": "plugins/apps"},
+    packages=["threatbus_vast"],
+    python_requires=">=3.7",
+    url="https://github.com/tenzir/threatbus",
+    version="2020.09.30",
+)

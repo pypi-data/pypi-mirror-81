679855a68b26d20a1c3f055a0050aa4cfd3464e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:47 +0000

    Add package swole/0.0.1/swole-0.0.1.tar.gz

---
+++ packages/swole/swole-0.0.1.tar.gz/swole-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as fr:
+    reqs = fr.read().strip().split('\n')
+
+
+setuptools.setup(
+    name="swole",
+    version="0.0.1",
+    author="Nicolas REMOND",
+    author_email="remondnicola@gmail.com",
+    description="Streamlit, but better.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/astariul/swole",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=reqs,
+)
---
+++ packages/swole/swole-0.0.1.tar.gz/swole-0.0.1/swole/widgets/input.py
@@ -0,0 +1,53 @@
+from dominate.tags import input_
+
+from swole.widgets.base import Widget
+
+
+class Input(Widget):
+    """ Widget to create an input.
+
+    Attributes:
+        type (`str`): Type of input.
+        placeholder (`str`): Placeholder of input.
+    """
+    def __init__(self, type="number", placeholder=None, **kwargs):
+        """ Constructor.
+
+        Arguments:
+            type (`str`, optional): Type of the input. Defaults to `number`.
+            placeholder (`str`, placeholder): Placeholder for the input. If
+                `None`, no placeholder is used. Defaults to `None`.
+        """
+        super().__init__(**kwargs)
+        self.type = type
+        self.placeholder = placeholder
+        self.value = placeholder
+        self.jquery_fn = "val"
+
+    def html(self):
+        attributes = {
+            "id": self.id,
+            "type": self.type,
+        }
+        self.add_css_class(attributes)
+
+        if self.placeholder is not None:
+            attributes["placeholder"] = self.placeholder
+
+        return input_(**attributes)
+
+    def get(self):
+        return self.value
+
+    def set(self, x):
+        if self.type == "number" and isinstance(x, str):
+            try:
+                # Find if it's an int or a float
+                number = float(x)
+                integer = int(number)
+                # number, integer = float(x), int(x)
+                self.value = integer if number == integer else number
+            except ValueError:      # Empty input
+                self.value = 0
+        else:
+            self.value = x

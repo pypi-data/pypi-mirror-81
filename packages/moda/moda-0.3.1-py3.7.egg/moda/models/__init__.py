101f0b4ef6d7f87fde508894c25921e6e32e2b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:26 +0000

    Add package moda/0.3.1/moda-0.3.1-py3.7.egg

---
+++ packages/moda/moda-0.3.1-py3.7.egg/moda/models/__init__.py
@@ -0,0 +1,5 @@
+from moda.models.stl.stl_model import STLTrendinessDetector
+from moda.models.ma_seasonal.ma_seasonal_model import MovingAverageSeasonalTrendinessDetector
+from moda.models.twitter.twitter_trendiness_detector import TwitterAnomalyTrendinessDetector
+from moda.models.lstm.lstm_anomaly import LSTMTrendinessDetector
+from moda.models.azure_anomaly_detection.azure_ad import AzureAnomalyTrendinessDetector

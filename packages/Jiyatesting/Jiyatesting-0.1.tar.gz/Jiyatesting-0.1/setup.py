c52b127499cb4dbc9d318aff8f5c75e4ad0c98eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:38 +0000

    Add package Jiyatesting/0.1/Jiyatesting-0.1.tar.gz

---
+++ packages/Jiyatesting/Jiyatesting-0.1.tar.gz/Jiyatesting-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Jiyatesting',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['Jiyatesting'],
+      author = "Jiya",
+      zip_safe = False)

6e09b26a9defdbcfd545339c50624f5caa69750f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:42 +0000

    Add package maznet/1.0.0/maznet-1.0.0-py3-none-any.whl

---
+++ packages/maznet/maznet-1.0.0-py3-none-any.whl/maznet/loss.py
@@ -0,0 +1,28 @@
+"""
+A loss function measures how good our predictions are,
+we use this to adjust the parameters of out network
+"""
+
+import numpy as np
+from maznet.tensor import Tensor
+
+
+class Loss:
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        raise NotImplementedError
+
+    def grad(self, predicted: Tensor, actual: Tensor) -> float:
+        raise NotImplementedError
+
+
+class MSE(Loss):
+    """
+    MSE is mean squared error
+    mse = (A - B)^2/n
+    """
+
+    def loss(self, predicted: Tensor, actual: Tensor) -> float:
+        return np.mean((predicted - actual) ** 2)
+
+    def grad(self, predicted: Tensor, actual: Tensor) -> float:
+        return (predicted - actual) * (2 / len(predicted))

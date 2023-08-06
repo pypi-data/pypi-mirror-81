1390a83fea75de7f7ca347379b0d7be8c9dd42c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:54 +0000

    Add package moonstone/0.4.0/moonstone-0.4.0.tar.gz

---
+++ packages/moonstone/moonstone-0.4.0.tar.gz/moonstone-0.4.0/moonstone/filtering/basics_filtering.py
@@ -0,0 +1,63 @@
+import logging
+from typing import List
+
+import pandas as pd
+
+from moonstone.filtering.base import BothAxisFiltering
+
+logger = logging.getLogger(__name__)
+
+
+class NoCountsFiltering(BothAxisFiltering):
+    """
+    Remove rows (default) or columns with no counts.
+    """
+
+    def filter(self) -> pd.DataFrame:
+        indices = self.df.sum(axis=self.axis) != 0.0
+        if self.axis == 1:
+            return self.df.loc[indices]
+        return self.df.loc[:, indices]
+
+
+class NamesFiltering(BothAxisFiltering):
+    """
+    Filtering based on row (default) or column names.
+    """
+
+    def __init__(self, dataframe: pd.DataFrame, names: List[str], axis: int = 0, keep: bool = True):
+        """
+        :param names: list of row or column names
+        :param axis: axis to apply filtering (index (0) or columns(1))
+        :param keep: keep column (discard them if set to False)
+        """
+        self.names = list(names)
+        self.keep = keep
+        self._log_action()
+        super().__init__(dataframe, axis=axis)
+
+    def _log_action(self):
+        if self.keep:
+            logger.info(f"Selecting {self.names} from dataframe...")
+        else:
+            logger.info(f"Removing {self.names} from dataframe...")
+
+    def _validate_parameters(self):
+        if isinstance(self.df.index, pd.MultiIndex):
+            error_message = f"{self.__class__.__name__} does not support filtering on MultiIndex dataframes." + \
+                " You might want to use moonstone.filtering.TaxonomyNamesFiltering instead."
+            raise TypeError(error_message)
+
+    def _select_names(self):
+        if self.axis == 0:
+            return self.df.loc[self.names, :]
+        return self.df.loc[:, self.names]
+
+    def _exclude_names(self):
+        return self.df.drop(self.names, axis=self.axis)
+
+    def filter(self) -> pd.DataFrame:
+        if self.keep:
+            return self._select_names()
+        else:
+            return self._exclude_names()
---
+++ packages/moonstone/moonstone-0.4.0.tar.gz/moonstone-0.4.0/moonstone/parsers/metadata.py
@@ -0,0 +1,82 @@
+import yaml
+from collections import defaultdict
+from typing import Dict, List
+
+from pandas import DataFrame
+
+from moonstone.analysis.columns_statistics import DataframeStatistics
+from moonstone.parsers.transform.cleaning import DataFrameCleaner
+from .base import BaseParser
+
+
+class MetadataParser(BaseParser):
+    """
+    Parse metadata file and allows to apply transformations on them (cleaning...).
+    """
+
+    def __init__(self, *args, index_col: str = 'sample', cleaning_operations: dict = None, **kwargs):
+        """
+        Cleaning operations are based on DataFrameCleaner object that allow to perform transformation
+        operations on different columns.
+
+        Format is the following:
+
+            {'col_name': [('operation1', 'operation1_options'), ('operation2', 'operation2_options')]}
+
+        :param index_col: name of the column used as dataframe index
+        :param cleaning_operations: cleaning operations to apply to the input table
+        """
+        self.index_col = index_col
+        self.cleaning_operations = cleaning_operations
+        if self.cleaning_operations is None:
+            self.cleaning_operations = {}
+        super().__init__(*args, **kwargs)
+
+    def to_dataframe(self) -> DataFrame:
+        dataframe = super().to_dataframe()
+        df_cleaner = DataFrameCleaner(dataframe)
+        for col_name, transformations in self.cleaning_operations.items():
+            for transformation in transformations:
+                transf_name = transformation[0]
+                transf_options = transformation[1]
+                df_cleaner.run_transform(col_name, transf_name, transf_options)
+        return df_cleaner.df.set_index(self.index_col)
+
+    def get_stats(self) -> List[Dict]:
+        """
+        :return: list of dict containing statistics about each column
+        """
+        return DataframeStatistics(self.dataframe).get_stats()
+
+
+class YAMLBasedMetadataParser:
+
+    def __init__(self, metadata_file_path, config_file_path, **kwargs):
+        self._parse_yaml_config(config_file_path)
+        self.metadata_parser = MetadataParser(
+            metadata_file_path, cleaning_operations=self.cleaning_operations,
+            parsing_options=self.parsing_options, **kwargs
+        )
+
+    def _parse_yaml_config(self, config_file_path):
+        with open(config_file_path, 'r') as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+        self.parsing_options = self._extract_parsing_options(config['parsing'])
+        self.cleaning_operations = self._extract_cleaning_operations(config['parsing'])
+
+    def _extract_parsing_options(self, parsing_config):
+        parsing_options = defaultdict(lambda: {})
+        for col_parsing_config in parsing_config:
+            if 'dtype' in col_parsing_config.keys():
+                parsing_options['dtype'][col_parsing_config['col_name']] = col_parsing_config['dtype']
+        return parsing_options
+
+    def _extract_cleaning_operations(self, parsing_config):
+        cleaning_operations = defaultdict(lambda: [])
+        for col_parsing_config in parsing_config:
+            if 'operations' in col_parsing_config.keys():
+                for operation in col_parsing_config['operations']:
+                    cleaning_operations[col_parsing_config['col_name']].append(
+                        (operation['name'], operation.get('options', {}))
+                    )
+        return cleaning_operations

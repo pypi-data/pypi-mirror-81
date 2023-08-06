c11b89969338522570d4bc9bdca8dc105c07ba38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:06 +0000

    Add package tello-lbs/1.0/tello_lbs-1.0.tar.gz

---
+++ packages/tello-lbs/tello_lbs-1.0.tar.gz/tello_lbs-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tello_lbs',
+      version='1.0',
+      description='Tello Edu Extention KIT controller',
+      packages=['tello_lbs'],
+      author_email='dimitriz@mail.ru',
+      zip_safe=False)
+

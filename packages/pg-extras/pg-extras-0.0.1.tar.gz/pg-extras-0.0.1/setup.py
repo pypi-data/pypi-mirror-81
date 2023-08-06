7d28fe9e2b00efd9634e75813c7a0fe524a8bb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:30 +0000

    Add package pg-extras/0.0.1/pg-extras-0.0.1.tar.gz

---
+++ packages/pg-extras/pg-extras-0.0.1.tar.gz/pg-extras-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pg-extras',
+    version='0.0.1',
+    description='WIP',
+    url='https://github.com/pawurb/python-pg-extras',
+    author='Pawel Urbanek',
+    author_email='contact@pawelurbanek.com',
+    license='MIT',
+    packages=['python-pg-extras'],
+    install_requires=[],
+
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
+)

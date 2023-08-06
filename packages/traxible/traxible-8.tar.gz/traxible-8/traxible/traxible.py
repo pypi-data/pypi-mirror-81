17dae7768eeef7cd289d6650e411c7dc3337f207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:03 +0000

    Add package traxible/8/traxible-8.tar.gz

---
+++ packages/traxible/traxible-8.tar.gz/traxible-8/traxible/traxible.py
@@ -0,0 +1,46 @@
+from fire import Fire
+from fabric import Connection
+
+
+class Traxible:
+    """
+    This is main module that allows basic remote operations
+    sush as ssh connection, send command or files, reconnect, ...
+
+    """
+
+    def __init__(self, host):
+        self.host = host
+        self.conn = Connection(self.host)
+
+    def run(self, commands, block=True, dry_run=False):
+        for command in commands:
+            print("> ", command)
+            if dry_run:
+                continue
+            try:
+                result = self.conn.run(command)
+                print(result.exited, "=" * 60)
+
+            except Exception as error:
+                if block is True:
+                    raise error
+
+    def put(self, source, destination, sudo=False, dry_run=False):
+        if dry_run:
+            print(f"> cp {source} {destination}")
+            return
+
+        if sudo is False:
+            return self.conn.put(source, destination)
+
+        self.conn.put(source, "/tmp/efe9e3023a502013767a0559a94a5e")
+        return self.run([f"sudo mv /tmp/efe9e3023a502013767a0559a94a5e {destination}"])
+
+    def reconnect(self):
+        self.conn.close()
+        self.conn.open()
+
+
+if __name__ == "__main__":
+    Fire(Traxible)

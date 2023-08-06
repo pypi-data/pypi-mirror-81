714c5d1e0aa6a931ba4240757e0b7aa85ee74a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:14 +0000

    Add package tableqa/0.0.9/tableqa-0.0.9-py3-none-any.whl

---
+++ packages/tableqa/tableqa-0.0.9-py3-none-any.whl/tableqa/__init__.py
@@ -0,0 +1,21 @@
+import nltk
+def _nltk_downloader():
+    try:
+        nltk.download('wordnet',quiet=True)
+        nltk.download('averaged_perceptron_tagger',quiet=True)
+        nltk.download('stopwords',quiet=True)
+        nltk.download('punkt',quiet=True)
+    except LookupError as e:
+        print(e)
+
+_nltk_downloader()
+
+
+from .nlp import *
+from .data_utils import *
+from .conditionmaps import *
+from .column_types import *
+from .clauses import *
+from .database import *
+from .agent import *
+from .chart import *

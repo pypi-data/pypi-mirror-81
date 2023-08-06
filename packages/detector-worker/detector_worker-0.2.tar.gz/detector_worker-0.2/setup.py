c696fd871f800e4c16f522de878a78de577a2cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package detector-worker/0.2/detector_worker-0.2.tar.gz

---
+++ packages/detector-worker/detector_worker-0.2.tar.gz/detector_worker-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='detector_worker',
+      version='0.2',
+      description='Worker class for incapsulating logic, required for Lionbridge Rnd detectors',
+      url='https://liox-teams.visualstudio.com/LE%20Research/_git/DntExtractor',
+      author='Rodion Cherny',
+      author_email='v-Rodion.Cherny@lionbridge.com',
+      license='MIT',
+      packages=['detector_worker'],
+      install_requires=[
+          'azure-storage-blob==12.3.2',
+          'azure-cosmosdb-table==1.0.6',
+          'azure-storage-queue==12.1.2',
+      ],
+      zip_safe=False)

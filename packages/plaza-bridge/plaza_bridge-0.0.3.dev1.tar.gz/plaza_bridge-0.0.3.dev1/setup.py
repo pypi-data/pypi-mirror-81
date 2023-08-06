917f0f975fc0fe6cdb4868c97ae368df1b595757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:13 +0000

    Add package plaza-bridge/0.0.3.dev1/plaza_bridge-0.0.3.dev1.tar.gz

---
+++ packages/plaza-bridge/plaza_bridge-0.0.3.dev1.tar.gz/plaza_bridge-0.0.3.dev1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='plaza_bridge',
+      version='0.0.3dev1',
+      description='Helper to build plaza bridges.',
+      author='kenkeiras',
+      author_email='kenkeiras@codigoparallevar.com',
+      url='https://gitlab.com/plaza-project/bridges/python-plaza-lib',
+      license='Apache License 2.0',
+      packages=['plaza_bridge'],
+      scripts=[],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+      ],
+      include_package_data=True,
+      install_requires=[
+          'websocket_client'
+      ],
+      zip_safe=False)

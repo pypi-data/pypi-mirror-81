055179e76fe29027c1323a688f486dd6d7aff8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:25 +0000

    Add package mewpy/0.0.6/mewpy-0.0.6.tar.gz

---
+++ packages/mewpy/mewpy-0.0.6.tar.gz/mewpy-0.0.6/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+files = ["model/data/*"]
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+#with open('HISTORY.rst') as history_file:
+#    history = history_file.read()
+
+setup(
+    name='mewpy',
+    version='0.0.6',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'python-libsbml',
+        'inspyred',
+        'reframed',
+        'cobra==0.17.1',
+        'jmetalpy',
+        'cobamp'],
+    author='BiSBII CEB University of Minho',
+    author_email='vpereira@ceb.uminho.pt',
+    description='mewpy - Metabolic Engineering in Python ',
+    license='Apache License Version 2.0',
+    keywords='strain optimization',
+    url='',
+    package_data={"": ["*.xml", "*.csv", "*.txt"], 'mewpy': files},
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

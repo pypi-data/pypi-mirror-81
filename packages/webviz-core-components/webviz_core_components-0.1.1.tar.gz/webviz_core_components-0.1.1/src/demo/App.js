31948b64abd7360a048f2030660966b405f42220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:31 +0000

    Add package webviz-core-components/0.1.1/webviz_core_components-0.1.1.tar.gz

---
+++ packages/webviz-core-components/webviz_core_components-0.1.1.tar.gz/webviz_core_components-0.1.1/src/demo/App.js
@@ -0,0 +1,40 @@
+/* eslint no-magic-numbers: 0 */
+import React, { Component } from "react";
+
+import { WebvizPluginPlaceholder } from "../lib";
+
+const steps = [
+    {
+        selector: "#blue-rect",
+        content: "This is my first step",
+    },
+    {
+        selector: "#green-rect",
+        content: "This is my second step",
+    },
+];
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.setProps = this.setProps.bind(this);
+    }
+
+    setProps(newProps) {
+        this.setState(newProps);
+    }
+
+    render() {
+        return (
+            <div>
+                <WebvizPluginPlaceholder
+                    setProps={this.setProps}
+                    tour_steps={steps}
+                />
+            </div>
+        );
+    }
+}
+
+export default App;

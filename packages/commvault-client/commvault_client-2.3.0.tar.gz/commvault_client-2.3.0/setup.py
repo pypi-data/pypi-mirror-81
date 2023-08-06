b28f157b87962dc8db53ae1223cc5cc1f64b644a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:06 +0000

    Add package commvault-client/2.3.0/commvault_client-2.3.0.tar.gz

---
+++ packages/commvault-client/commvault_client-2.3.0.tar.gz/commvault_client-2.3.0/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+import sys
+
+try:
+    import pypandoc
+    readme = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    with open('README.md') as f:
+        readme = f.read()
+
+install_requires = [
+    'cachetools>=1.1.5',
+    'requests>=2.7.0',
+    'xmltodict>=0.9.2',
+]
+
+tests_require = [
+    'pytest',
+    'requests-mock==0.7.0'
+]
+
+setup(
+    name='commvault_client',
+    version='2.3.0',
+    description='Python wrapper for Commvault api',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    author='Herkermer Sherwood',
+    author_email='theherk@gmail.com',
+    url='https://github.com/ronaldodduarte/pinkopy',
+    packages=find_packages(),
+    platforms=['all'],
+    license='MIT',
+    install_requires=install_requires,
+    setup_requires=['pytest-runner'],
+    tests_require=tests_require,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: Other/Proprietary License',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+    ],
+)
---
+++ packages/commvault-client/commvault_client-2.3.0.tar.gz/commvault_client-2.3.0/tests/pinkopy/test_helper.py
@@ -0,0 +1,64 @@
+import requests_mock
+
+
+def mock_session(base_session, service=None, user=None, pw=None, token=None,
+                 content_type=None, clients=None):
+    service = service or 'http://example.com'
+    user = user or 'user'
+    pw = pw or 'pw'
+    token = token or 'token'
+    content_type = content_type or 'application/json'
+    clients = clients or [
+        {'client': {
+            'clientEntity': {
+                'hostName': 'name',
+                'clientId': 2,
+                'clientName': 'client',
+                'displayName': 'display'
+            }
+        }
+        }
+    ]
+    get_response = {
+        'clientProperties': clients
+        }
+
+    post_response = {
+        'token': token
+    }
+    headers = {
+        'Accept': content_type,
+        'Content-type': content_type
+    }
+    with requests_mock.mock() as m:
+        m.get(service + '/Client', json=get_response)
+        m.post(service + '/Login', headers=headers, json=post_response)
+        config = {
+            'service': service,
+            'user': user,
+            'pw': pw
+        }
+        session = base_session(**config)
+        test_data = {
+            'Clients': clients,
+            'Content-type': content_type,
+            'Password': pw,
+            'Service': service,
+            'Session': session,
+            'Token': token,
+            'User': user
+        }
+
+        return test_data
+
+
+def validate_base_session(expected, actual):
+    cache_ttl = 1200
+    assert cache_ttl == actual.cache_ttl
+    assert expected['Service'] == actual.service
+    assert expected['User'] == actual.user
+    assert expected['Password'] == actual.pw
+    assert expected['Token'] == actual.headers['Authtoken']
+    assert expected['Content-type'] == actual.headers['Accept']
+    assert expected['Content-type'] == actual.headers['Content-type']
+    assert actual.use_cache

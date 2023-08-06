3152454a945c8e326496cf525f2ed0ef20ec6bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:39 +0000

    Add package py3socket/0.2.1.3/py3socket-0.2.1.3.tar.gz

---
+++ packages/py3socket/py3socket-0.2.1.3.tar.gz/py3socket-0.2.1.3/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+Flask-Sockets
+-------------
+
+Elegant WebSockets for your Flask apps.
+"""
+from setuptools import setup
+
+
+setup(
+    name='py3socket',
+    version='0.2.1.3',
+    url='https://github.com/ruqqus/flask-sockets',
+    license='See License',
+    author='Ruqqus LLC',
+    author_email='info@ruqqus.com',
+    description='Elegant WebSockets for your Flask apps, revised for Python 3.',
+    long_description=__doc__,
+    py_modules=['flask_sockets'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'gevent',
+        'gevent-websocket'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)

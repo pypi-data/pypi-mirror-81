63daa0f8017af5dcdeccd3927c247d9ba9b71fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:06 +0000

    Add package petisco/0.32.0/petisco-0.32.0.tar.gz

---
+++ packages/petisco/petisco-0.32.0.tar.gz/petisco-0.32.0/petisco/fixtures/persistence.py
@@ -0,0 +1,53 @@
+import os
+
+import pytest
+
+from petisco.application.petisco import Petisco
+
+
+@pytest.fixture
+def petisco_sql_database(request):
+    persistence_source_marker = request.node.get_closest_marker("persistence_source")
+    if persistence_source_marker is None:
+        persistence_source_name = "petisco"
+    else:
+        persistence_source_name = persistence_source_marker.args[0]
+    persistence_source_config = Petisco.get_instance().persistence_sources[
+        persistence_source_name
+    ]
+    if not persistence_source_config or not persistence_source_config["configured"]:
+        yield
+    else:
+        from sqlalchemy import create_engine
+        from petisco.persistence.sqlalchemy.sqlalchemy_persistence import (
+            SqlAlchemyPersistence,
+        )
+
+        sql_database = os.environ.get("SQL_DATABASE")
+        if not sql_database:
+            raise RuntimeError(
+                "Please add required SQL_DATABASE env variable (e.g pytest.ini, pytest.end2end.ini)"
+            )
+
+        Base = SqlAlchemyPersistence.get_instance().sources[persistence_source_name][
+            "base"
+        ]
+        Session = SqlAlchemyPersistence.get_instance().sources[persistence_source_name][
+            "session"
+        ]
+        connection = f"sqlite:///{sql_database}"
+        engine = create_engine(connection)
+        # Base.metadata.create_all(engine)
+        print(f"\nCREATE DATABASE {connection}")
+        ee = Petisco.get_instance()
+        print(ee)
+
+        yield
+
+        session = Session()
+        session.rollback()
+        session.close()
+        Base.metadata.drop_all(bind=engine)
+        os.remove(sql_database)
+
+        print(f"\nDELETE DATABASE {connection}")

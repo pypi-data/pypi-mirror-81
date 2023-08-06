170841deff4cd3d2150e03b28f3f90076a511a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:15 +0000

    Add package clause/1.1.5/clause-1.1.5.tar.gz

---
+++ packages/clause/clause-1.1.5.tar.gz/clause-1.1.5/README.md
@@ -0,0 +1,59 @@
+# Clause SDK for Python
+
+开源中文语义理解系统[Clause](https://github.com/chatopera/clause)的 Python 客户端。
+
+## 安装
+
+该 Python 包已经上传至[pypi](https://pypi.org/project/clause/)，用户可以使用 pip 直接安装。
+
+```
+pip install clause
+```
+
+当前稳定版本为 `1.x`。
+
+示例代码:
+
+```
+from clause import Client, Data
+from clause import CustomDict
+
+bot = Client(HOST_IP, HOST_PORT)
+# 创建自定义词典
+data = Data()
+data.customdict = CustomDict(name=customDictName, chatbotID=chatbot_id)
+response = self.bot.postCustomDict(data)
+```
+
+更多使用请参考`示例程序`。
+
+## 示例程序
+
+前提是已经部署了 Clause 服务，该示例程序介绍了如何在 Python 应用中，使用 Clause 服务构建聊天机器人，包括创建意图、创建说法、创建词典、引用系统词典、创建槽位、训练机器人和对话等部分。
+
+该示例程序实现的[对话场景介绍](https://github.com/chatopera/clause/wiki/%E7%A4%BA%E4%BE%8B%E7%A8%8B%E5%BA%8F)，熟悉对话场景有助于更好的掌握程序。
+
+```
+git clone https://github.com/chatopera/clause-sdk-python.git
+cd py-clause
+cp scripts/localrc.sample scripts/localrc
+vim scripts/localrc # 使用文本编辑器编辑 scripts/localrc，更新CL_HOST和CL_PORT
+./scripts/test.sh   # 执行测试程序
+```
+
+更详细的代码请参考[链接](https://github.com/chatopera/clause-sdk-python/blob/master/tests/tst-demo.py)。
+
+## API 接口详细介绍
+
+参考[链接](https://github.com/chatopera/clause-sdk-python/wiki)。
+
+## 开源许可协议
+
+Copyright (2019) <a href="https://www.chatopera.com/" target="_blank">北京华夏春松科技有限公司</a>
+
+[Apache License Version 2.0](https://github.com/chatopera/clause/blob/master/LICENSE)
+
+[![chatoper banner][co-banner-image]][co-url]
+
+[co-banner-image]: https://user-images.githubusercontent.com/3538629/42383104-da925942-8168-11e8-8195-868d5fcec170.png
+[co-url]: https://www.chatopera.com
---
+++ packages/clause/clause-1.1.5.tar.gz/clause-1.1.5/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from clause import __version__
+
+with open("README.md", "r") as fh:
+    long_description = '''
+中文语义理解服务 Python SDK
+----------------------------
+https://github.com/chatopera/clause-sdk-python
+'''
+
+setup(
+    name='clause',
+    version=__version__,
+    description='中文语义理解服务 Python SDK',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Hai Liang Wang',
+    author_email='hain@chatopera.com',
+    url='https://github.com/chatopera/clause-sdk-python',
+    license="Apache Software License",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Natural Language :: Chinese (Traditional)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+        'Topic :: Text Processing :: Linguistic'],
+    keywords='chatbot,machine-learning,NLU,NLP',
+    packages=find_packages(),
+    install_requires=[
+        "thrift>=0.11.0"
+    ],
+    package_data={
+        'clause': [
+            '**/*.gz',
+            '**/*.txt',
+            '**/*.vector',
+            'LICENSE']})

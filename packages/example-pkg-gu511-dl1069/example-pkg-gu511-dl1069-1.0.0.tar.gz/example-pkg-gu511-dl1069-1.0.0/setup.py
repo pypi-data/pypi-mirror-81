fb06e74c182e2f0bdfbe104b2ef7e30d79e77775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package example-pkg-gu511-dl1069/1.0.0/example-pkg-gu511-dl1069-1.0.0.tar.gz

---
+++ packages/example-pkg-gu511-dl1069/example-pkg-gu511-dl1069-1.0.0.tar.gz/example-pkg-gu511-dl1069-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup  
+
+setup(
+    name='example-pkg-gu511-dl1069',
+    version='1.0.0',
+    py_modules=['example-pkg-gu511-dl1069'],
+    author='sqmaxianlv',
+    author_email='mazai42432@gmail.com',
+    description='nothing',
+)

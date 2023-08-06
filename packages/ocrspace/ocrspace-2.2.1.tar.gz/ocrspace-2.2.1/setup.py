0b0d717af6f9ac41d5dc69a034ff0dd8f459a3a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:11 +0000

    Add package ocrspace/2.2.1/ocrspace-2.2.1.tar.gz

---
+++ packages/ocrspace/ocrspace-2.2.1.tar.gz/ocrspace-2.2.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'ocrspace',
+    packages = ['ocrspace'], # this must be the same as the name above
+    version = '2.2.1',
+    description = 'Perform OCR through ocr.space API',
+    author = ['Ali Najafi', 'Erik Boesen'],
+    author_email = 'me@erikboesen.com',
+    url = 'https://github.com/ErikBoesen/ocrspace',
+    keywords = ['ocr'],
+    classifiers = [],
+)

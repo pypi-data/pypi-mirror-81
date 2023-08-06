9899f5c51edbead76a33945b11dc6ca8fa159f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:18 +0000

    Add package Av1an/1.13.post5/Av1an-1.13.post5-py3-none-any.whl

---
+++ packages/Av1an/Av1an-1.13.post5-py3-none-any.whl/Scenedetection/pyscene.py
@@ -0,0 +1,45 @@
+#!/bin/env python
+
+from scenedetect.detectors import ContentDetector
+from scenedetect.scene_manager import SceneManager
+from scenedetect.video_manager import VideoManager
+
+from Av1an.logger import log
+
+
+def pyscene(video, threshold, min_scene_len):
+    """
+    Running PySceneDetect detection on source video for segmenting.
+    Optimal threshold settings 15-50
+    """
+    if not min_scene_len:
+        min_scene_len = 15
+
+    log(f'Starting PySceneDetect:\nThreshold: {threshold}, Min scene lenght: {min_scene_len}\n')
+    video_manager = VideoManager([str(video)])
+    scene_manager = SceneManager()
+    scene_manager.add_detector(ContentDetector(threshold=threshold, min_scene_len=min_scene_len))
+    base_timecode = video_manager.get_base_timecode()
+
+    # Work on whole video
+    video_manager.set_duration()
+
+    # Set downscale factor to improve processing speed.
+    video_manager.set_downscale_factor()
+
+    # Start video_manager.
+    video_manager.start()
+
+    scene_manager.detect_scenes(frame_source=video_manager, show_progress=True)
+
+    # Obtain list of detected scenes.
+    scene_list = scene_manager.get_scene_list(base_timecode)
+
+    scenes = [int(scene[0].get_frames()) for scene in scene_list]
+
+    # Remove 0 from list
+    if scenes[0] == 0:
+        scenes.remove(0)
+    log(f'Found scenes: {len(scenes)}\n')
+
+    return scenes

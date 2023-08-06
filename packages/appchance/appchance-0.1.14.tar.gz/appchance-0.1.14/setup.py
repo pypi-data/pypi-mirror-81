fff24f8a0165a644bdf9ba1fdf1fed9c46b86fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:35 +0000

    Add package appchance/0.1.14/appchance-0.1.14.tar.gz

---
+++ packages/appchance/appchance-0.1.14.tar.gz/appchance-0.1.14/setup.py
@@ -0,0 +1,26 @@
+import os, sys
+import setuptools
+
+with open("README.md", "r") as _file:
+    readme = _file.read()
+
+setuptools.setup(
+    name="appchance",
+    version="0.1.14",
+    author="Appchance Backend Special Forces Team",
+    author_email="backend@appchance.com",
+    description="Toolbelt for wizards and ninjas",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/appchance/pychance/",
+    packages=setuptools.find_packages(),
+    install_requires=["doit"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Django :: 3.0",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: POSIX :: Linux"
+    ],
+    python_requires='>=3',
+)

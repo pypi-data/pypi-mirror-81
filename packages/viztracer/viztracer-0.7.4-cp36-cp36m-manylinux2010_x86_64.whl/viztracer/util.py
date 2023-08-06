73b9644287e3d168cab37ebde8f696f28a3b047a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:59 +0000

    Add package viztracer/0.7.4/viztracer-0.7.4-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/viztracer/viztracer-0.7.4-cp36-cp36m-manylinux2010_x86_64.whl/viztracer/util.py
@@ -0,0 +1,27 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/viztracer/blob/master/NOTICE.txt
+
+def size_fmt(num, suffix='B'):
+    for unit in ['', 'Ki', 'Mi', 'Gi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Ti', suffix)
+
+
+class _bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+bcolors = _bcolors()
+
+
+def color_print(color, s):
+    print(bcolors.__getattribute__(color) + s + bcolors.ENDC)

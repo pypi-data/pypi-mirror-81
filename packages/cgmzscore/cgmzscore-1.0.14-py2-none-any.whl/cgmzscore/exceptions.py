95338747a9cc498a902b515dca520d818f04f254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:20 +0000

    Add package cgmzscore/1.0.14/cgmzscore-1.0.14-py2-none-any.whl

---
+++ packages/cgmzscore/cgmzscore-1.0.14-py2-none-any.whl/cgmzscore/exceptions.py
@@ -0,0 +1,11 @@
+class DataNotFound(RuntimeError):
+    pass
+
+class DataError(RuntimeError):
+    pass
+
+class InvalidAge(RuntimeError):
+    pass
+
+class InvalidMeasurement(RuntimeError):
+    pass

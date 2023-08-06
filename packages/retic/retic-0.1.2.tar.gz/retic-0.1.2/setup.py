3f50fcf5afb53c5bdb71e10369ccbf1701ad1edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:47 +0000

    Add package retic/0.1.2/retic-0.1.2.tar.gz

---
+++ packages/retic/retic-0.1.2.tar.gz/retic-0.1.2/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='retic',
+    version='0.1.2',
+    license='MIT',
+    description='Retic is a framework for Python.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Braian Staimer',
+    author_email='braianflorian@gmail.com',
+    url='https://github.com/reticpy/retic',
+    packages=setuptools.find_packages(),
+    include_package_data=True,    
+    download_url='https://github.com/reticpy/retic/archive/0.1.0-beta.tar.gz',
+    keywords=['PYTHON', 'WEB FRAMEWORK', 'MACHINE LEARNING WEB FRAMEWORK'],
+    install_requires=[
+        "environs>=7.4.0",
+        "httpmethods>=1.0.5",
+        "Werkzeug>=1.0.0",
+        "repath>=0.9.0"
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)

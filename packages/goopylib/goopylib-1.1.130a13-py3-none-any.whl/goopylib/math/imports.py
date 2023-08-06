3da528aa722d045529fd03a52536b69fb420d1fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:33 +0000

    Add package goopylib/1.1.130a13/goopylib-1.1.130a13-py3-none-any.whl

---
+++ packages/goopylib/goopylib-1.1.130a13-py3-none-any.whl/goopylib/math/imports.py
@@ -0,0 +1,10 @@
+from goopylib.math.PyBezierCurve import *
+from goopylib.math.CBezierCurve import *
+from goopylib.math.BSpline import *
+
+from goopylib.math.PyEasing import *
+
+from goopylib.math.Triangulation import *
+
+from goopylib.math.Curves import *
+from goopylib.math.Interpolations import *

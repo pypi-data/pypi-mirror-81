4544d501e2fd7376ba96e96b10aab563be884d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:54 +0000

    Add package igneous/1.1.0/igneous-1.1.0.tar.gz

---
+++ packages/igneous/igneous-1.1.0.tar.gz/igneous-1.1.0/README.md
@@ -0,0 +1,9 @@
+# python-igneous
+The Igneous API Client SDK
+
+This is the Python client library for Igneous' APIs.
+
+Please refer to the Igneous API documentation at https://kb.igneous.io/docs for an introduction ;
+
+For an example on how to use this SDK, see `tools/ig.py` , a command line tool for interacting with Igneous' APIs,
+written with this very `igneous` package
---
+++ packages/igneous/igneous-1.1.0.tar.gz/igneous-1.1.0/igneous/__init__.py
@@ -0,0 +1,15 @@
+"""
+The Igneous API Client SDK
+
+This is the Python client library for Igneous' APIs.
+
+Please refer to the Igneous API documentation at https://kb.igneous.io/docs for an introduction ;
+
+For an example on how to use this SDK, see `tools/ig.py` , a command line tool for interacting with Igneous' APIs,
+written with this very `igneous` package
+
+
+
+"""
+from .client import Client
+from .util import json_dumps

4222d4b457143ecbd8a3c70a211571d3dfd72d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:50 +0000

    Add package physunits/0.0.9/physunits-0.0.9.tar.gz

---
+++ packages/physunits/physunits-0.0.9.tar.gz/physunits-0.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="physunits", # Replace with your own username
+    version="0.0.9",
+    author="pacosalces",
+    author_email="pacosalces@gmail.com",
+    description="Physical units as global variables for simple numerical propagation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pacosalces/physunits",
+    license='LICENSE.txt',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

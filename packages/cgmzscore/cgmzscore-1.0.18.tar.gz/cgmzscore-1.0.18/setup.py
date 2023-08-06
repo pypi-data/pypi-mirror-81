a722eba4d46f6ffd0c223a0f7885bfb6e93d7515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:59 +0000

    Add package cgmzscore/1.0.18/cgmzscore-1.0.18.tar.gz

---
+++ packages/cgmzscore/cgmzscore-1.0.18.tar.gz/cgmzscore-1.0.18/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cgmzscore',
+    version='1.0.18',
+    license='BSD',
+
+
+    author='Prajwal Kumar Singh',
+    author_email='prajwalsingh651@gmail.com',
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    description="z-scores of anthropometric measurements of children below 5 years  based on WHO",
+    long_description=open('README').read(),
+
+    classifiers=[
+        'Intended Audience :: Healthcare Industry',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Operating System :: OS Independent',
+    ]
+)

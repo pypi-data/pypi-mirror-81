f1c6c79dc4c4c69506d33f580f7d8cb1a87ba1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:11 +0000

    Add package datamaker/1.0.1/datamaker-1.0.1.tar.gz

---
+++ packages/datamaker/datamaker-1.0.1.tar.gz/datamaker-1.0.1/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+metadata_path = "datamaker/common/metadata/"
+static_path = "datamaker/common/static/"
+datafiles = [
+    ("metadata", [metadata_path + "formats.json"]),
+    ("metadata", [metadata_path + "parameters.json"]),
+    ("static", [static_path + "cities.json"]),
+    ("static", [static_path + "countries.json"]),
+    ("static", [static_path + "names.json"]),
+    ("static", [static_path + "states.json"]),
+    ("static", [static_path + "streets.json"]),
+]
+
+
+setuptools.setup(
+    name="datamaker",
+    version="1.0.1",
+    author="Alex Broley",
+    author_email="alex.broley@gmail.com",
+    description="A package for generating tabluar datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tekkabroley/DataMaker",
+    packages=setuptools.find_packages(),
+    data_files=datafiles,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
+)

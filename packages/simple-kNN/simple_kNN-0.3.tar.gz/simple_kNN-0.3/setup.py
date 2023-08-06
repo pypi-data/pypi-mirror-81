d00a9a421661a654df75a1addf97adc044966531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:04 +0000

    Add package simple-kNN/0.3/simple_kNN-0.3.tar.gz

---
+++ packages/simple-kNN/simple_kNN-0.3.tar.gz/simple_kNN-0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='simple_kNN',
+      version='0.3',
+      description='Simple kNN algorithm with k-Fold Cross Validation',
+      author='Chaitanya Krishna Kasaraneni',
+      author_email='kc.kasaraneni@gmail.com',
+      long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chaitanyakasaraneni/simple-kNN",
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+        zip_safe=False)

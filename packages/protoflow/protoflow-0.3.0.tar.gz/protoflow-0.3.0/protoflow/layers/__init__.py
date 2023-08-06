c1910eb023ed528bd77fb2dd9a1e9eac9f9f07f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:13 +0000

    Add package protoflow/0.3.0/protoflow-0.3.0.tar.gz

---
+++ packages/protoflow/protoflow-0.3.0.tar.gz/protoflow-0.3.0/protoflow/layers/__init__.py
@@ -0,0 +1,10 @@
+from .competitions import KNNC, WTAC
+from .products import LpSIP
+from .prototypes import Prototypes1D
+
+__all__ = [
+    'KNNC',
+    'LpSIP',
+    'Prototypes1D',
+    'WTAC',
+]

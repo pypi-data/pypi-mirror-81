2e8e980f4134e4d313ac3820332ca42b294f6c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:03 +0000

    Add package compound-split/1.0.2.dev3/compound_split-1.0.2.dev3.tar.gz

---
+++ packages/compound-split/compound_split-1.0.2.dev3.tar.gz/compound_split-1.0.2.dev3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name='compound_split',
+    version='1.0.2.dev3',
+    install_requires=[],
+    python_requires='>=3',
+    author="Don Tuggener",
+    author_email="don.tuggener@gmail.com",
+    maintainer="Joel Niklaus",
+    maintainer_email="me@joelniklaus.ch",
+    description="Splits a compound into its body and head. So far German and Dutch are supported.",
+    license='GPL-3.0 License',
+    long_description=open('README.md', "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/JoelNiklaus/CompoundSplit",
+    packages=setuptools.find_packages(),
+    #include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ]
+)

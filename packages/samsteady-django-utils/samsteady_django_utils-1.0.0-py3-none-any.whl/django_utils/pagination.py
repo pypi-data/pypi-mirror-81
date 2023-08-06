1e7581914ef802dbef9fee5af92928b03c2a507a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:18 +0000

    Add package samsteady-django-utils/1.0.0/samsteady_django_utils-1.0.0-py3-none-any.whl

---
+++ packages/samsteady-django-utils/samsteady_django_utils-1.0.0-py3-none-any.whl/django_utils/pagination.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+from pprint import pprint
+
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.views import exception_handler
+
+
+
+class CustomPagination(PageNumberPagination):
+    page_size_query_param = 'page_size'
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('lastPage', self.page.paginator.count),
+            ('count', self.page.paginator.count),
+            ('current', self.page.number),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))

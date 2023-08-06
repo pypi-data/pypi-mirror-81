72701b05f04b390e85f57ad9df42afb171636c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:22 +0000

    Add package russianCVparser/1.0rc1/russianCVparser-1.0rc1.tar.gz

---
+++ packages/russianCVparser/russianCVparser-1.0rc1.tar.gz/russianCVparser-1.0rc1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('russianCVparser/README.md', encoding='utf-8') as inf:
+    ld = inf.read()
+
+setuptools.setup(name='russianCVparser',
+                 version='1.0rc1',
+                 description='Parser for CV in russian language. Supported formats: pdf, txt, docx',
+                 long_description=ld,
+                 long_description_content_type="text/markdown",
+                 packages=setuptools.find_packages(),
+                 author_email='onsunday1703@gmail.com',
+                 install_requires=[
+                     'natasha',
+                     'pdfminer',
+                     'docx2txt'
+                 ]
+                 ,
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 python_requires='>=3.6',
+                 )

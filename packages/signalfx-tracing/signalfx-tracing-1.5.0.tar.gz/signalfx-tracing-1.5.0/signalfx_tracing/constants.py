92d7a43bf6b0ef5578fdfd447d2fbe270a89fa4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:54 +0000

    Add package signalfx-tracing/1.5.0/signalfx-tracing-1.5.0.tar.gz

---
+++ packages/signalfx-tracing/signalfx-tracing-1.5.0.tar.gz/signalfx-tracing-1.5.0/signalfx_tracing/constants.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2018-2019 SignalFx. All rights reserved.
+
+instrumented_attr = '__sfx_instrumented'
+traceable_libraries = (
+    'celery', 'django', 'elasticsearch', 'falcon', 'flask', 'psycopg2', 'pymongo', 'pymysql', 'redis', 'requests',
+    'tornado', 'logging'
+)
+
+auto_instrumentable_libraries = (
+    'celery', 'elasticsearch', 'falcon', 'flask', 'psycopg2', 'pymongo', 'pymysql', 'redis', 'requests',
+    'tornado', 'logging'
+)
+
+logging_format = (
+    '%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+    '[signalfx.trace_id=%(sfxTraceId)s signalfx.span_id=%(sfxSpanId)s] '
+    '- %(message)s'
+)

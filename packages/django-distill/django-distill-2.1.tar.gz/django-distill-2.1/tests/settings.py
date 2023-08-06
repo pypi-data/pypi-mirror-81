36008aa637b3ac2b230fdfe1c7bbaa00becc6035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:25 +0000

    Add package django-distill/2.1/django-distill-2.1.tar.gz

---
+++ packages/django-distill/django-distill-2.1.tar.gz/django-distill-2.1/tests/settings.py
@@ -0,0 +1,40 @@
+import os
+
+
+try:
+    from django.urls import path
+    HAS_PATH = True
+except ImportError:
+    HAS_PATH = False
+
+
+try:
+    from django.urls import re_path
+    HAS_RE_PATH = True
+except ImportError:
+    HAS_RE_PATH = False
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+SECRET_KEY = 'test'
+
+
+ROOT_URLCONF = 'tests.urls'
+
+
+MIDDLEWARE = ['django.contrib.sessions.middleware.SessionMiddleware']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join('BASE_DIR' , 'test.sqlite3'),
+    }
+}
+
+
+INSTALLED_APPS = [
+    'tests',
+]

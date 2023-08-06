32e035495868740f343e29ca29d661c098565f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:41 +0000

    Add package giotto-tda-nightly/20201005.23/giotto_tda_nightly-20201005.23-cp36-cp36m-win_amd64.whl

---
+++ packages/giotto-tda-nightly/giotto_tda_nightly-20201005.23-cp36-cp36m-win_amd64.whl/gtda/__init__.py
@@ -0,0 +1,16 @@
+from ._version import __version__
+
+__all__ = [
+    'mapper',
+    'homology',
+    'time_series',
+    'graphs',
+    'diagrams',
+    'images',
+    'point_clouds',
+    'plotting',
+    'externals',
+    'utils',
+    'metaestimators',
+    '__version__'
+    ]

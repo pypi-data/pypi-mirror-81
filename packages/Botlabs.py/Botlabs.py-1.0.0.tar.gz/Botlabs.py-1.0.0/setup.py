17216e9b6c46621873bb3487af264f5aff7af58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package Botlabs.py/1.0.0/Botlabs.py-1.0.0.tar.gz

---
+++ packages/Botlabs.py/Botlabs.py-1.0.0.tar.gz/Botlabs.py-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Botlabs.py", # Replace with your own username
+    version="1.0.0",
+    author="Pjotr",
+    author_email="pjotrlspdfr@outlook.com",
+    description="A simple API wrapper for discordbotlabs.org",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pjotr07740/botlabs.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

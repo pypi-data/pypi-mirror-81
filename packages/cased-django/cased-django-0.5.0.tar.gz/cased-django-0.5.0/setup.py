bb6d09a1c2b410629662166d108f94bb375d783f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:57 +0000

    Add package cased-django/0.5.0/cased-django-0.5.0.tar.gz

---
+++ packages/cased-django/cased-django-0.5.0.tar.gz/cased-django-0.5.0/setup.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+setup(
+    name="cased-django",
+    version="0.5.0",
+    description="Django integration for Cased",
+    author="Cased",
+    author_email="support@cased.com",
+    url="https://github.com/cased/cased-django",
+    license="Apache",
+    keywords="cased api",
+    packages=["cased_django"],
+    install_requires=["cased>=0.3.2",],
+    zip_safe=False,
+    python_requires=">3.5",
+    project_urls={
+        "Bug Tracker": "https://github.com/cased/cased-django/issues",
+        "Source Code": "https://github.com/cased/cased-django",
+    },
+)

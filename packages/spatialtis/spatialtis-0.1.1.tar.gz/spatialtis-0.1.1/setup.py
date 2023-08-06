b7335888eef0cb13145f42d9172bb208295d545c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:14 +0000

    Add package spatialtis/0.1.1/spatialtis-0.1.1.tar.gz

---
+++ packages/spatialtis/spatialtis-0.1.1.tar.gz/spatialtis-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from setuptools import setup
+
+README = Path("README.md").read_text()
+
+setup(name="spatialtis",
+      description="spatial analysis toolkit for single-cell multiplexed tissue data",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      version="0.1.1",
+      author="Mr-Milk",
+      url="https://github.com/Mr-Milk/SpatialTis",
+      author_email="yb97643@um.edu.mo",
+      license="Apache License 2.0",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Intended Audience :: Science/Research",
+          "Topic :: Scientific/Engineering :: Bio-Informatics",
+      ],
+      python_requires='>=3.6',
+      install_requires=['anndata', 'numpy', 'pandas',
+                        'scipy', 'shapely', 'bokeh',
+                        'seaborn', 'colour', 'matplotlib', 'tqdm', 'pyecharts', 'ray',
+                        'spatialentropy', 'colorama', 'snapshot_phantomjs', 'neighborhood_analysis'],
+      extras_require={'all': ['scikit-image', 'python-igraph', 'leidenalg', 'alphashape', 'tifffile', ]}
+      )
---
+++ packages/spatialtis/spatialtis-0.1.1.tar.gz/spatialtis-0.1.1/spatialtis/spatial/__init__.py
@@ -0,0 +1,8 @@
+from ._distribution import spatial_distribution
+from ._exp_neighcell import exp_neighcells
+from ._exp_neighexp import exp_neighexp
+from ._heterogeneity import spatial_heterogeneity
+from ._hotspot import hotspot
+from ._neighborhood import neighborhood_analysis, spatial_enrichment_analysis
+from ._neighbors import Neighbors
+from ._network import communities

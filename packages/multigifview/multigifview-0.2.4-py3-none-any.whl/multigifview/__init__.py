308a3ecdabf5668fdadf0ce3d5183d02ccb82a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:50 +0000

    Add package multigifview/0.2.4/multigifview-0.2.4-py3-none-any.whl

---
+++ packages/multigifview/multigifview-0.2.4-py3-none-any.whl/multigifview/__init__.py
@@ -0,0 +1,11 @@
+from .__main__ import main, show_gifs
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ModuleNotFoundError:
+    from importlib_metadata import version, PackageNotFoundError
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    pass

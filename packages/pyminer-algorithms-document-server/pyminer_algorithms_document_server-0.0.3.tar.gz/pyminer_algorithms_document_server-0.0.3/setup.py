5a08fade359114977a584b311cb2e3f8368d66ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:04 +0000

    Add package pyminer-algorithms-document-server/0.0.3/pyminer_algorithms_document_server-0.0.3.tar.gz

---
+++ packages/pyminer-algorithms-document-server/pyminer_algorithms_document_server-0.0.3.tar.gz/pyminer_algorithms_document_server-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyminer_algorithms_document_server",
+    version="0.0.3",
+    author="panhaoyu",
+    author_email="panhaoyu.china@outlook.com",
+    description="A small documentation server for PyMiner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/py2cn/pyminer-algorithms-document-server",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['flask', 'markdown'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)

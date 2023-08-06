82b907be8a8354a7b3eebebdf389e4b9290a6370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:22 +0000

    Add package django-dramatiq-pg/1.3.2/django-dramatiq-pg-1.3.2.tar.gz

---
+++ packages/django-dramatiq-pg/django-dramatiq-pg-1.3.2.tar.gz/django-dramatiq-pg-1.3.2/src/django_dramatiq_pg/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+
+class BackgroundJob(models.Model):
+    class STATE(models.TextChoices):
+        QUEUED = "queued"
+        CONSUMED = "consumed"
+        REJECTED = "rejected"
+        DONE = "done"
+
+    message_id = models.UUIDField(primary_key=True)
+    queue_name = models.TextField(default="default")
+    state = models.CharField(max_length=16, default=STATE.QUEUED, choices=STATE.choices)
+    mtime = models.DateTimeField(default=timezone.now)
+    message = models.JSONField(blank=True, null=True)
+    result = models.JSONField(blank=True, null=True)
+    result_ttl = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = "queue"
+        indexes = (models.Index(fields=["state", "mtime"]),)

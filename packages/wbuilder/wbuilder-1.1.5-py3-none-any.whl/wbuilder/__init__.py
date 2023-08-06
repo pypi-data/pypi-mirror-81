04aae15a9faa0968e23446c83a84623a70d27e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:09 +0000

    Add package wbuilder/1.1.5/wbuilder-1.1.5-py3-none-any.whl

---
+++ packages/wbuilder/wbuilder-1.1.5-py3-none-any.whl/wbuilder/__init__.py
@@ -0,0 +1,6 @@
+""" wbuilder """
+from .wbuilder import ElemBuilder
+from .wbuilder import WebBuilder
+from .wbuilder import Blocks
+from .version import version as __version__
+__all__ = ["wbuilder"]

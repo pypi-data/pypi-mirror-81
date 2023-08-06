7def6ddbdecb2f2ab67259249c310ffdfc39c9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:46 +0000

    Add package egter/0.3.1/egter-0.3.1.tar.gz

---
+++ packages/egter/egter-0.3.1.tar.gz/egter-0.3.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='egter',
+    version='0.3.1',
+    packages=['egter', 'egter.crypt', 'egter.crypt.enigma', 'egter.crypt.steganography', 
+    			'egter.crypt.hash'],
+    url='https://notabug.org/EgTer/egter-py',
+    license='GNU GPL v3',
+    author='EgTer',
+    author_email='annom2017@mail.ru',
+    description='My collection of my tools'
+)

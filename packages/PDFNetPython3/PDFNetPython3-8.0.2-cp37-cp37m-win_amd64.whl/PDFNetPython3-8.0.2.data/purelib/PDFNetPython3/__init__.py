3de92d98077a07e7ddf78c5ca7c27bb02d39b62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:44 +0000

    Add package PDFNetPython3/8.0.2/PDFNetPython3-8.0.2-cp37-cp37m-win_amd64.whl

---
+++ packages/PDFNetPython3/PDFNetPython3-8.0.2-cp37-cp37m-win_amd64.whl/PDFNetPython3-8.0.2.data/purelib/PDFNetPython3/__init__.py
@@ -0,0 +1,9 @@
+import site, os
+site.addsitedir(os.path.dirname(__file__))
+from PDFNetPython import *
+
+def add_37(num1, num2):
+    return num1 + num2
+
+def mul_37(num1, num2):
+    return num1 * num2

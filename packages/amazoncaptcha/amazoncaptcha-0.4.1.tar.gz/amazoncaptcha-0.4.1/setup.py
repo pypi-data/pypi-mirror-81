6a4bb4b1aae07424043788e4059c9f86b820c8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:11 +0000

    Add package amazoncaptcha/0.4.1/amazoncaptcha-0.4.1.tar.gz

---
+++ packages/amazoncaptcha/amazoncaptcha-0.4.1.tar.gz/amazoncaptcha-0.4.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amazoncaptcha",
+    version="0.4.1",
+    description="Pure Python, lightweight, Pillow-based solver for the Amazon's text captcha.",
+    packages=['amazoncaptcha'],
+    py_modules=['objects', 'exceptions', 'utils'],
+    include_package_data = True,
+    package_data = {
+        '': ['*.json'],
+        'amazoncaptcha': ['training_data/*.*'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = [
+        "pillow ~= 7.2.0",
+        "requests ~= 2.24.0",
+        "selenium ~= 3.141.0"
+    ],
+    author="Anatolii Maliarov",
+    author_email="tly.mov@gmail.com",
+    url="https://github.com/a-maliarov/amazon-captcha-solver",
+)

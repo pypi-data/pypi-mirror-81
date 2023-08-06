2a647396a4d23a72791fccd04f96157caf4874dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:31 +0000

    Add package GPyM-TM/1.3.0/GPyM_TM-1.3.0.tar.gz

---
+++ packages/GPyM-TM/GPyM_TM-1.3.0.tar.gz/GPyM_TM-1.3.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GPyM_TM", 
+    version="1.3.0",
+    author="Jocelyn Mazarura",
+    author_email="<jocelyn.mazarura@up.ac.za>",
+    description="The following package enables users to perform text modelling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jrmazarura/GPM",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

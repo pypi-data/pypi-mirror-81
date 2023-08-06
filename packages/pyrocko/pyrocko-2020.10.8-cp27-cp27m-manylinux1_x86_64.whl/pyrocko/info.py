73f6bf557e6c61985f2a5df94b75ca4bff745370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:05 +0000

    Add package pyrocko/2020.10.8/pyrocko-2020.10.8-cp27-cp27m-manylinux1_x86_64.whl

---
+++ packages/pyrocko/pyrocko-2020.10.8-cp27-cp27m-manylinux1_x86_64.whl/pyrocko/info.py
@@ -0,0 +1,6 @@
+# This module is automatically created from setup.py
+git_sha1 = '49b9bad6d16ec9e4ff67126e87850ebeb933e4f9'
+local_modifications = False
+version = '2020.10.08'
+long_version = 'pyrocko-2020.10.08-49b9bad6d16ec9e4ff67126e87850ebeb933e4f9-2020-10-08_13:24:16'  # noqa
+installed_date = '2020-10-08_13:24:16'

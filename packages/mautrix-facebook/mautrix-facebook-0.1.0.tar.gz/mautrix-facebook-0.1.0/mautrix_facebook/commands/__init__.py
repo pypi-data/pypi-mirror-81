160b271f21ae2c825843071a856d506c1ccea6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:54 +0000

    Add package mautrix-facebook/0.1.0/mautrix-facebook-0.1.0.tar.gz

---
+++ packages/mautrix-facebook/mautrix-facebook-0.1.0.tar.gz/mautrix-facebook-0.1.0/mautrix_facebook/commands/__init__.py
@@ -0,0 +1,4 @@
+from .handler import (CommandProcessor, CommandHandler, CommandEvent, command_handler,
+                      SECTION_AUTH, SECTION_MISC, SECTION_CONNECTION)
+from .auth import enter_2fa_code
+from . import facebook as _, conn as _

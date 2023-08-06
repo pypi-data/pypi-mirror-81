f1b87975f48bee09dc4adc25e8c8e72246a77f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:16 +0000

    Add package gradle-profiler-pttest/0.0.2/gradle-profiler-pttest-0.0.2.tar.gz

---
+++ packages/gradle-profiler-pttest/gradle-profiler-pttest-0.0.2.tar.gz/gradle-profiler-pttest-0.0.2/gradle_profiler_pttest/app.py
@@ -0,0 +1,22 @@
+# app.py
+
+
+from . import analysis_reporter
+from . import benchmark_parser
+from . import benchmarks_analyser
+from . import cli_parser
+
+
+def main(argv=None):
+    try:
+        baseline_csv, modified_csv = cli_parser.parse(argv)
+
+        baseline = benchmark_parser.parse(baseline_csv)
+        modified = benchmark_parser.parse(modified_csv)
+
+        analysis = benchmarks_analyser.analyse(baseline, modified)
+        analysis_reporter.report(analysis)
+
+    except Exception as error:
+        print(error)
+        print("Could not complete analysis. Aborting.\n")
---
+++ packages/gradle-profiler-pttest/gradle-profiler-pttest-0.0.2.tar.gz/gradle-profiler-pttest-0.0.2/gradle_profiler_pttest/benchmarks_analyser.py
@@ -0,0 +1,21 @@
+# benchmarks_analyser.py
+
+from pingouin import ttest
+
+from .analysis_results import AnalysisResults
+from .analysis_results import PairedTTestDetails
+
+
+SIGNIFICANCE_LEVEL = 0.050
+
+
+def analyse(baseline, modified):
+    try:
+        stats = ttest(baseline.builds, modified.builds, paired=True, tail='greater').round(3)
+        tstatistic = stats.loc['T-test', 'T']
+        pvalue = stats.loc['T-test', 'p-val']
+        improvement_detected = pvalue < SIGNIFICANCE_LEVEL
+        details = PairedTTestDetails(tstatistic, pvalue, SIGNIFICANCE_LEVEL)
+        return AnalysisResults(baseline, modified, details, improvement_detected)
+    except:
+        raise Exception('Failed when runnig analyser for benchmarks')

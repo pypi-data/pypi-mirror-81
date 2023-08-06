0c1295a55a36fcba4a987cba2443b74a728cf55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:38 +0000

    Add package edx-enterprise-data/2.1.3/edx_enterprise_data-2.1.3-py3-none-any.whl

---
+++ packages/edx-enterprise-data/edx_enterprise_data-2.1.3-py3-none-any.whl/enterprise_data/api/v0/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+API endpoint for enterprise data app.
+"""
+
+
+default_app_config = 'enterprise_data.api.v0.apps.ApiAppConfig'  # pylint: disable=invalid-name
---
+++ packages/edx-enterprise-data/edx_enterprise_data-2.1.3-py3-none-any.whl/enterprise_data_roles/apps.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Enterprise Data Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class EnterpriseDataRolesConfig(AppConfig):
+    """
+    Configuration for the Enterprise Data Roles Django application.
+    """
+
+    name = 'enterprise_data_roles'

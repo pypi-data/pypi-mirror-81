2672ff9527bcce24f93d32ad2575abc49d14c339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:27 +0000

    Add package jaychkr123/1.1/jaychkr123-1.1.tar.gz

---
+++ packages/jaychkr123/jaychkr123-1.1.tar.gz/jaychkr123-1.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+
+    name="jaychkr123",
+    version=1.1,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+
+)

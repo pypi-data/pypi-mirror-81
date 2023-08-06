72cba1c8a838f1c9e802c345e30580ffa0355941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:37 +0000

    Add package bolinette/0.5.3/Bolinette-0.5.3.tar.gz

---
+++ packages/bolinette/Bolinette-0.5.3.tar.gz/Bolinette-0.5.3/bolinette/core/__init__.py
@@ -0,0 +1,9 @@
+from bolinette.core.cache import cache
+from bolinette.core.environment import init, Environment
+from bolinette.core.database import DatabaseEngine
+from bolinette.core.jwt import JWT
+from bolinette.core.resources import BolinetteResources, BolinetteResource
+from bolinette.core.sockets import BolinetteSockets
+from bolinette.core.mapping import Mapping
+from bolinette.core.context import BolinetteContext
+from bolinette.core.transaction import Transaction
---
+++ packages/bolinette/Bolinette-0.5.3.tar.gz/Bolinette-0.5.3/bolinette/defaults/controllers/timezone.py
@@ -0,0 +1,15 @@
+from bolinette import blnt
+from bolinette.decorators import controller, get
+from bolinette.defaults.services import TimezoneService
+from bolinette.utils import response
+
+
+@controller('tz', '/tz')
+class TimezoneController(blnt.Controller):
+    @property
+    def tz_service(self) -> TimezoneService:
+        return self.context.service('tz')
+
+    @get('')
+    async def all_timezones(self):
+        return response.ok('OK', await self.tz_service.get_all())

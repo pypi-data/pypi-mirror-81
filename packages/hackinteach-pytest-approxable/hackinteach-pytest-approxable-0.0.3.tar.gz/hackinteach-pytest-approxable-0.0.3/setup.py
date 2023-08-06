a0a84e74f276c4fefdf2166656cb36e77216eee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:37 +0000

    Add package hackinteach-pytest-approxable/0.0.3/hackinteach-pytest-approxable-0.0.3.tar.gz

---
+++ packages/hackinteach-pytest-approxable/hackinteach-pytest-approxable-0.0.3.tar.gz/hackinteach-pytest-approxable-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hackinteach-pytest-approxable",
+    version="0.0.3",
+    author="hackinteach",
+    author_email="webmaster@hackinteach.com",
+    description="A pytest approxable dict mixin for data class",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hackinteach/pytest-approxable",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)

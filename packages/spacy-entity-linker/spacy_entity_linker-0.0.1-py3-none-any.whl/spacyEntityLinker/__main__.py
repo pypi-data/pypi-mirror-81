89fc35a974c644f4edb51f53357fefb234c52f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:52 +0000

    Add package spacy-entity-linker/0.0.1/spacy_entity_linker-0.0.1-py3-none-any.whl

---
+++ packages/spacy-entity-linker/spacy_entity_linker-0.0.1-py3-none-any.whl/spacyEntityLinker/__main__.py
@@ -0,0 +1,24 @@
+if __name__ == "__main__":
+    import sys
+    import urllib
+    import urllib.request
+    import tarfile
+    import os
+
+    if len(sys.argv) < 2:
+        print("No arguments given")
+        pass
+
+    command = sys.argv.pop(1)
+
+    if command == "download_knowledge_base":
+        FILE_URL = "https://wikidatafiles.nyc3.digitaloceanspaces.com/Hosting/Hosting/SpacyEntityLinker/datafiles.tar.gz"
+        OUTPUT_TAR_FILE = os.path.abspath(os.path.dirname(__file__)) + '/../data/wikidb_filtered.tar.gz'
+        OUTPUT_DB_PATH = os.path.abspath(os.path.dirname(__file__)) + '/../data'
+        urllib.request.urlretrieve(FILE_URL, OUTPUT_TAR_FILE)
+
+        tar = tarfile.open(OUTPUT_TAR_FILE)
+        tar.extractall(OUTPUT_DB_PATH)
+        tar.close()
+
+        os.remove(OUTPUT_TAR_FILE)

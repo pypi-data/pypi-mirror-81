5f2ffffadd14874dc5dc19cac638e0312f3d9c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:38 +0000

    Add package rita-dsl/0.6.10/rita-dsl-0.6.10.tar.gz

---
+++ packages/rita-dsl/rita-dsl-0.6.10.tar.gz/rita-dsl-0.6.10/rita/__init__.py
@@ -0,0 +1,50 @@
+import os
+
+import logging
+import types
+
+from rita.config import with_config
+from rita.preprocess import preprocess_rules
+from rita.precompile import precompile
+
+
+logger = logging.getLogger(__name__)
+
+__version__ = (0, 6, 10, os.getenv("VERSION_PATCH"))
+
+
+def get_version():
+    normalized = list([i for i in __version__ if i is not None])
+    if len(normalized) == 4:
+        return "{0}.{1}.{2}-{3}".format(*normalized)
+    else:
+        return "{0}.{1}.{2}".format(*normalized)
+
+
+@with_config
+def compile_string(raw, config, use_engine=None, **kwargs):
+    from rita.parser import RitaParser
+    for k, v in kwargs.items():
+        config.set_variable(k, v)
+
+    parser = RitaParser(config)
+    parser.build()
+    root = parser.parse(precompile(raw))
+    logger.debug(root)
+    if use_engine:
+        compile_rules = config.set_engine(use_engine)
+    else:
+        compile_rules = config.default_engine
+    rules = list(preprocess_rules(root, config))
+    result = compile_rules(rules, config, **kwargs)
+    if isinstance(result, types.GeneratorType):
+        return list(result)
+    else:
+        return result
+
+
+def compile(fname, use_engine=None, **kwargs):
+    with open(fname, "r") as f:
+        raw = f.read()
+
+    return compile_string(raw, use_engine=use_engine, **kwargs)

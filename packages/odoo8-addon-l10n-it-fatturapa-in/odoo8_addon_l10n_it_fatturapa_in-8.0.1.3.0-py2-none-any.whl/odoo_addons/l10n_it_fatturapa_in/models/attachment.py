427ab27575307881251e4e54bf58d1c8bd2eb31a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:05 +0000

    Add package odoo8-addon-l10n-it-fatturapa-in/8.0.1.3.0/odoo8_addon_l10n_it_fatturapa_in-8.0.1.3.0-py2-none-any.whl

---
+++ packages/odoo8-addon-l10n-it-fatturapa-in/odoo8_addon_l10n_it_fatturapa_in-8.0.1.3.0-py2-none-any.whl/odoo_addons/l10n_it_fatturapa_in/models/attachment.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+import base64
+from openerp import fields, models, api, _
+
+
+class FatturaPAAttachmentIn(models.Model):
+    _name = "fatturapa.attachment.in"
+    _description = "FatturaPA import File"
+    _inherits = {'ir.attachment': 'ir_attachment_id'}
+    _inherit = ['mail.thread']
+    _order = 'id desc'
+
+    ir_attachment_id = fields.Many2one(
+        'ir.attachment', 'Attachment', required=True, ondelete="cascade")
+    in_invoice_ids = fields.One2many(
+        'account.invoice', 'fatturapa_attachment_in_id',
+        string="In Invoices", readonly=True)
+    xml_supplier_id = fields.Many2one(
+        "res.partner", string="Supplier", compute="_compute_xml_data",
+        store=True)
+    invoices_number = fields.Integer(
+        "Invoices number", compute="_compute_xml_data", store=True)
+    invoices_total = fields.Float(
+        "Invoices total", compute="_compute_xml_data", store=True,
+        help="Se indicato dal fornitore, Importo totale del documento al "
+             "netto dell'eventuale sconto e comprensivo di imposta a debito "
+             "del cessionario / committente"
+    )
+    registered = fields.Boolean(
+        "Registered", compute="_compute_registered", store=True)
+
+    @api.onchange('datas_fname')
+    def onchagne_datas_fname(self):
+        self.name = self.datas_fname
+
+    def get_xml_string(self):
+        return self.ir_attachment_id.get_xml_string()
+
+    @api.multi
+    @api.depends('ir_attachment_id.datas')
+    def _compute_xml_data(self):
+        for att in self:
+            fatt = self.env['wizard.import.fatturapa'].get_invoice_obj(att)
+            cedentePrestatore = fatt.FatturaElettronicaHeader.CedentePrestatore
+            partner_id = self.env['wizard.import.fatturapa'].getCedPrest(
+                cedentePrestatore)
+            att.xml_supplier_id = partner_id
+            att.invoices_number = len(fatt.FatturaElettronicaBody)
+            att.invoices_total = 0
+            for invoice_body in fatt.FatturaElettronicaBody:
+                att.invoices_total += float(
+                    invoice_body.DatiGenerali.DatiGeneraliDocumento.
+                    ImportoTotaleDocumento or 0
+                )
+
+    @api.multi
+    @api.depends('in_invoice_ids', 'invoices_number')
+    def _compute_registered(self):
+        for att in self:
+            if (
+                att.in_invoice_ids and
+                len(att.in_invoice_ids) == att.invoices_number
+            ):
+                att.registered = True
+            else:
+                att.registered = False
+
+    def extract_attachments(self, AttachmentsData, invoice_id):
+        AttachModel = self.env['fatturapa.attachments']
+        for attach in AttachmentsData:
+            if not attach.NomeAttachment:
+                name = _("Attachment without name")
+            else:
+                name = attach.NomeAttachment
+            content = attach.Attachment
+            _attach_dict = {
+                'name': name,
+                'datas': base64.b64encode(str(content)),
+                'datas_fname': name,
+                'description': attach.DescrizioneAttachment or '',
+                'compression': attach.AlgoritmoCompressione or '',
+                'format': attach.FormatoAttachment or '',
+                'invoice_id': invoice_id,
+            }
+            AttachModel.create(_attach_dict)

a7e6098a9ff4dfc7b5adfdf55d95c9b043c22ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:26 +0000

    Add package gitmodules/0.0.3/gitmodules-0.0.3.tar.gz

---
+++ packages/gitmodules/gitmodules-0.0.3.tar.gz/gitmodules-0.0.3/gitmodules/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'kuyaki'
+__copyright__ = '2020 KuYaki'
+__credits__ = ['kuyaki']
+__maintainer__ = 'kuyaki'
+__date__ = '2020/10/02'
+__version__ = '0.0.3'
+
+from gitmodules.gitmodules import extend_paths
+
+
+extend_paths()

3d9b8f7022e5498156be9b86e43e587fd48a9c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:27 +0000

    Add package stacksearch/1.3.6/stacksearch-1.3.6-py3-none-any.whl

---
+++ packages/stacksearch/stacksearch-1.3.6-py3-none-any.whl/stacksearch/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+stacksearch is a library that provides a easy way to search on all stackexchange sites.
+
+What it does is it sends a request to the stackexchange website, does some fancy HTML
+parsing, then returns a python dict containing questions and answers.
+
+
+"""
+
+__version__ = "1.3.6"
+
+__author__ = ["Bryan Hu"]

b9948ebdb850183c4edaad705e3af7b27009c6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:31 +0000

    Add package industrialucn/0.1.7.1/industrialucn-0.1.7.1.tar.gz

---
+++ packages/industrialucn/industrialucn-0.1.7.1.tar.gz/industrialucn-0.1.7.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="industrialucn",
+    version="0.1.7.1",
+    author="Hernan Caceres",
+    author_email="idustrial@ucn.cl",
+    description="Generic tool for numerical experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://industrial.ucn.cl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering"
+    ],
+    python_requires='>=3.7',
+)

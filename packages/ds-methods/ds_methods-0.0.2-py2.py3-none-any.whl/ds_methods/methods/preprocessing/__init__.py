ad32170e9ff69e0055396b05a351f5184d05c34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:57 +0000

    Add package ds-methods/0.0.2/ds_methods-0.0.2-py2.py3-none-any.whl

---
+++ packages/ds-methods/ds_methods-0.0.2-py2.py3-none-any.whl/ds_methods/methods/preprocessing/__init__.py
@@ -0,0 +1,5 @@
+from .moving_average.moving_average import MovingAverage
+from .normalisation.normalisation import Normalisation
+from .seasonal_decomposition.seasonal_decomposition import SeasonalDecomposition
+from .basic.basic import Basic
+from .selector.selector import Selector
---
+++ packages/ds-methods/ds_methods-0.0.2-py2.py3-none-any.whl/ds_methods/methods/preprocessing/selector/selector.py
@@ -0,0 +1,26 @@
+from pandas import DataFrame
+
+from ds_methods.common.validators import check_keys
+from ds_methods.common.types import MethodOutput
+
+from ds_methods.methods.base import BaseMethod
+
+from .schemas import options_schema
+
+
+class Selector(BaseMethod):
+    options_schema = options_schema
+
+    def make_(self, input_data: DataFrame) -> MethodOutput:
+        if 'include' in self.options:
+            input_data = input_data[self.options['include']]
+        if 'exclude' in self.options:
+            input_data.drop(self.options['exclude'], axis=1, inplace=True)
+
+        return MethodOutput(
+            data=input_data,
+            error=None,
+        )
+
+    def validate_input_(self, input_data: DataFrame) -> bool:
+        return check_keys(input_data, self.options.get('include', []) + self.options.get('exclude', []))

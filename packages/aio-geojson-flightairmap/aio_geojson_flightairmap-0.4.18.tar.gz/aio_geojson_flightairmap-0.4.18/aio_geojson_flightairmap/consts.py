fad03d585e4b80f7f77db72a409a26cd9859d7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:50 +0000

    Add package aio-geojson-flightairmap/0.4.18/aio_geojson_flightairmap-0.4.18.tar.gz

---
+++ packages/aio-geojson-flightairmap/aio_geojson_flightairmap-0.4.18.tar.gz/aio_geojson_flightairmap-0.4.18/aio_geojson_flightairmap/consts.py
@@ -0,0 +1,12 @@
+"""Flight Air Map constants."""
+
+ATTRIBUTION = "Flight Air Map Data"
+
+CUSTOM_ATTRIBUTE = 'custom_attribute'
+
+ATTR_FLIGHT_CODE = 'c'
+ATTR_AIRCRAFT_REGISTRATION = 'reg'
+ATTR_AIRCRAFT_TYPE = 'ai'
+ATTR_DEPARTURE_AIRPORT = 'dac'
+ATTR_ARRIVAL_AIRPORT = 'aac'
+ATTR_ID = 'fi'

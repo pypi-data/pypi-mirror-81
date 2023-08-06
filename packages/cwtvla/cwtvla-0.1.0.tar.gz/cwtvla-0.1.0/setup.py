c0a119ef0dcb60be04448945ba0258a6e109a6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:46 +0000

    Add package cwtvla/0.1.0/cwtvla-0.1.0.tar.gz

---
+++ packages/cwtvla/cwtvla-0.1.0.tar.gz/cwtvla-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cwtvla',
+    version='0.1.0',
+    description='ChipWhisperer Test Vector Leakage Assessment Library',
+    author='Alex Dewar',
+    author_email='adewar@newae.com',
+    license='GPLv2+',
+    packages=find_packages("cwtvla"),
+    install_requires=[
+        'scipy',
+        #cw not really necessary, but cw convenience functions obviously require CW to be installed
+        #'chipwhisperer' 
+    ]
+)

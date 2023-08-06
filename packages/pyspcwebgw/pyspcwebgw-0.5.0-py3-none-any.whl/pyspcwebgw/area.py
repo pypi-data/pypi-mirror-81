879a518b4f1ef3a31131a00d90189997b0dafee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:16 +0000

    Add package pyspcwebgw/0.5.0/pyspcwebgw-0.5.0-py3-none-any.whl

---
+++ packages/pyspcwebgw/pyspcwebgw-0.5.0-py3-none-any.whl/pyspcwebgw/area.py
@@ -0,0 +1,55 @@
+import logging
+
+from pyspcwebgw.const import AreaMode
+from pyspcwebgw.utils import _load_enum
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Area:
+    """Represents and SPC alarm system area."""
+    SUPPORTED_SIA_CODES = ('CG', 'NL', 'OG', 'BV')
+
+    def __init__(self, gateway, spc_area):
+        self._gateway = gateway
+        self._id = spc_area['id']
+        self._name = spc_area['name']
+        self._verified_alarm = False
+        self.zones = None
+
+        self.update(spc_area)
+
+    def __str__(self):
+        return '{id}: {name}. Mode: {mode}, last changed by {last}.'.format(
+            name=self.name, id=self.id,
+            mode=self.mode, last=self.last_changed_by)
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def verified_alarm(self):
+        return self._verified_alarm
+
+    @property
+    def mode(self):
+        return self._mode
+
+    @property
+    def last_changed_by(self):
+        return self._last_changed_by
+
+    def update(self, spc_area, sia_code=None):
+        _LOGGER.debug("Update area %s", self.id)
+
+        self._mode = _load_enum(AreaMode, spc_area['mode'])
+        self._verified_alarm = sia_code == 'BV'
+        if self._mode == AreaMode.UNSET:
+            self._last_changed_by = spc_area.get('last_unset_user_name', 'N/A')
+        else:
+            self._last_changed_by = spc_area.get('last_set_user_name', 'N/A')
---
+++ packages/pyspcwebgw/pyspcwebgw-0.5.0-py3-none-any.whl/pyspcwebgw/zone.py
@@ -0,0 +1,55 @@
+import logging
+
+from pyspcwebgw.const import ZoneInput, ZoneType, ZoneStatus
+from pyspcwebgw.utils import _load_enum
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Zone:
+    """Represents an SPC alarm system zone."""
+    SUPPORTED_SIA_CODES = ('ZO', 'ZC', 'ZX', 'ZD', 'ZM', 'BA',
+                           'BB', 'BU', 'BR', 'BC')
+
+    def __init__(self, area, spc_zone):
+        self._id = spc_zone['id']
+        self._name = spc_zone['zone_name']
+        self._area = area
+
+        self.update(spc_zone)
+
+    def __str__(self):
+        return '{id}: {name} ({type}). Input: {inp}, status: {status}'.format(
+            id=self.id, name=self.name, type=self.type,
+            inp=self.input, status=self.status)
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def input(self):
+        return self._input
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def status(self):
+        return self._status
+
+    @property
+    def area(self):
+        return self._area
+
+    def update(self, spc_zone, sia_code=None):
+        _LOGGER.debug("Update zone %s", self.id)
+
+        self._input = _load_enum(ZoneInput, spc_zone['input'])
+        self._type = _load_enum(ZoneType, spc_zone['type'])
+        self._status = _load_enum(ZoneStatus, spc_zone['status'])

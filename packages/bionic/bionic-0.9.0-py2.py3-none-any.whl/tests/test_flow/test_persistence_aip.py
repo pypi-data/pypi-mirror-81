1442b5e595f7c85eea1e34d7dfa3bcb568c581e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:22 +0000

    Add package bionic/0.9.0/bionic-0.9.0-py2.py3-none-any.whl

---
+++ packages/bionic/bionic-0.9.0-py2.py3-none-any.whl/tests/test_flow/test_persistence_aip.py
@@ -0,0 +1,55 @@
+import pytest
+
+import bionic as bn
+
+
+# This is detected by pytest and applied to all the tests in this module.
+pytestmark = [pytest.mark.no_parallel]
+
+
+@pytest.mark.needs_aip
+def test_aip_jobs(aip_builder):
+    builder = aip_builder
+
+    builder.assign("x", 2)
+    builder.assign("y", 3)
+
+    @builder
+    @bn.aip_task_config("n1-standard-4")
+    def a(x, y):
+        return x * y
+
+    @builder
+    @bn.aip_task_config("n1-standard-8")
+    def b(x, y):
+        return x + y
+
+    @builder
+    def c(a, b):
+        return a - b  # 6 (2 * 3) - 5 (2 + 3)
+
+    assert builder.build().get("c") == 1
+
+
+@pytest.mark.needs_gcs
+def test_fake_aip_jobs(fake_aip_builder):
+    builder = fake_aip_builder
+
+    builder.assign("x", 2)
+    builder.assign("y", 3)
+
+    @builder
+    @bn.aip_task_config("n1-standard-4")
+    def a(x, y):
+        return x * y
+
+    @builder
+    @bn.aip_task_config("n1-standard-8")
+    def b(x, y):
+        return x + y
+
+    @builder
+    def c(a, b):
+        return a - b  # 6 (2 * 3) - 5 (2 + 3)
+
+    assert builder.build().get("c") == 1

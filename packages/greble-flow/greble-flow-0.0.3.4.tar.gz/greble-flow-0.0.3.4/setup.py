bf42c4532d76dd9a3b1764b03f179e336d12bf99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:23 +0000

    Add package greble-flow/0.0.3.4/greble-flow-0.0.3.4.tar.gz

---
+++ packages/greble-flow/greble-flow-0.0.3.4.tar.gz/greble-flow-0.0.3.4/setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, "README.rst")).read()
+
+
+setup(
+    name="greble-flow",
+    version="0.0.3.4",
+    packages=find_packages(exclude=("tests", "example")),
+    description="Greble flow",
+    long_description=README,
+    author="Greble",
+    author_email="info@greble.io",
+    url="https://github.com/greble/python-flow-helper",
+    install_requires=["greble-flow>=0.0.1", "click", "flask"],
+)

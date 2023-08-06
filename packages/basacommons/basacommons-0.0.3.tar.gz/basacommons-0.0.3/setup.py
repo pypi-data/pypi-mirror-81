799dc7a52cbacf1f18e871271ad0a8dc8e976c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:19 +0000

    Add package basacommons/0.0.3/basacommons-0.0.3.tar.gz

---
+++ packages/basacommons/basacommons-0.0.3.tar.gz/basacommons-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='basacommons',
+    version='0.0.3',
+    description='Basic project utilities',
+    author='Enrique Basañez Mercader',
+    author_email='enrique.basanez@gmail.com',
+    url='https://github.com/ebasanez/python-commons',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    )

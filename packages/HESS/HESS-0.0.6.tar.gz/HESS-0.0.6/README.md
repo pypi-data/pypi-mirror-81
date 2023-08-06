3757918d5f3d61b0ea7099cef9765ac98a39fbef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:29 +0000

    Add package HESS/0.0.6/HESS-0.0.6.tar.gz

---
+++ packages/HESS/HESS-0.0.6.tar.gz/HESS-0.0.6/README.md
@@ -0,0 +1,8 @@
+# HESS black-box algorithm 
+
+http://www.peqnp.com
+
+# Installation
+```python
+pip install HESS
+```
---
+++ packages/HESS/HESS-0.0.6.tar.gz/HESS-0.0.6/setup.py
@@ -0,0 +1,13 @@
+import sys
+import setuptools
+from distutils.core import setup, Extension
+setup(
+    name='HESS',
+    version='0.0.6',
+    packages=['hess'],
+    url='http://www.peqnp.com',
+    license='Copyright (c) 2012-2020 Oscar Riveros. All rights reserved.',
+    author='Oscar Riveros',
+    author_email='contact@peqnp.science',
+    description='HESS black-box algorithm from http://www.peqnp.com',
+)

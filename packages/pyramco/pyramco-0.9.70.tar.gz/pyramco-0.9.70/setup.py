9c38dbe51df08279f7f05cfd8e789cfb29204038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:39 +0000

    Add package pyramco/0.9.70/pyramco-0.9.70.tar.gz

---
+++ packages/pyramco/pyramco-0.9.70.tar.gz/pyramco-0.9.70/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyramco',
+    version='0.9.70',
+    author='Jeremey Bingham',
+    author_email='info@mansard.net',
+    description='A complete wrapper class for the RAMCO API',
+    install_requires=['requests'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mansard/pyramco',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+    ],
+    python_requires='>=3.7',
+    py_modules=['pyramco'],
+    entry_points='''
+        [console_scripts]
+        pyramco = pyramco:pyramco
+    '''
+)

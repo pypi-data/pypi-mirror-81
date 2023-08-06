29219f96be5af5c5e6992017244afaa9e92faca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:47 +0000

    Add package Command-Line-Menu/0.1/Command%20Line%20Menu-0.1.tar.gz

---
+++ packages/Command-Line-Menu/Command%20Line%20Menu-0.1.tar.gz/Command Line Menu-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Command Line Menu',
+      version='0.1',
+      description='A simple module to create a menu for command line programs.',
+      url='https://github.com/KrazyKirby99999/command-line-menu',
+      author='KrazyKirby99999',
+      author_email='krazykirby99999@example.com',
+      license='MIT',
+      packages=['command-line-menu'],
+      zip_safe=False)

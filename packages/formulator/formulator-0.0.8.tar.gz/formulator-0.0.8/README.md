3d407b5d78407c5eb0fb2aaa76ec4bd14d3ec2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:14 +0000

    Add package formulator/0.0.8/formulator-0.0.8.tar.gz

---
+++ packages/formulator/formulator-0.0.8.tar.gz/formulator-0.0.8/README.md
@@ -0,0 +1,18 @@
+Formulator
+==========
+
+Formulator validates properties of python objects with standard field validation and typing abstractions.
+Formulator can be written in declarative style or embedded into existing code via annotations.
+
+Basic Usage
+-----------
+```python
+class Test:
+
+    def add(self, x, y):
+        return x + y
+```
+
+Feedback or Contrib
+-------------------
+As of right now, Formulator remains a one-off pluggable library. It's development has no stable base or contributors aside from the current maintainer, but forward compatibility is a guarantee.
---
+++ packages/formulator/formulator-0.0.8.tar.gz/formulator-0.0.8/setup.py
@@ -0,0 +1,30 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+tests_require = ['pytest']
+
+long_description = """
+Formulator is a plug-and-play property validator for python objects.
+"""
+
+setup(
+    name="formulator",
+    version="0.0.8",
+    author="Trijeet Sethi",
+    author_email="trijeets@gmail.com",
+    maintainer="Trijeet Sethi",
+    maintainer_email="trijeets@gmail.com",
+    description="Formulator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/Trijeet/formulator",
+    packages=['formulator'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Framework :: Pytest",
+    ],
+    python_requires='>=3.6',
+)

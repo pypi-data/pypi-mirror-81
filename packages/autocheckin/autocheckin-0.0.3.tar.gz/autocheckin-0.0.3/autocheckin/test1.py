93b72eaa1998165ba90c24802cc2e60488cdf133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:11 +0000

    Add package autocheckin/0.0.3/autocheckin-0.0.3.tar.gz

---
+++ packages/autocheckin/autocheckin-0.0.3.tar.gz/autocheckin-0.0.3/autocheckin/test1.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8-*-
+
+# 所有方法
+from autocheckin import allcheckin, puyun, fpork
+
+if __name__ == '__main__':
+    puyun({"1": "2"})
+    fpork({"3": "4"})
+    allcheckin({"1": "2"}, {"3": "4"})

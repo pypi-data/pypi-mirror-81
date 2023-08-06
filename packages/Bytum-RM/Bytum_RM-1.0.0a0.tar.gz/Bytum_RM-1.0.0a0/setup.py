668ca6e4038cae39bb1b07a160e2fc69a9b9c186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:00 +0000

    Add package Bytum-RM/1.0.0a0/Bytum_RM-1.0.0a0.tar.gz

---
+++ packages/Bytum-RM/Bytum_RM-1.0.0a0.tar.gz/Bytum_RM-1.0.0a0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Bytum_RM',
+    version='1.0.0.a',
+    description='The ReMake edition of Bytum,it was made by the same Author,but the name has changed.',
+    packages=['Bytum_RM'], 
+    author='TunaFish2K',
+    author_email='tunafish2k@lieyan.space'
+)

3953131374135d490d7c9b1d9aa2c8d3b8ba7d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:36 +0000

    Add package django-verified-email-field/1.7.0/django-verified-email-field-1.7.0.tar.gz

---
+++ packages/django-verified-email-field/django-verified-email-field-1.7.0.tar.gz/django-verified-email-field-1.7.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name            = 'django-verified-email-field',
+    version         = '1.7.0',
+    description     = 'Simple model and form field to get verified email',
+    author          = 'Jakub Dorňák',
+    author_email    = 'jakub.dornak@misli.cz',
+    license         = 'BSD',
+    url             = 'https://github.com/misli/django-verified-email-field',
+    packages        = find_packages(),
+    include_package_data = True,
+    install_requires = [
+    ],
+    classifiers     = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Natural Language :: Czech',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)

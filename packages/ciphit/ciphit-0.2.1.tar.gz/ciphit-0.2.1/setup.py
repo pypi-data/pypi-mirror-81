fd87bbbe9657078d266086af0cc1a4429c74a3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:23 +0000

    Add package ciphit/0.2.1/ciphit-0.2.1.tar.gz

---
+++ packages/ciphit/ciphit-0.2.1.tar.gz/ciphit-0.2.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+
+with open('README.md', 'r') as readme_fp:
+    long_description = readme_fp.read()
+
+with open('requirements.txt', 'r') as req_fp:
+    required_libs = req_fp.readlines()
+
+
+setuptools.setup(
+    name='ciphit',
+    scripts=['ciphit/__main__.py'],
+    description='ciphit is a cryptography CLI-tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.2.1',
+    author='Sagar Kumar',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=setuptools.find_packages(exclude=['install.sh']),
+    install_requires=required_libs,
+    url='https://github.com/sgrkmr/ciphit',
+    keywords='cli commandline user-interface ui cryptography',
+    python_requires='>=3.7',
+    entry_points={
+    'console_scripts': [
+        'ciphit = ciphit.__main__:main',
+        ],
+    },
+)

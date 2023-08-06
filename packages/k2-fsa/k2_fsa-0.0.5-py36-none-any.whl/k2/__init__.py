da78637961243e126666ba53f569e33d066d6564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:11 +0000

    Add package k2-fsa/0.0.5/k2_fsa-0.0.5-py36-none-any.whl

---
+++ packages/k2-fsa/k2_fsa-0.0.5-py36-none-any.whl/k2/__init__.py
@@ -0,0 +1,12 @@
+from .array import Array
+from .fsa import Fsa
+from .symbol_table import SymbolTable
+from _k2 import Arc
+
+# please keep the list sorted
+__all__ = [
+    'Arc',
+    'Array',
+    'Fsa',
+    'SymbolTable',
+]

43834ba4ea5a630a215b0d99baca88c0fc4f9967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:38 +0000

    Add package edx-enterprise-data/2.1.3/edx-enterprise-data-2.1.3.tar.gz

---
+++ packages/edx-enterprise-data/edx-enterprise-data-2.1.3.tar.gz/edx-enterprise-data-2.1.3/enterprise_data/apps.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Enterprise Data Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class EnterpriseDataAppConfig(AppConfig):
+    """
+    Configuration for the enterprise Django application.
+    """
+
+    name = "enterprise_data"
---
+++ packages/edx-enterprise-data/edx-enterprise-data-2.1.3.tar.gz/edx-enterprise-data-2.1.3/enterprise_data/tests/mixins.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Mixins for enterprise data tests.
+"""
+
+
+from edx_rest_framework_extensions.auth.jwt.cookies import jwt_cookie_name
+from edx_rest_framework_extensions.auth.jwt.tests.utils import generate_jwt_token, generate_unversioned_payload
+
+
+class JWTTestMixin:
+    """
+    Mixin for JWT test related utils.
+    """
+
+    def set_jwt_cookie(self, system_wide_role='enterprise_admin', context='some_context'):
+        """
+        Set jwt token in cookies
+        """
+        role_data = '{system_wide_role}'.format(system_wide_role=system_wide_role)
+        if context is not None:
+            role_data += ':{context}'.format(context=context)
+
+        payload = generate_unversioned_payload(self.user)
+        payload.update({
+            'roles': [role_data]
+        })
+        jwt_token = generate_jwt_token(payload)
+
+        self.client.cookies[jwt_cookie_name()] = jwt_token

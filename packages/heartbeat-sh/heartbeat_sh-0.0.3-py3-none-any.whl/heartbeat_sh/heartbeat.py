08dc73ff4c2f86904e83420007cf912778216956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:33 +0000

    Add package heartbeat-sh/0.0.3/heartbeat_sh-0.0.3-py3-none-any.whl

---
+++ packages/heartbeat-sh/heartbeat_sh-0.0.3-py3-none-any.whl/heartbeat_sh/heartbeat.py
@@ -0,0 +1,25 @@
+from datetime import timedelta
+
+import requests
+
+
+class HeartbeatClient:
+    def __init__(self, subdomain: str):
+        self.subdomain = subdomain
+
+        self.proto = "https"
+        self.host = "heartbeat.sh"
+        self.base_url = f"{self.proto}://{self.subdomain}.{self.host}/"
+
+    def send_beat(self, name: str, warning_timeout: timedelta = None, error_timeout: timedelta = None):
+        query = ""
+        if warning_timeout is not None:
+            query += f"?warning={int(warning_timeout.total_seconds())}"
+        if error_timeout is not None:
+            query += "&" if len(query) > 0 else "?"
+            query += f"error={int(error_timeout.total_seconds())}"
+
+        requests.post(f"{self.base_url}beat/{name}{query}")
+
+    def delete_beat(self, name):
+        requests.delete(f"{self.base_url}beat/{name}")

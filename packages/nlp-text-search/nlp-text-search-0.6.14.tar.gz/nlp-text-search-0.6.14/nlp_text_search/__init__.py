c7351ffe1098f68a0a1d9fb69dee431658d2ef01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:23 +0000

    Add package nlp-text-search/0.6.14/nlp-text-search-0.6.14.tar.gz

---
+++ packages/nlp-text-search/nlp-text-search-0.6.14.tar.gz/nlp-text-search-0.6.14/nlp_text_search/__init__.py
@@ -0,0 +1,5 @@
+from .dists import Dist, LinearizedDist
+from .settings import create_settings
+from .search_engine import AbstractSearchEngine, VPTreeSearchEngine, BruteForceSearchEngine
+from .search_engine import SaveableVPTreeSearchEngine, BaseSearchEngine, DefaultSearchEngine
+from .models.bilstm_siamese_network import BiLSTMSiameseNetwork

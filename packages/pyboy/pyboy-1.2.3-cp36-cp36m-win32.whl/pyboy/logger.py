8947afdd9125ca1d2f90343f63d877788782947f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:16 +0000

    Add package pyboy/1.2.3/pyboy-1.2.3-cp36-cp36m-win32.whl

---
+++ packages/pyboy/pyboy-1.2.3-cp36-cp36m-win32.whl/pyboy/logger.py
@@ -0,0 +1,27 @@
+#
+# License: See LICENSE.md file
+# GitHub: https://github.com/baekalfen/PyBoy
+#
+
+import os
+import logging
+
+LOGLEVEL = os.environ.get("PYBOY_LOGLEVEL", "INFO")
+
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter(
+        "%(relativeCreated)-8d %(name)-30s %(levelname)-8s %(message)s"
+        )
+    )
+
+logger = logging.getLogger("pyboy")
+logger.setLevel(LOGLEVEL)
+logger.addHandler(handler)
+
+
+def log_level(level):
+    if level == "DISABLE":
+        logging.disable(level=logging.CRITICAL)
+    else:
+        logger.setLevel(level)

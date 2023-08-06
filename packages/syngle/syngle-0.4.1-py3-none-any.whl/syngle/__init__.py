ce3165e3cf73e2866e996268c7f2391d03f701af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:07 +0000

    Add package syngle/0.4.1/syngle-0.4.1-py3-none-any.whl

---
+++ packages/syngle/syngle-0.4.1-py3-none-any.whl/syngle/__init__.py
@@ -0,0 +1,8 @@
+"""Improve convenience of imports."""
+
+from syngle.decorator import singleton
+from syngle.metaclass import Singleton
+
+__version__ = "0.4.1"
+
+__all__ = ["Singleton", "singleton"]

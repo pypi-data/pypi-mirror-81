06c069823792be4f4e6c7cbdb3b902d5792f6e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package scrape-amazon/0.1.6/scrape_amazon-0.1.6.tar.gz

---
+++ packages/scrape-amazon/scrape_amazon-0.1.6.tar.gz/scrape_amazon-0.1.6/scrape_amazon/cli.py
@@ -0,0 +1,26 @@
+"""Defines various scraper functions."""
+import argparse
+from types import ModuleType
+
+from .util.scrape import scrape_reviews
+from .util.urlFunctions import construst_reviews_URL
+
+parser = argparse.ArgumentParser(description="Scrape Amazon Product Reviews")
+parser.add_argument("domain", type=str, help="amazon domain")
+parser.add_argument("product_id", type=str, help="product id for scraping")
+parser.add_argument("output_path", type=str, help="output_path for saving")
+
+
+def get_reviews_cli() -> ModuleType:
+
+    args = parser.parse_args()
+    print(f"[INFO] Scraping Reviews of Amazon ProductID - {args.product_id}")
+    """Scraper
+    Args:
+        product_id: Amazon Product ID.
+    Returns:
+        Scraped Dataframe
+    """
+    all_reviews_url = construst_reviews_URL(args.domain, args.product_id)
+    reviews = scrape_reviews(all_reviews_url)
+    reviews.to_csv(args.output_path)

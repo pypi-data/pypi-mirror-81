4ab7287870648c69ebe97a0cfee8de55991be6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:50 +0000

    Add package NewtonLaw/0.2/NewtonLaw-0.2-py3-none-any.whl

---
+++ packages/NewtonLaw/NewtonLaw-0.2-py3-none-any.whl/NewtonLaws/LawOfForce.py
@@ -0,0 +1,16 @@
+# Laws Of Conservation Of Momentum
+
+def Force(m,a):
+    f = m*a
+    print(f)
+
+def Mass(f,a):
+    m = f/a
+    print(m)
+
+def Acceleration(f,m):
+    a = f/m
+    print(a)
+
+def about():
+    print("this file have three functions i.e - Force, Mass, Acceleration. it uses formula of *f = m*a  * in force func parameters are m,a. in Mass - f,a. in acceleration - f,m.")
---
+++ packages/NewtonLaw/NewtonLaw-0.2-py3-none-any.whl/NewtonLaws/Maths.py
@@ -0,0 +1,21 @@
+# some maths operations
+
+def sqrt(num):
+    num = (num**0.5)
+    print(num)
+
+def sqPo(num,power):
+    num = (num**power)
+    print(num)
+
+def AOT(b,h):
+    a = (1/2*b*h)
+    print(a)
+
+def AOR(l,b):
+    a = l*b
+    print(a)
+
+def AOS(s):
+    a = s**2
+    print(a)

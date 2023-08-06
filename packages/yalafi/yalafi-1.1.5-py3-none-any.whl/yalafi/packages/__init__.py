3dda0362f6ee739f1ff311080fa8c2b20dcacd3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:19 +0000

    Add package yalafi/1.1.5/yalafi-1.1.5-py3-none-any.whl

---
+++ packages/yalafi/yalafi-1.1.5-py3-none-any.whl/yalafi/packages/__init__.py
@@ -0,0 +1,20 @@
+
+#   this list is used by yalafi/tex2txt.py
+#
+load_table = {
+    '*': [
+        'amsmath',
+        'amsthm',
+        'biblatex',
+        'glossaries',
+        'glossaries-extra',
+        'graphicx',
+        'hyperref',
+        'listings',
+        'pgfplots',
+        'tikz',
+        'xcolor',
+    ],
+    '': [
+    ],
+}

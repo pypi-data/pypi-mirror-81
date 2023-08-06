bc1d03ab67da69dc93ba72336d8cd2152b1c011c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:10 +0000

    Add package pyexcel-htmlr/0.6.0/pyexcel_htmlr-0.6.0-py2.py3-none-any.whl

---
+++ packages/pyexcel-htmlr/pyexcel_htmlr-0.6.0-py2.py3-none-any.whl/pyexcel_htmlr/__init__.py
@@ -0,0 +1,37 @@
+"""
+    pyexcel_htmlr
+    ~~~~~~~~~~~~~~~~~~~
+    Read html table using messytables
+    :copyright: (c) 2015-2017 by Onni Software Ltd & its contributors
+    :license: New BSD License
+"""
+from pyexcel_io.io import get_data as read_data
+from pyexcel_io.io import isstream
+from pyexcel_io.plugins import IOPluginInfoChainV2
+
+from ._version import __author__, __version__  # noqa
+
+__FILE_TYPE__ = "html"
+IOPluginInfoChainV2(__name__).add_a_reader(
+    relative_plugin_class_path="htmlr.HtmlPageInContent",
+    locations=["content"],
+    file_types=[__FILE_TYPE__, "htm"],
+    stream_type="text",
+).add_a_reader(
+    relative_plugin_class_path="htmlr.HtmlPageInStream",
+    locations=["memory"],
+    file_types=[__FILE_TYPE__, "htm"],
+    stream_type="text",
+).add_a_reader(
+    relative_plugin_class_path="htmlr.HtmlPageInFile",
+    locations=["file"],
+    file_types=[__FILE_TYPE__, "htm"],
+    stream_type="text",
+)
+
+
+def get_data(afile, file_type=None, **keywords):
+    """standalone module function for reading module supported file type"""
+    if isstream(afile) and file_type is None:
+        file_type = __FILE_TYPE__
+    return read_data(afile, file_type=file_type, **keywords)

b446d5f1850d009ab017439686202733afd9e907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:03 +0000

    Add package bg-atlasapi/1.0.0/bg_atlasapi-1.0.0-py3-none-any.whl

---
+++ packages/bg-atlasapi/bg_atlasapi-1.0.0-py3-none-any.whl/bg_atlasapi/bg_atlas.py
@@ -0,0 +1,194 @@
+from pathlib import Path
+import tarfile
+import requests
+
+from rich import print as rprint
+
+from bg_atlasapi import utils, config, core, descriptors
+
+
+COMPRESSED_FILENAME = "atlas.tar.gz"
+
+
+def _version_tuple_from_str(version_str):
+    return tuple([int(n) for n in version_str.split(".")])
+
+
+def _version_str_from_tuple(version_tuple):
+    return f"{version_tuple[0]}.{version_tuple[1]}"
+
+
+class BrainGlobeAtlas(core.Atlas):
+    """Add remote atlas fetching and version comparison functionalities
+    to the core Atlas class.
+
+    Parameters
+    ----------
+    atlas_name : str
+        Name of the atlas to be used.
+    brainglobe_dir : str or Path object
+        Default folder for brainglobe downloads.
+    interm_download_dir : str or Path object
+        Folder to download the compressed file for extraction.
+    check_latest : bool (optional)
+        If true, check if we have the most recent atlas (default=True). Set
+        this to False to avoid waiting for remote server response on atlas
+        instantiation and to suppress warnings.
+    print_authors : bool (optional)
+        If true, disable default listing of the atlas reference.
+
+    """
+
+    atlas_name = None
+    _remote_url_base = descriptors.remote_url_base
+
+    def __init__(
+        self,
+        atlas_name,
+        brainglobe_dir=None,
+        interm_download_dir=None,
+        check_latest=True,
+        print_authors=True,
+    ):
+        self.atlas_name = atlas_name
+
+        # Read BrainGlobe configuration file:
+        conf = config.read_config()
+
+        # Use either input locations or locations from the config file,
+        # and create directory if it does not exist:
+        for dir, dirname in zip(
+            [brainglobe_dir, interm_download_dir],
+            ["brainglobe_dir", "interm_download_dir"],
+        ):
+            if dir is None:
+                dir = conf["default_dirs"][dirname]
+
+            # If the default folder does not exist yet, make it:
+            dir_path = Path(dir)
+            dir_path.mkdir(exist_ok=True)
+            setattr(self, dirname, dir_path)
+
+        # Look for this atlas in local brainglobe folder:
+        if self.local_full_name is None:
+            if self.remote_version is None:
+                raise ValueError(f"{atlas_name} is not a valid atlas name!")
+
+            rprint(
+                f"[magenta2]Bgatlas_api: {self.atlas_name} not found locally. Downloading...[magenta2]"
+            )
+            self.download_extract_file()
+
+        # Instantiate after eventual download:
+        super().__init__(self.brainglobe_dir / self.local_full_name)
+
+        if check_latest:
+            self.check_latest_version()
+        if print_authors:
+            print(self)
+
+    @property
+    def local_version(self):
+        """If atlas is local, return actual version of the downloaded files;
+        Else, return none.
+        """
+        full_name = self.local_full_name
+
+        if full_name is None:
+            return None
+
+        return _version_tuple_from_str(full_name.split("_v")[-1])
+
+    @property
+    def remote_version(self):
+        """Remote version read from GIN conf file. If we are offline, return
+        None.
+        """
+        remote_url = self._remote_url_base.format("last_versions.conf")
+
+        # Grasp remote version if a connection is available:
+        try:
+            versions_conf = utils.conf_from_url(remote_url)
+        except requests.ConnectionError:
+            return
+
+        try:
+            return _version_tuple_from_str(
+                versions_conf["atlases"][self.atlas_name]
+            )
+        except KeyError:
+            return None
+
+    @property
+    def local_full_name(self):
+        """As we can't know the local version a priori, search candidate dirs
+        using name and not version number. If none is found, return None.
+        """
+        pattern = f"{self.atlas_name}_v*"
+        candidate_dirs = list(self.brainglobe_dir.glob(pattern))
+
+        # If multiple folders exist, raise error:
+        if len(candidate_dirs) > 1:
+            raise FileExistsError(
+                f"Multiple versions of atlas {self.atlas_name} in {self.brainglobe_dir}"
+            )
+        # If no one exist, return None:
+        elif len(candidate_dirs) == 0:
+            return
+        # Else, return actual name:
+        else:
+            return candidate_dirs[0].name
+
+    @property
+    def remote_url(self):
+        """Format complete url for download."""
+
+        if self.remote_version is not None:
+            name = f"{self.atlas_name}_v{self.remote_version[0]}.{self.remote_version[1]}.tar.gz"
+
+            return self._remote_url_base.format(name)
+
+    def download_extract_file(self):
+        """Download and extract atlas from remote url."""
+        utils.check_internet_connection()
+
+        # Get path to folder where data will be saved
+        destination_path = self.interm_download_dir / COMPRESSED_FILENAME
+
+        # Try to download atlas data
+        utils.retrieve_over_http(self.remote_url, destination_path)
+
+        # Uncompress in brainglobe path:
+        tar = tarfile.open(destination_path)
+        tar.extractall(path=self.brainglobe_dir)
+        tar.close()
+
+        destination_path.unlink()
+
+    def check_latest_version(self):
+        """Checks if the local version is the latest available
+        and prompts the user to update if not.
+        """
+        if self.remote_version is None:  # in this case, we are offline
+            return
+        local = _version_str_from_tuple(self.local_version)
+        online = _version_str_from_tuple(self.remote_version)
+
+        if local != online:
+            rprint(
+                f"[b][magenta2]Bg_atlasapi[/b]: [b]{self.atlas_name}[/b] version [b]{local}[/b] is not the latest available ([b]{online}[/b]). "
+                + "To update the atlas run in the terminal:[/magenta2]\n"
+                + f"    [gold1]brainglobe update -a {self.atlas_name}[/gold1]"
+            )
+            return False
+        return True
+
+    def __repr__(self):
+        """Fancy print for the atlas providing authors information."""
+        meta = self.metadata
+        name_split = self.atlas_name.split("_")
+        pretty_name = "{} {} atlas (res. {})".format(*name_split)
+        pretty_string = (
+            f"{pretty_name}\nFrom: {meta['atlas_link']} ({meta['citation']} )"
+        )
+        return pretty_string
---
+++ packages/bg-atlasapi/bg_atlasapi-1.0.0-py3-none-any.whl/bg_atlasapi/list_atlases.py
@@ -0,0 +1,158 @@
+from rich.table import Table, box, Style
+from rich import print as rprint
+
+from bg_atlasapi import config, utils, descriptors
+from bg_atlasapi.bg_atlas import BrainGlobeAtlas
+
+
+"""
+    Some functionality to list all available and downloaded brainglobe atlases
+"""
+
+
+def get_downloaded_atlases(with_version=False):
+    """Get a list of all the downloaded atlases and their version.
+
+    Returns
+    -------
+    list
+        A list of tuples with the locally available atlases and their version
+    """
+
+    # Get brainglobe directory:
+    brainglobe_dir = config.get_brainglobe_dir()
+
+    return [
+        f.name.split("_v")[0]
+        for f in brainglobe_dir.glob("*_*_*_v*")
+        if f.is_dir()
+    ]
+
+
+def get_local_atlas_version(atlas_name):
+    """Get version of a downloaded available atlas.
+
+    Arguments
+    ---------
+    atlas_name : str
+        Name of the atlas.
+
+    Returns
+    -------
+    str
+        Version of atlas.
+    """
+
+    brainglobe_dir = config.get_brainglobe_dir()
+    return [
+        f.name.split("_v")[1]
+        for f in brainglobe_dir.glob(f"*{atlas_name}*")
+        if f.is_dir()
+    ][0]
+
+
+def get_atlases_lastversions():
+    """
+    Returns
+    -------
+    dict
+        A dictionary with metadata about already installed atlases.
+    """
+
+    # Read from URL all available last versions:
+    available_atlases = utils.conf_from_url(
+        descriptors.remote_url_base.format("last_versions.conf")
+    )
+    available_atlases = dict(available_atlases["atlases"])
+
+    # Get downloaded atlases looping over folders in brainglobe directory:
+    atlases = {}
+    for name in get_downloaded_atlases():
+        if name in available_atlases.keys():
+            local_version = get_local_atlas_version(name)
+            atlases[name] = dict(
+                downloaded=True,
+                local=name,
+                version=local_version,
+                latest_version=str(available_atlases[name]),
+                updated=str(available_atlases[name]) == local_version,
+            )
+    return atlases
+
+
+def show_atlases(show_local_path=False):
+    """Prints a formatted table with the name and version of local (downloaded)
+    and online (available) atlases. To do so, dowload info on
+    the latest atlas version and compares it with what it's stored
+    locally.
+
+    Arguments
+    ---------
+    show_local_path : bool
+        If true, local path of the atlases are in the table with the rest
+        (optional, default=False).
+
+    """
+
+    # Get available_atlases:
+    available_atlases = utils.conf_from_url(
+        BrainGlobeAtlas._remote_url_base.format("last_versions.conf")
+    )
+    available_atlases = dict(available_atlases["atlases"])
+
+    # Get local atlases:
+    atlases = get_atlases_lastversions()
+
+    # Get atlases not yet downloaded:
+    for atlas in available_atlases.keys():
+
+        if atlas not in atlases.keys():
+            atlases[str(atlas)] = dict(
+                downloaded=False,
+                local="[red]---[/red]",
+                version="[red]---[/red]",
+                latest_version=str(available_atlases[atlas]),
+                updated=None,
+            )
+
+    # Print table:
+    table = Table(
+        show_header=True,
+        header_style="bold green",
+        title="\n\nBrainglobe Atlases",
+        expand=False,
+        box=box.ROUNDED,
+    )
+
+    table.add_column("Name", no_wrap=True, width=32)
+    table.add_column("Downloaded", justify="center")
+    table.add_column("Local version", justify="center")
+    table.add_column("Latest version", justify="center")
+    if show_local_path:
+        table.add_column("Local path")
+
+    for n, (atlas, info) in enumerate(atlases.items()):
+        if info["downloaded"]:
+            downloaded = "[green]:heavy_check_mark:[/green]"
+        else:
+            downloaded = "[red]---[/red]"
+
+        row = [
+            "[b]" + atlas + "[/b]",
+            downloaded,
+            info["version"],
+            info["latest_version"],
+        ]
+
+        if show_local_path:
+            row.append(info["local"])
+
+        table.add_row(*row)
+
+        if info["updated"] is not None:
+            if not info["updated"]:
+                table.row_styles.append(
+                    Style(color="black", bgcolor="magenta2")
+                )
+
+    rprint(table)

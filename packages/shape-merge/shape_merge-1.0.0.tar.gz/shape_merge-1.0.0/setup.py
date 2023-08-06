753fa876ffcb4962b84a61c2f249118054edf402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:05 +0000

    Add package shape-merge/1.0.0/shape_merge-1.0.0.tar.gz

---
+++ packages/shape-merge/shape_merge-1.0.0.tar.gz/shape_merge-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = [
+    "shapely == 1.7.0",
+    "geojson >= 2.5.0",
+]
+
+setup(
+    name="shape_merge",
+    version="1.0.0",
+    author="Fuzail Palnak",
+    author_email="fuzailpalnak@gmail.com",
+    url="https://github.com/cypherics/ShapeMerge",
+    description="Library that Merges Geo Spatial Shapes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    python_requires='~=3.3',
+    install_requires=install_requires,
+    keywords=["GIS, Merge, Shapely, Fiona, Polygon, MultiPolygon, Geometry"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+    ],
+)

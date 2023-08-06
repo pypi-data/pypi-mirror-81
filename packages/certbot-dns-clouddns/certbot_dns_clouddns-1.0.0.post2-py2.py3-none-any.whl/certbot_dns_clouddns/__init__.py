fde1db19a2b5820c14112aa9a8ac3f4190dda1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:41 +0000

    Add package certbot-dns-clouddns/1.0.0.post2/certbot_dns_clouddns-1.0.0.post2-py2.py3-none-any.whl

---
+++ packages/certbot-dns-clouddns/certbot_dns_clouddns-1.0.0.post2-py2.py3-none-any.whl/certbot_dns_clouddns/__init__.py
@@ -0,0 +1,5 @@
+"""
+The `~certbot_dns_clouddns.dns_clouddns` plugin automates the process of
+completing a ``dns-01`` challenge (`~acme.challenges.DNS01`) by creating, and
+subsequently removing, TXT records using the CloudDNS REST API.
+"""

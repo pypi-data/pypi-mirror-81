07924c7f8ac2f07f83c7e9c067c50104399ce4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:27 +0000

    Add package meshed/0.0.2/meshed-0.0.2-py2.py3-none-any.whl

---
+++ packages/meshed/meshed-0.0.2-py2.py3-none-any.whl/meshed/__init__.py
@@ -0,0 +1,9 @@
+"""Tools that enable operations on graphs where grapsh are represented by an adjacency Mapping.
+
+"""
+__version__ = "0.0.2"
+
+from meshed.itools import (
+    edges,
+    nodes,
+)

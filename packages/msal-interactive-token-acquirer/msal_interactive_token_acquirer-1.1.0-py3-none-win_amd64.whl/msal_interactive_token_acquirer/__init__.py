f29b7729d8ca2771099c3fe3d7ed17becc6b5d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:49 +0000

    Add package msal-interactive-token-acquirer/1.1.0/msal_interactive_token_acquirer-1.1.0-py3-none-win_amd64.whl

---
+++ packages/msal-interactive-token-acquirer/msal_interactive_token_acquirer-1.1.0-py3-none-win_amd64.whl/msal_interactive_token_acquirer/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.1.0"
+__all__ = ["MsalInteractiveTokenAcquirer", "MsalError"]
+
+from .msal_interactive_token_acquirer import (MsalInteractiveTokenAcquirer,
+                                              MsalError)

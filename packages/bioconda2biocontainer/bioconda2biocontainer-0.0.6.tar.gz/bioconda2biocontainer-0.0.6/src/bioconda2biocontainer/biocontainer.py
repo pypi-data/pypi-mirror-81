7e7fb9aa3f9dda463ee63ee0a2cd6b3beb815dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:04 +0000

    Add package bioconda2biocontainer/0.0.6/bioconda2biocontainer-0.0.6.tar.gz

---
+++ packages/bioconda2biocontainer/bioconda2biocontainer-0.0.6.tar.gz/bioconda2biocontainer-0.0.6/src/bioconda2biocontainer/biocontainer.py
@@ -0,0 +1,75 @@
+import json
+
+import requests
+
+base_url = "http://api.biocontainers.pro/ga4gh/trs/v2/tools"
+
+
+def request_url_to_dict(url):
+    response = requests.get('{}'.format(url))
+    if response.status_code == 200:
+        return json.loads(response.text)
+    return response.status_code
+
+
+def find_package_by_name(package_name):
+    data = request_url_to_dict('{0}/{1}'.format(base_url, package_name))
+    if type(data) == int:
+        return data
+    if data and 'versions' in data:
+        versions = data['versions']
+        data['versions'] = sorted(versions, key=lambda i: i['meta_version'], reverse=True)
+    return data
+
+
+def find_package_by_term(search_term):
+    data = request_url_to_dict('{0}?all_fields_search={1}'.format(base_url, search_term))
+    return data
+
+
+def filter_by_container_registry(container_type, registry_host, i):
+    if registry_host and container_type and \
+            i['registry_host'].startswith(registry_host) and \
+            container_type == i['image_type']:
+        return True
+    if not registry_host and container_type and \
+            'image_type' in i and \
+            container_type == i['image_type']:
+        return True
+    if registry_host and not container_type and \
+            i['registry_host'].startswith(registry_host):
+        return True
+    if not registry_host and not container_type:
+        return True
+    return False
+
+
+def find_latest_image(package_name, package_version, all=False, sort_by_size=False,
+                      sort_by_download=False, container_type=None, registry_host=None):
+    data = request_url_to_dict('{0}/{1}/versions/{1}-{2}'.format(
+        base_url, package_name, package_version))
+    if type(data) == dict or type(data) == list:
+        versions = []
+        if data and 'images' in data:
+            for i in data['images']:
+                if filter_by_container_registry(container_type, registry_host, i):
+                    if 'downloads' not in i:
+                        i['downloads'] = 0
+                    if 'updated' not in i:
+                        i['updated'] = ''
+                    if 'size' not in i:
+                        i['size'] = 0
+                    versions.append(i)
+        if sort_by_size:
+            versions = sorted(versions, key=lambda i: i['size'])
+        elif sort_by_download:
+            versions = sorted(versions, key=lambda i: i['downloads'], reverse=True)
+        else:
+            versions = sorted(versions, key=lambda i: i['updated'], reverse=True)
+        if all:
+            return versions
+        if versions:
+            return versions[0]
+        else:
+            return None
+    return data

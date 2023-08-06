74e0cf28fe7e62d322d148057de0d708d7112037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:59 +0000

    Add package paypayopa/0.7.1/paypayopa-0.7.1-py3-none-any.whl

---
+++ packages/paypayopa/paypayopa-0.7.1-py3-none-any.whl/paypayopa/constants/url.py
@@ -0,0 +1,7 @@
+class URL(object):
+    SANDBOX_BASE_URL = 'https://stg-api.sandbox.paypay.ne.jp'
+    PRODUCTION_BASE_URL = 'https://api.paypay.ne.jp'
+    CODE = "/v2/codes"
+    PAYMENT = "/v2/payments"
+    ACCOUNT_LINK = "/v1/qr/sessions"
+    PENDING_PAYMENT = "/v1/requestOrder"
---
+++ packages/paypayopa/paypayopa-0.7.1-py3-none-any.whl/paypayopa/resources/__init__.py
@@ -0,0 +1,13 @@
+from .code import Code
+from .payment import Payment
+from .account import Account
+from .preauth import Preauth
+from .pending import Pending
+
+__all__ = [
+        'Code',
+        'Payment',
+        'Account',
+        'Preauth',
+        'Pending',
+]

3949eadfd7a4ac0c0e68e684a183a17960391573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package django-trips/0.2.6.1/django_trips-0.2.6.1-py3-none-any.whl

---
+++ packages/django-trips/django_trips-0.2.6.1-py3-none-any.whl/trips/api/urls.py
@@ -0,0 +1,12 @@
+"""Urls for trips app"""
+from django.urls import path
+
+from trips.api import views
+
+app_name = "trips-api"
+
+urlpatterns = [
+    path('trips/', views.TripListCreateAPIView.as_view(), name="trips"),
+    path('trip/<int:pk>/', views.TripRetrieveUpdateDestroyAPIView.as_view(), name="trip-item"),
+    path('trip/<slug:slug>/', views.TripRetrieveUpdateDestroyAPIView.as_view(), name="trip-item-slug"),
+]

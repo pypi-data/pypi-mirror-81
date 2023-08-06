57b15d480fd1fe109c83d0d9d24a2f2a91fef7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:57 +0000

    Add package feigelib/1.0.0/feigelib-1.0.0.tar.gz

---
+++ packages/feigelib/feigelib-1.0.0.tar.gz/feigelib-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='feigelib',  # 包名
+      version='1.0.0',  # 版本号
+      description='',
+      long_description='',
+      author='tjs',
+      author_email='3357135715@qq.com',
+      url='https://www.rajh.com',
+      license='',
+      install_requires=[],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Utilities'
+      ],
+      keywords='',
+      packages=find_packages('src'),  # 必填，就是包的代码主目录
+      package_dir={'': 'src'},  # 必填
+      include_package_data=True,
+      )

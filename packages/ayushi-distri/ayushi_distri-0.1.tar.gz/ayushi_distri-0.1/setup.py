5cc8dabe6244861f4466965bb3feadbbfced0149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:05 +0000

    Add package ayushi-distri/0.1/ayushi_distri-0.1.tar.gz

---
+++ packages/ayushi-distri/ayushi_distri-0.1.tar.gz/ayushi_distri-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ayushi_distri',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ayushi_distri'],
+      zip_safe=False)

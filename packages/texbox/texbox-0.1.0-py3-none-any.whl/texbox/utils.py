a53381635486c5636d9255584d9cd6ed702f84a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:02 +0000

    Add package texbox/0.1.0/texbox-0.1.0-py3-none-any.whl

---
+++ packages/texbox/texbox-0.1.0-py3-none-any.whl/texbox/utils.py
@@ -0,0 +1,32 @@
+import json
+from argparse import HelpFormatter
+
+
+def jprint(obj, indent=4, ensure_ascii=False):
+    print(json.dumps(obj, indent=indent, ensure_ascii=ensure_ascii))
+
+
+def split_irregular_list_of_lists(lst):
+    strs, lists = [], []
+
+    for item in lst:
+        (lists, strs)[isinstance(item, str)].append(item)
+
+    return strs, lists
+
+
+def natural_sort_list_of_lists(lst, inner_idx, desc):
+    return sorted(lst, key=lambda item: item[inner_idx].casefold(), reverse=desc)
+
+
+# Source: https://stackoverflow.com/questions/18275023/dont-show-long-options-twice-in-print-help-from-argparse
+class CustomHelpFormatter(HelpFormatter):
+    # `-s, --long ARGS` instead of `-s ARGS, --long ARGS`
+    def _format_action_invocation(self, action):
+        if not action.option_strings or action.nargs == 0:
+            return super()._format_action_invocation(action)
+        else:
+            default = self._get_default_metavar_for_optional(action)
+            args_string = self._format_args(action, default)
+
+            return ", ".join(action.option_strings) + " " + args_string

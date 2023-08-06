05281e015127939aec92ccce2c912b0c71a5b86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:57 +0000

    Add package django-rosetta/0.9.5/django-rosetta-0.9.5.tar.gz

---
+++ packages/django-rosetta/django-rosetta-0.9.5.tar.gz/django-rosetta-0.9.5/testproject/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^rosetta/', include('rosetta.urls'))
+]
+
+urlpatterns += staticfiles_urlpatterns()

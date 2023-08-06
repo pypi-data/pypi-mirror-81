b0616f7e40e595fdb4ab872061553fc7d65e2861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:52 +0000

    Add package archivy/0.5.0/archivy-0.5.0.tar.gz

---
+++ packages/archivy/archivy-0.5.0.tar.gz/archivy-0.5.0/archivy/__init__.py
@@ -0,0 +1,49 @@
+import logging
+import sys
+from pathlib import Path
+from threading import Thread
+
+import elasticsearch
+from flask import Flask
+import pypandoc
+
+from archivy import extensions
+from archivy.api import api_bp
+from archivy.check_changes import run_watcher
+from archivy.config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.logger.setLevel(logging.INFO)
+app.register_blueprint(api_bp, url_prefix='/api')
+
+
+# check if pandoc is installed, otherwise install
+try:
+    pypandoc.get_pandoc_version()
+except OSError:
+    app.logger.info("Installing pandoc")
+    pypandoc.download_pandoc()
+
+# create dir that will hold data if it doesn't already exist
+DIRNAME = app.config["APP_PATH"] + "/data/"
+Path(DIRNAME).mkdir(parents=True, exist_ok=True)
+
+if app.config["ELASTICSEARCH_ENABLED"]:
+    with app.app_context():
+        es = extensions.get_elastic_client()
+        try:
+            print(
+                es.indices.create(
+                    index=app.config["INDEX_NAME"],
+                    body=app.config["ELASTIC_CONF"]))
+        except elasticsearch.ElasticsearchException:
+            app.logger.info("Elasticsearch index already created")
+
+# prevent pytest from hanging because of running thread
+if 'pytest' not in sys.argv[0]:
+    Thread(target=run_watcher, args=[app]).start()
+
+app.jinja_options["extensions"].append("jinja2.ext.do")
+
+from archivy import routes  # noqa:
---
+++ packages/archivy/archivy-0.5.0.tar.gz/archivy-0.5.0/archivy/data.py
@@ -0,0 +1,135 @@
+import os
+import glob
+import platform
+import subprocess
+from pathlib import Path
+from shutil import rmtree
+
+import frontmatter
+from flask import current_app
+from werkzeug.utils import secure_filename
+
+
+SEP = os.path.sep
+
+
+# FIXME: ugly hack to make sure the app path is evaluated at the right time
+def get_data_dir():
+    return os.path.join(current_app.config['APP_PATH'], "data" + SEP)
+
+
+# struct to create tree like file-structure
+class Directory:
+    def __init__(self, name):
+        self.name = name
+        self.child_files = []
+        self.child_dirs = {}
+
+
+FILE_GLOB = "-[0-9][0-9]-[0-9][0-9]-[0-9][0-9]-*"
+
+
+def get_by_id(dataobj_id):
+    results = glob.glob(f"{get_data_dir()}**{SEP}{dataobj_id}{FILE_GLOB}", recursive=True)
+    return results[0] if results else None
+
+
+def get_items(collections=[], path="", structured=True, json_format=False):
+    datacont = Directory("root") if structured else []
+    if structured:
+        for filename in glob.glob(get_data_dir() + path + f"**{SEP}*",
+                                  recursive=True):
+            paths = filename.split(f"{SEP}data{SEP}")[1].split(SEP)
+
+            if filename.endswith(".md"):
+                data = frontmatter.load(filename)
+            data = frontmatter.load(
+                filename) if filename.endswith(".md") else None
+
+            current_dir = datacont
+
+            # iterate through paths
+            for segment in paths:
+                if segment.endswith(".md") or segment.endswith(".epub"):
+                    current_dir.child_files.append(data)
+                else:
+                    # directory has not been saved in tree yet
+                    if segment not in current_dir.child_dirs:
+                        current_dir.child_dirs[segment] = Directory(segment)
+                    current_dir = current_dir.child_dirs[segment]
+    else:
+        for filename in glob.glob(f"{get_data_dir()}{path}**{SEP}[0-9]*{FILE_GLOB}",
+                                  recursive=True):
+            data = frontmatter.load(filename)
+            if len(collections) == 0 or \
+                    any([collection == data["type"]
+                        for collection in collections]):
+                if json_format:
+                    dict_dataobj = data.__dict__
+                    # remove unnecessary yaml handler
+                    dict_dataobj.pop("handler")
+                    datacont.append(dict_dataobj)
+                else:
+                    datacont.append(data)
+    return datacont
+
+
+def create(contents, title, path=""):
+    path_to_md_file = os.path.join(
+        get_data_dir(), path, "{}.md".format(secure_filename(title)))
+    with open(path_to_md_file, "w") as file:
+        file.write(contents)
+
+    return path_to_md_file
+
+
+def get_item(dataobj_id):
+    file = get_by_id(dataobj_id)
+    if file:
+        data = frontmatter.load(file)
+        data["fullpath"] = file
+        return data
+    return None
+
+
+def delete_item(dataobj_id):
+    file = get_by_id(dataobj_id)
+
+    if file:
+        os.remove(file)
+
+
+def get_dirs():
+    dirnames = glob.glob(get_data_dir() + f"**{SEP}*", recursive=True)
+
+    # parse dirnames into relative paths
+    dirnames = [name.split("{SEP}data{SEP}")[1]
+                for name in dirnames if not name.endswith(".md")]
+
+    # append name for root dir
+    dirnames.append("not classified")
+    return dirnames
+
+
+def create_dir(name):
+    sanitized_name = SEP.join([secure_filename(pathname)
+                               for pathname in name])
+    Path(get_data_dir() + sanitized_name).mkdir(parents=True)
+    return sanitized_name
+
+
+def delete_dir(name):
+    try:
+        rmtree(get_data_dir() + name)
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def open_file(path):
+    if platform.system() == "Windows":
+        os.startfile(path)
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", path])
+    else:
+        subprocess.Popen(["xdg-open", path])

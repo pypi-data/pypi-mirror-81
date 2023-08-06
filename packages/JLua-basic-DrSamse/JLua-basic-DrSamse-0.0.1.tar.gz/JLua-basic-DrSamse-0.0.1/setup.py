92a4befe878b2e97c38a7cb69008b1849741de29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:51 +0000

    Add package JLua-basic-DrSamse/0.0.1/JLua-basic-DrSamse-0.0.1.tar.gz

---
+++ packages/JLua-basic-DrSamse/JLua-basic-DrSamse-0.0.1.tar.gz/JLua-basic-DrSamse-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JLua-basic-DrSamse",
+    version="0.0.1",
+    author="Samuel Nösslböck",
+    author_email="samuel.noesslboeck@gmail.com",
+    description="A Package for Json-like Lua usement",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DrSamse/JLua",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

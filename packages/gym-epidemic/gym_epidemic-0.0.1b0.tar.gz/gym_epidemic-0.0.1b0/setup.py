e1ef4a7fe39b93118f5d2e4631ee02a6a0222fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:52 +0000

    Add package gym-epidemic/0.0.1b0/gym_epidemic-0.0.1b0.tar.gz

---
+++ packages/gym-epidemic/gym_epidemic-0.0.1b0.tar.gz/gym_epidemic-0.0.1b0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+  name = 'gym_epidemic',        
+  packages = ['gym_epidemic'],   
+  version = '0.0.1.b',
+  license='BSD-3',  
+  description = 'An OpenAI Gym to benchmark AI Reinforcement Learning algorithms in epidemic control problems',  
+  author = 'Marcus Lapeyrolerie',                 
+  author_email = 'marcuslapeyrolerie@me.com',      
+  url = 'https://github.com/boettiger-lab/gym_epidemic',   
+  download_url = 'https://github.com/boettiger-lab/gym_epidemic/releases/tag/v0.0.3',
+  keywords = ['Reinforcement Learning', 'Epidemic Control', 'Epidemics', 
+              "COVID-19", "AI", "stable-baselines", "OpenAI Gym", 
+              "Artificial Intelligence", "Epidemiology"],
+  install_requires=[ 
+          'gym',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)

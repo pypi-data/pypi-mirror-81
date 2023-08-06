0c4018023c0a843705fc53e3adf9d4cf787a7c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:47 +0000

    Add package tox-ini-fmt/0.4.0/tox_ini_fmt-0.4.0-py3-none-any.whl

---
+++ packages/tox-ini-fmt/tox_ini_fmt-0.4.0-py3-none-any.whl/tox_ini_fmt/formatter/section_order.py
@@ -0,0 +1,53 @@
+import itertools
+from configparser import ConfigParser
+from typing import Dict, List, Optional
+
+from .tox_section import order_env_list
+
+
+def order_sections(parser: ConfigParser, pin_toxenvs: List[str]) -> None:
+    """
+    Start with tox, then testenv. The testenv elements follow the order within envlist. Then all other testenv elements,
+    and end it with any other sections present in the file (e.g. pytest/mypy configuration).
+    """
+    order = ["tox", "testenv"]
+    order.extend(f"testenv:{env}" for env in load_and_order_env_list(parser, pin_toxenvs))
+    order.extend(s for s in parser.sections() if s not in order and s.startswith("testenv:"))
+    order.extend(s for s in parser.sections() if s not in order and not s.startswith("testenv:"))
+    sections: Dict[str, Dict[str, str]] = {}
+    for section in order:
+        if parser.has_section(section):
+            sections[section] = dict(parser[section])
+            parser.pop(section)
+    for k, v in sections.items():
+        parser[k] = v
+
+
+def load_and_order_env_list(parser: ConfigParser, pin_toxenvs: List[str]) -> List[str]:
+    if not parser.has_section("tox"):
+        return []
+    if "envlist" not in parser["tox"]:
+        return []
+    result = explode_env_list(parser["tox"]["envlist"])
+    missing = [e for e in pin_toxenvs if e not in result]
+    if missing:
+        raise RuntimeError(f"missing tox environment(s) to pin {', '.join(missing)}")
+    order_env_list(result, pin_toxenvs)
+    return result
+
+
+def explode_env_list(env_list: str) -> List[str]:
+    result: List[str] = []
+    for entry in env_list.split("\n"):
+        entry = entry.strip()
+        if entry:
+            parts = []
+            for part in entry.split("-"):
+                if part[0] == "{" and part[-1] == "}":
+                    sub_part = part[1:-1]
+                else:
+                    sub_part = part
+                sub_parts = [i.strip() for i in sub_part.split(",")]
+                parts.append(sub_parts)
+            result.extend("-".join(i).strip("-") for i in itertools.product(*parts))
+    return result
---
+++ packages/tox-ini-fmt/tox_ini_fmt-0.4.0-py3-none-any.whl/tox_ini_fmt/formatter/util.py
@@ -0,0 +1,38 @@
+import re
+from configparser import ConfigParser
+from typing import Callable, Mapping, Optional
+
+
+def to_boolean(payload: str) -> str:
+    return "true" if payload.lower() == "true" else "false"
+
+
+def fix_and_reorder(parser: ConfigParser, name: str, fix_cfg: Mapping[str, Callable[[str], str]]) -> None:
+    section = parser[name]
+    for key, fix in fix_cfg.items():
+        if key in section:
+            section[key] = fix(section[key])
+    # reorder keys within section
+    new_section = {k: section.pop(k) for k in fix_cfg.keys() if k in section}
+    new_section.update(sorted(section.items()))  # sort any remaining keys
+    parser[name] = new_section
+
+
+RE_ITEM_REF = re.compile(
+    r"""
+        (?<!\\)[{]
+        (?:(?P<sub_type>[^[:{}]+):)?    # optional sub_type for special rules
+        (?P<substitution_value>(?:\[[^,{}]*\])?[^:,{}]*)  # substitution key
+        (?::(?P<default_value>[^{}]*))?   # default value
+        [}]
+        """,
+    re.VERBOSE,
+)
+
+
+def is_substitute(value: str) -> bool:
+    match = RE_ITEM_REF.match(value)
+    if match:
+        sub_key = match.group("substitution_value")
+        return sub_key.startswith("[") and "]" in sub_key
+    return False

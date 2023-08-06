485827dff6860dc4a0cbd032c2fec6a4d046bf20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:41 +0000

    Add package pype-cli/0.5.4/pype_cli-0.5.4-py3-none-any.whl

---
+++ packages/pype-cli/pype_cli-0.5.4-py3-none-any.whl/pype/template_minimal.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Not documented yet."""
+
+import click
+
+import pype
+
+
+@click.command(name=pype.fname_to_name(__file__), help=__doc__)
+def main():
+    """Script's main entry point."""
+    pass

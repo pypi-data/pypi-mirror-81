62453b4bc67da7d923808fc4fb84e6de135229be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:55 +0000

    Add package python-hermes/0.0.3/python-hermes-0.0.3.tar.gz

---
+++ packages/python-hermes/python-hermes-0.0.3.tar.gz/python-hermes-0.0.3/hermes/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+
+__version__ = '0.0.3'
+
+from hermes.gauges import set_gauge, increment_gauge, decrement_gauge, \
+    gauge_wrapper, hermes_gauge
+from hermes.counters import increment_counter, counter_wrapper
+from hermes.hermes import set_hermes_config

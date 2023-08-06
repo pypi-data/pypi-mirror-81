7a378a0348ba634a8f5bdbbe15d1aca305672627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:41 +0000

    Add package arpys/0.2.3/arpys-0.2.3.tar.gz

---
+++ packages/arpys/arpys-0.2.3.tar.gz/arpys-0.2.3/arpys/__init__.py
@@ -0,0 +1,10 @@
+name = 'arpys'
+
+import arpys.dataloaders as dataloader
+import arpys.postprocessing as postprocessing
+
+#print('Loading arpys.__init__.py')
+
+dl = dataloader
+pp = postprocessing 
+

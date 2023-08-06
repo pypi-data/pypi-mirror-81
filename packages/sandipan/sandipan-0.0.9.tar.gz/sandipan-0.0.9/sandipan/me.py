e584bcf07ee3aed7e174e4095cdeef14b8a39b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:10 +0000

    Add package sandipan/0.0.9/sandipan-0.0.9.tar.gz

---
+++ packages/sandipan/sandipan-0.0.9.tar.gz/sandipan-0.0.9/sandipan/me.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+def personal_details():
+    name, age = "Sandipan Roy", 23
+    address = "Berhampore, West Bengal, India"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+def social_links():
+    twitter = "twitter.com/bytehackr"
+    linkedin = "linkedin/in/bytehackr"
+    instagram = "instagram.com/bytehackr"
+    email = "'sandipan@parrotsec.org' or 'me@sandipan.ml'"
+    print("Email: {}\nTwitter: {}\nLinkedin: {}".format(email, twitter, linkedin))
+
+
+print("-----------------")
+personal_details()
+print("-----------------")
+social_links()
---
+++ packages/sandipan/sandipan-0.0.9.tar.gz/sandipan-0.0.9/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Sandipan Roy",
+    author_email="me@sandipan.ml",
+    name='sandipan',
+    license="MIT",
+    description='Take it Easy; Make it Easy;',
+    version='v0.0.9',
+    long_description=README,
+    url='https://sandipan.ml',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['requests'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)

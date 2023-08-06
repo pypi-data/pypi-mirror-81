5293b0891e70dd52d6f8469aaf8f2c9255df1357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:01 +0000

    Add package trolleer/0.0.1/trolleer-0.0.1-py3-none-any.whl

---
+++ packages/trolleer/trolleer-0.0.1-py3-none-any.whl/trolleer/__init__.py
@@ -0,0 +1,20 @@
+import time as tm
+
+def leaveMssg(mssgtype):
+   print(mssgtype + ", are you leaving now!")
+   tm.sleep(2)
+
+def spam(mssg):
+   print(mssg * 30)
+   
+def intLagger(inter):
+    print("iuawefiuaaweiufh" * inter)
+   
+def poop(interg):
+    print("pbttt!!" * interg)
+
+def makeTroll(troll):
+    print(troll)
+
+
+tm.sleep(5)

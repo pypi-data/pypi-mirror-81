a29bb49bd60fedd957b67ec9881e81aa8aafc939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:47 +0000

    Add package wikibaseintegrator/0.9.0/wikibaseintegrator-0.9.0-py3-none-any.whl

---
+++ packages/wikibaseintegrator/wikibaseintegrator-0.9.0-py3-none-any.whl/wikibaseintegrator/tests/test_item_creation.py
@@ -0,0 +1,40 @@
+import unittest
+
+from wikibaseintegrator import wbi_core
+
+
+class TestItemCreation(unittest.TestCase):
+    def test_new_item_creation(self):
+        data = [
+            wbi_core.String(value='test', prop_nr='P1'),
+            wbi_core.String(value='test1', prop_nr='P2'),
+            wbi_core.Math("xxx", prop_nr="P3"),
+            wbi_core.ExternalID("xxx", prop_nr="P4"),
+            wbi_core.ItemID("Q123", prop_nr="P5"),
+            wbi_core.Time('+%Y-%m-%dT%H:%M:%SZ', "P6"),
+            wbi_core.Url("http://www.google.com", "P7"),
+            wbi_core.MonolingualText("xxx", prop_nr="P8"),
+            wbi_core.Quantity(-5.04, prop_nr="P9"),
+            wbi_core.Quantity(5.06, upper_bound=9.99, lower_bound=-2.22, unit="Q11573", prop_nr="P10"),
+            wbi_core.CommonsMedia("xxx", prop_nr="P11"),
+            wbi_core.GlobeCoordinate(1.2345, 1.2345, 12, prop_nr="P12"),
+            wbi_core.GeoShape("Data:xxx.map", prop_nr="P13"),
+            wbi_core.Property("P123", "P14"),
+            wbi_core.TabularData("Data:xxx.tab", prop_nr="P15"),
+            wbi_core.MusicalNotation("\relative c' { c d e f | g2 g | a4 a a a | g1 |}", prop_nr="P16"),
+            wbi_core.Lexeme("L123", prop_nr="P17"),
+            wbi_core.Form("L123-F123", prop_nr="P18"),
+            wbi_core.Sense("L123-S123", prop_nr="P19")
+        ]
+        core_props = set(["P{}".format(x) for x in range(20)])
+
+        for d in data:
+            item = wbi_core.ItemEngine(new_item=True, data=[d], core_props=core_props)
+            assert item.get_json_representation()
+            item = wbi_core.ItemEngine(new_item=True, data=[d], core_props=set())
+            assert item.get_json_representation()
+
+        item = wbi_core.ItemEngine(new_item=True, data=data, core_props=core_props)
+        assert item.get_json_representation()
+        item = wbi_core.ItemEngine(new_item=True, data=data, core_props=set())
+        assert item.get_json_representation()
---
+++ packages/wikibaseintegrator/wikibaseintegrator-0.9.0-py3-none-any.whl/wikibaseintegrator/tests/test_wdi_login.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+import os
+import sys
+
+from wikibaseintegrator import wbi_login
+
+# look for environment variables. if none set, don't do anything
+WDUSER = os.getenv("WDUSER")
+WDPASS = os.getenv("WDPASS")
+
+
+def test_login():
+    if WDUSER and WDPASS:
+        wbi_login.Login(WDUSER, WDPASS)
+    else:
+        print("no WDUSER or WDPASS found in environment variables", file=sys.stderr)

b34ba494ba9950c5e6ed8a41133977cb556ae2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package mittepro/2.4.1/mittepro-2.4.1.tar.gz

---
+++ packages/mittepro/mittepro-2.4.1.tar.gz/mittepro-2.4.1/mittepro/test_variables.py
@@ -0,0 +1,20 @@
+variables = {
+    "recipients": [
+        "<thiago.decastro2@gmail.com>"
+    ],
+    'context': {'GMERGE': 'Mah oia Soh'},
+    'context_per_recipient': {
+        'thiago.decastro2@gmail.com': {'MERGE': 'Malandro salafrario'},
+    },
+    "from_": 'Beutrano <thiago.dsn.cir@alterdata.com.br>',
+    'template_slug': 'tpl-teste',
+    "message_text": "Using this message instead.",
+    "message_html": "<em>Using this message <strong>instead</strong>.</em>",
+    "key": '1e4be7cdd03545958e34',
+    "secret": 'cf8cdba282104ed88f0a',
+    "files_names": [
+        # 'foo.pdf',
+        # 'bar.jpg',
+        # 'foo_bar.txt',
+    ]
+}

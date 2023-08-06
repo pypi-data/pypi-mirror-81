92dd5494b50bfa4c8be4f0e7a82a712fec02ec9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:04 +0000

    Add package pottery/0.83/pottery-0.83.tar.gz

---
+++ packages/pottery/pottery-0.83.tar.gz/pottery-0.83/pottery/exceptions.py
@@ -0,0 +1,75 @@
+# --------------------------------------------------------------------------- #
+#   exceptions.py                                                             #
+#                                                                             #
+#   Copyright © 2015-2020, Rajiv Bakulesh Shah, original author.              #
+#   All rights reserved.                                                      #
+# --------------------------------------------------------------------------- #
+
+
+from typing import Iterable
+from typing import Optional
+
+from redis import Redis
+
+
+class PotteryError(Exception):
+    'Base exception class for Pottery containers.'
+
+    def __init__(self, redis: Redis, key: Optional[str]) -> None:
+        self._redis = redis
+        self._key = key
+
+    def __repr__(self) -> str:
+        return "{}(redis={}, key='{}')".format(
+            self.__class__.__name__,
+            self._redis,
+            self._key,
+        )
+
+    def __str__(self) -> str:
+        return "redis={} key='{}'".format(self._redis, self._key)
+
+class KeyExistsError(PotteryError):
+    'Initializing a container on a Redis key that already exists.'
+
+class RandomKeyError(PotteryError, RuntimeError):
+    "Can't create a random Redis key; all of our attempts already exist."
+
+    def __init__(self, redis: Redis) -> None:
+        super().__init__(redis, None)
+
+    def __repr__(self) -> str:
+        return "{}(redis={})".format(self.__class__.__name__, self._redis)
+
+    def __str__(self) -> str:
+        return 'redis={}'.format(self._redis)
+
+
+class PrimitiveError(Exception):
+    'Base exception class for distributed primitives.'
+
+    def __init__(self, masters: Iterable[Redis], key: str) -> None:
+        self._masters = masters
+        self._key = key
+
+    def __repr__(self) -> str:
+        return "{}(masters={}, key='{}')".format(
+            self.__class__.__name__,
+            list(self._masters),
+            self._key,
+        )
+
+    def __str__(self) -> str:
+        return "masters={}, key='{}'".format(list(self._masters), self._key)
+
+class QuorumNotAchieved(PrimitiveError, RuntimeError):
+    ...
+
+class TooManyExtensions(PrimitiveError, RuntimeError):
+    ...
+
+class ExtendUnlockedLock(PrimitiveError, RuntimeError):
+    ...
+
+class ReleaseUnlockedLock(PrimitiveError, RuntimeError):
+    ...
---
+++ packages/pottery/pottery-0.83.tar.gz/pottery-0.83/setup.py
@@ -0,0 +1,44 @@
+# --------------------------------------------------------------------------- #
+#   setup.py                                                                  #
+#                                                                             #
+#   Copyright © 2015-2020, Rajiv Bakulesh Shah, original author.              #
+#   All rights reserved.                                                      #
+# --------------------------------------------------------------------------- #
+
+
+from setuptools import find_packages
+from setuptools import setup
+
+import pottery
+
+
+setup(
+    name=pottery.__name__,
+    version=pottery.__version__,
+    description=pottery.__description__,
+    long_description=pottery.__long_description__,
+    url=pottery.__url__,
+    author=pottery.__author__,
+    author_email=pottery.__author_email__,
+    license=pottery.__license__,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    keywords=pottery.__keywords__,
+    packages=find_packages(exclude=('contrib', 'docs', 'tests*')),
+    install_requires=('redis>=3.4.1', 'mmh3'),
+    extras_require={},
+    package_data={'pottery': ('py.typed',)},
+    data_files=tuple(),
+    entry_points={},
+)

9b0aa4314977868ae91c232ba84bb27cfdf115e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:36 +0000

    Add package odoo13-addon-account-avatax-sale/13.0.2.1.1/odoo13_addon_account_avatax_sale-13.0.2.1.1-py3-none-any.whl

---
+++ packages/odoo13-addon-account-avatax-sale/odoo13_addon_account_avatax_sale-13.0.2.1.1-py3-none-any.whl/odoo/addons/account_avatax_sale/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Taxes on Sales Orders using Avalara Avatax API",
+    "version": "13.0.2.1.1",
+    "author": "Open Source Integrators, Fabrice Henrion, Odoo SA,"
+    " Odoo Community Association (OCA)",
+    "summary": "Sales Orders with automatic Tax application using Avatax",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": ["account_avatax", "sale"],
+    "data": ["views/sale_order_view.xml", "views/partner_view.xml"],
+    "auto_install": True,
+    "development_status": "Beta",
+}

c7a170ed2a61afa92e3e17f82af834e8a8f3f0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:40 +0000

    Add package bi-ga-dist/1.0/bi_ga_dist-1.0.tar.gz

---
+++ packages/bi-ga-dist/bi_ga_dist-1.0.tar.gz/bi_ga_dist-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bi_ga_dist',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['bi_ga_dist'],
+      author="Taha B Uyar",
+      zip_safe=False)

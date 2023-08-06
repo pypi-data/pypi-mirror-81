c8a7143366feb07d5a5393e4e615aca1bb921c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:33 +0000

    Add package PizzaCutter/1.1.9/pizzacutter-1.1.9.tar.gz

---
+++ packages/PizzaCutter/pizzacutter-1.1.9.tar.gz/pizzacutter-1.1.9/CHANGES.rst
@@ -0,0 +1,59 @@
+Changelog
+=========
+
+- new MAJOR version for incompatible API changes,
+- new MINOR version for added functionality in a backwards compatible manner
+- new PATCH version for backwards compatible bug fixes
+
+v1.1.9
+--------
+2020-10-09: service release
+    - update travis build matrix for linux 3.9-dev
+    - update travis build matrix (paths) for windows 3.9 / 3.10
+
+v1.1.8
+--------
+2020-08-08: service release
+    - fix documentation
+    - fix travis
+    - deprecate pycodestyle
+    - implement flake8
+
+v1.1.7
+---------
+2020-08-01: fix pypi deploy
+
+v1.1.6
+---------
+2020-07-31: fix travis build
+
+v0.1.5
+---------
+2020-07-30: release
+    - add helper "find_version_number_in_file"
+
+v0.1.4
+---------
+2020-07-29: release
+    - use the new pizzacutter template
+    - use cli_exit_tools
+
+v0.1.3
+---------
+2020-07-16: release
+    - change the location of the python default template
+
+v0.1.2
+---------
+2020-07-16: release
+    - release on pypi
+
+v0.1.1
+---------
+2020-07-16: release
+    - fix cli test
+    - enable traceback option on cli errors
+
+v0.1.0
+---------
+2020-05-24: Initial public release
---
+++ packages/PizzaCutter/pizzacutter-1.1.9.tar.gz/pizzacutter-1.1.9/pizzacutter/pizzacutter_cli.py
@@ -0,0 +1,111 @@
+# STDLIB
+import pathlib3x as pathlib
+import sys
+from typing import Optional
+
+# EXT
+import click
+
+# OWN
+import cli_exit_tools
+
+# PROJ
+try:
+    from . import __init__conf__
+    from . import pizzacutter
+except (ImportError, ModuleNotFoundError):  # pragma: no cover
+    # imports for pytest
+    import __init__conf__                   # type: ignore  # pragma: no cover
+    import pizzacutter                      # type: ignore  # pragma: no cover
+
+# CONSTANTS
+CLICK_CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+def info() -> None:
+    """
+    >>> info()
+    Info for ...
+
+    """
+    __init__conf__.print_info()
+
+
+def build(conf_file: str, template_dir: str = '', target_dir: str = '', dry_run: bool = False, overwrite: bool = False, write_outside: bool = False) -> None:
+    """ Builds the Project from the Template
+
+    >>> # Setup
+    >>> path_test_dir = pathlib.Path(__file__).parent.parent.resolve() / 'tests'
+    >>> path_template_dir = path_test_dir / 'pizzacutter_test_template_01'
+    >>> path_target_dir = path_test_dir / 'pizzacutter_test_project_01_result'
+    >>> path_conf_file = path_template_dir / 'PizzaCutterTestConfig_01.py'
+
+    >>> # Test only pass "conf_file", dry run
+    >>> build(conf_file=str(path_conf_file), template_dir='', target_dir='', dry_run=True)
+
+    >>> # Test pass "conf_file", "template_dir" and "target_dir" dry run
+    >>> build(conf_file=str(path_conf_file), template_dir=str(path_template_dir), target_dir=str(path_target_dir), dry_run=True)
+
+    """
+
+    path_conf_file = pathlib.Path(conf_file).resolve()
+
+    if template_dir:
+        path_template_dir = pathlib.Path(template_dir).resolve()
+    else:
+        path_template_dir = path_conf_file.parent
+
+    if target_dir:
+        path_target_dir = pathlib.Path(target_dir).resolve()
+    else:
+        path_target_dir = pathlib.Path.cwd().resolve()
+
+    pizzacutter.build(path_conf_file=path_conf_file, path_template_dir=path_template_dir, path_target_dir=path_target_dir,
+                      dry_run=dry_run, allow_overwrite=overwrite, allow_outside_write=write_outside)
+
+
+@click.group(help=__init__conf__.title, context_settings=CLICK_CONTEXT_SETTINGS)
+@click.version_option(version=__init__conf__.version,
+                      prog_name=__init__conf__.shell_command,
+                      message='{} version %(version)s'.format(__init__conf__.shell_command))
+@click.option('--traceback/--no-traceback', is_flag=True, type=bool, default=None, help='return traceback information on cli')
+def cli_main(traceback: Optional[bool] = None) -> None:
+    if traceback is not None:
+        cli_exit_tools.config.traceback = traceback
+
+
+@cli_main.command('info', context_settings=CLICK_CONTEXT_SETTINGS)
+def cli_info() -> None:
+    """ get program informations """
+    info()
+
+
+@cli_main.command('build', context_settings=CLICK_CONTEXT_SETTINGS)
+@click.argument('conf_file', type=click.Path(dir_okay=False, file_okay=True, exists=True, readable=True, resolve_path=True))
+@click.option('-p', '--template_dir', type=click.Path(dir_okay=True, file_okay=False, exists=False, resolve_path=False),
+              help='use different template Folder with given CONF_FILE', default='')
+@click.option('-t', '--target_dir', type=click.Path(dir_okay=True, file_okay=False, exists=False, resolve_path=False),
+              help='set target directory, default: current directory', default='')
+@click.option('-d', '--dry_run', is_flag=True, help='dry run', default=False)
+@click.option('-o', '--overwrite', is_flag=True, help='allow overwriting of files', default=True)
+@click.option('-w', '--write_outside', is_flag=True, help='allow write outside the project dir', default=False)
+def cli_build(conf_file: str, template_dir: str = '', target_dir: str = '',
+              dry_run: bool = False, overwrite: bool = False, write_outside: bool = False) -> None:
+    """ build or rebuild from CONF_FILE"""
+    build(conf_file=conf_file,
+          template_dir=template_dir,
+          target_dir=target_dir,
+          dry_run=dry_run,
+          overwrite=overwrite,
+          write_outside=write_outside)
+
+
+# entry point if main
+if __name__ == '__main__':
+    try:
+        cli_main()
+    except Exception as exc:
+        cli_exit_tools.print_exception_message()
+        sys.exit(cli_exit_tools.get_system_exit_code(exc))
+    finally:
+        cli_exit_tools.flush_streams()

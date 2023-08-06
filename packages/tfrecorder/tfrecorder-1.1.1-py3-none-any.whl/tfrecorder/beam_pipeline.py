eca50de6cc4cd0c9e2b1b2e94a55e42bd5303bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:41 +0000

    Add package tfrecorder/1.1.1/tfrecorder-1.1.1-py3-none-any.whl

---
+++ packages/tfrecorder/tfrecorder-1.1.1-py3-none-any.whl/tfrecorder/beam_pipeline.py
@@ -0,0 +1,354 @@
+# Lint as: python3
+
+# Copyright 2020 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""TFRecorder Beam Pipeline.
+
+This file implements the full Beam pipeline for TFRecorder.
+"""
+
+import collections
+from typing import Any, Callable, Dict, Generator, List, Optional, Union
+
+import functools
+import logging
+import os
+
+import apache_beam as beam
+from apache_beam import pvalue
+import pandas as pd
+import tensorflow_transform as tft
+from tensorflow_transform import beam as tft_beam
+
+from tfrecorder import beam_image
+from tfrecorder import common
+from tfrecorder import schema
+from tfrecorder import types
+
+
+def _get_job_name(job_label: str = None) -> str:
+  """Returns Beam runner job name.
+
+  Args:
+    job_label: A user defined string that helps define the job.
+
+  Returns:
+    A job name compatible with apache beam runners, including a time stamp to
+      insure uniqueness.
+  """
+
+  job_name = 'tfrecorder-' + common.get_timestamp()
+  if job_label:
+    job_label = job_label.replace('_', '-')
+    job_name += '-' + job_label
+
+  return job_name
+
+
+def _get_job_dir(output_path: str, job_name: str) -> str:
+  """Returns Beam processing job directory."""
+
+  return os.path.join(output_path, job_name)
+
+
+def _get_pipeline_options(
+    runner: str,
+    job_name: str,
+    job_dir: str,
+    project: str,
+    region: str,
+    tfrecorder_wheel: str,
+    dataflow_options: Union[Dict[str, Any], None]
+    ) -> beam.pipeline.PipelineOptions:
+  """Returns Beam pipeline options."""
+
+  options_dict = {
+      'runner': runner,
+      'staging_location': os.path.join(job_dir, 'staging'),
+      'temp_location': os.path.join(job_dir, 'tmp'),
+      'job_name': job_name,
+      'teardown_policy': 'TEARDOWN_ALWAYS',
+      'save_main_session': True,
+      'pipeline_type_check': False,
+  }
+
+  if project:
+    options_dict['project'] = project
+  if region:
+    options_dict['region'] = region
+  if runner == 'DataflowRunner':
+    options_dict['extra_packages'] = [tfrecorder_wheel]
+  if dataflow_options:
+    options_dict.update(dataflow_options)
+
+  return beam.pipeline.PipelineOptions(flags=[], **options_dict)
+
+
+def _partition_fn(
+    element: Dict[str, str],
+    unused_num_partitions: int = -1,
+    split_key: str = 'split') -> int:
+  """Returns index used to partition an element from a PCollection."""
+  del unused_num_partitions
+  dataset_type = element[split_key].decode('utf-8')
+  try:
+    index = schema.SplitKeyType.allowed_values.index(dataset_type)
+  except ValueError as e:
+    logging.warning('Unable to index dataset type %s: %s.',
+                    dataset_type, str(e))
+    index = schema.SplitKeyType.allowed_values.index('DISCARD')
+  return index
+
+def _get_write_to_tfrecord(output_dir: str,
+                           prefix: str,
+                           compress: bool = True,
+                           num_shards: int = 0) \
+                           -> beam.io.tfrecordio.WriteToTFRecord:
+  """Returns `beam.io.tfrecordio.WriteToTFRecord` object.
+  This configures a Beam sink to output TFRecord files.
+  Args:
+    output_dir: Directory to output TFRecord files.
+    prefix: TFRecord file prefix.
+    compress: If True, GZip compress TFRecord files.
+    num_shards: Number of file shards to split the TFRecord data.
+  """
+
+  path = os.path.join(output_dir, prefix)
+  suffix = '.tfrecord'
+  if compress:
+    compression_type = 'gzip'
+    suffix += '.gz'
+  else:
+    compression_type = 'uncompressed'
+
+  return beam.io.tfrecordio.WriteToTFRecord(
+      path,
+      file_name_suffix=suffix,
+      compression_type=compression_type,
+      num_shards=num_shards,
+  )
+
+
+def _preprocessing_fn(inputs: Dict[str, Any],
+                      schema_map: Dict[str, collections.namedtuple]):
+  """TensorFlow Transform preprocessing function."""
+
+  outputs = {}
+  for name, supported_type in schema_map.items():
+    if supported_type.type_name == 'string_label':
+      outputs[name] = tft.compute_and_apply_vocabulary(inputs[name])
+    else:
+      outputs[name] = inputs[name]
+  return outputs
+
+
+# pylint: disable=abstract-method
+
+class ToCSVRows(beam.DoFn):
+  """Adds image to PCollection."""
+
+  def __init__(self):
+    """Constructor."""
+    super().__init__()
+    self.row_count = beam.metrics.Metrics.counter(self.__class__, 'row_count')
+
+
+  # pylint: disable=unused-argument
+  # pylint: disable=arguments-differ
+  def process(
+      self,
+      element: List[str],
+      ) -> Generator[Dict[str, Any], None, None]:
+    """Converts an input pandas DataFrame row (List of strings) into a flat
+      column seperated row. This is necessary so that the input DataFrame
+      can operate with TF Transform Coders."""
+    element = ','.join([str(item) for item in element])
+    self.row_count.inc()
+    yield element
+
+
+def get_split_counts(df: pd.DataFrame, split_key: str):
+  """Returns number of rows for each data split type given dataframe."""
+  assert split_key in df.columns
+  return df[split_key].value_counts().to_dict()
+
+
+def _transform_and_write_tfr(
+    dataset: pvalue.PCollection,
+    tfr_writer: Callable[[], beam.io.tfrecordio.WriteToTFRecord],
+    raw_metadata: types.BeamDatasetMetadata,
+    preprocessing_fn: Optional[Callable] = None,
+    transform_fn: Optional[types.TransformFn] = None,
+    label: str = 'data'):
+  """Applies TF Transform to dataset and outputs it as TFRecords."""
+
+  dataset_metadata = (dataset, raw_metadata)
+
+  if transform_fn:
+    transformed_dataset, transformed_metadata = (
+        (dataset_metadata, transform_fn)
+        | f'Transform{label}' >> tft_beam.TransformDataset())
+  else:
+    if not preprocessing_fn:
+      preprocessing_fn = lambda x: x
+    (transformed_dataset, transformed_metadata), transform_fn = (
+        dataset_metadata
+        | f'AnalyzeAndTransform{label}' >>
+        tft_beam.AnalyzeAndTransformDataset(preprocessing_fn))
+
+  transformed_data_coder = tft.coders.ExampleProtoCoder(
+      transformed_metadata.schema)
+  _ = (
+      transformed_dataset
+      | f'Encode{label}' >> beam.Map(transformed_data_coder.encode)
+      | f'Write{label}' >> tfr_writer(prefix=label.lower()))
+
+  return transform_fn
+
+
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-locals
+def build_pipeline(
+    df: pd.DataFrame,
+    job_label: str,
+    runner: str,
+    project: str,
+    region: str,
+    output_dir: str,
+    compression: str,
+    num_shards: int,
+    schema_map: Dict[str, collections.namedtuple],
+    tfrecorder_wheel: str,
+    dataflow_options: Dict[str, Any]) -> beam.Pipeline:
+  """Runs TFRecorder Beam Pipeline.
+
+  Args:
+    df: Pandas DataFrame
+    job_label: User description for the beam job.
+    runner: Beam Runner: (e.g. DataflowRunner, DirectRunner).
+    project: GCP project ID (if DataflowRunner)
+    region: GCP compute region (if DataflowRunner)
+    output_dir: GCS or Local Path for output.
+    compression: gzip or None.
+    num_shards: Number of shards.
+    schema_map: A schema map (Dictionary mapping Dataframe columns to types)
+     used to derive the input and target schema.
+    tfrecorder_wheel: Path to TFRecorder wheel for DataFlow
+    dataflow_options: Dataflow Runner Options (optional)
+
+  Returns:
+    beam.Pipeline
+
+  Note: These inputs must be validated upstream (by client.create_tfrecord())
+  """
+
+  job_name = _get_job_name(job_label)
+  job_dir = _get_job_dir(output_dir, job_name)
+  options = _get_pipeline_options(
+      runner,
+      job_name,
+      job_dir,
+      project,
+      region,
+      tfrecorder_wheel,
+      dataflow_options)
+
+  p = beam.Pipeline(options=options)
+  with tft_beam.Context(temp_dir=os.path.join(job_dir, 'tft_tmp')):
+
+    converter = schema.get_tft_coder(df.columns, schema_map)
+    flatten_rows = ToCSVRows()
+
+    # Each element in the data PCollection will be a dict
+    # including the image_csv_columns and the image features created from
+    # extract_images_fn.
+    data = (
+        p
+        | 'ReadFromDataFrame' >> beam.Create(df.values.tolist())
+        | 'ToCSVRows' >> beam.ParDo(flatten_rows)
+        | 'DecodeCSV' >> beam.Map(converter.decode)
+    )
+
+    # Extract images if an image_uri key exists.
+    image_uri_key = schema.get_key(schema.ImageUriType, schema_map)
+    if image_uri_key:
+      extract_images_fn = beam_image.ExtractImagesDoFn(image_uri_key)
+
+      data = (
+          data
+          | 'ReadImage' >> beam.ParDo(extract_images_fn)
+      )
+
+    # If the schema contains a valid split key, partition the dataset.
+    split_key = schema.get_key(schema.SplitKeyType, schema_map)
+
+    # Note: This will not always reflect actual number of samples per dataset
+    # written as TFRecords. The succeeding `Partition` operation may mark
+    # additional samples from other splits as discarded. If a split has all
+    # its samples discarded, the pipeline will still generate a TFRecord
+    # file for that split, albeit empty.
+    split_counts = get_split_counts(df, split_key)
+
+    # Raw metadata is the TFT metadata after image insertion but before TFT
+    # e.g Image columns have been added if necessary.
+    raw_metadata = schema.get_raw_metadata(df.columns, schema_map)
+
+    # Require training set to be available in the input data. The transform_fn
+    # and transformed_metadata will be generated from the training set and
+    # applied to the other datasets, if any
+    assert 'TRAIN' in split_counts
+
+    # Split dataset into train, validation, test sets.
+    partition_fn = functools.partial(_partition_fn, split_key=split_key)
+    train_data, val_data, test_data, discard_data = (
+        data | 'SplitDataset' >> beam.Partition(
+            partition_fn, len(schema.SplitKeyType.allowed_values)))
+
+    raw_schema_map = schema.get_raw_schema_map(schema_map=schema_map)
+    preprocessing_fn = functools.partial(
+        _preprocessing_fn,
+        schema_map=raw_schema_map)
+
+    tfr_writer = functools.partial(
+        _get_write_to_tfrecord, output_dir=job_dir, compress=compression,
+        num_shards=num_shards)
+
+    transform_fn = _transform_and_write_tfr(
+        train_data, tfr_writer, preprocessing_fn=preprocessing_fn,
+        raw_metadata=raw_metadata,
+        label='Train')
+
+    if 'VALIDATION' in split_counts:
+      _transform_and_write_tfr(
+          val_data, tfr_writer, transform_fn=transform_fn,
+          raw_metadata=raw_metadata,
+          label='Validation')
+
+    if 'TEST' in split_counts:
+      _transform_and_write_tfr(
+          test_data, tfr_writer, transform_fn=transform_fn,
+          raw_metadata=raw_metadata,
+          label='Test')
+
+    _ = (
+        discard_data
+        | 'WriteDiscardedData' >> beam.io.WriteToText(
+            os.path.join(job_dir, 'discarded-data')))
+
+    # Note: `transform_fn` already contains the transformed metadata
+    _ = (transform_fn | 'WriteTransformFn' >> tft_beam.WriteTransformFn(
+        job_dir))
+
+  return p
---
+++ packages/tfrecorder/tfrecorder-1.1.1-py3-none-any.whl/tfrecorder/client_test.py
@@ -0,0 +1,329 @@
+# Lint as: python3
+
+# Copyright 2020 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for client."""
+
+import os
+from typing import List
+
+import csv
+import tempfile
+import unittest
+
+import mock
+import pandas as pd
+
+from tfrecorder import client
+from tfrecorder import test_utils
+from tfrecorder import schema
+
+
+# pylint: disable=protected-access
+
+
+class ClientTest(unittest.TestCase):
+  """Misc tests for `client` module."""
+
+  def setUp(self):
+    self.test_df = test_utils.get_test_df()
+    self.test_region = 'us-central1'
+    self.test_project = 'foo'
+    self.test_wheel = '/my/path/wheel.whl'
+
+  @mock.patch('tfrecorder.client.beam_pipeline')
+  def test_create_tfrecords_direct_runner(self, mock_beam):
+    """Tests `create_tfrecords` Direct case."""
+    mock_beam.build_pipeline().run().wait_until_finished.return_value = {
+        'rows':6}
+    r = client.create_tfrecords(
+        self.test_df,
+        runner='DirectRunner',
+        output_dir='/tmp/direct_runner')
+    self.assertTrue('metrics' in r)
+
+  @mock.patch('tfrecorder.client.beam_pipeline')
+  def test_create_tfrecords_dataflow_runner(self, mock_beam):
+    """Tests `create_tfrecords` Dataflow case."""
+    mock_beam.build_pipeline().run().job_id.return_value = 'foo_id'
+
+    df2 = self.test_df.copy()
+    df2['image_uri'] = 'gs://' + df2['image_uri']
+
+    outdir = '/tmp/dataflow_runner'
+
+    expected = {
+        'job_id': 'foo_id',
+        'dataflow_url': 'https://console.cloud.google.com/dataflow/jobs/' +
+                        'us-central1/foo_id?project=foo'}
+
+    os.makedirs(outdir, exist_ok=True)
+    r = client.create_tfrecords(
+        df2,
+        runner='DataflowRunner',
+        output_dir=outdir,
+        region=self.test_region,
+        project=self.test_project,
+        tfrecorder_wheel=self.test_wheel)
+    self.assertEqual(r, expected)
+
+  def test_path_split(self):
+    """Tests `_path_split`."""
+
+    filename = 'image_file.jpg'
+    dirpaths = ['/path/to/image/dir', 'gs://path/to/image/dir']
+    for dir_ in dirpaths:
+      filepath = os.path.join(dir_, filename)
+      act_dirpath, act_filename = client._path_split(filepath)
+      self.assertEqual(act_dirpath, dir_)
+      self.assertEqual(act_filename, filename)
+
+
+class InputValidationTest(unittest.TestCase):
+  """'Tests for validation input data."""
+
+  def setUp(self):
+    self.test_df = test_utils.get_test_df()
+    self.test_region = 'us-central1'
+    self.test_project = 'foo'
+    self.test_wheel = '/my/path/wheel.whl'
+    self.test_schema_map = schema.image_csv_schema
+
+  def test_valid_dataframe(self):
+    """Tests valid DataFrame input."""
+    self.assertIsNone(
+        client._validate_data(
+            self.test_df,
+            schema.image_csv_schema))
+
+  def test_missing_image(self):
+    """Tests missing image column."""
+    with self.assertRaises(AttributeError):
+      df2 = self.test_df.copy()
+      df2.drop('image_uri', inplace=True, axis=1)
+      client._validate_data(df2, schema.image_csv_schema)
+
+  def test_missing_label(self):
+    """Tests missing label column."""
+    with self.assertRaises(AttributeError):
+      df2 = self.test_df.copy()
+      df2.drop('label', inplace=True, axis=1)
+      client._validate_data(df2, schema.image_csv_schema)
+
+  def test_missing_split(self):
+    """Tests missing split column."""
+    with self.assertRaises(AttributeError):
+      df2 = self.test_df.copy()
+      df2.drop('split', inplace=True, axis=1)
+      client._validate_data(df2, schema.image_csv_schema)
+
+  def test_valid_runner(self):
+    """Tests valid runner."""
+    self.assertIsNone(client._validate_runner(
+        runner='DirectRunner',
+        project=self.test_project,
+        region=self.test_region,
+        tfrecorder_wheel=None))
+
+  def test_invalid_runner(self):
+    """Tests invalid runner."""
+    with self.assertRaises(AttributeError):
+      client._validate_runner(
+          runner='FooRunner',
+          project=self.test_project,
+          region=self.test_region,
+          tfrecorder_wheel=None)
+
+
+  def test_gcs_path_with_dataflow_runner_missing_param(self):
+    """Tests DataflowRunner with missing required parameter."""
+    for p, r in [
+        (None, self.test_region), (self.test_project, None), (None, None)]:
+      with self.assertRaises(AttributeError) as context:
+        client._validate_runner(
+            runner='DataflowRunner',
+            project=p,
+            region=r,
+            tfrecorder_wheel=self.test_wheel)
+      self.assertTrue('DataflowRunner requires valid `project` and `region`'
+                      in repr(context.exception))
+
+
+  def test_gcs_path_with_dataflow_runner_missing_wheel(self):
+    """Tests DataflowRunner with missing required whl path."""
+    with self.assertRaises(AttributeError) as context:
+      client._validate_runner(
+          runner='DataflowRunner',
+          project=self.test_project,
+          region=self.test_region,
+          tfrecorder_wheel=None)
+      self.assertTrue('requires a tfrecorder whl file for remote execution.'
+                      in repr(context.exception))
+
+
+def _make_csv_tempfile(data: List[List[str]]) -> tempfile.NamedTemporaryFile:
+  """Returns `NamedTemporaryFile` representing an image CSV."""
+
+  f = tempfile.NamedTemporaryFile(mode='w+t', suffix='.csv')
+  writer = csv.writer(f, delimiter=',')
+  for row in data:
+    writer.writerow(row)
+  f.seek(0)
+  return f
+
+
+def get_sample_image_csv_data() -> List[List[str]]:
+  """Returns sample CSV data in Image CSV format."""
+
+  data = test_utils.get_test_data()
+  header = list(data.keys())
+  content = [list(row) for row in zip(*data.values())]
+  return [header] + content
+
+
+class ReadImageDirectoryTest(unittest.TestCase):
+  """Tests `_read_image_directory`."""
+
+  def setUp(self):
+    self.image_data = test_utils.get_test_df()
+    self.split_key = schema.get_key(
+        schema.SplitKeyType, schema.image_csv_schema)
+    self.label_key = schema.get_key(
+        schema.StringLabelType, schema.image_csv_schema)
+    self.image_uri_key = schema.get_key(
+        schema.ImageUriType, schema.image_csv_schema)
+    self.tempfiles = []
+    self.tempdir = None
+
+  def tearDown(self):
+    for fp in self.tempfiles:
+      fp.close()
+
+    self.tempdir.cleanup()
+
+  def test_normal(self):
+    """Tests conversion of expected directory structure on local machine."""
+
+    g = self.image_data.groupby([self.split_key, self.label_key])
+
+    self.tempdir = tempfile.TemporaryDirectory()
+    rows = []
+    for (split, label), indices in g.groups.items():
+      dir_ = os.path.join(self.tempdir.name, split, label)
+      os.makedirs(dir_)
+      for f in list(self.image_data.loc[indices, self.image_uri_key]):
+        _, name = os.path.split(f)
+        fp = tempfile.NamedTemporaryFile(
+            dir=dir_, suffix='.jpg', prefix=name)
+        self.tempfiles.append(fp)
+        rows.append([split, fp.name, label])
+
+    columns = list(schema.image_csv_schema.keys())
+    actual = client._read_image_directory(self.tempdir.name)
+    actual.sort_values(by=columns, inplace=True)
+    actual.reset_index(drop=True, inplace=True)
+    expected = pd.DataFrame(rows, columns=columns)
+    expected.sort_values(by=columns, inplace=True)
+    expected.reset_index(drop=True, inplace=True)
+    pd.testing.assert_frame_equal(actual, expected)
+
+
+class ReadCSVTest(unittest.TestCase):
+  """Tests `read_csv`."""
+
+  def setUp(self):
+    data = get_sample_image_csv_data()
+    self.header = data.pop(0)
+    self.sample_data = data
+
+  def test_valid_csv_no_header_no_names_specified(self):
+    """Tests a valid CSV without a header and no header names given."""
+    f = _make_csv_tempfile(self.sample_data)
+    actual = client.read_csv(f.name, header=None)
+    self.assertEqual(list(actual.columns), list(schema.image_csv_schema.keys()))
+    self.assertEqual(actual.values.tolist(), self.sample_data)
+
+  def test_valid_csv_no_header_names_specified(self):
+    """Tests valid CSV without a header, but header names are given."""
+    f = _make_csv_tempfile(self.sample_data)
+    actual = client.read_csv(f.name, header=None, names=self.header)
+    self.assertEqual(list(actual.columns), self.header)
+    self.assertEqual(actual.values.tolist(), self.sample_data)
+
+  def test_valid_csv_with_header_no_names_specified(self):
+    """Tests valid CSV with header, and no header names given (inferred)."""
+
+    f = _make_csv_tempfile([self.header] + self.sample_data)
+    actual = client.read_csv(f.name)
+    self.assertEqual(list(actual.columns), self.header)
+    self.assertEqual(actual.values.tolist(), self.sample_data)
+
+  def test_valid_csv_with_header_names_specified(self):
+    """Tests valid CSV with header, and header names given (override)."""
+
+    f = _make_csv_tempfile([self.header] + self.sample_data)
+    actual = client.read_csv(f.name, names=self.header, header=0)
+    self.assertEqual(list(actual.columns), self.header)
+    self.assertEqual(actual.values.tolist(), self.sample_data)
+
+
+class ToDataFrameTest(unittest.TestCase):
+  """Tests `to_dataframe`."""
+
+  def setUp(self) -> None:
+    sample_data = get_sample_image_csv_data()
+    columns = sample_data.pop(0)
+    self.input_df = pd.DataFrame(sample_data, columns=columns)
+
+  @mock.patch.object(client, 'read_csv', autospec=True)
+  def test_input_csv(self, read_csv):
+    """Tests valid input CSV file."""
+    expected = self.input_df
+    read_csv.return_value = expected
+    f = _make_csv_tempfile(get_sample_image_csv_data())
+    actual = client.to_dataframe(f.name)
+    pd.testing.assert_frame_equal(actual, expected)
+
+  def test_input_dataframe_no_names_specified(self):
+    """Tests valid input dataframe with no header names specified."""
+    actual = client.to_dataframe(self.input_df)
+    pd.testing.assert_frame_equal(actual, self.input_df)
+
+  def test_input_dataframe_with_header(self):
+    """Tests valid input dataframe with header specified."""
+    names = list(self.input_df.columns[0:-1])
+    actual = client.to_dataframe(self.input_df, names=names)
+    pd.testing.assert_frame_equal(actual, self.input_df[names])
+
+  @mock.patch.object(client, '_read_image_directory')
+  def test_input_image_dir(self, mock_fn):
+    """Tests valid input image directory."""
+
+    mock_fn.return_value = self.input_df
+
+    with tempfile.TemporaryDirectory() as input_data:
+      actual = client.to_dataframe(input_data)
+      pd.testing.assert_frame_equal(actual, self.input_df)
+
+  def test_error_invalid_inputs(self):
+    """Tests error handling with different invalid inputs."""
+    inputs = [0, 'not_a_csv_file', list(), dict()]
+    for input_data in inputs:
+      with self.assertRaises(ValueError):
+        client.to_dataframe(input_data)
+
+
+if __name__ == '__main__':
+  unittest.main()

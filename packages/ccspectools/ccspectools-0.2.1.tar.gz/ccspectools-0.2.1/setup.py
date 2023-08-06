17bd19e0f17dceca08be214241c28f9237e7925b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:58 +0000

    Add package ccspectools/0.2.1/ccspectools-0.2.1.tar.gz

---
+++ packages/ccspectools/ccspectools-0.2.1.tar.gz/ccspectools-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import ast
+import sys
+
+#setup_requires = ['setuptools >= 30.3.0']
+setup_requires = ['setuptools >= 30.3.0', 'setuptools-git-version']
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    setup_requires.append('pytest-runner')
+
+
+setup(description="ccspectools",
+      long_description=open('README.md').read(),
+      version_format = '{tag}.dev{commitcount}+{gitsha}', 
+      setup_requires=setup_requires)

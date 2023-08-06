797995c984a98e30958ee2e7eaec8fe89e10c6a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:43 +0000

    Add package wallabag-client/1.0.6/wallabag_client-1.0.6-py3-none-any.whl

---
+++ packages/wallabag-client/wallabag_client-1.0.6-py3-none-any.whl/wallabag/entry.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+class Entry:
+    entry_id = 0
+    title = ""
+    content = ""
+    url = ""
+    read = False
+    starred = False
+
+    def __init__(self, item):
+        self.entry_id = item['id']
+
+        title = item['title']
+        title = title.replace("\n", "")
+        title = " ".join(title.split())
+        self.title = title
+
+        self.content = item['content']
+        self.url = item['url']
+        self.read = item['is_archived'] == 1
+        self.starred = item['is_starred'] == 1
+
+    def create_list(items):
+        ret = list()
+        for item in items:
+            ret.append(Entry(item))
+        return ret

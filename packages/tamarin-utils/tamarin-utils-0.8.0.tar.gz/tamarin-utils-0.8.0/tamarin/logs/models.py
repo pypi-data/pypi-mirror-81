639c96a744e1fad197a2e40cafc69ee51936af6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:46 +0000

    Add package tamarin-utils/0.8.0/tamarin-utils-0.8.0.tar.gz

---
+++ packages/tamarin-utils/tamarin-utils-0.8.0.tar.gz/tamarin-utils-0.8.0/tamarin/logs/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Log(models.Model):
+    class Category(models.TextChoices):
+        RESERVE = 'reserve'
+
+    category = models.CharField(max_length=1000, choices=Category.choices)
+    description = models.TextField()
+    reference_url = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_created=True, auto_now_add=True)
+
+    def __str__(self):
+        return F"{self.category}"
+
+    class Meta:
+        ordering = ('-created_at',)
+        verbose_name = 'log'
+        verbose_name_plural = 'log'

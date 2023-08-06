cbcbd2a4f09ebe05844986f217a540029cb191ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:23 +0000

    Add package siki/0.1.5.3/siki-0.1.5.3.tar.gz

---
+++ packages/siki/siki-0.1.5.3.tar.gz/siki-0.1.5.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/evn python
+#coding=utf-8
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="siki",
+    version="0.1.5.3",
+    author="Orlando Chen",
+    author_email="seagochen@hotmail.com",
+    description="A collection of tools that may be used to help users coding with Python in an easy way",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seagochen/Siki",
+    packages=setuptools.find_packages(),
+    install_requires=['pptree'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)

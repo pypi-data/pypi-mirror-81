633e3ee44b251e9b5c14c734df7886ead8ed7d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:54 +0000

    Add package object2urdf/0.12/object2urdf-0.12.tar.gz

---
+++ packages/object2urdf/object2urdf-0.12.tar.gz/object2urdf-0.12/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='object2urdf',
+    version='0.12',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Manage URDF files for a library of objects',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['numpy', 'scipy', 'trimesh', 'pillow'],
+    url='https://github.com/cbteeple/object2urdf',
+    author='Clark Teeple',
+    author_email='cbteeple@gmail.com',
+)

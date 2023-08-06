5c5bf9cf65a7a6e057147d7ed7174658ad5e2cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:06 +0000

    Add package moclaphar/0.0.38/moclaphar-0.0.38.tar.gz

---
+++ packages/moclaphar/moclaphar-0.0.38.tar.gz/moclaphar-0.0.38/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+setuptools.setup(
+    name="moclaphar",
+    version="0.0.38",
+    license='MIT',
+    author="Jongkuk Lim",
+    author_email="lim.jeikei@gmail.com",
+    description="This packages mainly aims to make an easy process for dataset manipulation.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/JeiKeiLim/moclaphar",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=[
+        'scipy>=1.4.1',
+        'matplotlib>=3.2.1',
+        'seaborn>=0.10.1',
+        'h5py>=2.10.0',
+        'pandas>=1.0.3',
+        'moviepy>=1.0.3',
+        'tqdm>=4.46.0',
+        'opencv-python>=4.2.0'
+      ],
+)

5673b0272e2660688fd3d92c89e83bddb9dc53d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:31 +0000

    Add package Project0/0.0.1/Project0-0.0.1.tar.gz

---
+++ packages/Project0/Project0-0.0.1.tar.gz/Project0-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Project0',
+    version='0.0.1',
+    description='Offline vocal assistant and tts',
+    url='http://github.com/fenrir77/project0',
+    author='Fenrir77',
+    author_email='fenrir7377@gmail.com',
+    license='MIT',
+    packages=['Project0'],
+    zip_safe=False
+)

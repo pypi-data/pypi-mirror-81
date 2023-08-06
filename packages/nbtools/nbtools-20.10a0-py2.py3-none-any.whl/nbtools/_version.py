87c13439c39a6a1e6ee0c94af90a1d28611bb002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:57 +0000

    Add package nbtools/20.10a0/nbtools-20.10a0-py2.py3-none-any.whl

---
+++ packages/nbtools/nbtools-20.10a0-py2.py3-none-any.whl/nbtools/_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Regents of the University of California & the Broad Institute.
+# Distributed under the terms of the Modified BSD License.
+
+version_info = (20, 10, 'a', 0)
+__version__ = ".".join(map(str, version_info))
---
+++ packages/nbtools/nbtools-20.10a0-py2.py3-none-any.whl/nbtools/uioutput.py
@@ -0,0 +1,31 @@
+from ipywidgets import VBox, widget_serialization
+from traitlets import Unicode, List, Dict, Instance
+from ._frontend import module_name, module_version
+from .basewidget import BaseWidget
+
+
+class UIOutput(BaseWidget):
+    """
+    Widget used to render Python output in a UI
+    """
+    _model_name = Unicode('UIOutputModel').tag(sync=True)
+    _model_module = Unicode(module_name).tag(sync=True)
+    _model_module_version = Unicode(module_version).tag(sync=True)
+
+    _view_name = Unicode('UIOutputView').tag(sync=True)
+    _view_module = Unicode(module_name).tag(sync=True)
+    _view_module_version = Unicode(module_version).tag(sync=True)
+
+    name = Unicode('Python Results').tag(sync=True)
+    status = Unicode('').tag(sync=True)
+    files = List(Unicode, []).tag(sync=True)
+    text = Unicode('').tag(sync=True)
+    visualization = Unicode('').tag(sync=True)
+    appendix = Instance(VBox).tag(sync=True, **widget_serialization)
+    extra_file_menu_items = Dict().tag(sync=True)
+
+    def __init__(self, **kwargs):
+        # Initialize the child widget container
+        self.appendix = VBox()
+
+        BaseWidget.__init__(self, **kwargs)

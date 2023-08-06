88c4b39f41ae7758c6b1f5ba302d11b1c3754edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:14 +0000

    Add package iot-demo-webapp/1.1.0/iot-demo-webapp-1.1.0.tar.gz

---
+++ packages/iot-demo-webapp/iot-demo-webapp-1.1.0.tar.gz/iot-demo-webapp-1.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='iot-demo-webapp',
+    version='1.1.0',
+    description='Simple webapp for demo purposes.',
+    py_modules=['iot-demo-webapp'],
+    install_requires=['Flask'],
+    include_package_data=True,
+    package_data = {'': ['LICENSE']}
+)

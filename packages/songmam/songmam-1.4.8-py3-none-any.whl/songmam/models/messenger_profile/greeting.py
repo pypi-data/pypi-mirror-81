fe027868e9a1ebd907e9df0dca6028c485f060f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:33 +0000

    Add package songmam/1.4.8/songmam-1.4.8-py3-none-any.whl

---
+++ packages/songmam/songmam-1.4.8-py3-none-any.whl/songmam/models/messenger_profile/greeting.py
@@ -0,0 +1,16 @@
+# https://developers.facebook.com/docs/messenger-platform/reference/messenger-profile-api/greeting
+from typing import List
+
+from pydantic import validator
+
+from songmam.models.messaging.locale import ThingWithLocale
+
+
+class GreetingPerLocale(ThingWithLocale):
+    text: str  # Text Could be Personalized e.g. "Hello {{user_first_name}}!"
+
+    @validator("text")
+    def char_limit_to_160(cls, value):
+        if len(value) > 160:
+            raise ValueError("Must be in UTF-8. 160 character limit.")
+        return value

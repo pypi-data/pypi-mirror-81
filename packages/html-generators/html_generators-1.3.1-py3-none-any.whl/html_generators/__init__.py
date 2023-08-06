f228360fc87e985e4b6f4dda463009841183a408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:21 +0000

    Add package html-generators/1.3.1/html_generators-1.3.1-py3-none-any.whl

---
+++ packages/html-generators/html_generators-1.3.1-py3-none-any.whl/html_generators/__init__.py
@@ -0,0 +1,10 @@
+'''
+Note - we recommend importing as:
+    import html_generators as h
+'''
+from .document import Document
+from .comment import Comment
+# Note - imports Element and VoidElement, as well as all html elements
+from .elements import *
+from .fragment import Fragment
+from .mark_safe import MarkSafe

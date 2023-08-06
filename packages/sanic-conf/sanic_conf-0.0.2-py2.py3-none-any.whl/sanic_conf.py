a83c2ab96139606d5306a6ddb9b85f821c37a502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:11 +0000

    Add package sanic-conf/0.0.2/sanic_conf-0.0.2-py2.py3-none-any.whl

---
+++ packages/sanic-conf/sanic_conf-0.0.2-py2.py3-none-any.whl/sanic_conf.py
@@ -0,0 +1,22 @@
+import importlib
+import os
+
+__version__ = '0.0.2'
+ENVIRONMENT_VARIABLE = 'SANIC_SETTINGS_MODULE'
+
+
+class Settings:
+    def __init__(self):
+        settings_module = os.environ.get(ENVIRONMENT_VARIABLE)
+        if not settings_module:
+            raise RuntimeError(
+                "You must define the environment variable %s before "
+                "accessing settings." % ENVIRONMENT_VARIABLE
+            )
+
+        module = importlib.import_module(settings_module)
+
+        for setting in dir(module):
+            if setting.isupper():
+                setting_value = getattr(module, setting)
+                setattr(self, setting, setting_value)

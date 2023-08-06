00fe53d5ba27e912be83c2c949e51cbe15440cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:53 +0000

    Add package osmapi/1.3.0/osmapi-1.3.0.tar.gz

---
+++ packages/osmapi/osmapi-1.3.0.tar.gz/osmapi-1.3.0/setup.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from codecs import open
+from setuptools import setup
+import re
+
+with open('osmapi/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+try:
+    import pypandoc
+    from unidecode import unidecode
+    description = open('README.md', encoding='utf-8').read()
+    description = unidecode(description)
+    description = pypandoc.convert(description, 'rst', format='md')
+except (IOError, OSError, ImportError):
+    description = 'Python wrapper for the OSM API'
+
+setup(
+    name='osmapi',
+    packages=['osmapi'],
+    version=version,
+    install_requires=['requests'],
+    description='Python wrapper for the OSM API',
+    long_description=description,
+    author='Etienne Chové',
+    author_email='chove@crans.org',
+    maintainer='Stefan Oderbolz',
+    maintainer_email='odi@metaodi.ch',
+    url='https://github.com/metaodi/osmapi',
+    download_url='https://github.com/metaodi/osmapi/archive/v%s.zip' % version,
+    keywords=['openstreetmap', 'osm', 'api'],
+    license='GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Software Development :: Libraries',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

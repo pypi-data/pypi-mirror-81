55bbf8572a0074f6d4d40da939911fd0aaf74dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:22 +0000

    Add package apispec/4.0.0b1.post0/apispec-4.0.0b1.post0-py2.py3-none-any.whl

---
+++ packages/apispec/apispec-4.0.0b1.post0-py2.py3-none-any.whl/apispec/__init__.py
@@ -0,0 +1,7 @@
+"""Contains main apispec classes: `APISpec` and `BasePlugin`"""
+
+from .core import APISpec
+from .plugin import BasePlugin
+
+__version__ = "4.0.0b1.post0"
+__all__ = ["APISpec", "BasePlugin"]

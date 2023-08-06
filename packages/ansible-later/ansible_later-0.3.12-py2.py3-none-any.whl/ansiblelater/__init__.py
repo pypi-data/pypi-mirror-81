800f96308d014330e2741c48387fe048d7ddc9e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:19 +0000

    Add package ansible-later/0.3.12/ansible_later-0.3.12-py2.py3-none-any.whl

---
+++ packages/ansible-later/ansible_later-0.3.12-py2.py3-none-any.whl/ansiblelater/__init__.py
@@ -0,0 +1,13 @@
+"""Default package."""
+
+__author__ = "Robert Kaussow"
+__project__ = "ansible-later"
+__license__ = "MIT"
+__maintainer__ = "Robert Kaussow"
+__email__ = "mail@thegeeklab.de"
+__url__ = "https://github.com/thegeeklab/ansible-later"
+__version__ = "0.3.12"
+
+from ansiblelater import logger
+
+LOG = logger.get_logger("ansiblelater")

ff6d65e1fa842ae65b989943ce26ccf363b2b56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:56 +0000

    Add package stacksearch/1.3.8/stacksearch-1.3.8-py3-none-any.whl

---
+++ packages/stacksearch/stacksearch-1.3.8-py3-none-any.whl/stacksearch/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Welcome to stacksearch.
+
+Stacksearch is a simple tool aimed for people who never want to leave the terminal.
+Stacksearch can also search other Stack Exchange websites, not just StackOverflow.
+"""
+
+__version__ = "1.3.8"
+
+__author__ = ["Bryan Hu", "KomodoKode"]
+
+__all__ = ["__main__", "Search"]

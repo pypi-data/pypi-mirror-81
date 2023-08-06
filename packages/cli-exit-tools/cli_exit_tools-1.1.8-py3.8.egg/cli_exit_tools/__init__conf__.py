8b886ceeff5803246f103de14be8d0bc1fb132d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:01 +0000

    Add package cli-exit-tools/1.1.8/cli_exit_tools-1.1.8-py3.8.egg

---
+++ packages/cli-exit-tools/cli_exit_tools-1.1.8-py3.8.egg/cli_exit_tools/__init__conf__.py
@@ -0,0 +1,20 @@
+name = 'cli_exit_tools'
+title = 'functions to exit an cli application properly'
+version = 'v1.1.8'
+url = 'https://github.com/bitranox/cli_exit_tools'
+author = 'Robert Nowotny'
+author_email = 'bitranox@gmail.com'
+shell_command = 'cli_exit_tools'
+
+
+def print_info() -> None:
+    print("""\
+
+Info for cli_exit_tools:
+
+    functions to exit an cli application properly
+
+    Version : v1.1.8
+    Url     : https://github.com/bitranox/cli_exit_tools
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com""")

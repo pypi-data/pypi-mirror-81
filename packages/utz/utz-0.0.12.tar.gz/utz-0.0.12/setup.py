4198533960673015d80eae39c62abf55300e9531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:49 +0000

    Add package utz/0.0.12/utz-0.0.12.tar.gz

---
+++ packages/utz/utz-0.0.12.tar.gz/utz-0.0.12/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="utz",
+    version="0.0.12",
+    author="Ryan Williams",
+    author_email="ryan@runsascoded.com",
+    description="Misc stdlib, pandas, subprocess, and other utilities, exposed for easy importing + boilerplate-reduction",
+    install_requires=[
+        "GitPython",
+        "joblib",
+        "pandas",
+        "pyyaml",
+    ],
+    extras_require={
+        'pdf': [
+            'reportlab==3.5.42',
+            'pyPDF2==1.26.0',
+        ],
+    },
+    scripts=['utz.sh',],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/runsascoded/utz",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

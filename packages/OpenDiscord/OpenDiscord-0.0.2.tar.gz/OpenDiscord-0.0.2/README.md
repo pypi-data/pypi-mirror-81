1f00f232d60fb8dd16f4c5365cd460dda1c76764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:48 +0000

    Add package OpenDiscord/0.0.2/OpenDiscord-0.0.2.tar.gz

---
+++ packages/OpenDiscord/OpenDiscord-0.0.2.tar.gz/OpenDiscord-0.0.2/README.md
@@ -0,0 +1,14 @@
+<div class="header">
+  <h1>OpenDiscord</h1>
+  <img src="https://img.shields.io/discord/715558840959238154">
+  <img src="https://img.shields.io/github/repo-size/pjotr07740/OpenDiscord">
+  <img src="https://img.shields.io/pypi/dm/OpenDiscord">
+  <img src="https://img.shields.io/github/issues/pjotr07740/OpenDiscord">
+  <img src="https://img.shields.io/pypi/l/OpenDiscord">
+</div>
+<h2>About</h2>
+<p>OpenDiscord is an API Wrapper for Discord Bot listing sites.</p>
+<h2>Install</h2>
+<p>Coming soon</p>
+<h2>Documentation</h2>
+<p>Comming soon</p>
---
+++ packages/OpenDiscord/OpenDiscord-0.0.2.tar.gz/OpenDiscord-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OpenDiscord", # Replace with your own username
+    version="0.0.2",
+    author="Pjotr",
+    author_email="pjotrlspdfr@outlook.com",
+    description="An API wrapper for common discord bot listing sites.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pjotr07740/OpenDiscord",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

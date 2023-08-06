cef16996427608893562b34aa78053123c8ce18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:29 +0000

    Add package celerytimer/1.1/celerytimer-1.1.tar.gz

---
+++ packages/celerytimer/celerytimer-1.1.tar.gz/celerytimer-1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding='UTF-8') as f:
+    long_description = f.read()
+setuptools.setup(
+    name="celerytimer",
+    version="1.01",
+    author="dteer",
+    author_email="dteer@qq.com",
+    description="Celery-based timing tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dteer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

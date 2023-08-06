117f559a1261afd56e15a377f411e68b2fe88b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:16 +0000

    Add package adversal-embedding/0.0.4/adversal_embedding-0.0.4.tar.gz

---
+++ packages/adversal-embedding/adversal_embedding-0.0.4.tar.gz/adversal_embedding-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import os
+
+import setuptools
+from setuptools import setup
+
+install_requires = [
+    "numpy",
+    "keras",
+    "tensorflow==1.15.0",
+]
+
+
+setup(
+    # _PKG_NAME will be used in Makefile for dev release
+    name=os.getenv("_PKG_NAME", "adversal_embedding"),
+    version="0.0.4",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url="https://github.com/xiaomihao/adversal_embedding",
+    license="Apache 2.0",
+    author="Xiao Mi",
+    author_email="1922188869@qq.com",
+    description="adversal_embedding",
+    install_requires=install_requires,
+)

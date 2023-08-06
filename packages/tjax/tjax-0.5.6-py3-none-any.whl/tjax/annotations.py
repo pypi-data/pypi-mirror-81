32afe3928af46d2e5471ded63cfc5f465224f4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:41 +0000

    Add package tjax/0.5.6/tjax-0.5.6-py3-none-any.whl

---
+++ packages/tjax/tjax-0.5.6-py3-none-any.whl/tjax/annotations.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from typing import Any, TYPE_CHECKING, Sequence, Tuple, Union
+
+from chex import Array
+
+__all__ = ['Shape',
+           'ShapeLike',
+           'SliceLike',
+           'Array',
+           'RealArray',
+           'ComplexArray',
+           'PyTree']
+
+
+Shape = Tuple[int, ...]
+ShapeLike = Union[int, Sequence[int]]
+SliceLike = Tuple[Union[int, None, slice], ...]
+RealArray = Array
+ComplexArray = Array
+PyTree = Any
+# PyTree = Union[Array,
+#                'PyTreeLike',
+#                Tuple['PyTree', ...],
+#                List['PyTree'],
+#                Dict[Hashable, 'PyTree'],
+#                None]
+
+
+if TYPE_CHECKING:
+    from .pytree_like import PyTreeLike

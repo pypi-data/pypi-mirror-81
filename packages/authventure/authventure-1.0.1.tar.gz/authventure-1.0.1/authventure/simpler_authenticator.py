2765c0c2502b35fd127f65c6986552e33a4bc1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:11 +0000

    Add package authventure/1.0.1/authventure-1.0.1.tar.gz

---
+++ packages/authventure/authventure-1.0.1.tar.gz/authventure-1.0.1/authventure/simpler_authenticator.py
@@ -0,0 +1,32 @@
+import jwt
+
+from .exceptions.invalid_token_exception import InvalidTokenException
+
+
+class SimplerAuthenticator:
+    def __init__(self, secret: str):
+        self.secret = secret
+
+    def create_user_unexpirable_token(self, user_id: str) -> str:
+        return jwt.encode(
+            {"userId": user_id},
+            self.secret,
+            algorithm='HS256'
+        )
+
+    def get_user_id_from_unexpirable_token(self, token: str) -> str:
+        try:
+            payload = jwt.decode(
+                token,
+                self.secret,
+                algorithms='HS256',
+                options={'verify_signature': True, 'verify_exp': False}
+            )
+        except (
+            jwt.exceptions.InvalidSignatureError,
+            jwt.exceptions.DecodeError,
+            UnicodeDecodeError
+        ):
+            raise InvalidTokenException('Invalid token')
+
+        return payload['userId']

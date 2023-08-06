bcd6b2d836ad3451267f8a7be7be9bcfd3472fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:35 +0000

    Add package lightText/0.2.2/lightText-0.2.2.tar.gz

---
+++ packages/lightText/lightText-0.2.2.tar.gz/lightText-0.2.2/lighttext/__init__.py
@@ -0,0 +1,3 @@
+from .component import *
+from .text_mining import *
+from .string_search import *
---
+++ packages/lightText/lightText-0.2.2.tar.gz/lightText-0.2.2/lighttext/string_search/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/2 8:55
+# @Author  : lightsmile
+# @Software: PyCharm
+
+from .keyword import KeywordProcessor
+from .ner import NER
+
+__all__ = ['KeywordProcessor', 'NER']

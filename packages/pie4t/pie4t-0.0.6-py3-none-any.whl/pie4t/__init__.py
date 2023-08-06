db8e5fe79672958894ee1c13ce31ecb15126d3ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:31 +0000

    Add package pie4t/0.0.6/pie4t-0.0.6-py3-none-any.whl

---
+++ packages/pie4t/pie4t-0.0.6-py3-none-any.whl/pie4t/__init__.py
@@ -0,0 +1,94 @@
+
+from . import common
+from .engine import PhysicsEngine
+
+物理引擎 = PhysicsEngine
+
+__all__ = [ 
+            'add_circle', '新增圓球', '物理引擎',
+            'simulate', '模擬主迴圈','模擬進行中', '移除',
+            'add_segment','新增線段','add_box','新增方塊',
+
+
+
+            ]
+
+
+
+######## top level function
+
+def add_circle(*args, **kwargs):
+    if not common.is_engine_created:
+        PhysicsEngine()
+    return common.stage.新增圓球(*args, **kwargs)
+
+新增圓球 = add_circle
+
+
+def add_box(*args, **kwargs):
+    if not common.is_engine_created:
+        PhysicsEngine()
+    return common.stage.新增方塊(*args, **kwargs)
+
+新增方塊 = add_box
+
+def add_segment(*args, **kwargs):
+    if not common.is_engine_created:
+        PhysicsEngine()
+    return common.stage.新增線段(*args, **kwargs)
+
+新增線段 = add_segment
+
+
+
+def 移除(obj):
+    if not common.is_engine_created:
+        PhysicsEngine()
+    return common.stage.移除(obj)
+
+
+
+def simulate():
+    if not common.is_engine_created:
+        PhysicsEngine()
+
+    common.stage.simulate()
+
+
+模擬主迴圈 = simulate
+
+模擬進行中 = simulate
+
+#def module_init():
+    # module reference
+    #__main__.pie4t_module = sys.modules['pie4t']
+
+    ## check batch mode
+    # had __file__ and import pie4t in files
+    # if hasattr(__main__ , '__file__') and __main__.__file__.endswith('py'):
+    #     #print('has __file__')
+    #     try:
+    #         with open(__main__.__file__, encoding='utf8') as f:
+    #             lines = f.readlines()
+    #             for i in lines:
+    #                 if 'import' in i and 'pie4t' in i:
+    #                     __main__.pie4t_module.is_batch_mode = True
+                        
+    #                     print(__file__, ': is batch mode,')
+    #                     break
+    #     except FileNotFoundError:
+    #         pass
+
+    # create game win
+    #__main__.pie4t_module.game_win = PhysicsEngine()
+    #__main__.pie4t_module.is_initialized = True
+
+### module init
+
+#module_init()
+
+
+
+if __name__ == '__main__' :
+    pass
+    
---
+++ packages/pie4t/pie4t-0.0.6-py3-none-any.whl/pie4t/segment.py
@@ -0,0 +1,54 @@
+import pymunk
+import arcade
+
+from . import common
+
+
+class Segment:
+    def __init__(self, a, b, 寬=None):
+        self.is_lazy_setup = False
+
+
+
+        # pymunk part
+        self.a = a
+        self.b = b
+
+        if 寬 is not None:
+            self.thickness = 寬
+        else:
+            self.thickness = common.SEG_THICKNESS
+
+        self.phy_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        self.phy_body.position = a
+        a_b_delta = (b[0] - a[0], b[1]-a[1])
+        self.phy_shape = pymunk.Segment(self.phy_body,(0,0), a_b_delta, 
+                                self.thickness//2)
+        self.phy_shape.friction = 1
+        self.phy_shape.elasticity = 1
+        self.phy_shape.collision_type = common.COLLITYPE_SEGMENT
+
+        self.phy_shape.filter = pymunk.ShapeFilter(categories=common.CATE_SEGMENT)
+
+        self.phy_shape.obj = self
+
+        common.stage.space.add(self.phy_shape)
+        
+        if common.stage.is_engine_running:
+            self.lazy_setup()
+            common.stage.is_engine_running = True
+
+    def lazy_setup(self):
+        if not self.is_lazy_setup:
+            #arcade part
+            self.shape_element = arcade.ShapeElementList()
+            a = self.a
+            b = self.b
+            self.line_shape = arcade.create_line(a[0], a[1], b[0], b[1],
+                     arcade.color.YELLOW_ORANGE, self.thickness)
+            self.is_lazy_setup = True
+
+    def draw(self):
+        self.line_shape.draw()
+        
+    

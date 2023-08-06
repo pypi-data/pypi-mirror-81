f170403d4f797f86de10c35b7e25fd431e1ea505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:54 +0000

    Add package scrapli-ssh2/2020.10.7/scrapli_ssh2-2020.10.7.tar.gz

---
+++ packages/scrapli-ssh2/scrapli_ssh2-2020.10.7.tar.gz/scrapli_ssh2-2020.10.7/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""scrapli_ssh2 - ssh2-python transport plugin for scrapli"""
+import setuptools
+
+from scrapli_ssh2 import __version__
+
+__author__ = "Carl Montanari"
+
+with open("README.md", "r", encoding="utf-8") as f:
+    README = f.read()
+
+setuptools.setup(
+    name="scrapli_ssh2",
+    version=__version__,
+    author=__author__,
+    author_email="carl.r.montanari@gmail.com",
+    description="ssh2-python transport plugin for the scrapli SSH|Telnet screen scraping library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scrapli/scrapli_ssh2",
+    packages=setuptools.find_packages(),
+    install_requires=["scrapli>=2020.06.06", "ssh2-python>=0.22.0,<1.0.0"],
+    extras_require={},
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+    ],
+    python_requires=">=3.6",
+)

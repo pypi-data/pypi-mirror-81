780b7c448f09b732c7d4d45b196051afd922da25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:00 +0000

    Add package h2o-pysparkling-2.1/3.30.1.3-1/h2o_pysparkling_2.1-3.30.1.3-1.tar.gz

---
+++ packages/h2o-pysparkling-2.1/h2o_pysparkling_2.1-3.30.1.3-1.tar.gz/h2o_pysparkling_2.1-3.30.1.3-1/ai/h2o/sparkling/ml/algos/H2ODeepLearning.py
@@ -0,0 +1,68 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from pyspark import keyword_only
+
+from ai.h2o.sparkling import Initializer
+from ai.h2o.sparkling.ml.Utils import Utils
+from ai.h2o.sparkling.ml.algos.H2OSupervisedAlgoBase import H2OSupervisedAlgoBase
+from ai.h2o.sparkling.ml.params import H2ODeepLearningParams
+
+
+class H2ODeepLearning(H2ODeepLearningParams, H2OSupervisedAlgoBase):
+
+    @keyword_only
+    def __init__(self,
+                 epochs=10.0,
+                 l1=0.0,
+                 l2=0.0,
+                 hidden=[200, 200],
+                 reproducible=False,
+                 activation="Rectifier",
+                 quantileAlpha=0.5,
+                 modelId=None,
+                 keepCrossValidationPredictions=False,
+                 keepCrossValidationFoldAssignment=False,
+                 parallelizeCrossValidation=True,
+                 distribution="AUTO",
+                 labelCol="label",
+                 foldCol=None,
+                 weightCol=None,
+                 offsetCol=None,
+                 splitRatio=1.0,
+                 seed=-1,
+                 nfolds=0,
+                 allStringColumnsToCategorical=True,
+                 columnsToCategorical=[],
+                 predictionCol="prediction",
+                 detailedPredictionCol="detailed_prediction",
+                 withDetailedPredictionCol=True,
+                 featuresCols=[],
+                 convertUnknownCategoricalLevelsToNa=False,
+                 convertInvalidNumbersToNa=False,
+                 namedMojoOutputColumns=True,
+                 stoppingRounds=5,
+                 stoppingMetric="AUTO",
+                 stoppingTolerance=0.001,
+                 withContributions=False,
+                 withLeafNodeAssignments=False):
+        Initializer.load_sparkling_jar()
+        super(H2ODeepLearning, self).__init__()
+        self._java_obj = self._new_java_obj("ai.h2o.sparkling.ml.algos.H2ODeepLearning", self.uid)
+        self._setDefaultValuesFromJava()
+        kwargs = Utils.getInputKwargs(self)
+        self._set(**kwargs)

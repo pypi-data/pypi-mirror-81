189316df975c63280fb327d347099222ed984947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:55 +0000

    Add package aws-cdk.aws-apigatewayv2/1.66.0/aws-cdk.aws-apigatewayv2-1.66.0.tar.gz

---
+++ packages/aws-cdk.aws-apigatewayv2/aws-cdk.aws-apigatewayv2-1.66.0.tar.gz/aws-cdk.aws-apigatewayv2-1.66.0/src/aws_cdk/aws_apigatewayv2/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_certificatemanager._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/aws-apigatewayv2",
+    "1.66.0",
+    __name__[0:-6],
+    "aws-apigatewayv2@1.66.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()

0fb0d1d1e77db80d785483fbd825d06082f365c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:39 +0000

    Add package km3pipe/9.0.0b2/km3pipe-9.0.0b2.tar.gz

---
+++ packages/km3pipe/km3pipe-9.0.0b2.tar.gz/km3pipe-9.0.0b2/examples/plot_tot_histogram.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+==================
+ToT histogram.
+==================
+
+Create a simple histogram of the PMT signals (ToTs) in all events.
+"""
+
+# Author: Tamas Gal <tgal@km3net.de>
+# License: BSD-3
+
+import tables as tb
+import matplotlib.pyplot as plt
+import km3pipe.style
+
+km3pipe.style.use("km3pipe")
+
+filename = "data/atmospheric_muons_sample.h5"
+
+with tb.File(filename) as f:
+    tots = f.get_node("/hits/tot")[:]
+
+plt.hist(tots, bins=max(tots), log=True, edgecolor="none")
+plt.title("ToT distribution")
+plt.xlabel("ToT [ns]")

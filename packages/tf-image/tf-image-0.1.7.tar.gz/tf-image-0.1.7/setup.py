5a3fa3049f341149c8a818d6137178d751577cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:52 +0000

    Add package tf-image/0.1.7/tf-image-0.1.7.tar.gz

---
+++ packages/tf-image/tf-image-0.1.7.tar.gz/tf-image-0.1.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tf-image",
+    version="0.1.7",
+    description="Image augmentation operations for TensorFlow 2+.",
+    url="https://github.com/Ximilar-com/tf-image",
+    author="Ximilar.com Team, Michal Lukac, Libor Vanek, ...",
+    author_email="tech@ximilar.com",
+    license="MIT",
+    packages=find_packages(),
+    keywords="machine learning, multimedia, image",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    namespace_packages=["tf_image"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)

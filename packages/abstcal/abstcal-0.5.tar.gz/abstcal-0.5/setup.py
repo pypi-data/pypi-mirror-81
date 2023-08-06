f276b6f2adf9bd38c07f223e3554a48edcc015af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:39 +0000

    Add package abstcal/0.5/abstcal-0.5.tar.gz

---
+++ packages/abstcal/abstcal-0.5.tar.gz/abstcal-0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="abstcal",
+    version="0.5",
+    author="Yong Cui",
+    author_email="yong.cui01@gmail.com",
+    description="Calculate abstinence using the timeline followback data in substance research",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ycui1/abstcal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

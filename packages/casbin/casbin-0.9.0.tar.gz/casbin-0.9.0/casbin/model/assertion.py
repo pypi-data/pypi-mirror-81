88eced8d4f6a56df23d9d2416345ee0ff2d67737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:10 +0000

    Add package casbin/0.9.0/casbin-0.9.0.tar.gz

---
+++ packages/casbin/casbin-0.9.0.tar.gz/casbin-0.9.0/casbin/model/assertion.py
@@ -0,0 +1,31 @@
+from casbin import log
+
+
+class Assertion:
+    def __init__(self):
+        self.key = ""
+        self.value = ""
+        self.tokens = []
+        self.policy = []
+        self.rm = None
+
+    def build_role_links(self, rm):
+        self.rm = rm
+        count = self.value.count("_")
+
+        for rule in self.policy:
+            if count < 2:
+                raise RuntimeError('the number of "_" in role definition should be at least 2')
+
+            if len(rule) < count:
+                raise RuntimeError("grouping policy elements do not meet role definition")
+
+            if count == 2:
+                self.rm.add_link(rule[0], rule[1])
+            elif count == 3:
+                self.rm.add_link(rule[0], rule[1], rule[2])
+            elif count == 4:
+                self.rm.add_link(rule[0], rule[1], rule[2], rule[3])
+
+        log.log_print("Role links for: " + self.key)
+        self.rm.print_roles()

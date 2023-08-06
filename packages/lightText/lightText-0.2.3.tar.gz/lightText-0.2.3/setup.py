1c238f61c89bd68a040dcf1180e56fb20396ceec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:40 +0000

    Add package lightText/0.2.3/lightText-0.2.3.tar.gz

---
+++ packages/lightText/lightText-0.2.3.tar.gz/lightText-0.2.3/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+import setuptools
+
+with open('./version.txt', 'r', encoding='utf8') as f:
+    version = f.read()
+
+with open('./README.md', 'r', encoding='utf8') as f:
+    long_description = f.read()
+
+with open('./requirements.txt', 'r', encoding='utf8') as f:
+    install_requires = list(map(lambda x: x.strip(), f.readlines()))
+
+setup(
+    name='lightText',
+    version=version,
+    description="lightsmile's text library",
+    author='lightsmile',
+    author_email='iamlightsmile@gmail.com',
+    url='https://github.com/smilelight/lightText',
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    long_description=long_description,
+    include_package_data=True,
+    long_description_content_type='text/markdown',
+    license='Apache-2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+)

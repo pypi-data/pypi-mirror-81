0a93d7d7f18038e756ee9e82902ee1cbde42bcb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:00 +0000

    Add package ksfest/0.1.21/ksfest-0.1.21.tar.gz

---
+++ packages/ksfest/ksfest-0.1.21.tar.gz/ksfest-0.1.21/tests/test_ksfest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `ksfest` package."""
+
+
+import unittest
+import click
+from click.testing import CliRunner
+from ksfest import ksfest
+
+
+class TestKsfest(unittest.TestCase):
+    """Tests for `ksfest` package."""
+
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""

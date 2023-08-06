b673305da665eb5abff5399eb0e24f0578f3476e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:14 +0000

    Add package lib-programname/1.1.8/lib_programname-1.1.8-py3-none-any.whl

---
+++ packages/lib-programname/lib_programname-1.1.8-py3-none-any.whl/lib_programname/__init__conf__.py
@@ -0,0 +1,20 @@
+name = 'lib_programname'
+title = 'get reliably the name of the executed script'
+version = 'v1.1.8'
+url = 'https://github.com/bitranox/lib_programname'
+author = 'Robert Nowotny'
+author_email = 'bitranox@gmail.com'
+shell_command = 'lib_programname'
+
+
+def print_info() -> None:
+    print("""\
+
+Info for lib_programname:
+
+    get reliably the name of the executed script
+
+    Version : v1.1.8
+    Url     : https://github.com/bitranox/lib_programname
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com""")

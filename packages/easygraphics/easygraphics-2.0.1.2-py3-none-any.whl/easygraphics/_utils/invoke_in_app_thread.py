d3700d4dd41289d2978f16c87b6f54f9dac98748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:12 +0000

    Add package easygraphics/2.0.1.2/easygraphics-2.0.1.2-py3-none-any.whl

---
+++ packages/easygraphics/easygraphics-2.0.1.2-py3-none-any.whl/easygraphics/_utils/invoke_in_app_thread.py
@@ -0,0 +1,220 @@
+#####################################################################
+#                                                                   #
+# invoke_in_main.py                                                 #
+#                                                                   #
+# Copyright 2013, Christopher Billington, Philip Starkey            #
+#                                                                   #
+# This file is part of the qtutils project                          #
+# (see https://bitbucket.org/philipstarkey/qtutils )                #
+# and is licensed under the 2-clause, or 3-clause, BSD License.     #
+# See the license.txt file in the root of the project               #
+# for the full license.                                             #
+#
+#                                                                   #
+#####################################################################
+
+import functools
+# tailored and modified by roy
+import sys
+import threading
+import time
+from queue import Queue
+
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QEvent, QObject, QCoreApplication
+
+
+def _reraise(exc_info):
+    exc_type, value, traceback = exc_info
+    raise value.with_traceback(traceback)
+
+
+class CallEvent(QEvent):
+    """An event containing a request for a function call."""
+    EVENT_TYPE = QEvent(QEvent.registerEventType())
+
+    def __init__(self, queue, exceptions_in_main, fn, *args, **kwargs):
+        QEvent.__init__(self, self.EVENT_TYPE)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self._returnval = queue
+        # Whether to raise exceptions in the main thread or store them
+        # for raising in the calling thread:
+        self._exceptions_in_main = exceptions_in_main
+
+
+class Caller(QObject):
+    """An event handler which calls the function held within a CallEvent."""
+
+    def event(self, event):
+        event.accept()
+        exception = None
+        try:
+            result = event.fn(*event.args, **event.kwargs)
+        except Exception:
+            # Store for re-raising the exception in the calling thread:
+            exception = sys.exc_info()
+            result = None
+
+            if not event._exceptions_in_main:
+                raise
+        finally:
+            event._returnval.put([result, exception])
+        return True
+
+
+_caller = None
+
+
+def init_invoke_in_app():
+    global _caller
+    _caller = Caller()
+    _wait_for_quit = False
+
+
+def destroy_invoke_in_app():
+    global _caller, _wait_for_quit
+    _caller = None
+    _wait_for_quit = False
+
+
+def _start_app_thread():
+    app = QtWidgets.QApplication([])
+    font = app.font()
+    font.setPixelSize(_font_size)
+    app.setFont(font)
+    app.setQuitOnLastWindowClosed(False)
+    init_invoke_in_app()
+    # init finished, can draw now
+    return app
+
+
+
+_wait_for_quit = False
+
+
+def wait_for_quit():
+    global _wait_for_quit
+    _wait_for_quit = True
+
+_app_lock = threading.Lock()
+def invoke_in_app_thread(fn, *args, **kwargs):
+    """Queue up the executing of a function in the main thread and return immediately.
+
+    This function queues up a custom :code:`QEvent` to the Qt event loop.
+    This event executes the specified function :code:`fn` in the Python
+    MainThread with the specified arguments and keyword arguments, and returns
+    a Python Queue which will eventually hold the result from the executing of
+    :code:`fn`. To access the result, use :func:`qtutils.invoke_in_main.get_inmain_result`.
+
+    This function can be used from the MainThread, but such use will just directly call the function, bypassing the Qt event loop.
+
+    Arguments:
+        fn: A reference to the function or method to run in the MainThread.
+
+        *args: Any arguments to pass to :code:`fn` when it is called from the
+               MainThread.
+
+        **kwargs: Any keyword arguments to pass to :code:`fn` when it is called
+                  from the MainThread
+
+    Returns:
+       A Python Queue which will eventually hold the result
+       :code:`(fn(*args, **kwargs), exception)` where
+       :code:`exception=[type,value,traceback]`.
+    """
+    _app_lock.acquire()
+    try:
+        if _caller is None:
+            if QtWidgets.QApplication.instance() is None:
+                app = _start_app_thread()
+                ex = None
+                try:
+                    result = fn(*args, **kwargs)
+                except Exception as e:
+                    ex = e
+                destroy_invoke_in_app()
+                app.quit()
+                app = None
+                if ex is not None:
+                    raise ex
+                return result
+            else:
+                return fn(*args, **kwargs)
+        elif _wait_for_quit:  # the app is quitting. don't show the dialog
+            return None
+        result = get_in_app_thread_result(_in_app_thread_later(fn, True, *args, **kwargs))
+        return result
+    finally:
+        _app_lock.release()
+
+
+def _in_app_thread_later(fn, exceptions_in_main, *args, **kwargs):
+    """Asks the mainloop to call a function when it has time. Immediately
+    returns the queue that was sent to the mainloop.  A call to queue.get()
+    will return a list of [result,exception] where exception=[type,value,traceback]
+    of the exception.  Functions are guaranteed to be called in the order
+    they were requested."""
+    queue = Queue()
+    QCoreApplication.postEvent(_caller, CallEvent(queue, exceptions_in_main, fn, *args, **kwargs))
+    return queue
+
+
+def get_in_app_thread_result(queue):
+    """ Processes the result of :func:`qtutils.invoke_in_main.inmain_later`.
+
+    This function takes the queue returned by :code:`inmain_later` and blocks
+    until a result is obtained. If an exception occurred when executing the
+    function in the MainThread, it is raised again here (it is also raised in the
+    MainThread). If no exception was raised, the result from the execution of the
+    function is returned.
+
+    Arguments:
+        queue: The Python Queue object returned by :code:`inmain_later`
+
+    Returns:
+        The result from executing the function specified in the call to
+        :code:`inmain_later`
+
+    """
+    result, exception = queue.get()
+    if exception is not None:
+        _reraise(exception)
+    return result
+
+
+_font_size = 18
+
+
+def set_app_font(size: int):
+    global _font_size
+    _font_size = size
+
+
+def invoke_in_thread():
+    """ A decorator which enforces the execution of the decorated thread to occur in the MainThread.
+
+    Returns:
+        The decorator returns a function that has wrapped the decorated function
+        in the appropriate call to :code:`inmain` or :code:`inmain_later` (if
+        you are unfamiliar with how decorators work, please see the Python
+        documentation).
+
+        When calling the decorated function, the result is either the result of
+        the function executed in the MainThread (if :code:`wait_for_return=True`)
+        or a Python Queue to be used with
+        :func:`qtutils.invoke_in_main.get_inmain_result` at a later time.
+
+    """
+
+    def wrap(fn):
+        """A decorator which sets any function to always run in the main thread."""
+
+        @functools.wraps(fn)
+        def f(*args, **kwargs):
+            return invoke_in_app_thread(fn, *args, **kwargs)
+
+        return f
+
+    return wrap
---
+++ packages/easygraphics/easygraphics-2.0.1.2-py3-none-any.whl/easygraphics/graphwin.py
@@ -0,0 +1,467 @@
+import os
+import threading
+import time
+import queue
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+
+from easygraphics.image import Image
+from easygraphics.consts import Color, MouseMessageType
+
+__all__ = ['GraphWin']
+
+
+class GraphWin(QtWidgets.QWidget):
+    """
+    Main Window for painting graphics
+
+    we use an Image object (self._canvas) to save the painted contents
+
+    how to process repaint event:
+
+    if we are in immediate mode (RENDER_AUTO, self._immediate=True) , \
+    we directly paint the saved contents to the window
+
+    if we are in manual refresh mode (RENDER_MANUAL, self._immediate=False), \
+    we use another image object( self._device_image) as an intermediary .\
+    The contents on this object is painted to the window  and this object is synced with self._canvas manually
+    """
+
+    def __init__(self, width: int, height: int):
+        super().__init__(flags=QtCore.Qt.Window | QtCore.Qt.MSWindowsFixedSizeDialogHint)
+        self._width = width
+        self._height = height
+        self.setFixedWidth(width)
+        self.setFixedHeight(height)
+        self._wait_event = threading.Event()
+        self._key_msg_queue = queue.Queue(50)
+        self._key_char_msg_queue = queue.Queue(50)
+        self._mouse_msg_queue = queue.Queue(50)
+        self.setGeometry(100, 100, width, height)
+        self._init_screen(width, height)
+        self._is_run = True
+        self._immediate = True
+        self.set_immediate(True)
+        self._skip_count = 0
+        self._frames_to_skip_count = 0
+        self._last_fps_time = 0
+        self._frames_skipped = 0
+        self._capture_dir = "."
+        self._capture_count = 0
+
+    def resize(self,width:int,height:int):
+        self._width = width
+        self._height = height
+        self.setGeometry(100, 100, width, height)
+        _old_canvas = self._canvas
+        _old_canvas.close()
+        self._init_screen(width, height)
+
+    def get_width(self):
+        return self._width
+
+    def get_height(self):
+        return self._height
+
+    def _init_screen(self, width, height):
+        screen_image = QtGui.QImage(width, height, QtGui.QImage.Format_ARGB32_Premultiplied)
+        screen_image.fill(Color.WHITE)
+        self._canvas = Image(screen_image)
+        self._device_image = screen_image.copy()
+        self.real_update()
+
+    def get_canvas(self):
+        return self._canvas
+
+    def paintEvent(self, e):
+        if self._immediate:
+            self._canvas.draw_to_device(self)
+        else:
+            p = QtGui.QPainter()
+            p.begin(self)
+            p.drawImage(0, 0, self._device_image)
+            p.end()
+
+    def set_immediate(self, immediate: bool):
+        """
+        set if the graphics window will be updated immediately after things are drawn
+
+        possible values:
+
+        * **True** (default, auto mode) update the window immediately after every drawing
+        * **False** (manual mode) only update the window after pause()/delay()/delay_fps()/delay_jfps() is called.
+
+        manual mode is used for animations
+
+        :param immediate:  if the graphics window will be updated immediately
+        """
+        self._immediate = immediate
+        if immediate:
+            self._canvas.add_updated_listener(self.update)
+        else:
+            self._canvas.remove_updated_listener(self.update)
+
+    def close(self):
+        self._key_msg_queue.queue.clear()
+        self._key_char_msg_queue.queue.clear()
+        self._mouse_msg_queue.queue.clear()
+        if self._immediate:
+            self._canvas.remove_updated_listener(self.update)
+        self._is_run = False
+        self._wait_event.set()
+
+    def is_immediate(self) -> bool:
+        """
+        get if the graphics window will be updated immediately after things are drawn
+
+        :return:
+        """
+        return self._immediate
+
+    def mousePressEvent(self, e: QtGui.QMouseEvent):
+        self._wait_event.set()
+        mouse_msg=MouseMessage(e, MouseMessageType.PRESS_MESSAGE)
+        try:
+            self._mouse_msg_queue.put_nowait(mouse_msg)
+        except queue.Full as err:
+            pass
+
+    def mouseDoubleClickEvent(self, e: QtGui.QMouseEvent) -> None:
+        mouse_msg=MouseMessage(e, MouseMessageType.DOUBLE_CLICK_MESSAGE)
+        try:
+            self._mouse_msg_queue.put_nowait(mouse_msg)
+        except queue.Full as err:
+            pass
+
+    def mouseReleaseEvent(self, e: QtGui.QMouseEvent):
+        mouse_msg=MouseMessage(e, MouseMessageType.RELEASE_MESSAGE)
+        try:
+            self._mouse_msg_queue.put_nowait(mouse_msg)
+        except queue.Full as err:
+            pass
+
+    def keyReleaseEvent(self, e: QtGui.QKeyEvent) -> None:
+        self._wait_event.set()
+        key_msg = KeyMessage(e)
+        try:
+            self._key_msg_queue.put_nowait(key_msg)
+        except queue.Full as err:
+            pass
+
+    def keyPressEvent(self, e: QtGui.QKeyEvent):
+        self._wait_event.set()
+        if e.key() == QtCore.Qt.Key_F10:
+            modifiers = e.modifiers()
+            if modifiers & (QtCore.Qt.ControlModifier |
+                            QtCore.Qt.ShiftModifier |
+                            QtCore.Qt.AltModifier):
+                self._capture_count += 1
+                self._canvas.save(self._capture_dir + os.sep + "save{0}.png".format(self._capture_count))
+        if e.key() < 127 or e.key() == QtCore.Qt.Key_Return:
+            # ascii char key pressed
+            key_char_msg = _KeyCharMsg(e)
+            try:
+                self._key_char_msg_queue.put_nowait(key_char_msg)
+            except queue.Full as err:
+                pass
+        key_msg = KeyMessage(e)
+        try:
+            self._key_msg_queue.put_nowait(key_msg)
+        except queue.Full as err:
+            pass
+
+    def pause(self):
+        """
+        pause and wait for mouse click or keyboard hit
+        """
+        if not self._is_run:
+            return
+        self.real_update()
+        self._wait_event.clear()
+        self._wait_event.wait()
+
+    def closeEvent(self, e: QtGui.QCloseEvent):
+        self.close()
+
+    def is_run(self) -> bool:
+        return self._is_run
+
+    def real_update(self):
+        """
+        really update and repaint the window
+
+        the intermediary image (self._device_image) is synced with the canvas
+        """
+        self._canvas.draw_to_device(self._device_image)
+        self.update()
+
+    def delay(self, milliseconds: float):
+        """
+        Delay the program for specified milliseconds.
+
+        :param milliseconds: time to delay
+        """
+        if self._immediate:
+            raise RuntimeError("Must set render mode to MANUAL to use delay()!")
+        if not self._is_run:
+            return
+        self.real_update()
+        nanotime = milliseconds * 1000000
+        start_wait_time = time.perf_counter_ns()
+        if time.perf_counter_ns() - start_wait_time < nanotime:
+            QtCore.QThread.usleep((start_wait_time + nanotime - time.perf_counter_ns()) // 1000)
+
+    def delay_fps(self, fps: int) -> bool:
+        """
+        Delay to control fps without frame skipping. Never skip frames.
+
+        :param fps: the desire fps
+        :return: False the graphics window is closed. True otherwise.
+        """
+        if self._immediate:
+            raise RuntimeError("Must set render mode to MANUAL to use delay()!")
+        if not self._is_run:
+            return False
+        nanotime = 1000000000 // fps
+        if self._last_fps_time == 0:
+            self._last_fps_time = time.perf_counter_ns()
+        self.real_update()
+        tt = time.perf_counter_ns()
+        # print(tt)
+        if tt - self._last_fps_time < nanotime:
+            QtCore.QThread.usleep((self._last_fps_time + nanotime - tt) // 1000)
+        self._last_fps_time = time.perf_counter_ns()
+        return True
+
+    def delay_jfps(self, fps: int, max_skip_count: int = 10) -> bool:
+        """
+        Delay to control fps with frame skipping.
+
+        If we don't have enough time to delay, we'll skip some frames.
+
+        :param fps: frames per second (max is 1000)
+        :param max_skip_count: max num of  frames to skip
+        :return: True if this frame should not be skipped
+        """
+        if self._immediate:
+            raise RuntimeError("Must set render mode to MANUAL to use delay()!")
+        if not self._is_run:
+            return False
+        nanotime = 1000000000 // fps
+        if self._frames_to_skip_count > 0:
+            self._frames_to_skip_count -= 1
+            self._frames_skipped += 1
+            return False
+        if self._last_fps_time == 0:
+            self._last_fps_time = time.perf_counter_ns()
+
+        nowtime = time.perf_counter_ns()
+        if self._last_fps_time + nanotime < nowtime:
+            if self._frames_skipped <= max_skip_count:
+                # we don't have to draw this frame, so let's skip it
+                self._frames_to_skip_count = round((nowtime - self._last_fps_time) // nanotime)
+                if max_skip_count <= 0:
+                    self._frames_to_skip_count -= 1
+                    self._last_fps_time = time.perf_counter_ns()
+                    return False
+                elif self._frames_to_skip_count > max_skip_count - self._frames_skipped:
+                    self._frames_to_skip_count = (max_skip_count - self._frames_skipped) - 1
+                    self._frames_skipped += 1
+                    self._last_fps_time = time.perf_counter_ns()
+                    return False
+            else:
+                self._frames_skipped = 0
+        self.real_update()
+        tt = time.perf_counter_ns()
+        sleep_time = (self._last_fps_time + nanotime - tt) // 1000
+        if sleep_time > 0:
+            QtCore.QThread.usleep(sleep_time)
+        self._last_fps_time = time.perf_counter_ns()
+        return True
+
+    def get_char(self) -> str:
+        """
+        Get the ascii char inputted by keyboard.
+
+        If not any char key is pressed in last 100 ms, the program will stop and wait for the next key hitting.
+
+        :return: the character inputted by keyboard
+        """
+        if not self._is_run:
+            return ' '
+        nt = time.perf_counter_ns()
+        self.real_update()
+        while True:
+            try:
+                key_char_msg = self._key_char_msg_queue.get(True,0.1)
+                return key_char_msg.char
+            except queue.Empty as err:
+                if not self._is_run:
+                    return ''
+                pass
+
+
+
+    def get_key(self) -> "KeyMessage":
+        """
+        Get the key inputted by keyboard.
+
+        If not any  key is pressed in last 100 ms, the program will stop and wait for the next key hitting.
+
+        :return: key message
+        """
+        if not self._is_run:
+            return QtCore.Qt.Key_Escape, QtCore.Qt.NoModifier
+        nt = time.perf_counter_ns()
+        self.real_update()
+        while True:
+            try:
+                key_msg = self._key_msg_queue.get(True,0.1)
+                return key_msg
+            except queue.Empty as err:
+                if not self._is_run:
+                    return KeyMessage(None)
+                pass
+
+
+
+    def get_mouse_msg(self) -> "MouseMessage":
+        """
+        Get the mouse message.
+
+        If there is not any  mouse message, the program will stop and wait for it.
+
+        :return: mouse message
+        """
+        if not self._is_run:
+            return 0, 0, 0, QtCore.Qt.NoButton
+        nt = time.perf_counter_ns()
+        self.real_update()
+        while True:
+            try:
+                mouse_msg = self._mouse_msg_queue.get(True,0.1)
+                return mouse_msg
+            except queue.Empty as err:
+                if not self._is_run:
+                    return MouseMessage(None,None)
+                pass
+
+    def get_click(self) -> "MouseMessage":
+        """
+        Get the mouse click message.
+
+        If there isn't any clicked event, the program will stop and wait for it.
+
+        :return: mouse message
+        """
+        while self._is_run:
+            msg = self.get_mouse_msg()
+            if msg == MouseMessageType.NO_MESSAGE or\
+                    msg.type == MouseMessageType.RELEASE_MESSAGE and (msg.button & QtCore.Qt.LeftButton) > 0:
+                return msg
+        return MouseMessage(None,None)
+
+    def has_kb_hit(self) -> bool:
+        """
+        See if any ascii char key hit message in the message queue.
+
+        Use it with get_char().
+
+        :return:  True if hit, False otherwise
+        """
+        return not self._key_char_msg_queue.empty()
+
+    def has_kb_msg(self) -> bool:
+        """
+        See if any key hit message in the message queue.
+
+        Use it with get_key().
+
+        :return:  True if hit, False otherwise
+        """
+        return not self._key_msg_queue.empty()
+
+    def has_mouse_msg(self) -> bool:
+        """
+        See if there is any mouse message(event) in the message queue.
+
+        Use it with get_mouse_msg().
+
+        :return:  True if any mouse message, False otherwise
+        """
+        return not self._mouse_msg_queue.empty()
+
+    def get_cursor_pos(self) -> (int, int):
+        """
+        Get position of the mouse cursor
+
+        :return: position's coordinate values (x,y)
+        """
+        p = self.mapFromGlobal(QtGui.QCursor.pos())
+        return p.x(), p.y()
+
+    def clear_key_msg(self):
+        """
+        Clear all keyboard hit messages.
+        """
+        self._key_msg_queue.queue.clear()
+
+    def clear_char_msg(self):
+        """
+        Clear all char key hit messages.
+        """
+        self._key_char_msg_queue.queue.clear()
+
+    def clear_mouse_msg(self):
+        """
+        Clear all mouse messages.
+        """
+        self._mouse_msg_queue.queue.clear()
+
+class KeyMessage:
+    """
+    class for saving keyboard message
+    """
+
+    def __init__(self,key_event: QtGui.QKeyEvent):
+        if key_event is None:
+            self.key = None
+            self.char = ''
+            self.count = 0
+            self.modifiers = QtCore.Qt.NoModifier
+            self.type = QtCore.QEvent.None_
+        else:
+            self.key = key_event.key()
+            self.char = key_event.text()
+            self.count = key_event.count()
+            self.modifiers = key_event.modifiers()
+            self.type = key_event.type()
+
+
+class _KeyCharMsg:
+    """
+    class for saving keyboard hit char
+    """
+
+    def __init__(self, key_event: QtGui.QKeyEvent):
+        self.char = key_event.text()
+
+class MouseMessage:
+    def __init__(self, e: QtGui.QMouseEvent, _type: MouseMessageType):
+        if e is None:
+            self.x = 0
+            self.y = 0
+            self.modifiers = QtCore.Qt.NoModifier
+            self.global_x = 0
+            self.global_y = 0
+            self.type = MouseMessageType.NO_MESSAGE
+            self.button = QtCore.Qt.NoButton
+        else:
+            self.x = e.x()
+            self.y = e.y()
+            self.modifiers = e.modifiers()
+            self.global_x = e.globalX()
+            self.global_y = e.globalY()
+            self.type = _type
+            self.button = e.button()

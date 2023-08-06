0ad83d4ac45e0d89a476fef6405bd1577601f9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:48 +0000

    Add package pycolour/0.0.8/pycolour-0.0.8-py3-none-any.whl

---
+++ packages/pycolour/pycolour-0.0.8-py3-none-any.whl/pycolour/__init__.py
@@ -0,0 +1,13 @@
+def red():
+    red = '\033[31m'
+
+black   = '\033[30m'
+green   = '\033[32m'
+yellow  = '\033[33m'
+blue    = '\033[34m'
+purple  = '\033[35m'
+cyan    = '\033[36m'
+white   = '\033[37m'
+bold    = '\033[1m'
+normal  = '\033[22m'
+end     = '\033[0m'

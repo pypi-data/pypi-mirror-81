9192dbed612358f0d07951d425bf9c78a5480f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:41 +0000

    Add package dislib/0.6.0/dislib-0.6.0.tar.gz

---
+++ packages/dislib/dislib-0.6.0.tar.gz/dislib-0.6.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+setuptools.setup(
+    name="dislib",
+    version=open('VERSION').read().strip(),
+    author="Barcelona Supercomputing Center",
+    author_email="javier.alvarez@bsc.es",
+    description="The distributed computing library on top of PyCOMPSs",
+    url="http://dislib.bsc.es",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Distributed Computing",
+    ],
+    install_requires=[
+        "scikit-learn",
+        "numpy",
+        "scipy"
+    ],
+    scripts=["bin/dislib", "bin/dislib_cmd.py"],
+)

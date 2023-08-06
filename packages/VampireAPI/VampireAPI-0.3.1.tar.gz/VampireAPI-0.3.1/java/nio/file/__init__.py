4cc31cd0020ff3f6d7d55beabfddb4b0e7e12a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:12 +0000

    Add package VampireAPI/0.3.1/VampireAPI-0.3.1.tar.gz

---
+++ packages/VampireAPI/VampireAPI-0.3.1.tar.gz/VampireAPI-0.3.1/java/nio/file/__init__.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2020 - Sebastian Ritter <bastie@users.noreply.github.com>
+# SPDX-License-Identifier: Apache-2.0
+
+'''
+
+  The blood of java.nio.nio.nio.file package.
+
+  full implemented:
+
+    * none
+
+  parts implemented:
+
+    * java.nio.nio.nio.file.Files
+    * java.nio.nio.nio.file.FileSystem
+    * java.nio.nio.nio.file.FileSystems
+    * java.nio.nio.nio.file.Path
+
+  non implemented:
+
+    * all other
+'''
---
+++ packages/VampireAPI/VampireAPI-0.3.1.tar.gz/VampireAPI-0.3.1/java/python/lang/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2020 - Sebastian Ritter <bastie@users.noreply.github.com>
+# SPDX-License-Identifier: Apache-2.0
+
+'''
+    Python provides file wrapper for stdin, stdout and stderr
+
+    >>> import sys
+    >>> import os
+    >>>
+    >>> sys.stdout
+    <_io.TextIOWrapper name='<stdout>' mode='w' encoding='utf-8'>
+    >>> sys.stderr
+    <_io.TextIOWrapper name='<stderr>' mode='w' encoding='utf-8'>
+    >>> sys.stdin
+    <_io.TextIOWrapper name='<stdin>' mode='r' encoding='utf-8'>
+    >>>
+'''

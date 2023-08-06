8ea101e7ad0abc95c1fd9d105dfb1f3ab34eac3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:15 +0000

    Add package login-gui/1.0.1/login_gui-1.0.1-py3-none-any.whl

---
+++ packages/login-gui/login_gui-1.0.1-py3-none-any.whl/login_gui/user.py
@@ -0,0 +1,76 @@
+#    _________             .__
+#   /   _____/____    _____|  |__ _____
+#   \_____  \\__  \  /  ___/  |  \\__  \
+#   /        \/ __ \_\___ \|   Y  \/ __ \_
+#  /_______  (____  /____  >___|  (____  /
+#          \/     \/     \/     \/     \/
+#    _________       _____  __
+#   /   _____/ _____/ ____\/  |_
+#   \_____  \ /  _ \   __\\   __\
+#   /        (  <_> )  |   |  |
+#  /_______  /\____/|__|   |__|
+#          \/
+#  Copyright (c) 2020.
+
+"""
+all user data are convert to base64
+"""
+
+__all__ = ['User']
+
+import base64
+import binascii
+
+from py_singleton import singleton
+
+
+def _is_base64(string):
+    """check if data are base 64"""
+    try:
+        return base64.b64encode(base64.b64decode(string)) == string
+    except binascii.Error:
+        return False
+
+
+@singleton
+class User:
+    """
+    LoggerPackage manager, connection all loggers with package logging.
+    Handles all logger files
+    """
+    __user64 = bytes
+
+    def __init__(self):  # pylint: disable=W0221
+        self.__user64 = bytes
+
+    def save(self, value):
+        """
+        function for saving of value
+        """
+        self.user64 = value
+        return self.user64
+
+    def __str__(self):
+        """
+        return decode user64 to string
+        """
+        # noinspection PyArgumentList
+        return self.user64.decode()
+
+    def decode(self):
+        """
+        getter for user
+        """
+        return base64.b64decode(self.user64).decode()
+
+    @property
+    def user64(self):
+        """save base64 user data"""
+        return self.__user64
+
+    @user64.setter
+    def user64(self, value):
+        if _is_base64(value) or isinstance(value, bytes):
+            self.__user64 = value
+            return
+        self.__user64 = base64.b64encode(value.encode('ascii'))
---
+++ packages/login-gui/login_gui-1.0.1-py3-none-any.whl/tests/test_user.py
@@ -0,0 +1,62 @@
+#     _________             .__
+#    /   _____/____    _____|  |__ _____
+#    \_____  \\__  \  /  ___/  |  \\__  \
+#    /        \/ __ \_\___ \|   Y  \/ __ \_
+#   /_______  (____  /____  >___|  (____  /
+#           \/     \/     \/     \/     \/
+#     _________       _____  __
+#    /   _____/ _____/ ____\/  |_
+#    \_____  \ /  _ \   __\\   __\
+#    /        (  <_> )  |   |  |
+#   /_______  /\____/|__|   |__|
+#           \/
+#   Copyright (c) 2020.
+"""
+test of user class
+"""
+import unittest
+
+from login_gui import User
+from login_gui.user import _is_base64
+
+
+class UserCase(unittest.TestCase):
+    """
+    Test for user singleton class
+    """
+    def setUp(self) -> None:
+        """
+        user setup function
+        """
+        self.user = User()
+        self.user.save('user:password')
+
+    def test_save(self):
+        """
+        test singleton
+        """
+        user2 = User()
+        user3 = User().save('testuser')
+
+        self.assertEqual(self.user, user2)
+        self.assertEqual(self.user.user64, user3)
+        self.assertEqual(user2.user64, user3)
+
+    def test_convert_to_string(self):
+        """
+        test convert to string function
+        """
+        self.assertEqual(str(self.user), 'dXNlcjpwYXNzd29yZA==')
+
+    def test_decode(self):
+        """
+        test decode function
+        """
+        self.assertEqual(self.user.decode(), 'user:password')
+
+    def test_check_function(self):
+        """
+        test for test function
+        """
+        self.assertTrue(_is_base64(self.user.user64))
+        self.assertFalse(_is_base64('string'))

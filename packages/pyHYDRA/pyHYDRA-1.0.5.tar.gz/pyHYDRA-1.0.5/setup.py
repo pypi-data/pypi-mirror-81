e5ce189856e2a24b2bdffdf4cf6d7564c7ab103a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:22 +0000

    Add package pyHYDRA/1.0.5/pyHYDRA-1.0.5.tar.gz

---
+++ packages/pyHYDRA/pyHYDRA-1.0.5.tar.gz/pyHYDRA-1.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyHYDRA",
+    version="1.0.5",
+    author="junhao.wen",
+    author_email="junhao.wen89@email.com",
+    description="A fast python implementation of HYDRA for classification and clustering",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anbai106/pyhydra",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pyhydra = pyhydra.main:main',
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
+

2d9bc64ce5e4f2cf3011bc3c8baf3ef23960fdcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:06 +0000

    Add package arris-tg2492lg/1.1.0/arris-tg2492lg-1.1.0.tar.gz

---
+++ packages/arris-tg2492lg/arris-tg2492lg-1.1.0.tar.gz/arris-tg2492lg-1.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arris-tg2492lg",
+    version="1.1.0",
+    author="vanbalken",
+    description="Python client for the Arris TG2492LG",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vanbalken/arris-tg2492lg",
+    packages=setuptools.find_packages(exclude=['tests']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

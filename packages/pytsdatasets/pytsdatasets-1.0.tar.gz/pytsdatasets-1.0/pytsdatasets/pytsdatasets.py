352450e9816c5c1f05049e4f6523336c4db33d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:18 +0000

    Add package pytsdatasets/1.0/pytsdatasets-1.0.tar.gz

---
+++ packages/pytsdatasets/pytsdatasets-1.0.tar.gz/pytsdatasets-1.0/pytsdatasets/pytsdatasets.py
@@ -0,0 +1,31 @@
+import os.path
+import pandas as pd
+
+pkgdir = os.path.dirname(__file__)
+
+# Jhonson & Jhonson
+jj = pd.read_csv(f'{pkgdir}/data/jj.csv', index_col=0, parse_dates=True)
+jj.index = jj.index.to_period("Q")
+
+# Global temperatures
+globaltemp = pd.read_csv(f'{pkgdir}/data/globaltemp.csv', index_col=0,
+                         parse_dates=True)
+globaltemp.index = globaltemp.index.to_period("Y")
+
+# CO2
+co2 = pd.read_csv(f'{pkgdir}/data/co2.csv', index_col=0, parse_dates=True)
+co2.index = co2.index.to_period("M")
+
+# Air Passengers
+airpassengers = pd.read_csv(f'{pkgdir}/data/airpassengers.csv', index_col=0,
+                            parse_dates=True)
+airpassengers.index = airpassengers.index.to_period("M")
+
+# US Change
+uschange = pd.read_csv(f'{pkgdir}/data/uschange.csv', index_col=0,
+                       parse_dates=True)
+uschange.index = uschange.index.to_period("Q")
+
+# H02
+h02 = pd.read_csv(f'{pkgdir}/data/h02.csv', index_col=0, parse_dates=True)
+h02.index = h02.index.to_period("M")
---
+++ packages/pytsdatasets/pytsdatasets-1.0.tar.gz/pytsdatasets-1.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setuptools.setup(
+    name='pytsdatasets',
+    version='1.0',
+    description='A python package that provides some time series datasets available R distribution and in astsa R package.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Mario Juez-Gil',
+    author_email='mariojg@ubu.es',
+    url='https://github.com/mjuez/pytsdatasets',
+    download_url='https://github.com/mjuez/pytsdatasets/archive/v1_0.tar.gz',
+    license="GPLv3",
+    install_requires=["pandas==1.1.2"],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers ',
+        'Intended Audience :: Education ',
+        'Intended Audience :: Science/Research ',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Education',
+        'Topic :: Database',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
+)

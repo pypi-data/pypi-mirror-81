6bdc87e9c3828bf84411e769ab077270799b6b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:54 +0000

    Add package cngi-prototype/0.0.64/cngi_prototype-0.0.64.tar.gz

---
+++ packages/cngi-prototype/cngi_prototype-0.0.64.tar.gz/cngi_prototype-0.0.64/cngi/conversion/__init__.py
@@ -0,0 +1,14 @@
+##################################
+# Conversion functions
+#
+# This package has a dependency on legacy CASA/casacore.  It is bundled with
+# the rest of CNGI for the prototype only. It will be a separate package in 
+# the future.
+#
+##################################
+from .convert_ms import *
+from .convert_image import *
+from .convert_asdm import *
+from .save_ms import *
+from .save_asdm import *
+from .save_image import *

12b325efd0a6effb08f1bb900160d6596b804e5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:30 +0000

    Add package enforce-pep8/0.0.11/enforce_pep8-0.0.11-py3-none-any.whl

---
+++ packages/enforce-pep8/enforce_pep8-0.0.11-py3-none-any.whl/punish/__init__.py
@@ -0,0 +1,60 @@
+"""Package contains interfaces to enforce PEP8 coding conventions.
+
+It will help to maintain programmers sanity by raising corresponding
+exceptions in case of not complying with PEP8 style.
+
+Please check for https://www.python.org/dev/peps/pep-0008/ to get familiar with PEP8 style.
+"""
+from typing import Tuple
+from punish.style import (
+    AbstractStyle,
+    BadAttributeNameError,
+    BadClassNameError,
+    DuplicateAttributeError,
+    MatchSignatureMeta,
+    NoDuplicateMeta,
+    NoLowerCaseMeta,
+    NoMixedCaseMeta,
+    PepStyleMeta,
+    SignatureError,
+    SingletonMeta,
+    abstractstyle,
+)
+from punish.type import (
+    AbstractContextManager,
+    Float,
+    Integer,
+    OrderTypedMeta,
+    String,
+    Typed,
+    enforce_type,
+    typed_property,
+)
+
+__author__: str = "Volodymyr Yahello"
+__email__: str = "vyahello@gmail.com"
+__package_name__: str = "enforce-pep8"
+__version__: str = "0.0.11"
+
+__all__: Tuple[str, ...] = (
+    "AbstractStyle",
+    "BadAttributeNameError",
+    "BadClassNameError",
+    "DuplicateAttributeError",
+    "MatchSignatureMeta",
+    "NoDuplicateMeta",
+    "NoLowerCaseMeta",
+    "NoMixedCaseMeta",
+    "PepStyleMeta",
+    "SignatureError",
+    "SingletonMeta",
+    "abstractstyle",
+    "AbstractContextManager",
+    "Float",
+    "Integer",
+    "OrderTypedMeta",
+    "String",
+    "Typed",
+    "enforce_type",
+    "typed_property",
+)

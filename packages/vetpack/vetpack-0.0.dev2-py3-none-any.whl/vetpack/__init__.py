23cf7c612c995fdd4a6d3e4fbc77e5510227106e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:47 +0000

    Add package vetpack/0.0.dev2/vetpack-0.0.dev2-py3-none-any.whl

---
+++ packages/vetpack/vetpack-0.0.dev2-py3-none-any.whl/vetpack/__init__.py
@@ -0,0 +1,11 @@
+"""
+An open-source package for vetting candidate exoplanetary transit signals.
+
+* Code: https://github.com/disruptiveplanets/vetpack
+"""
+from .centroiding import *  # noqa
+
+__author__ = "Benjamin V. Rackham"
+__email__ = "brackham@mit.edu"
+__license__ = "MIT"
+__description__ = "Tools for vetting transit signals"

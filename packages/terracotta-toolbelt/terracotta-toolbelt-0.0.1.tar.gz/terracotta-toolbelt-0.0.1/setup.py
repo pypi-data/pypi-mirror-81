50c833daa6e669a50277721db42a048aa6190234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:26 +0000

    Add package terracotta-toolbelt/0.0.1/terracotta-toolbelt-0.0.1.tar.gz

---
+++ packages/terracotta-toolbelt/terracotta-toolbelt-0.0.1.tar.gz/terracotta-toolbelt-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import pathlib
+import os
+
+from setuptools import setup
+
+
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name='terracotta-toolbelt',
+    author="Emil Haldrup Eriksen",
+    author_email="emil.h.eriksen@gmail.com",
+    description="A small Terracotta tool collection",
+    version=os.getenv("CI_COMMIT_TAG", "v0.0.1").strip("v"),
+    url='https://github.com/thedirtyfew/terracotta-toolbelt',
+    packages=setuptools.find_packages(),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires='>=3.6',
+    install_requires=['terracotta', 'flask', 'rasterio']
+)
+

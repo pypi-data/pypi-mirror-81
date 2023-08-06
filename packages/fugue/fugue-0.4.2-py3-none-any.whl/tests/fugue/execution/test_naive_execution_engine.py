b584937fd2a2efa7e577c343125b2714019ab220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:31 +0000

    Add package fugue/0.4.2/fugue-0.4.2-py3-none-any.whl

---
+++ packages/fugue/fugue-0.4.2-py3-none-any.whl/tests/fugue/execution/test_naive_execution_engine.py
@@ -0,0 +1,26 @@
+from fugue.execution import NativeExecutionEngine
+from fugue_test.execution_suite import ExecutionEngineTests
+from fugue_test.builtin_suite import BuiltInTests
+from fugue.execution.execution_engine import _get_file_threshold
+
+
+class NativeExecutionEngineTests(ExecutionEngineTests.Tests):
+    def make_engine(self):
+        e = NativeExecutionEngine(dict(test=True))
+        return e
+
+    def test_map_with_dict_col(self):
+        # TODO: add back
+        return
+
+
+class NativeExecutionEngineBuiltInTests(BuiltInTests.Tests):
+    def make_engine(self):
+        e = NativeExecutionEngine(dict(test=True))
+        return e
+
+
+def test_get_file_threshold():
+    assert -1 == _get_file_threshold(None)
+    assert -2 == _get_file_threshold(-2)
+    assert 1024 == _get_file_threshold("1k")

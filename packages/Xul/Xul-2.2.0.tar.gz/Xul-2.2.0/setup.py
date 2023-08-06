c01b3e3d9b855c0246b4089858a2c5776134d220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:59 +0000

    Add package Xul/2.2.0/Xul-2.2.0.tar.gz

---
+++ packages/Xul/Xul-2.2.0.tar.gz/Xul-2.2.0/setup.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+"""Setup script for building XML Utilities."""
+
+import codecs
+from os import path
+from setuptools import setup, find_packages
+from xul import __version__
+
+here = path.abspath(path.dirname(__file__))
+
+def long_description():
+    """Get the long description from the README.rst file."""
+    with codecs.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name="Xul",
+    version=__version__,
+
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=["lxml>=2.0"],
+    extras_require={
+        'syntax': ["Pygments>=2.0"]
+    },
+
+    author='Peter Adrichem',
+    author_email='Peter.Adrichem@gmail.com',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Text Processing :: Markup :: XML",
+        "Topic :: Utilities"
+    ],
+    description="XML (XPath, XSD, XSLT) Utilities",
+    keywords="xml xpath xslt xsd dtd 'xml schema' 'relax ng' rng",
+    license='MIT',
+    long_description=long_description(),
+    long_description_content_type='text/x-rst',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    platforms='CPython',
+    url='https://xul.readthedocs.io/',
+    project_urls={
+        'Documentation': 'https://xul.readthedocs.io/',
+        'Source': 'https://bitbucket.org/peteradrichem/xul'
+    },
+    entry_points={
+        'console_scripts': [
+            'transform = xul.cmd.transform:main',
+            'ppx = xul.cmd.ppx:main',
+            'xp = xul.cmd.xp:main',
+            'validate = xul.cmd.validate:main'
+        ]
+    }
+)
---
+++ packages/Xul/Xul-2.2.0.tar.gz/Xul-2.2.0/xul/ppxml.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+"""Pretty Print XML with UTF-8 encoding."""
+
+
+from __future__ import print_function
+
+import sys
+import io
+
+# pylint: disable=no-name-in-module
+# lxml ElementTree <https://lxml.de/>
+from lxml.etree import tostring
+
+# Import my own modules.
+from .etree import build_etree
+
+
+__all__ = ['prettyprint', 'pp_xml']
+
+def _private_pp(etree, syntax=True, xml_declaration=None):
+    """Pretty print XML ElementTree with (optional) syntax highlighting.
+
+    https://lxml.de/api.html#serialisation
+    https://lxml.de/api/lxml.etree-module.html#tostring
+    """
+    try:
+        # lxml.etree.tostring returns bytes (bytestring).
+        # https://docs.python.org/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.tostring
+        etree_string = tostring(
+            etree, encoding='UTF-8',
+            xml_declaration=xml_declaration, pretty_print=True)
+
+        if syntax:
+            # pygments.highlight() will return a Unicode string …
+            # https://pygments.org/docs/formatters/
+            etree_string = highlight(etree_string, lexer, Terminal256Formatter())
+        else:
+            # Bytes(string) => unicode string.
+            etree_string = etree_string.decode("utf-8")
+
+        # Fix output encoding (piping Python2 output to less).
+        if sys.stdout.encoding is None:
+            sys.stdout = io.open(sys.stdout.fileno(), 'w', encoding='utf8')
+        print(etree_string)
+    except IOError as e:
+        # Catch 'IOError: [Errno 32] Broken pipe' (multiple etrees).
+        if e.errno != 32:
+            sys.stderr.write("IOError: %s [%s]\n" % (e.strerror, e.errno))
+
+
+try:
+    # pylint: disable=wrong-import-position
+    from pygments.lexers import get_lexer_by_name
+    from pygments.formatters import Terminal256Formatter
+    from pygments import highlight
+except ImportError:
+    # pylint: disable=unused-argument
+    def prettyprint(etree, syntax=False, xml_declaration=None):
+        """Plain pretty print XML ElementTree (without syntax highlighting)."""
+        return _private_pp(etree, syntax=False, xml_declaration=xml_declaration)
+else:
+    lexer = get_lexer_by_name('xml')
+    def prettyprint(etree, syntax=True, xml_declaration=None):
+        """Pretty print XML ElementTree with (optional) syntax highlighting."""
+        return _private_pp(etree, syntax=syntax, xml_declaration=xml_declaration)
+
+
+def pp_xml(xml_source, parser=None, syntax=True, xml_declaration=None):
+    """Pretty Print XML source.
+
+    xml_source -- XML file, file-like object or URL
+    parser -- (optional) XML parser (lxml.etree.XMLParser)
+    syntax -- syntax highlighting (or not)
+    xml_declaration -- print XML declaration (or not)
+    """
+    xml_tree = build_etree(xml_source, parser=parser)
+    if xml_tree:
+        prettyprint(xml_tree, syntax=syntax, xml_declaration=xml_declaration)

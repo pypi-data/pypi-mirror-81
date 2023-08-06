8e95186c1678ee0f9ba3056ef8dbb0277616ab2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:37 +0000

    Add package Flask-Fulfil/0.3.2/Flask-Fulfil-0.3.2.tar.gz

---
+++ packages/Flask-Fulfil/Flask-Fulfil-0.3.2.tar.gz/Flask-Fulfil-0.3.2/flask_fulfil/__init__.py
@@ -0,0 +1,66 @@
+'''
+    flaskext.fulfil
+    ---------------
+
+    A Flask extension providing Fulfil.IO Api access.
+
+    :copyright: (c) 2016 by Fulfil.IO Inc.
+    :license: BSD, see LICENSE for more details.
+'''
+from fulfil_client import Client, BearerAuth
+from .utils import client_url
+
+
+__version__ = '0.3.2'
+__author__ = 'Fulfil.Io Inc.'
+__license__ = 'BSD'
+__copyright__ = '(c) 2016 by Fulfil.IO Inc.'
+__all__ = ['Fulfil']
+
+
+class Fulfil(object):
+    """
+
+    To get started you will wrap your application's app object something like
+    this::
+
+        app = Flask(__name__)
+        fulfil = Fulfil(app)
+
+    Configuration::
+
+        FULFIL_SUBDOMAIN: the subdomain of your Fulfil account.
+        FULFIL_API_KEY: The API_KEY to access Fulfil services.
+
+    :param app: The Flask application object. Defaults to None.
+    """
+    client = None
+
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        '''Initalizes the application with the extension.
+        :param app: The Flask application object.
+        '''
+        offline_access_token = app.config.get('FULFIL_OFFLINE_ACCESS_TOKEN')
+        if offline_access_token:
+            self.client = Client(
+                app.config['FULFIL_SUBDOMAIN'],
+                auth=BearerAuth(offline_access_token),
+                retry_on_rate_limit=True,
+            )
+        else:
+            self.client = Client(
+                app.config['FULFIL_SUBDOMAIN'],
+                app.config['FULFIL_API_KEY'],
+                retry_on_rate_limit=True,
+            )
+        app.jinja_env.filters['client_url'] = client_url
+
+    def model(self, name):
+        return self.client.model(name)
+
+    def record(self, model_name, record_id):
+        return self.client.record(model_name, record_id)
---
+++ packages/Flask-Fulfil/Flask-Fulfil-0.3.2.tar.gz/Flask-Fulfil-0.3.2/setup.py
@@ -0,0 +1,34 @@
+'''
+Flask-Fulfil
+============
+
+Fulfil.IO for your Flask app
+'''
+
+from setuptools import setup
+
+setup(
+    name='Flask-Fulfil',
+    version='0.3.2',
+    url='https://github.com/fulfilio/flask-fulfil',
+    license='BSD',
+    author='Fulfil.IO Inc.',
+    author_email='hello@fulfil.io',
+    description='Fulfil.IO for Flask Apps',
+    long_description=__doc__,
+    packages=['flask_fulfil'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Flask', 'fulfil_client>=0.9.0', 'six'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
+
+

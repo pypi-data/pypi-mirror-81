052d8fe6bc79f56aacdab7826d3a661874e68ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:02 +0000

    Add package UUnonymous/0.0.3/UUnonymous-0.0.3.tar.gz

---
+++ packages/UUnonymous/UUnonymous-0.0.3.tar.gz/UUnonymous-0.0.3/uunonymous/dummy.py
@@ -0,0 +1,5 @@
+from uunonymous import Anonymize
+
+
+anon = Anonymize({ 'casper': 1234, 'fleur': 5678 })
+anon.substitute('/Users/casperkaandorp/Desktop/Youth/echo', '/Users/casperkaandorp/Desktop/Youth/echo/res')

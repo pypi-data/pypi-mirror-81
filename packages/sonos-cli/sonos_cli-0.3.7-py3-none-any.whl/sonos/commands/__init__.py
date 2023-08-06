0f671c896f60a21d299dbcee065bcfd0a09795c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:55 +0000

    Add package sonos-cli/0.3.7/sonos_cli-0.3.7-py3-none-any.whl

---
+++ packages/sonos-cli/sonos_cli-0.3.7-py3-none-any.whl/sonos/commands/__init__.py
@@ -0,0 +1,25 @@
+from .config import set_config
+from .get import get
+from .login import login
+from .logout import logout
+from .next import next
+from .pause import pause
+from .play import play
+from .prev import prev
+from .set import set
+from .status import status
+from .volume import volume
+
+__all__ = [
+    "set_config",
+    "get",
+    "login",
+    "logout",
+    "next",
+    "pause",
+    "play",
+    "prev",
+    "set",
+    "status",
+    "volume",
+]

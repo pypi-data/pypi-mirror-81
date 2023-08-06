7272cbcbb52fab736c8f0b91e712e7f07a5f9d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:11 +0000

    Add package simpleCalculator222-WebSoft-tech/0.0.1/simpleCalculator222_WebSoft_tech-0.0.1-py3-none-any.whl

---
+++ packages/simpleCalculator222-WebSoft-tech/simpleCalculator222_WebSoft_tech-0.0.1-py3-none-any.whl/Calculator/__init__.py
@@ -0,0 +1,15 @@
+
+
+def add_numbers(num1, num2):
+    print(num1 + num2)
+    
+def subtract_numbers(num1, num2):
+    print(num1 - num2)
+    
+def multiply_numbers(num1, num2):
+    print(num1 * num2)
+    
+def divide_numbers(num1, num2):
+    print(num1 / num2) 
+    
+

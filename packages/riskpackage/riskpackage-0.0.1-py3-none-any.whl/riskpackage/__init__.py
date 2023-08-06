e09961ddd26dbf55335ff7f490b332228a4e869e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:53 +0000

    Add package riskpackage/0.0.1/riskpackage-0.0.1-py3-none-any.whl

---
+++ packages/riskpackage/riskpackage-0.0.1-py3-none-any.whl/riskpackage/__init__.py
@@ -0,0 +1,14 @@
+class ReportsUtility:
+    def __init__(self):
+        pass
+
+    def say_hello(self, name):
+        print(f"Hello, {self.name}, i am inside reports")
+
+class CohortReports:
+    def __init__(self):
+        pass
+
+    def cohort(self, name):
+        print(f"Hello, {self.name}, I am inside cohort ")
+    

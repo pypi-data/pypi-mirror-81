cf13e2197adf17e5d12baa290956d1e59b9eb5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:31 +0000

    Add package esvg/0.0.1/esvg-0.0.1.tar.gz

---
+++ packages/esvg/esvg-0.0.1.tar.gz/esvg-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='esvg',
+      version='0.0.1',
+      description='A library to generate animated SVGs',
+      url='http://github.com/lletfrix/esvg',
+      author='Rafael Sánchez',
+      author_email='lletfrix@gmail.com',
+      license='MIT',
+      packages=['esvg'],
+      zip_safe=False)

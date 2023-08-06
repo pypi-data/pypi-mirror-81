3a4c40954f956474f24560d52c41e2faf90433b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:16 +0000

    Add package tap-asana/2.1.1/tap-asana-2.1.1.tar.gz

---
+++ packages/tap-asana/tap-asana-2.1.1.tar.gz/tap-asana-2.1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-asana",
+    version="2.1.1",
+    description="Singer.io tap for extracting Asana data",
+    author="Stitch",
+    url="http://github.com/singer-io/tap-asana",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_asana"],
+    install_requires=[
+        "asana==0.10.2",
+        'singer-python==5.9.0'
+    ],
+    extras_require={
+        'dev': [
+            'pylint',
+            'ipdb',
+            'requests==2.20.0',
+            'nose'
+        ]
+    },
+    entry_points="""
+    [console_scripts]
+    tap-asana=tap_asana:main
+    """,
+    packages=["tap_asana"],
+    package_data = {
+        "schemas": ["tap_asana/schemas/*.json"]
+    },
+    include_package_data=True,
+)
---
+++ packages/tap-asana/tap-asana-2.1.1.tar.gz/tap-asana-2.1.1/tap_asana/streams/tasks.py
@@ -0,0 +1,65 @@
+
+from singer import utils
+from tap_asana.context import Context
+from tap_asana.streams.base import Stream
+
+
+class Tasks(Stream):
+  name = 'tasks'
+  replication_key = "modified_at"
+  replication_method = 'INCREMENTAL'
+  fields = [
+    "gid",
+    "resource_type",
+    "name",
+    "approval_status",
+    "assignee_status",
+    "completed",
+    "completed_at",
+    "completed_by",
+    "created_at",
+    "dependencies",
+    "dependents",
+    "due_at",
+    "due_on",
+    "external",
+    "hearted",
+    "hearts",
+    "html_notes",
+    "is_rendered_as_seperator",
+    "liked",
+    "likes",
+    "memberships",
+    "modified_at",
+    "notes",
+    "num_hearts",
+    "num_likes",
+    "num_subtasks",
+    "resource_subtype",
+    "start_on",
+    "assignee",
+    "custom_fields",
+    "followers",
+    "parent",
+    "permalink_url",
+    "projects",
+    "tags",
+    "workspace"
+  ]
+
+
+  def get_objects(self):
+    bookmark = self.get_bookmark()
+    session_bookmark = bookmark
+    modified_since = bookmark.strftime("%Y-%m-%dT%H:%M:%S.%f")
+    opt_fields = ",".join(self.fields)
+    for workspace in self.call_api("workspaces"):
+      for project in self.call_api("projects", workspace=workspace["gid"]):
+        for task in self.call_api("tasks", project=project["gid"], opt_fields=opt_fields, modified_since=modified_since): 
+          session_bookmark = self.get_updated_session_bookmark(session_bookmark, task[self.replication_key])
+          if self.is_bookmark_old(task[self.replication_key]):
+            yield task
+    self.update_bookmark(session_bookmark)
+
+
+Context.stream_objects['tasks'] = Tasks

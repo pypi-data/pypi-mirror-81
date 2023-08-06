21343af534340193cc516e18194abcc6aec076d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:48 +0000

    Add package bayes-optim/0.1.2/bayes-optim-0.1.2.tar.gz

---
+++ packages/bayes-optim/bayes-optim-0.1.2.tar.gz/bayes-optim-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name="bayes-optim",
+    version="0.1.2",
+    author="Hao Wang",
+    author_email="wangronin@gmail.com",
+    packages=find_packages(),
+    description="A Bayesian Optimization Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wangronin/Bayesian-Optimization",
+    package_dir={'bayes_optim': 'bayes_optim'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    download_url='https://github.com/wangronin/Bayesian-Optimization/archive/v0.1.2.tar.gz',
+    python_requires='>=3.6',
+    install_requires=[
+        'dill>=0.3.2',
+        'joblib>=0.16.0',
+        'numpy>=1.19.0',
+        'pyDOE==0.3.8',
+        'scikit-learn>=0.23.0',
+        'scipy>=1.5.0',
+        'sklearn==0.0',
+        'tabulate>=0.8.7',
+        'threadpoolctl>=2.1.0'
+    ]
+)

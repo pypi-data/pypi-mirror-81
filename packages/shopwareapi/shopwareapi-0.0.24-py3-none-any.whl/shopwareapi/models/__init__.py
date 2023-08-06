6934b440443f5f814e15763c59bdd0032c662450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:19 +0000

    Add package shopwareapi/0.0.24/shopwareapi-0.0.24-py3-none-any.whl

---
+++ packages/shopwareapi/shopwareapi-0.0.24-py3-none-any.whl/shopwareapi/models/__init__.py
@@ -0,0 +1,7 @@
+from .currency import Currency
+from .product import Product
+from .category import Category
+from .price import Price
+from .tax import Tax
+from .manufacturer import Manufacturer
+from .saleschannel import SalesChannel

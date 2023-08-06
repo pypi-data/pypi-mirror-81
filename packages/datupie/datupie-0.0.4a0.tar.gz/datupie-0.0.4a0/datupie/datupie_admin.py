dccbe543b3b366b27dd205394e3be81fbf800116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:19 +0000

    Add package datupie/0.0.4a0/datupie-0.0.4a0.tar.gz

---
+++ packages/datupie/datupie-0.0.4a0.tar.gz/datupie-0.0.4a0/datupie/datupie_admin.py
@@ -0,0 +1,10 @@
+import argparse
+from datupie.function_maps import FUNCTION_MAP
+
+def main():    
+#if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', choices=FUNCTION_MAP.keys())
+    args = parser.parse_args()
+    func = FUNCTION_MAP[args.command]
+    func()

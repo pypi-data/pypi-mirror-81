f19cd9311d9734cd5688a578e8985ec10dd2c241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:52 +0000

    Add package optimparameters/1.0.6/optimparameters-1.0.6.tar.gz

---
+++ packages/optimparameters/optimparameters-1.0.6.tar.gz/optimparameters-1.0.6/README.rst
@@ -0,0 +1,5 @@
+=======================
+Optimization Parameters
+=======================
+
+A simple parameters module for optimization problems. Attributes and methods are provided to aid in fitting routines.
---
+++ packages/optimparameters/optimparameters-1.0.6.tar.gz/optimparameters-1.0.6/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+import os
+
+# Get requirements
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="optimparameters",
+    version="1.0.6",
+    author="Bryson Cale",
+    author_email="bryson.cale1@gmail.com",
+    description="Simple optimization parameters implementation.",
+    longdescription=long_description,
+    long_description_content_type="text/x-rst",
+    packages = setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    url="https://github.com/astrobc1/optimparameters",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix"
+    ],
+    python_requires='>=3.6'
+)

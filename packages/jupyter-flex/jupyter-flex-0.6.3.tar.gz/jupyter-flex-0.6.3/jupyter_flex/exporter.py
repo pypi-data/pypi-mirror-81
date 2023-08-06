1907612e0aefb1285aff02987022c9f8bf531b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:50 +0000

    Add package jupyter-flex/0.6.3/jupyter-flex-0.6.3.tar.gz

---
+++ packages/jupyter-flex/jupyter-flex-0.6.3.tar.gz/jupyter-flex-0.6.3/jupyter_flex/exporter.py
@@ -0,0 +1,36 @@
+import base64
+import os
+import sys
+from traitlets import default
+
+import jinja2
+from nbconvert.exporters.html import HTMLExporter
+
+from .utils import DEV_MODE
+
+
+@jinja2.contextfunction
+def include_external_file(ctx, name):
+    """Include a file relative to the notebook"""
+    with open(os.path.abspath(name), "r") as f:
+        content = f.read()
+    return jinja2.Markup(content)
+
+
+class FlexExporter(HTMLExporter):
+    # "File -> Download as" menu in the notebook
+    export_from_notebook = "Flex Dashboard"
+
+    @default("template_name")
+    def _template_name_default(self):
+        return "flex"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.environment.globals["dev_mode"] = DEV_MODE
+
+    def _init_resources(self, resources):
+        resources = super()._init_resources(resources)
+
+        resources["include_external_file"] = include_external_file
+        return resources

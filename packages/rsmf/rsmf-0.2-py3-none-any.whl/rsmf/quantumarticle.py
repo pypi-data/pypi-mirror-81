f059e93ca628d2500d62d7e1c2486fdb606f1fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:37 +0000

    Add package rsmf/0.2/rsmf-0.2-py3-none-any.whl

---
+++ packages/rsmf/rsmf-0.2-py3-none-any.whl/rsmf/quantumarticle.py
@@ -0,0 +1,52 @@
+"""
+Support for the quantumarticle documentclass of Quantum journal.
+"""
+
+import matplotlib.pyplot as plt
+
+from .revtexlike import RevtexLikeFormatter, RevtexLikeParser
+
+
+class QuantumarticleFormatter(RevtexLikeFormatter):
+    """Sets up the plot with the fitting arguments so that the font sizes of the plot
+    and the font sizes of the document are well aligned.
+
+    Args:
+        columns (str, optional):  the columns you used to set up your quantumarticle,
+            either "onecolumn" or "twocolumn". Defaults to "twocolumn".
+        paper (str, optional): the paper size you used to set up your quantumarticle,
+            either "a4paper" or "letterpaper". Defaults to "a4paper".
+        fontsize (int, optional): the fontsize you used to set up your quantumarticle,
+            either 10, 11 or 12. Defaults to 10.
+    """
+
+    _columnwidths = {
+        "onecolumn": {"a4paper": 5.93, "letterpaper": 6.16},
+        "twocolumn": {"a4paper": 3.22, "letterpaper": 3.34},
+    }
+
+    _wide_columnwidths = {
+        "onecolumn": {"a4paper": 5.93, "letterpaper": 6.16},
+        "twocolumn": {"a4paper": 6.72, "letterpaper": 6.95},
+    }
+
+    _colors = {"quantumviolet": "#53257F", "quantumgray": "#555555"}
+
+    def __init__(self, columns="twocolumn", paper="a4paper", fontsize=10, **kwargs):
+        super().__init__(columns, paper, fontsize)
+
+    def set_rcParams(self):
+        """Adjust the rcParams to the default values for Quantumarticle."""
+        super().set_rcParams()
+
+        plt.rcParams["font.family"] = "sans-serif"
+
+        plt.rcParams[
+            "pgf.preamble"
+        ] = r"\usepackage{lmodern} \usepackage[utf8x]{inputenc} \usepackage[T1]{fontenc}"
+
+        plt.rcParams["axes.edgecolor"] = self._colors["quantumgray"]
+
+
+"""Parser for the quantumarticle document class."""
+quantumarticle_parser = RevtexLikeParser(["{quantumarticle}"], QuantumarticleFormatter)
---
+++ packages/rsmf/rsmf-0.2-py3-none-any.whl/rsmf/setup.py
@@ -0,0 +1,80 @@
+"""
+Main routines to invoke the module from code.
+"""
+
+import re
+from pathlib import Path
+
+from .quantumarticle import quantumarticle_parser
+from .revtex import revtex_parser
+
+_PARSERS = [quantumarticle_parser, revtex_parser]
+
+_comment_regex = re.compile("(%.*)")
+
+
+def _clean_preamble(preamble):
+    """Clean the preamble.
+
+    This removes comments that could contain spurious information.
+
+    Args:
+        preamble (str): The preamble to be cleaned
+
+    Returns:
+        str: The cleaned preamble
+    """
+    preamble = _comment_regex.sub("", preamble)
+    return preamble
+
+
+def _extract_preamble(path):
+    """Extract the preamble of a tex document.
+
+    Args:
+        path (Union[str,pathlib.Path]): Path to the tex file
+
+    Returns:
+        str: The preamble of the tex file
+    """
+    lines = []
+
+    with open(path, "r") as file:
+        for line in file:
+            if "\\begin{document}" in line:
+                break
+
+            lines.append(line)
+
+    return "".join(lines)
+
+
+def setup(arg):
+    """Get a formatter corresponding to the document's class.
+
+    Args:
+        arg (str): Either path to a tex file or preamble of a tex file, containing at least the \documentclass command.
+
+    Raises:
+        Exception: When no formatter for the given document was found.
+
+    Returns:
+        object: A formatter for the given document/preamble.
+    """
+    if Path(arg).exists():
+        preamble = _extract_preamble(arg)
+    else:
+        preamble = arg
+
+    preamble = _clean_preamble(preamble)
+
+    result = None
+    for parser in _PARSERS:
+        result = parser(preamble)
+
+        if result:
+            return result
+
+    raise Exception(
+        "No formatter was found for the given argument. This means either there is no formatter, or, if you gave a file path that it does not exist."
+    )

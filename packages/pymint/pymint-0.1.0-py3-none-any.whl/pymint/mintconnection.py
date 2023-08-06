6a44c82d752cd1521d6f67018494938d3202cef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:34 +0000

    Add package pymint/0.1.0/pymint-0.1.0-py3-none-any.whl

---
+++ packages/pymint/pymint-0.1.0-py3-none-any.whl/pymint/mintconnection.py
@@ -0,0 +1,21 @@
+from pymint.mintparams import MINTParams
+from parchmint.connection import Connection
+from pymint.minttarget import MINTTarget
+from typing import List
+
+class MINTConnection(Connection):
+
+    def __init__(self, name: str, technology: str, params:dict, source: MINTTarget, sinks: List[MINTTarget], layer:str = '0') -> None:
+        
+        self.name = name
+        self.ID = name
+        self.entity = technology
+        self.params = MINTParams(params)
+        self.source = source
+        self.sinks = sinks
+        self.layer = layer
+
+
+    def toMINT(self) -> str:
+        ret = "{} {} from {} to {} {} ;".format(self.entity, self.name, self.source.toMINT(), " ".join([item.toMINT() for item in self.sinks]), self.params.toMINT())
+        return ret

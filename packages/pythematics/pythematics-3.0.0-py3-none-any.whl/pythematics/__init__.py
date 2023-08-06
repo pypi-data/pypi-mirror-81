76033897556fba0cc4ab071d81b7268c4155836e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:27 +0000

    Add package pythematics/3.0.0/pythematics-3.0.0-py3-none-any.whl

---
+++ packages/pythematics/pythematics-3.0.0-py3-none-any.whl/pythematics/__init__.py
@@ -0,0 +1,21 @@
+__all__ = [
+    "basic",
+    "constants",
+    "functions",
+    "linear",
+    "powers",
+    "random",
+    "polynomials",
+    "num_theory",
+    "trigonometric",
+]
+
+from . import basic
+from . import constants
+from . import functions
+from . import linear
+from . import powers
+from . import random
+from . import polynomials
+from . import num_theory
+from . import trigonometric

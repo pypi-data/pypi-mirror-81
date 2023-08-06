235bc530ff6c603b13c4eb44dea220443b38f5a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:24 +0000

    Add package hyperglass/1.0.0b59/hyperglass-1.0.0b59.tar.gz

---
+++ packages/hyperglass/hyperglass-1.0.0b59.tar.gz/hyperglass-1.0.0b59/hyperglass/api/events.py
@@ -0,0 +1,27 @@
+"""API Events."""
+
+# Project
+from hyperglass.util import check_redis
+from hyperglass.exceptions import HyperglassError
+from hyperglass.configuration import REDIS_CONFIG, params
+
+
+async def _check_redis():
+    """Ensure Redis is running before starting server.
+
+    Raises:
+        HyperglassError: Raised if Redis is not running.
+
+    Returns:
+        {bool} -- True if Redis is running.
+    """
+    try:
+        await check_redis(db=params.cache.database, config=REDIS_CONFIG)
+    except RuntimeError as e:
+        raise HyperglassError(str(e), level="danger") from None
+
+    return True
+
+
+on_startup = (_check_redis,)
+on_shutdown = ()

b99404c8c2625fe6154153b5493c8ae2249c1562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:45 +0000

    Add package graiax-nem/0.0.4.dev5/graiax-nem-0.0.4.dev5.tar.gz

---
+++ packages/graiax-nem/graiax-nem-0.0.4.dev5.tar.gz/graiax-nem-0.0.4.dev5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='graiax-nem',
+    version='0.0.4.dev5',
+    author='Xiao_Jin',
+    keywords='graia graiax nem graiax-nem graia-message',
+    description='适用于 Graia Framework 的消息解析过滤器',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jinzhijie/graiax-nem',
+    packages=['graiax/nem'],
+    install_requires=['graia-application-mirai'],
+    python_requires='>=3.7',
+)

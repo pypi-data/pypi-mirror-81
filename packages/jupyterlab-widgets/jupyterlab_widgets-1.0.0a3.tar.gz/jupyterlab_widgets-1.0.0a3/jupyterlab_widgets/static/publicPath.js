2298adb08d05428324028a4f29df8b6731dd9d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:49 +0000

    Add package jupyterlab-widgets/1.0.0a3/jupyterlab_widgets-1.0.0a3.tar.gz

---
+++ packages/jupyterlab-widgets/jupyterlab_widgets-1.0.0a3.tar.gz/jupyterlab_widgets-1.0.0a3/jupyterlab_widgets/static/publicPath.js
@@ -0,0 +1,16 @@
+
+function getOption(name) {
+  let configData = Object.create(null);
+  // Use script tag if available.
+  if (typeof document !== 'undefined' && document) {
+    const el = document.getElementById('jupyter-config-data');
+
+    if (el) {
+      configData = JSON.parse(el.textContent || '{}');
+    }
+  }
+  return configData[name] || '';
+}
+
+// eslint-disable-next-line no-undef
+__webpack_public_path__ = getOption('fullLabextensionsUrl') + '/@jupyter-widgets/jupyterlab-manager/';

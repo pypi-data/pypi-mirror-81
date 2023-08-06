b9a2f6e1db9b126fd613b112dc40afd833c9bc2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:06 +0000

    Add package nlu/1.0.0rc1/nlu-1.0.0rc1-py3-none-any.whl

---
+++ packages/nlu/nlu-1.0.0rc1-py3-none-any.whl/nlu/components/normalizer.py
@@ -0,0 +1,12 @@
+from nlu.pipe_components import SparkNLUComponent
+
+class Normalizer(SparkNLUComponent):
+    def __init__(self, annotator_class='normalizer', language='en', component_type='normalizer', get_default=True, nlp_ref='',model=None):
+        SparkNLUComponent.__init__(self, annotator_class, component_type)
+        if model != None :
+            self.model = model
+
+        elif annotator_class == 'normalizer':
+            from nlu import SparkNLPNormalizer
+            if get_default : self.model =  SparkNLPNormalizer.get_default_model()
+            else : self.model =  SparkNLPNormalizer.get_pretrained_model(nlp_ref, language) # there is no pretrained API for Normalizer in SparkNLP yet

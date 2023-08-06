a24143fabcf4aaa1038fbd7476209cde0980b10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:31 +0000

    Add package mcdm/1.2/mcdm-1.2-py3-none-any.whl

---
+++ packages/mcdm/mcdm-1.2-py3-none-any.whl/mcdm/correlation/pearson.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2020 Dimitrios-Georgios Akestoridis
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import numpy as np
+
+
+def pearson(z_matrix):
+    """Python implementation of the Pearson correlation method.
+
+    For more information, see the following publication:
+      * J. L. Rodgers and W. A. Nicewander, "Thirteen ways to look at the
+        correlation coefficient," The American Statistician, vol. 42, no. 1,
+        pp. 59--66, 1988. DOI: 10.2307/2685263.
+    """
+    # Make sure that the matrix is a float64 NumPy array
+    z_matrix = np.array(z_matrix, dtype=np.float64)
+
+    return np.corrcoef(z_matrix, rowvar=False)

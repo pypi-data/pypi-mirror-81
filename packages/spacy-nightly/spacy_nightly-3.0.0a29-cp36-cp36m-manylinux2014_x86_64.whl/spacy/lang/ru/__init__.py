402c6f2ea57ed867a9bd57df1c8875ecb31be909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:11 +0000

    Add package spacy-nightly/3.0.0a29/spacy_nightly-3.0.0a29-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/spacy-nightly/spacy_nightly-3.0.0a29-cp36-cp36m-manylinux2014_x86_64.whl/spacy/lang/ru/__init__.py
@@ -0,0 +1,39 @@
+from typing import Optional
+from thinc.api import Model
+
+from .stop_words import STOP_WORDS
+from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
+from .lex_attrs import LEX_ATTRS
+from .lemmatizer import RussianLemmatizer
+from ...language import Language
+from ...lookups import Lookups
+
+
+class RussianDefaults(Language.Defaults):
+    tokenizer_exceptions = TOKENIZER_EXCEPTIONS
+    lex_attr_getters = LEX_ATTRS
+    stop_words = STOP_WORDS
+
+
+class Russian(Language):
+    lang = "ru"
+    Defaults = RussianDefaults
+
+
+@Russian.factory(
+    "lemmatizer",
+    assigns=["token.lemma"],
+    default_config={"model": None, "mode": "pymorphy2", "lookups": None},
+    default_score_weights={"lemma_acc": 1.0},
+)
+def make_lemmatizer(
+    nlp: Language,
+    model: Optional[Model],
+    name: str,
+    mode: str,
+    lookups: Optional[Lookups],
+):
+    return RussianLemmatizer(nlp.vocab, model, name, mode=mode, lookups=lookups)
+
+
+__all__ = ["Russian"]

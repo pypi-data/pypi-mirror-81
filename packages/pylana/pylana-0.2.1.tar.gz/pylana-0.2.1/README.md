34b9f0498b4c116492eb379ba3d1a6de0120b7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:51 +0000

    Add package pylana/0.2.1/pylana-0.2.1.tar.gz

---
+++ packages/pylana/pylana-0.2.1.tar.gz/pylana-0.2.1/README.md
@@ -0,0 +1,59 @@
+# PyLana
+[![Python 3.7](https://img.shields.io/badge/python-3.7-blue.svg
+)](https://www.python.org/downloads/release/python-374/)
+[![Pylana](https://img.shields.io/badge/pylana-v0.2.1-blue)](https://pypi.org/project/pylana/)
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://github.com/lanalabs/pylana/graphs/commit-activity)
+
+PyLana is Python API for [LANA Process Mining](https://lanalabs.com/). It focuses on resource management, but provides methods that can be used to access the processed data as well.
+
+This package is still in initial development state. Anything may change at any time. The public API should not be considered stable.
+
+## Installation
+
+You can install PyLana directly from PyPi with e.g. 
+
+```bash
+$ pip install pylana
+```
+
+# How to get started
+
+To connect with an api at e.g. 'https:://cloud-backend.lanalabs.com', first create an API with
+
+```python
+from pylana import create_api
+
+api = create_api('https', 'cloud-backend.lanalabs.com', <API Key>)
+```
+
+You will require an API key that is valid for your LANA deployment. The returned api stores the url for a LANA Process Mining api as well as your authentication. After creation you can us it to manage the LANA process mining resources. Among other things you can upload data from python pandas data frames directly or connect logs and shiny dashboard resources referencing them by their names.
+
+To upload a new log called "new-event-log" and shiny-dashboard named "new-shiny-dashboard", and connect them with each other, you can use the following code
+
+```python
+upload_response = api.upload_event_log_df(
+                            'new-event-log', df_event_log,
+                            time_format='YYYY-mm-dd',
+                            df_case=df_case_attributes)
+shiny_dashboard = api.create_shiny_dashboard('new-shiny-dashboard')
+connection_response = api.connect_shiny_dashboard(
+                                upload_response.json()['id'],
+                                shiny_dashboard['id'])
+```
+
+We also provide basic methods for direct http requests to LANA API endpoints, for example
+
+```python
+response_list = api.get('/api/v2/dashboards')
+```
+
+will return a response with a list of dashboard metadata. For details about the endpoints refer to the swagger documentation of the LANA API.
+
+# How to contribute
+
+See the details in [CONTRIBUTING.md](CONTRIBUTING.md).
+
+# License
+
+[Apache License 2.0](http://www.apache.org/licenses/)
+
---
+++ packages/pylana/pylana-0.2.1.tar.gz/pylana-0.2.1/pylana/dashboards.py
@@ -0,0 +1,237 @@
+"""
+dashboard management api requests
+"""
+
+from typing import List
+
+from requests import Response
+
+from pylana.resources import ResourceAPI
+
+
+class DashboardAPI(ResourceAPI):
+
+    def list_dashboards(self, **kwargs) -> List[dict]:
+        """
+        List all dashboards available to the user.
+        
+        Args:
+            **kwargs:
+                Keyword arguments passed to requests functions.
+            
+        Returns: 
+            A list of strings denoting dashboard names.
+        """
+        return self.list_resources('v2/dashboards', **kwargs)
+
+    def get_dashboard_ids(self, contains: str = '.*', **kwargs) \
+            -> List[str]:
+        """
+        List ids of dashboard pages with matching names.
+
+        Args:
+           contains:  
+                A string denoting a regular expression
+                matched against the dashboard titles.
+            **kwargs: 
+                Keyword arguments passed to requests functions.
+            
+        Returns:
+            A list of strings denoting the ids of the dashboard pages.
+        """
+        return self.get_resource_ids('v2/dashboards', contains, **kwargs)
+
+    def get_dashboard_id(self, contains: str, **kwargs) -> str:
+        """
+        Get dashboard id with matching name.
+
+        Args:
+            contains:  
+                A string denoting a regular expression
+                matched against the dashboard name.
+            **kwargs: 
+                Keyword arguments passed to requests functions.
+            
+        Returns: 
+            A string denoting the id of the dashboard page.
+
+        Raises:
+            Exception:
+                None or more than one dashboard name matches the
+                regular expression.
+        """
+        return self.get_resource_id('v2/dashboards', contains, **kwargs)
+
+    # TODO json lacks sharing options
+    def create_dashboard(self, name: str, items: List[dict], 
+                         is_active: bool = False, **kwargs) -> Response:
+        """
+        Create a dashboard with a list of dashboard items.
+
+        Args:
+            name:  
+                A string denoting the dashboard name.
+            items:
+                A list of dicts denoting the dashboard items (referenced as
+                charts in LANA Process Mining)
+            is_active:
+                A boolean denoting if the dashboard should be set
+                as the last one selected by the user.
+            **kwargs: 
+                Keyword arguments passed to requests functions.
+            
+        Returns:
+            The requests response of the lana api call.
+        """
+        request_data = {'title': name,
+                        'items': items,
+                        'isActive': is_active}
+
+        return self.create_resource('v2/dashboards/', json=request_data, **kwargs)
+
+    def describe_dashboard(self, contains: str = None,
+                           dashboard_id: str = None, **kwargs) -> dict:
+        """
+        Get dashboard metadata.
+
+        Args:
+            contains: 
+                A string denoting a regular expression
+                matched against the dashboard name. 
+                Matching several names raises an exception.
+            dashboard_id: 
+                A string denoting the id of the dashboard page.
+            **kwargs: 
+                Keyword arguments passed to requests functions.
+            
+        Returns:
+            A dictionary with the dashboard data.
+        """
+        return self.describe_resource('v2/dashboards', contains, dashboard_id, 
+                                      **kwargs)
+    
+    def describe_dashboard_items(self, contains: str = None,
+                                 dashboard_id: str = None, **kwargs) \
+            -> List[dict]:
+        """
+        Get dashboard item metadata.
+
+        Args:
+            contains: 
+                A string denoting a regular expression
+                matched against the dashboard name. 
+                Matching several names raises an exception.
+            dashboard_id: 
+                A string denoting the id of the dashboard page.
+            **kwargs: 
+                Keyword arguments passed to requests functions.
+            
+        Returns:
+            A list with the dashboard items as dicts (items are referenced as
+            charts in LANA Process Mining)
+        """
+        dashboard = self.describe_dashboard(contains, dashboard_id, **kwargs)
+        
+        return dashboard["items"]
+
+    def delete_dashboard(self, dashboard_id: str, **kwargs) \
+            -> Response:
+        """
+        Delete a dashboard by its id.
+        
+        Args:
+            dashboard_id: 
+                A string denoting the id of the dashboard page.
+            **kwargs: 
+                Keyword arguments passed to requests functions.
+
+        Returns:
+            The requests response of the lana api call.
+        """
+        return self.delete_resource('v2/dashboards', dashboard_id,
+                                    **kwargs)
+
+    def delete_dashboards(self, contains: str = None,
+                          ids: List[str] = None, **kwargs) -> \
+            List[Response]:
+        """
+        Delete dashboards with matching names.
+
+        Args:
+            contains: 
+                A string denoting a regular expression. 
+                It is ignored when ids are passed.
+            ids: 
+                A list of strings denoting ids of dashboard pages.
+            **kwargs:
+                Keyword arguments passed to requests functions.
+                
+        Returns:
+            A list of request responses of the calls to the lana api.
+        """
+        return self.delete_resources('v2/dashboards', contains, ids,
+                                     **kwargs)
+
+    # TODO consider sharing by names
+    def share_dashboard(self, dashboard_id: str, organization_ids: List[str],
+                        **kwargs) -> Response:
+        """
+        Share a dashboard with organizations by ids.
+        Args:
+            dashboard_id:
+                A string denoting the id of the dashboard page.
+            organization_ids:
+                A list of strings denoting ids of organizations to share with.
+            **kwargs:
+                Keyword arguments passed to requests functions.
+
+        Returns:
+            The requests response of the lana api call.
+        """
+        body = {
+            "shareWithOrganizations": organization_ids
+        }
+        return self.patch(f"/api/v2/dashboards/{dashboard_id}/sharing", json=body,
+                          **kwargs)
+
+    def unshare_dashboard(self, dashboard_id: str, organization_ids: List[str],
+                          **kwargs) -> Response:
+        """
+        Unshare a dashboard with organizations by ids.
+
+        Args:
+            dashboard_id:
+                A string denoting the id of the dashboard page.
+            organization_ids:
+                A list of strings denoting ids of organizations to unshare the
+                dashboard with.
+            **kwargs:
+                Keyword arguments passed to requests functions.
+
+        Returns:
+            The requests response of the lana api call.
+        """
+        body = {
+            "unshareWithOrganizations": organization_ids
+        }
+        return self.patch(f"/api/v2/dashboards/{dashboard_id}/sharing", json=body,
+                          **kwargs)
+
+    def connect_dashboard(self, log_id, dashboard_id, **kwargs) \
+            -> Response:
+        """
+        Connect a dashboard with a log by their ids.
+        
+        Args:
+            log_id: 
+                A string denoting the id of the log in LANA.
+            dashboard_id:
+                A string denoting the id of the dashboard page.
+            **kwargs: 
+                Keyword arguments passed to requests functions.
+
+        Returns:
+            The requests response of the lana api call.
+        """
+        dct = {'log_id': log_id, 'dashboard_id': dashboard_id}
+        return self.connect_resources(dct, **kwargs)

0bd70dfccd707c52c5a32ce6c40fba6872fef2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:30 +0000

    Add package lib-log-utils/1.4.10/lib_log_utils-1.4.10-py3.9.egg

---
+++ packages/lib-log-utils/lib_log_utils-1.4.10-py3.9.egg/lib_log_utils/__init__.py
@@ -0,0 +1,11 @@
+from .lib_log_utils import *
+
+# this needs to come after the module imports, otherwise circular import under windows
+from . import __init__conf__
+__title__ = __init__conf__.title
+__version__ = __init__conf__.version
+__name__ = __init__conf__.name
+__url__ = __init__conf__.url
+__author__ = __init__conf__.author
+__author_email__ = __init__conf__.author_email
+__shell_command__ = __init__conf__.shell_command

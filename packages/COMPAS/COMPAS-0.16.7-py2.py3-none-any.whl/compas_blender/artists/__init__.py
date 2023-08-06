fcbbc7815117f9930c93257da48f79edbd37e5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:57 +0000

    Add package COMPAS/0.16.7/COMPAS-0.16.7-py2.py3-none-any.whl

---
+++ packages/COMPAS/COMPAS-0.16.7-py2.py3-none-any.whl/compas_blender/artists/__init__.py
@@ -0,0 +1,38 @@
+"""
+********************************************************************************
+artists
+********************************************************************************
+
+.. currentmodule:: compas_blender.artists
+
+Artists for visualising (painting) COMPAS data structures in Blender.
+
+
+Base Classes
+============
+
+.. autosummary::
+    :toctree: generated/
+
+    BaseArtist
+
+
+Classes
+=======
+
+.. autosummary::
+    :toctree: generated/
+
+    NetworkArtist
+    MeshArtist
+
+"""
+
+from ._artist import *  # noqa: F401 F403
+
+from .networkartist import *  # noqa: F401 F403
+from .meshartist import *  # noqa: F401 F403
+# from .volmeshartist import *  # noqa: F401 F403
+
+
+__all__ = [name for name in dir() if not name.startswith('_')]

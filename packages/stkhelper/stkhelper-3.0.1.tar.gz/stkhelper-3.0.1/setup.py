701ce2cf8504bd36404400983a889984c1ded4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:58 +0000

    Add package stkhelper/3.0.1/stkhelper-3.0.1.tar.gz

---
+++ packages/stkhelper/stkhelper-3.0.1.tar.gz/stkhelper-3.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+info = open('README.md').read()
+
+setup(
+      name='stkhelper',
+      version='3.0.1',
+      description='Uses the STK software to create simulations for satellite testing',
+      long_description=info,
+      long_description_content_type='text/markdown',
+      author='W. Conor McFerren',
+      author_email='cnmcferren@gmail.com',
+      url="https://github.com/cnmcferren/stkhelper",
+      packages=find_packages(),
+      install_requires=[
+          'comtypes',
+          'pypiwin32',
+          'datetime']
+)

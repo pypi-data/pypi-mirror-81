e7f160dbff14d315459c01149625c0203e86df59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:42 +0000

    Add package semaphore-bot/0.7.1/semaphore_bot-0.7.1-py3-none-any.whl

---
+++ packages/semaphore-bot/semaphore_bot-0.7.1-py3-none-any.whl/semaphore/chat_context.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# Semaphore: A simple (rule-based) bot library for Signal Private Messenger.
+# Copyright (C) 2020
+# Lazlo Westerhof <semaphore@lazlo.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+"""This module contains an object that represents the context of a chat."""
+from typing import Any, Dict, Match
+
+import attr
+
+from .job_queue import JobQueue
+from .message import Message
+
+
+@attr.s(auto_attribs=True)
+class ChatContext():
+    """This object represents the context of a chat."""
+
+    message: Message
+    match: Match
+    job_queue: JobQueue
+    data: Dict[str, Any] = {}

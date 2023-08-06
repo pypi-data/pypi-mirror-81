078e98861004de24f6f251dd5c7917835b2f54fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:56 +0000

    Add package oxfordcanvastools/1.0/oxfordcanvastools-1.0.tar.gz

---
+++ packages/oxfordcanvastools/oxfordcanvastools-1.0.tar.gz/oxfordcanvastools-1.0/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+import setuptools
+
+setuptools.setup(
+    # Module name (lowercase)
+    name='oxfordcanvastools',
+    version='1.0',
+
+    # Description
+    description='A minimal set of tools for interacting with the Canvas API at Oxford',
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+
+    # License name
+    license='MIT license',
+
+    # Maintainer information
+    maintainer='Fergus Cooper',
+    maintainer_email='fergus.cooper@cs.ox.ac.uk',
+    url='https://github.com/SABS-R3/oxfordcanvastools',
+
+    # Packages to include
+    packages=['oxfordcanvastools'],
+
+    # List of dependencies
+    install_requires=[
+        'requests'
+    ],
+    python_requires='>=3.6',
+)

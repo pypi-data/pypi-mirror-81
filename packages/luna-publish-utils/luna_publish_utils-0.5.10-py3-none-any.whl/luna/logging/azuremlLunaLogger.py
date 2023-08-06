8c8284d987775d3656fda43e16a09258b7185c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:46 +0000

    Add package luna-publish-utils/0.5.10/luna_publish_utils-0.5.10-py3-none-any.whl

---
+++ packages/luna-publish-utils/luna_publish_utils-0.5.10-py3-none-any.whl/luna/logging/azuremlLunaLogger.py
@@ -0,0 +1,23 @@
+from luna.logging.baseLunaLogger import BaseLunaLogger
+from azureml.core import Workspace, Run, Experiment
+import os
+
+class AzureMLLunaLogger(BaseLunaLogger):
+    
+    def log_metric(self, key, value):
+        run = Run.get_context(allow_offline=False)
+        run.log(key, value)
+
+    def log_metrics(self, metrics):
+        run = Run.get_context(allow_offline=False)
+        for key in metrics:
+            run.log(key, metrics[key])
+
+    def upload_artifacts(self, local_path, upload_path):
+        run = Run.get_context(allow_offline=False)
+        if os.path.isdir(local_path):
+            run.upload_folder(upload_path, local_path)
+        elif os.path.isfile(local_path):
+            run.upload_file(upload_path, local_path)
+        else:
+            print("can find the specified path {}".format(local_path))

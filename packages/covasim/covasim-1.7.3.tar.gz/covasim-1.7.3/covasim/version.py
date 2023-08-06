7d29f8408d966729aa39d919fceac26556c76bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:32 +0000

    Add package covasim/1.7.3/covasim-1.7.3.tar.gz

---
+++ packages/covasim/covasim-1.7.3.tar.gz/covasim-1.7.3/covasim/version.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['__version__', '__versiondate__', '__license__']
+
+__version__ = '1.7.3'
+__versiondate__ = '2020-09-30'
+__license__ = f'Covasim {__version__} ({__versiondate__}) — © 2020 by IDM'

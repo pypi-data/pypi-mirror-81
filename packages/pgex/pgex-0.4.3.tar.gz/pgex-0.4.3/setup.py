0d02578952d7155fbd2ae518a0e3472174c7ed24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:26 +0000

    Add package pgex/0.4.3/pgex-0.4.3.tar.gz

---
+++ packages/pgex/pgex-0.4.3.tar.gz/pgex-0.4.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pgex",
+    version="0.4.3",
+    author="Ivan Foke",
+    author_email="ivan.foke@gmail.com",
+    description="A Python PyGame package extension",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IvanFoke/pgex",
+    # download_url="https://github.com/IvanFoke/pgex/archive/v0.0.3.tar.gz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+# python setup.py sdist bdist_wheel

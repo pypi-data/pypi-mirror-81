a28533e3a104955113c18cbdd7e9f148a807b0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:03 +0000

    Add package rj-ml-utils/0.0.3/rj-ml-utils-0.0.3.tar.gz

---
+++ packages/rj-ml-utils/rj-ml-utils-0.0.3.tar.gz/rj-ml-utils-0.0.3/rj_ml_utils/feature_selection.py
@@ -0,0 +1,19 @@
+doc_string = """
+This is a math Module
+Do Some thing
+"""
+
+
+def __doc__():
+    return doc_string
+
+
+def get_constant_variance_columns():
+    pass
+
+
+def get_multi_collinear_columns():
+    pass
+
+
+

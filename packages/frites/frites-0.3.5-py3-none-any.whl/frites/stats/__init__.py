8865fc5fea132f310478094ca20eb7968f21e36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:44 +0000

    Add package frites/0.3.5/frites-0.3.5-py3-none-any.whl

---
+++ packages/frites/frites-0.3.5-py3-none-any.whl/frites/stats/__init__.py
@@ -0,0 +1,4 @@
+"""Fixed and random effect methods."""
+from .stats_mcp import (testwise_correction_mcp, cluster_correction_mcp,  # noqa
+                        cluster_threshold)
+from .stats_param import (ttest_1samp, rfx_ttest)  # noqa
---
+++ packages/frites/frites-0.3.5-py3-none-any.whl/frites/utils/__init__.py
@@ -0,0 +1,4 @@
+"""Utility functions."""
+from .preproc import savgol_filter  # noqa
+from .window import define_windows, plot_windows  # noqa
+from .wrapper import jit  # noqa

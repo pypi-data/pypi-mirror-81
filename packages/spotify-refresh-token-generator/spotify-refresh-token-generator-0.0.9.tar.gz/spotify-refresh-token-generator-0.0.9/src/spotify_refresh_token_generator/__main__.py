888186d75debf550c5af53e4eaaed58f973f089d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:52 +0000

    Add package spotify-refresh-token-generator/0.0.9/spotify-refresh-token-generator-0.0.9.tar.gz

---
+++ packages/spotify-refresh-token-generator/spotify-refresh-token-generator-0.0.9.tar.gz/spotify-refresh-token-generator-0.0.9/src/spotify_refresh_token_generator/__main__.py
@@ -0,0 +1,38 @@
+from typing import List
+
+import prompt
+import spotipy
+
+
+def main():
+    print('\nRefresh Token: %s' % __generate_token(
+        prompt.string("Spotify username: "),
+        prompt.string("Client ID: "),
+        prompt.string("Client Secret: "),
+        prompt.string("Redirect URI: ")
+    ))
+
+
+def __generate_token(username: str, client_id: str, client_secret: str, redirect_uri: str) -> str:
+    scopes: List[str] = [
+        'user-library-read',
+        'playlist-read-private',
+        'playlist-modify-public',
+        'playlist-modify-private',
+        'ugc-image-upload'
+    ]
+    sp_oauth = spotipy.SpotifyOAuth(
+        client_id,
+        client_secret,
+        redirect_uri,
+        scope=' '.join(scopes),
+        username=username,
+        show_dialog=True
+    )
+    code = sp_oauth.get_auth_response()
+    token = sp_oauth.get_access_token(code)
+    return token['refresh_token']
+
+
+if __name__ == '__main__':
+    main()

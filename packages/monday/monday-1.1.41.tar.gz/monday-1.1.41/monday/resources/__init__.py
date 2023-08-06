e3c14926653ca89f210709d27f1a87cdae9ff82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:23 +0000

    Add package monday/1.1.41/monday-1.1.41.tar.gz

---
+++ packages/monday/monday-1.1.41.tar.gz/monday-1.1.41/monday/resources/__init__.py
@@ -0,0 +1,7 @@
+from .items import ItemResource
+from .updates import UpdateResource
+from .tags import TagResource
+from .boards import BoardResource
+from .users import UserResource
+
+__all__ = ['ItemResource', 'UpdateResource', 'TagResource', 'BoardResource', 'UserResource']

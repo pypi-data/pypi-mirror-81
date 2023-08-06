11a9e58b7e8bc23dc78045920a91118ad15cafac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:25 +0000

    Add package polzybackend/0.0.3/polzybackend-0.0.3.tar.gz

---
+++ packages/polzybackend/polzybackend-0.0.3.tar.gz/polzybackend-0.0.3/polzybackend/app.py
@@ -0,0 +1,4 @@
+from base import create_app
+from config import Config
+
+app = create_app(Config)
---
+++ packages/polzybackend/polzybackend-0.0.3.tar.gz/polzybackend-0.0.3/polzybackend/base/main/__init__.py
@@ -0,0 +1,5 @@
+from polzybackend import Blueprint
+
+bp = Blueprint('main', __name__)
+
+from base.main import routes

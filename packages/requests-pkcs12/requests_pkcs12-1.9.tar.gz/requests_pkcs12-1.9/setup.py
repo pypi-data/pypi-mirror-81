2fe8d922a8a32e6c36d4059050635ebff7c84cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:21 +0000

    Add package requests-pkcs12/1.9/requests_pkcs12-1.9.tar.gz

---
+++ packages/requests-pkcs12/requests_pkcs12-1.9.tar.gz/requests_pkcs12-1.9/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+setup(
+    name='requests_pkcs12',
+    version='1.9',
+    description='Add PKCS#12 support to the requests library in a clean way, without monkey patching or temporary files',
+    long_description=open('README.rst', 'rb').read().decode('UTF-8'),
+    url='https://github.com/m-click/requests_pkcs12',
+    author='Volker Diels-Grabsch',
+    author_email='volker.diels-grabsch@m-click.aero',
+    license='ISC',
+    install_requires=[
+        'pyOpenSSL (>=0.14)',
+        'requests (>=2.18.4)',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
+    py_modules=['requests_pkcs12'],
+)

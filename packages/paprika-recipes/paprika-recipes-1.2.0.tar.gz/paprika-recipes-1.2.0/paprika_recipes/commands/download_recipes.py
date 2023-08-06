7ef98ecc2afb380ea7de27e9f47068465d776712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:19 +0000

    Add package paprika-recipes/1.2.0/paprika-recipes-1.2.0.tar.gz

---
+++ packages/paprika-recipes/paprika-recipes-1.2.0.tar.gz/paprika-recipes-1.2.0/paprika_recipes/commands/download_recipes.py
@@ -0,0 +1,35 @@
+import argparse
+from pathlib import Path
+
+from rich.progress import track
+
+from ..command import BaseCommand
+from ..remote import Remote
+from ..utils import dump_yaml, get_password_for_email
+
+
+class Command(BaseCommand):
+    @classmethod
+    def get_help(cls) -> str:
+        return """Downloads all recipes from a paprika account to a directory."""
+
+    @classmethod
+    def add_arguments(cls, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument("email", type=str)
+        parser.add_argument("export_path", type=Path)
+
+    def handle(self) -> None:
+        password = get_password_for_email(self.options.email)
+
+        self.options.export_path.mkdir(parents=True, exist_ok=True)
+
+        remote = Remote(self.options.email, password)
+
+        for recipe in track(
+            remote, total=remote.count(), description="Downloading Recipes"
+        ):
+            with open(
+                self.options.export_path / Path(f"{recipe.name}.paprikarecipe.yaml"),
+                "w",
+            ) as outf:
+                dump_yaml(recipe.as_dict(), outf)
---
+++ packages/paprika-recipes/paprika-recipes-1.2.0.tar.gz/paprika-recipes-1.2.0/paprika_recipes/commands/upload_recipes.py
@@ -0,0 +1,35 @@
+import argparse
+from pathlib import Path
+
+from rich.progress import track
+from yaml import safe_load
+
+from ..remote import Remote, RemoteRecipe
+from ..command import BaseCommand
+from ..utils import dump_yaml, get_password_for_email
+
+
+class Command(BaseCommand):
+    @classmethod
+    def get_help(cls) -> str:
+        return """Uploads a directory of recipes to a paprika account."""
+
+    @classmethod
+    def add_arguments(cls, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument("email", type=str)
+        parser.add_argument("import_path", type=Path)
+
+    def handle(self) -> None:
+        password = get_password_for_email(self.options.email)
+        archive = Remote(self.options.email, password)
+
+        files = list(self.options.import_path.iterdir())
+
+        for recipe_file in track(files, description="Uploading Recipes"):
+            with open(recipe_file, "r") as inf:
+                uploaded = archive.upload_recipe(RemoteRecipe.from_dict(safe_load(inf)))
+
+            with open(recipe_file, "w") as outf:
+                dump_yaml(uploaded.as_dict(), outf)
+
+        archive.notify()

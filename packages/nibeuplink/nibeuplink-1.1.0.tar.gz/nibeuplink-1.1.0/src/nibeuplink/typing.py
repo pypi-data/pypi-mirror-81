dd73b9a753d636facd9328c0c79e1a400900d30c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:18 +0000

    Add package nibeuplink/1.1.0/nibeuplink-1.1.0.tar.gz

---
+++ packages/nibeuplink/nibeuplink-1.1.0.tar.gz/nibeuplink-1.1.0/src/nibeuplink/typing.py
@@ -0,0 +1,32 @@
+from typing import Dict, NewType, Any, Union, Optional, List
+from typing_extensions import (
+    TypedDict
+)
+
+SystemId = int
+Identifier = Union[str, int]
+ParameterId = Identifier
+Parameter = Dict[str, Any]
+StatusItemIcon = Dict[str, Any]
+ParameterSet = Dict[ParameterId, Parameter]
+
+
+class SmartHomeSystem(TypedDict):
+    name: str
+
+
+class Thermostat(TypedDict):
+    smartHomeSystem: SmartHomeSystem
+    name: str
+    climateSystems: Optional[List[int]]
+    currentTemperature: Optional[str]
+    targetTemperature: Optional[str]
+
+
+class SetThermostatModel(TypedDict):
+    externalId: int
+    name: str
+    actualTemp: Optional[int] # Multiplied by 10
+    targetTemp: Optional[int] # Multiplied by 10
+    valvePosition: Optional[int]
+    climateSystems: Optional[List[int]]

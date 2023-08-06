50a00ba78d82742e7d2c5fe1b6d0bdbbe0fb9d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:01 +0000

    Add package gcloud-rest-bigquery/4.1.1/gcloud_rest_bigquery-4.1.1-py2.py3-none-any.whl

---
+++ packages/gcloud-rest-bigquery/gcloud_rest_bigquery-4.1.1-py2.py3-none-any.whl/gcloud/rest/bigquery/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    from future import standard_library
+standard_library.install_aliases()
+from pkg_resources import get_distribution
+__version__ = get_distribution('gcloud-rest-bigquery').version
+
+from gcloud.rest.bigquery.bigquery import Disposition
+from gcloud.rest.bigquery.bigquery import SCOPES
+from gcloud.rest.bigquery.bigquery import SourceFormat
+from gcloud.rest.bigquery.bigquery import Table
+
+
+__all__ = ['__version__', 'Disposition', 'SCOPES', 'SourceFormat', 'Table']

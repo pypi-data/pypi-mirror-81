2abc210c9032659dd976e41f831700d2b4d88fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:35 +0000

    Add package govee-api-laggat/0.1.16/govee_api_laggat-0.1.16.tar.gz

---
+++ packages/govee-api-laggat/govee_api_laggat-0.1.16.tar.gz/govee_api_laggat-0.1.16/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+INSTALL_REQUIRES = [
+    "aiohttp", "aiohttp[speedups]"
+]
+
+setuptools.setup(
+    name="govee_api_laggat",
+    version="0.1.16",
+    author="Florian Lagg @LaggAt",
+    author_email="florian.lagg@gmail.com",
+    description="Implementation of the govee API to control LED strips and bulbs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LaggAt/govee-api-laggat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=INSTALL_REQUIRES
+)

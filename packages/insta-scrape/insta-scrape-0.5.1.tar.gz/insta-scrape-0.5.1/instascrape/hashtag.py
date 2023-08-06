21e7aea6f054501c83588325c2ea06d9e0a51db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:12 +0000

    Add package insta-scrape/0.5.1/insta-scrape-0.5.1.tar.gz

---
+++ packages/insta-scrape/insta-scrape-0.5.1.tar.gz/insta-scrape-0.5.1/instascrape/hashtag.py
@@ -0,0 +1,97 @@
+from __future__ import annotations
+
+import sys
+import os
+# sys.path.insert(0, os.path.abspath('..'))
+
+from instascrape import static_scraper
+from instascrape import json_scraper
+
+class Hashtag(static_scraper.StaticHTMLScraper):
+    """
+    Representation of an Instagram hashtag page.
+
+    Attribues
+    ---------
+    url : str
+        Full URL to a Instagram hashtag
+
+    Methods
+    -------
+    static_load(session=requests.Session())
+        Makes request to URL, instantiates BeautifulSoup, finds JSON data,
+        then parses JSON data.
+    """
+
+    def _scrape_json(self, json_data: dict):
+        """Scrape the JSON"""
+        self.data = HashtagJSON(json_data)
+        self._load_json_into_namespace(self.data)
+
+    @classmethod
+    def from_hashtag(cls, hashtag: str):
+        """
+        Factory method for convenience to create Hashtag instance given
+        just a hashtag name instead of a full URL.
+
+        Parameters
+        ----------
+        hashtag : str
+            Name of the Hashtag for scraping
+
+        Returns
+        -------
+        Hashtag(url: str)
+            Instance of Hashtag with URL created from the given hashtag name
+
+        Example
+        -------
+        >>>Hashtag.from_hashtag('pythonprogramming')
+        <https://www.instagram.com/tags/pythonprogramming/: Hashtag>
+        """
+        url = f"https://www.instagram.com/tags/{hashtag}/"
+        return cls(url, name=hashtag)
+
+
+class HashtagJSON(json_scraper.JSONScraper):
+    """
+    Tool for parsing data fron Instagram hashtag JSON data
+
+    Attributes
+    ----------
+    json_dict : dict
+        Python dictionary containing the Instagram JSON data
+    name : str, optional
+        Custom name that will represent this JSON data
+
+    Methods
+    -------
+    parse_json() -> None
+        Parses the JSON data regarding a single Instagram hashtag
+    load_value(data_dict: dict, key: str, fail_return: Any=None)
+        Returns value in dictionary at the specified key. If value doesn't
+        exist, returns a default value
+    from_json_string(json_str: str, name: str = None)
+        Loads a json string as a dictionary and returns a JSONData object with
+        that dictionary.
+    from_json_file(json_fpath: str, name: str = None)
+        Loads a json file at a given JSON filepath into a dictionary and
+        returns a JSONData object with that dictionary
+    """
+
+    def parse_json(self, *args, **kwargs) -> None:
+        super().parse_json(*args, **kwargs)
+
+        tag_data = self.json_dict["entry_data"]["TagPage"][0]["graphql"]["hashtag"]
+        self.id = tag_data["id"]
+        self.name = tag_data["name"]
+        self.allow_following = tag_data["allow_following"]
+        self.is_following = tag_data["is_following"]
+        self.is_top_media_only = tag_data["is_top_media_only"]
+        self.profile_pic_url = tag_data["profile_pic_url"]
+        self.amount_of_posts = tag_data["edge_hashtag_to_media"]["count"]
+
+if __name__ == "__main__":
+    url = "https://www.instagram.com/explore/tags/worldviewmag/"
+    worldviewmag = Hashtag(url)
+    worldviewmag.static_load()
---
+++ packages/insta-scrape/insta-scrape-0.5.1.tar.gz/insta-scrape-0.5.1/instascrape/login_and_signup.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from instascrape import static_scraper
+from instascrape import json_scraper
+
+class LoginAndSignupPage(static_scraper.StaticHTMLScraper):
+    """
+    Representation of an Instagram profile page.
+
+    Attribues
+    ---------
+    url : str
+        Full URL to an existing Instagram profile
+
+    Methods
+    -------
+    static_load(session=requests.Session())
+        Makes request to URL, instantiates BeautifulSoup, finds JSON data,
+        then parses JSON data.
+    """
+
+    def _scrape_json(self, json_data: dict):
+        """Scrape JSON data and load into instances namespace"""
+        self.data = LoginAndSignupJSON(json_data)
+        self._load_json_into_namespace(self.data)
+
+
+class LoginAndSignupJSON(json_scraper.JSONScraper):
+    def parse_json(self, *args, **kwargs) -> None:
+        super().parse_json(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    url = r"https://www.instagram.com/accounts/login"
+    login = LoginAndSignupPage(url)
+    login.static_load()

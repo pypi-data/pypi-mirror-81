555ce6d6616bb5ad336ca2988347fb0f14ee75d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:42 +0000

    Add package mlphon/3.0.1/mlphon-3.0.1.tar.gz

---
+++ packages/mlphon/mlphon-3.0.1.tar.gz/mlphon-3.0.1/mlphon/__init__.py
@@ -0,0 +1,9 @@
+from .phonetic_analysis import PhoneticAnalyser
+from .tagparse import (
+    parse_syllabletags,
+    parse_phonemetags,
+    split_as_phonemes,
+    split_as_syllables,
+)
+
+__all__ = ["Phonetic_analyser"]

7595358e60162d7b8d42960f7bfc66ce9fb42221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:26 +0000

    Add package Geode-SimplexRemesh/3.1.1/Geode_SimplexRemesh-3.1.1-cp36-cp36m-win_amd64.whl

---
+++ packages/Geode-SimplexRemesh/Geode_SimplexRemesh-3.1.1-cp36-cp36m-win_amd64.whl/geode_simplexremesh/solid.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2020 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+from geode_common import helpers
+
+from .geode_simplexremesh_py_solid import *
+

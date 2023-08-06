ca2e50bb6dfea1a17432ccd773471fdc472cd7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:59 +0000

    Add package integral-data-mirror/1.0/integral-data-mirror-1.0.tar.gz

---
+++ packages/integral-data-mirror/integral-data-mirror-1.0.tar.gz/integral-data-mirror-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='integral-data-mirror',
+    version='1.0',
+    py_modules=["ddosadm","datamirror"],
+    scripts=["download_data.sh"],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)

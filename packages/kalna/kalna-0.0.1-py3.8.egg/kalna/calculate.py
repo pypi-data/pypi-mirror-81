96cdfc38a777287ff61a442adc84d42e66b5ff3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:23 +0000

    Add package kalna/0.0.1/kalna-0.0.1-py3.8.egg

---
+++ packages/kalna/kalna-0.0.1-py3.8.egg/kalna/calculate.py
@@ -0,0 +1,8 @@
+#Main Code file contains the methods to calculate square and cube of a number
+
+def calculateSquare(num):
+    square = num * num
+    return square
+
+def calculateCube(num):
+    return (num * num *num)

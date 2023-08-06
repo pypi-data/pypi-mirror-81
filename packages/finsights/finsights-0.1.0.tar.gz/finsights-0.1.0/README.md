80c65b29d5512d473d127b4d30a9fd59d11d0453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:43 +0000

    Add package finsights/0.1.0/finsights-0.1.0.tar.gz

---
+++ packages/finsights/finsights-0.1.0.tar.gz/finsights-0.1.0/README.md
@@ -0,0 +1,13 @@
+# Introduction
+
+Finsights is a package to derive financial insights. It aims to covers the multi-facted areas associated with investment research. This includes getting of data from various sources, preprocessing of data, and analysis-related functions to obtain insights.
+
+## Chapters
+
+1. Data
+
+2. Analysis
+
+## Compatibility
+
+This package was developed in Python 3.8.5 on MacOS.
---
+++ packages/finsights/finsights-0.1.0.tar.gz/finsights-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="finsights",
+    version="0.1.0",
+    description="Derive investment insights",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/qiujunda/finsights",
+    author="Finsights",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: MacOS"
+    ],
+    packages=["finsights"],
+    python_requires=">=3.8.5"
+)

3ff4fd1377677e50e891a7066c63b6f1af75da18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:18 +0000

    Add package programaker-bridge/0.0.3.dev1/programaker_bridge-0.0.3.dev1.tar.gz

---
+++ packages/programaker-bridge/programaker_bridge-0.0.3.dev1.tar.gz/programaker_bridge-0.0.3.dev1/plaza_bridge/__init__.py
@@ -0,0 +1,5 @@
+from .blocks import (BlockArgument, BlockContext, CallbackBlockArgument,
+                     CollectionBlockArgument, VariableBlockArgument)
+from .bridge import PlazaBridge
+from .registration import (FormBasedServiceRegistration,
+                           MessageBasedServiceRegistration)

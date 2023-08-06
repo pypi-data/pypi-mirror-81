62fdb6fc00f6af04cccb1e9559f288306e88d65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:09 +0000

    Add package spacy-nightly/3.0.0a29/spacy_nightly-3.0.0a29-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/spacy-nightly/spacy_nightly-3.0.0a29-cp36-cp36m-macosx_10_9_x86_64.whl/spacy/lang/nl/__init__.py
@@ -0,0 +1,46 @@
+from typing import Optional
+
+from thinc.api import Model
+
+from .stop_words import STOP_WORDS
+from .lex_attrs import LEX_ATTRS
+from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
+from .punctuation import TOKENIZER_PREFIXES, TOKENIZER_INFIXES
+from .punctuation import TOKENIZER_SUFFIXES
+from .lemmatizer import DutchLemmatizer
+from ...lookups import Lookups
+from ...language import Language
+
+
+class DutchDefaults(Language.Defaults):
+    tokenizer_exceptions = TOKENIZER_EXCEPTIONS
+    prefixes = TOKENIZER_PREFIXES
+    infixes = TOKENIZER_INFIXES
+    suffixes = TOKENIZER_SUFFIXES
+    lex_attr_getters = LEX_ATTRS
+    stop_words = STOP_WORDS
+
+
+class Dutch(Language):
+    lang = "nl"
+    Defaults = DutchDefaults
+
+
+@Dutch.factory(
+    "lemmatizer",
+    assigns=["token.lemma"],
+    default_config={"model": None, "mode": "rule", "lookups": None},
+    default_score_weights={"lemma_acc": 1.0},
+)
+def make_lemmatizer(
+    nlp: Language,
+    model: Optional[Model],
+    name: str,
+    mode: str,
+    lookups: Optional[Lookups],
+):
+    lookups = DutchLemmatizer.load_lookups(nlp.lang, mode, lookups)
+    return DutchLemmatizer(nlp.vocab, model, name, mode=mode, lookups=lookups)
+
+
+__all__ = ["Dutch"]

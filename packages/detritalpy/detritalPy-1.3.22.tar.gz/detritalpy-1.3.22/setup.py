6c82356232ad375123f29481b166d729e22187ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:43 +0000

    Add package detritalpy/1.3.22/detritalPy-1.3.22.tar.gz

---
+++ packages/detritalpy/detritalPy-1.3.22.tar.gz/detritalpy-1.3.22/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="detritalpy",
+    packages=['detritalpy'],    
+    version="1.3.22",
+    author="Glenn Sharman",
+    author_email="gsharman@uark.edu",
+    description="A Python-based toolset for visualizing and analyzing detrital geo-thermochronologic data",
+    keywords = 'detrital, zircon, provenance, stratigraphy, geochronology, thermochronology',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grsharman/detritalpy",
+    download_url="https://github.com/grsharman/detritalPy/archive/v1.3.22.tar.gz",
+    install_requires=['numpy','matplotlib','pandas','xlrd','folium','vincent','simplekml',
+        'scipy','sklearn','KDEpy','peakutils'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)

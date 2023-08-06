79fba64d084b139a8a6bac082097d3e599029c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:01 +0000

    Add package wasteland-sort/1.0.2/wasteland_sort-1.0.2.tar.gz

---
+++ packages/wasteland-sort/wasteland_sort-1.0.2.tar.gz/wasteland_sort-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import pathlib
+
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README").read_text()
+
+setuptools.setup(
+    name="wasteland_sort",
+    version="1.0.2",
+    author="Markian Hromiak",
+    author_email="markian.hromiak@yahoo.com",
+    description="Fully tested and documented version of the Wasteland sorting algorithm",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MHromiak/wasteland_sort.git",
+    packages=setuptools.find_packages(exclude=("test",)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.2',    
+)

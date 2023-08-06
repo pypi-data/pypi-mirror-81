acad21ced6666662fea3000b14277a3097c27a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:39 +0000

    Add package swid-generator/1.0.4/swid_generator-1.0.4-py2.py3-none-any.whl

---
+++ packages/swid-generator/swid_generator-1.0.4-py2.py3-none-any.whl/swid_generator/argparser.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+from argparse import ArgumentParser
+
+from . import settings, meta
+from .generators.swid_generator import all_matcher
+from swid_generator.argparser_helper import entity_name_string, regid_string, hash_string, os_string, arch_string, meta_for_string
+from swid_generator.argparser_helper import RequirementCheckAction, TargetAction, package_path, certificate_path
+
+
+class MainArgumentParser(object):
+    def __init__(self, environment_registry):
+        self.environments = environment_registry
+        self.arg_parser = ArgumentParser('swid_generator',
+                                         description='Generate SWID tags and Software IDs'
+                                                     'from dpkg, pacman or rpm package manager')
+        self.arg_parser.add_argument('-v', '--version', action='version',
+                                     version='%(prog)s ' + meta.version)
+
+        # Parent parser for common options
+        parent_parser = ArgumentParser(add_help=False)
+
+        parent_parser.add_argument('--env', choices=environment_registry.get_environment_strings(),
+                                   default='auto',
+                                   help='The package manager environment to be used. Defaults to "auto". '
+                                        'If the environment can not be autodetected, '
+                                        'the exit code is set to 3.')
+        parent_parser.add_argument('--doc-separator', dest='document_separator', default='\n',
+                                   help='The separator string by which the SWID XML '
+                                        'documents are separated. Example: For '
+                                        'one newline, use $\'\\n\'.')
+        parent_parser.add_argument('--regid', dest='regid', type=regid_string,
+                                   default=regid_string(settings.DEFAULT_REGID),
+                                   help='The regid to use in the generated output. '
+                                        'May not contain any whitespace '
+                                        'characters. Default is "%s".' % settings.DEFAULT_REGID)
+        parent_parser.add_argument('--id-prefix', dest='id_prefix',
+                                   help='Override the default {os}-{arch}- tagId prefix.')
+        parent_parser.add_argument('--dpkg-include-package-arch', action='store_true', default=False,
+                                   help='Include package architecture in tagId and version, for dpkg.')
+
+        subparsers = self.arg_parser.add_subparsers(help='Commands: ', dest='command')
+
+        # Subparser for swid command
+        swid_parser = subparsers.add_parser('swid', help='SWID tag output', parents=[parent_parser],
+                                            description='Generate SWID tags.')
+
+        swid_parser.add_argument('--entity-name', dest='entity_name', type=entity_name_string,
+                                 default=entity_name_string(settings.DEFAULT_ENTITY_NAME),
+                                 help='The entity name used in the <Entity> XML tag. '
+                                      'Default is "%s".' % settings.DEFAULT_ENTITY_NAME)
+        swid_parser.add_argument('--os', dest='os_string', type=os_string,
+                                 default=None,
+                                 help='The OS string used in the tagId attribute. '
+                                      'Default is derived from the OS of the local host.')
+        swid_parser.add_argument('--arch', dest='architecture', type=arch_string,
+                                 default=None,
+                                 help='The HW architecture used in the tagId attribute. '
+                                      'Default is derived from the HW architecture of the local host.')
+        swid_parser.add_argument('--meta-for', dest='meta_for', type=meta_for_string,
+                                 default='os',
+                                 help='Specify whether the Meta element should hold information '
+                                      'about OS ("os", the default), or about the package ("package").')
+        swid_parser.add_argument('--schema-location', action='store_true', default=False,
+                                 help='Add xsi:schemaLocation attribute with schema URIs to validate the '
+                                      'resulting XML documents.')
+        swid_parser.add_argument('--lang', dest='xml_lang', type=str,
+                                 default=str(settings.DEFAULT_XML_LANG),
+                                 help='Value of xml:lang attribute. '
+                                      'Default is "%s".' % settings.DEFAULT_XML_LANG)
+        swid_parser.add_argument('--pretty', action='store_true', default=False,
+                                 help='Indent the XML output.')
+        swid_parser.add_argument('--full', action='store_true', default=False,
+                                 help='Dump the full SWID tags including directory/file tags for each package.')
+        swid_parser.add_argument('--hierarchic', action='store_true', default=False,
+                                 help='Change directory structure to hierarchic.')
+        swid_parser.add_argument('--hash', dest='hash_algorithms', type=hash_string,
+                                 default=hash_string(settings.DEFAULT_HASH_ALGORITHM),
+                                 help='Define the algorithm for the file hashes ("sha256", "sha384", "sha512"). '
+                                 'Multiple hashes can be added with comma separated. ("sha256,sha384") '
+                                 'Default is "%s"' % settings.DEFAULT_HASH_ALGORITHM)
+        swid_parser.add_argument('--pkcs12', dest='pkcs12', type=certificate_path,
+                                 action=RequirementCheckAction,
+                                 const=environment_registry,
+                                 help='The PKCS#12 container with key and certificate to sign the xml output.')
+        swid_parser.add_argument('--pkcs12-pwd', dest='password',
+                                 help='If the PKCS#12 file is password protected, '
+                                      'the password needs to be provided.')
+
+        swid_parser.set_defaults(matcher=all_matcher)
+
+        targeted_group = swid_parser.add_argument_group(
+            title='targeted requests',
+            description='You may do a targeted request against either a Software-ID, a package name, '
+                        'a package file or a folder structure. '
+                        'The output only contains a SWID tag if the argument fully matches '
+                        'the given target. If no matching SWID tag is found, the output is empty '
+                        'and the exit code is set to 1. ')
+
+        # mutually exclusive arguments --package/--software-id
+        mutually_group = targeted_group.add_mutually_exclusive_group()
+        mutually_group.add_argument('--software-id', dest='match_software_id', metavar='SOFTWARE-ID', action=TargetAction,
+                                    help='Do a targeted request for the specified Software-ID. '
+                                    'A Software-ID is made up as follows: "{regid}__{unique-id}". '
+                                    'Example: '
+                                    '"strongswan.org__Ubuntu_12.04-i686-strongswan-4.5.2-1.2". '
+                                    'If no matching package is found, the output is empty and the '
+                                    'exit code is set to 1.')
+        mutually_group.add_argument('--package', dest='package_name', metavar='PACKAGE',
+                                    action=TargetAction,
+                                    help='Do a targeted request for the specified package name. '
+                                         'The package name corresponds to a package name returned by the '
+                                         'environment\'s package manager, e.g "glibc-headers" on a '
+                                         'dpkg managed environment. '
+                                         'If no matching package is found, the output is empty and the '
+                                         'exit code is set to 1.')
+        mutually_group.add_argument('--package-file', dest='file_path', type=package_path,
+                                    action=RequirementCheckAction,
+                                    const=environment_registry,
+                                    help='Create SWID-Tag based on information of a Package-File. '
+                                         'Rpm-Environment: *.rpm File, Dpkg-Environment: *.deb File, '
+                                         'Pacman-Environment: *.pgk.tar.xz File')
+        targeted_group.add_argument('--evidence', dest='evidence_paths', action='append', metavar='PATH',
+                                    help='Create a SWID Tag from one or more directories on the filesystem. '
+                                         'This changes the payload element to an evidence element.')
+        targeted_group.add_argument('--name', dest='name', default=None, type=entity_name_string,
+                                    help='Specify a name for a directory based SWID-Tag. '
+                                         'Default is "{first-evidence-path}_{os-string}"')
+        targeted_group.add_argument('--version-string', dest='version', type=entity_name_string, default=None,
+                                    help='Specify the version for a directory based SWID-Tag. '
+                                         'Default is "1.0.0"')
+        targeted_group.add_argument('--new-root', dest='new_root', metavar='PATH', type=entity_name_string,
+                                    default=None,
+                                    help='Change the displayed "root"-folder from the provided directory to '
+                                         'a different path.')
+        # Subparser for software-id command
+        subparsers.add_parser('software-id', help='Software id output', parents=[parent_parser],
+                              description='Generate Software-IDs.')
+
+    def parse(self, arguments=None):
+        return self.arg_parser.parse_args(arguments)
+
+    def print_usage(self):
+        self.arg_parser.print_usage()
---
+++ packages/swid-generator/swid_generator-1.0.4-py2.py3-none-any.whl/swid_generator/main.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Christian Fässler, Danilo Bargen, Jonas Furrer.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+import sys
+
+from glob import glob
+from shutil import rmtree
+from .argparser import MainArgumentParser
+from .environments.environment_registry import EnvironmentRegistry
+from .environments.dpkg_environment import DpkgEnvironment
+from .environments.rpm_environment import RpmEnvironment
+from .environments.pacman_environment import PacmanEnvironment
+from .generators.swid_generator import create_swid_tags
+from .generators.softwareid_generator import create_software_ids
+from .print_functions import print_swid_tags, print_software_ids
+from .exceptions import AutodetectionError, EnvironmentNotInstalledError, CommandManagerError
+from .patches import unicode_patch
+
+
+TMP_FOLDER = '/tmp/'
+PREFIX_FOLDER = 'swid_*'
+
+
+def main():
+
+    # Register environments
+    environment_registry = EnvironmentRegistry()
+    environment_registry.register('rpm', RpmEnvironment)
+    environment_registry.register('dpkg', DpkgEnvironment)
+    environment_registry.register('pacman', PacmanEnvironment)
+
+    # Parse arguments
+    parser = MainArgumentParser(environment_registry)
+    options = parser.parse()  # without any parameter it takes arguments passed by command line
+
+    def get_environment():
+        try:
+            return environment_registry.get_environment(options.env)
+        except EnvironmentNotInstalledError:
+            print('Error: the given environment is not installed')
+            sys.exit(3)
+        except AutodetectionError:
+            print('Error: Could not autodetect environment.')
+            parser.print_usage()
+            sys.exit(3)
+
+    # Handle commands
+    if options.command == 'swid':
+        swid_args = {
+            'environment': get_environment(),
+            'entity_name': options.entity_name,
+            'regid': options.regid,
+            'os_string': options.os_string,
+            'architecture': options.architecture,
+            'dpkg_include_package_arch': options.dpkg_include_package_arch,
+            'meta_for': options.meta_for,
+            'full': options.full,
+            'matcher': options.matcher,
+            'hash_algorithms': options.hash_algorithms,
+            'hierarchic': options.hierarchic,
+            'file_path': options.file_path,
+            'evidence_paths': options.evidence_paths,
+            'new_root_path': options.new_root,
+            'name': options.name,
+            'version': options.version,
+            'pkcs12_file': options.pkcs12,
+            'xml_lang': options.xml_lang,
+            'schema_location': options.schema_location,
+            'id_prefix': options.id_prefix,
+        }
+
+        signature_args = {
+            'pkcs12_file': options.pkcs12,
+            'pkcs12_password': options.password
+        }
+
+        if options.evidence_paths is not None and len(options.evidence_paths) > 0:
+            """
+            If the parameter 'name' and 'version' are missing, then the following default-arguments are set:
+            name = {first_evidence_path}_{os_string}
+            version = 1.0.0
+            """
+            swid_args['full'] = True
+
+            if options.name is None:
+                swid_args['name'] = "_".join((unicode_patch(options.evidence_paths[0]), swid_args['environment'].get_os_string()))
+
+            if options.version is None:
+                swid_args['version'] = "1.0.0"
+
+        try:
+
+            swid_tags = create_swid_tags(**swid_args)
+            print_swid_tags(swid_tags, signature_args, separator=options.document_separator, pretty=options.pretty)
+
+            # Garbage-Collection, clean tmp folder, delete swid_*-Folders
+            files_to_delete = glob(TMP_FOLDER + PREFIX_FOLDER)
+            for file_path in files_to_delete:
+                rmtree(file_path.encode('utf-8'))
+
+        # if --match was used no matching packages were found
+        except StopIteration:
+            sys.exit(1)
+
+        except (UnicodeEncodeError, UnicodeDecodeError, UnicodeError):
+            unicode_error_message = \
+                "Error: An Unicode-Encode/Decode error has occurred. Please check the locales settings on your system.\n" \
+                "The stdout-encoding must be utf-8 compatible and the '$LANG' environment-variable must be set."
+            print('\x1b[1;31;0m' + unicode_error_message + '\x1b[0m')
+            sys.exit(4)
+        # Mainly except for evidence-tag generation. e.g Errors: no such file or Operation not permitted
+        except OSError as e:
+            print(e)
+            sys.exit(4)
+
+        except CommandManagerError as e:
+            print("Error: An external command has encountered an unexpected error.")
+            print(e)
+            sys.exit(5)
+
+    elif options.command == 'software-id':
+        sid_args = {
+            'env': get_environment(),
+            'regid': options.regid,
+            'id_prefix': options.id_prefix,
+            'dpkg_include_package_arch': options.dpkg_include_package_arch,
+        }
+        software_ids = create_software_ids(**sid_args)
+        print_software_ids(software_ids, separator=options.document_separator)
+
+    else:
+        print('Error: Please choose a subcommand: '
+              'swid for swid output, software-id for software id output')
+        parser.print_usage()
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()

30ffcf89dddc6ccc150091d3aa00f563c646e378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:26 +0000

    Add package extPep-identifier-blackjack/0.0.9/extPep_identifier-blackjack-0.0.9.tar.gz

---
+++ packages/extPep-identifier-blackjack/extPep_identifier-blackjack-0.0.9.tar.gz/extPep_identifier-blackjack-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="extPep_identifier-blackjack", # Replace with your own username
+    version="0.0.9",
+    author="Xinhao Shao",
+    author_email="",
+    description="Extended peptides identification given mass-spec search result and custom proteomic database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://git.pepchem.org/gaolab/api_ribo_ext ",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

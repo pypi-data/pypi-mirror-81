53dd03ba1bf8c24f2c326679b97c74d33472381c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:05 +0000

    Add package nlu/1.0.0/nlu-1.0.0.tar.gz

---
+++ packages/nlu/nlu-1.0.0.tar.gz/nlu-1.0.0/nlu/components/sentence_detectors/deep_sentence_detector/deep_sentence_detector.py
@@ -0,0 +1,14 @@
+import nlu.pipe_components
+import sparknlp
+from sparknlp.base import *
+from sparknlp.annotator import *
+
+class SentenDetectorDeep:
+    @staticmethod
+    def get_default_model():
+        return DeepSentenceDetector() \
+            .setInputCols(["document", "token", "ner_chunk"]) \
+            .setOutputCol("sentence") \
+            .setIncludePragmaticSegmenter(True) \
+            .setEndPunctuation([".", "?"])
+# --conf spark.app.name =Spark NLP

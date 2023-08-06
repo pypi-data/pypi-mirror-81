014d71f81d74586d2ac0b6e7918bd6c8491418af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:03 +0000

    Add package pytekcoopbank/0.0.3/pytekcoopbank-0.0.3.tar.gz

---
+++ packages/pytekcoopbank/pytekcoopbank-0.0.3.tar.gz/pytekcoopbank-0.0.3/tekcoopbank/transactions.py
@@ -0,0 +1,24 @@
+"""
+Transaction Status subclass of Bank.
+"""
+import requests
+from . import bank
+
+
+class TransactionStatus(bank.Bank):
+    def send(self, messageReference, callback=None):
+        token = self.token
+        url = self.host + "/Enquiry/TransactionStatus/2.0.0"
+        payload = {
+            "MessageReference": messageReference,
+        }
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}",
+        }
+        response = requests.post(url, headers=headers,
+                                 data=payload, verify=False)
+        if callback is not None:
+            return callback(response)
+        else:
+            return response

f67026a315891456fa947c643c47d819ef016d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:12 +0000

    Add package bbd/0.0.5/bbd-0.0.5.tar.gz

---
+++ packages/bbd/bbd-0.0.5.tar.gz/bbd-0.0.5/src/bbd/census/geography.py
@@ -0,0 +1,10 @@
+class Geography:
+    """Geography available for download"""
+
+    TRACT = "tract"
+    CD = "congressional district"
+    COUNTY = "county"
+    STATE = "state"
+    ZCTA = "zcta"
+    BLOCK = "block"
+    BLOCKGROUP = "block group"

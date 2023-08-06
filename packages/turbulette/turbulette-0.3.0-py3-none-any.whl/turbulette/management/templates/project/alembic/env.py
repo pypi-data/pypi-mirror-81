6b09953c9ea81d41562baf90749aab0061e725bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:23 +0000

    Add package turbulette/0.3.0/turbulette-0.3.0-py3-none-any.whl

---
+++ packages/turbulette/turbulette-0.3.0-py3-none-any.whl/turbulette/management/templates/project/alembic/env.py
@@ -0,0 +1,9 @@
+import sys
+from os.path import abspath, join
+from os import getcwd
+from turbulette.management.alembic_env import run_migrations
+
+
+# Add project folder to python path
+sys.path.append(abspath(join(getcwd())))
+run_migrations("{{ settings }}")

4495a5a8484a6ddcec787e8c5f2af8910734cbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:44 +0000

    Add package pynevin/0.1.2/pynevin-0.1.2.tar.gz

---
+++ packages/pynevin/pynevin-0.1.2.tar.gz/pynevin-0.1.2/pynevin/__init__.py
@@ -0,0 +1,4 @@
+from .module1 import *
+from .module2 import foo2
+from .visualsim import *
+
---
+++ packages/pynevin/pynevin-0.1.2.tar.gz/pynevin-0.1.2/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.2'
+PACKAGE_NAME = 'pynevin'
+AUTHOR = 'Nevin'
+AUTHOR_EMAIL = 'pynevin@idarkduck.com'
+URL = 'https://github.com/iDuckDark/PyNevin'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Describe your package in one sentence'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )

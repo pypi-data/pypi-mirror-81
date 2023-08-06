64f79e08258483822e99b3f3416fe7b65408e7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:18 +0000

    Add package spacy-nightly/3.0.0a29/spacy_nightly-3.0.0a29-cp37-cp37m-win_amd64.whl

---
+++ packages/spacy-nightly/spacy_nightly-3.0.0a29-cp37-cp37m-win_amd64.whl/spacy/lang/el/__init__.py
@@ -0,0 +1,46 @@
+from typing import Optional
+from thinc.api import Model
+
+from .tokenizer_exceptions import TOKENIZER_EXCEPTIONS
+from .stop_words import STOP_WORDS
+from .lex_attrs import LEX_ATTRS
+from .syntax_iterators import SYNTAX_ITERATORS
+from .punctuation import TOKENIZER_PREFIXES, TOKENIZER_SUFFIXES, TOKENIZER_INFIXES
+from .lemmatizer import GreekLemmatizer
+from ...lookups import Lookups
+from ...language import Language
+
+
+class GreekDefaults(Language.Defaults):
+    tokenizer_exceptions = TOKENIZER_EXCEPTIONS
+    prefixes = TOKENIZER_PREFIXES
+    suffixes = TOKENIZER_SUFFIXES
+    infixes = TOKENIZER_INFIXES
+    lex_attr_getters = LEX_ATTRS
+    stop_words = STOP_WORDS
+    syntax_iterators = SYNTAX_ITERATORS
+
+
+class Greek(Language):
+    lang = "el"
+    Defaults = GreekDefaults
+
+
+@Greek.factory(
+    "lemmatizer",
+    assigns=["token.lemma"],
+    default_config={"model": None, "mode": "rule", "lookups": None},
+    default_score_weights={"lemma_acc": 1.0},
+)
+def make_lemmatizer(
+    nlp: Language,
+    model: Optional[Model],
+    name: str,
+    mode: str,
+    lookups: Optional[Lookups],
+):
+    lookups = GreekLemmatizer.load_lookups(nlp.lang, mode, lookups)
+    return GreekLemmatizer(nlp.vocab, model, name, mode=mode, lookups=lookups)
+
+
+__all__ = ["Greek"]

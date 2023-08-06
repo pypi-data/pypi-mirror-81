59e80805c6bc9efdf696860f76a0a296490e6376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:52 +0000

    Add package html-test-report/1.0.0/html_test_report-1.0.0.tar.gz

---
+++ packages/html-test-report/html_test_report-1.0.0.tar.gz/html_test_report-1.0.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name                 = 'html_test_report',
+    version              = '1.0.0',
+    description          = 'Python unittest runner with html report.',
+    url                  = 'https://github.com/adelplanque/html-test-report',
+    packages             = ('html_test_report', ),
+    long_description     = long_description,
+    long_description_content_type = "text/markdown",
+    entry_points         = {
+        "console_scripts": [
+            "html-test = html_test_report.html_test:main",
+        ],
+        'nose.plugins.0.10': [
+            'html-test = html_test_report.nose_plugin:HtmlTestNosePlugin'
+        ]
+    },
+    package_data         = {
+        'html_test_report': [
+            'templates/test-case.html',
+        ]
+    },
+    install_requires=[
+        "jinja2",
+        "pygments",
+        "six",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+    ]
+)

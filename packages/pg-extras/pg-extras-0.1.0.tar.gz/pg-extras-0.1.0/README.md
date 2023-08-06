43d9362d386ff2d9a901d4794dd2514f1d706066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:57 +0000

    Add package pg-extras/0.1.0/pg-extras-0.1.0.tar.gz

---
+++ packages/pg-extras/pg-extras-0.1.0.tar.gz/pg-extras-0.1.0/README.md
@@ -0,0 +1,1 @@
+## Python PG Extras
---
+++ packages/pg-extras/pg-extras-0.1.0.tar.gz/pg-extras-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pg-extras",
+    version="0.1.0",
+    author="Pawel Urbanek",
+    author_email="contact@pawelurbanek.com",
+    description="Python PostgreSQL database performance insights. Locks, index usage, buffer cache hit ratios, vacuum stats and more.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pawurb/python-pg-extras",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

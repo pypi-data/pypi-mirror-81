eb8c9bfc1bb380c5d37924845b2a2d019b521fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:56 +0000

    Add package bob.db.caspeal/2.1.11/bob.db.caspeal-2.1.11.zip

---
+++ packages/bob.db.caspeal/bob.db.caspeal-2.1.11.zip/bob.db.caspeal-2.1.11/README.rst
@@ -0,0 +1,44 @@
+.. vim: set fileencoding=utf-8 :
+.. Thu 18 Aug 14:47:00 CEST 2016
+
+.. image:: https://img.shields.io/badge/docs-available-orange.svg
+   :target: https://www.idiap.ch/software/bob/docs/bob/bob.db.caspeal/master/index.html
+.. image:: https://gitlab.idiap.ch/bob/bob.db.caspeal/badges/v2.1.11/pipeline.svg
+   :target: https://gitlab.idiap.ch/bob/bob.db.caspeal/commits/v2.1.11
+.. image:: https://gitlab.idiap.ch/bob/bob.db.caspeal/badges/v2.1.11/coverage.svg
+   :target: https://gitlab.idiap.ch/bob/bob.db.caspeal/commits/v2.1.11
+.. image:: https://img.shields.io/badge/gitlab-project-0000c0.svg
+   :target: https://gitlab.idiap.ch/bob/bob.db.caspeal
+
+
+====================================
+ CAS-PEAL Database Interface for Bob
+====================================
+
+This package is part of the signal-processing and machine learning toolbox
+Bob_ and it contains an interface for the evaluation protocols of the `CAS-PEAL`_ R1 database.
+It worth noting that this package does not contain the original `CAS-PEAL`_ data files, which need to be obtained through the link above.
+
+
+
+Installation
+------------
+
+Complete Bob's `installation`_ instructions. Then, to install this package,
+run::
+
+  $ conda install bob.db.caspeal
+
+
+Contact
+-------
+
+For questions or reporting issues to this software package, contact our
+development `mailing list`_.
+
+
+.. Place your references here:
+.. _bob: https://www.idiap.ch/software/bob
+.. _installation: https://www.idiap.ch/software/bob/install
+.. _mailing list: https://www.idiap.ch/software/bob/discuss
+.. _cas-peal: http://www.jdl.ac.cn/peal
---
+++ packages/bob.db.caspeal/bob.db.caspeal-2.1.11.zip/bob.db.caspeal-2.1.11/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# author: Manuel Guenther <manuel.guenther@idiap.ch>
+# date:   Wed Feb 13 12:35:29 CET 2013
+
+from setuptools import setup, find_packages, dist
+dist.Distribution(dict(setup_requires=['bob.extension']))
+
+from bob.extension.utils import load_requirements
+install_requires = load_requirements()
+
+# Define package version
+version = open("version.txt").read().rstrip()
+
+# The only thing we do in this file is to call the setup() function with all
+# parameters that define our package.
+setup(
+
+    name='bob.db.caspeal',
+    version=version,
+    description='CAS-PEAL Database Access API for Bob',
+    url='http://gitlab.idiap.ch/bob/bob.db.caspeal',
+    license='BSD',
+    author='Manuel Guenther',
+    author_email='manuel.guenther@idiap.ch',
+    long_description=open('README.rst').read(),
+
+    # This line is required for any distutils based packaging.
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires = install_requires,
+
+
+
+    entry_points = {
+      # bob database declaration
+      'bob.db': [
+        'caspeal = bob.db.caspeal.driver:Interface',
+      ],
+
+      # bob unittest declaration
+      'bob.test': [
+        'caspeal = bob.db.caspeal.test:CasPealDatabaseTest',
+      ],
+    },
+
+    classifiers = [
+      'Framework :: Bob',
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Database :: Front-Ends',
+    ],
+)

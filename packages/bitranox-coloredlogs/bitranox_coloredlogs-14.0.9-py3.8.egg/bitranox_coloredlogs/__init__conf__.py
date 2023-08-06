e0d527a1db2c1e6bf0c3211fa25b8fcad155a982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:06 +0000

    Add package bitranox-coloredlogs/14.0.9/bitranox_coloredlogs-14.0.9-py3.8.egg

---
+++ packages/bitranox-coloredlogs/bitranox_coloredlogs-14.0.9-py3.8.egg/bitranox_coloredlogs/__init__conf__.py
@@ -0,0 +1,20 @@
+name = 'bitranox_coloredlogs'
+title = 'fork of xolox coloredlogs with possibility to log to travis and jupyter'
+version = 'v14.0.9'
+url = 'https://github.com/bitranox/bitranox_coloredlogs'
+author = 'Robert Nowotny'
+author_email = 'bitranox@gmail.com'
+shell_command = 'coloredlogs'
+
+
+def print_info() -> None:
+    print("""\
+
+Info for bitranox_coloredlogs:
+
+    fork of xolox coloredlogs with possibility to log to travis and jupyter
+
+    Version : v14.0.9
+    Url     : https://github.com/bitranox/bitranox_coloredlogs
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com""")

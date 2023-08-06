f0f9ac92c99148e55a40e6b297d0c529bcad7e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:36 +0000

    Add package gqrxHamlib/3.0.7/gqrxHamlib-3.0.7.tar.gz

---
+++ packages/gqrxHamlib/gqrxHamlib-3.0.7.tar.gz/gqrxHamlib-3.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='gqrxHamlib',
+      version = '3.0.7',
+      description = 'gqrx-Hamlib interface',
+      url='http://github.com/g0fcu/gqrx-hamlib-gui',
+      author='Simon Kennedy',
+      license='GPL',
+      packages=find_packages(),
+      #install_requires=['xmlrpc.client'],
+      entry_points={
+          'console_scripts': [
+               'gqrxHamlib=gqrxHamlib:main'
+                  ]
+                }
+)

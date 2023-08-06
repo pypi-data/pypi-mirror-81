953dd41c68bd10f29d5bdcdc90a7d2348cf04ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:55 +0000

    Add package salt-lint/0.4.2/salt_lint-0.4.2-py3-none-any.whl

---
+++ packages/salt-lint/salt_lint-0.4.2-py3-none-any.whl/saltlint/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Warpnet B.V.
+
+"""A command-line utility that checks for best practices in SaltStack.
+"""
+
+NAME = 'salt-lint'
+VERSION = '0.4.2'
+DESCRIPTION = __doc__
+
+__author__ = 'Warpnet B.V.'
+__license__ = 'MIT'
+__version__ = VERSION
---
+++ packages/salt-lint/salt_lint-0.4.2-py3-none-any.whl/saltlint/rules/YamlHasOctalValueRule.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016 Will Thames and contributors
+# Copyright (c) 2018 Ansible Project
+# Modified work Copyright (c) 2019 Warpnet B.V.
+
+import re
+from saltlint.linter import SaltLintRule
+
+
+class YamlHasOctalValueRule(SaltLintRule):
+    id = '210'
+    shortdesc = 'Numbers that start with `0` should always be encapsulated in quotation marks'
+    description = 'Numbers that start with `0` should always be encapsulated in quotation marks'
+    severity = 'HIGH'
+    tags = ['formatting']
+    version_added = 'v0.0.6'
+
+    bracket_regex = re.compile(r"(?<=:)\s{0,}0[0-9]{1,}\s{0,}((?={#)|(?=#)|(?=$))")
+
+    def match(self, file, line):
+        return self.bracket_regex.search(line)

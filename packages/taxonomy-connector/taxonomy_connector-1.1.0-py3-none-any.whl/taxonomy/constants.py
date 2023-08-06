1969fb18b2e599dd85739cd074e0112170e40708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:52 +0000

    Add package taxonomy-connector/1.1.0/taxonomy_connector-1.1.0-py3-none-any.whl

---
+++ packages/taxonomy-connector/taxonomy_connector-1.1.0-py3-none-any.whl/taxonomy/constants.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Constants used by the taxonomy connector.
+"""
+
+JOBS_QUERY_FILTER = {
+    'filter': {
+        'when': {
+            'start': '2020-01',
+            'end': '2020-06',
+            'type': 'active'
+        },
+        'posting_duration': {
+            'lower_bound': 0,
+            'upper_bound': 90
+        }
+    },
+    'rank': {
+        'by': 'unique_postings',
+        'min_unique_postings': 1000,
+        'limit': 20
+    },
+    'nested_rank': {
+        'by': 'significance',
+        'limit': 10
+    }
+}
+
+JOB_POSTINGS_QUERY_FILTER = {
+    'filter': {
+        'when': {
+            'start': '2020-01',
+            'end': '2020-06',
+            'type': 'active'
+        },
+        'posting_duration': {
+            'lower_bound': 0,
+            'upper_bound': 90
+        }
+    },
+    'rank': {
+        'by': 'unique_postings',
+        'min_unique_postings': 1000,
+        'limit': 20,
+        'extra_metrics': [
+            'median_posting_duration',
+            'median_salary',
+            'unique_companies'
+        ]
+    }
+}

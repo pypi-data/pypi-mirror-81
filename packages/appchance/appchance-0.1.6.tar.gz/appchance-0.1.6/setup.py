ef17132600c99bf2441b76d04205e7944e6bc19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:15 +0000

    Add package appchance/0.1.6/appchance-0.1.6.tar.gz

---
+++ packages/appchance/appchance-0.1.6.tar.gz/appchance-0.1.6/setup.py
@@ -0,0 +1,36 @@
+import os, sys
+import setuptools
+from setuptools.command.install import install as _install
+
+
+def _post_install(dir):
+    from subprocess import call
+    call([sys.executable, 'post_setup.py'],
+         cwd=os.path.join(dir, 'appchance'))
+
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        self.execute(_post_install, (self.install_lib,),
+                     msg="Running post install task")
+
+with open("README.md", "r") as _file:
+    readme = _file.read()
+
+setuptools.setup(
+    name="appchance",
+    version="0.1.6",
+    author="Radosław Przytuła",
+    author_email="radoslaw.przytula@appchance.com",
+    description="In Appchance we trust",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/appchance/pychance/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.7',
+    cmdclass={'install': install},
+)

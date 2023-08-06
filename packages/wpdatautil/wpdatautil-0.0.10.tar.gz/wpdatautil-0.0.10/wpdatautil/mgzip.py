e9ff6594cadb268fb7aab6705451919188dfe13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:51 +0000

    Add package wpdatautil/0.0.10/wpdatautil-0.0.10.tar.gz

---
+++ packages/wpdatautil/wpdatautil-0.0.10.tar.gz/wpdatautil-0.0.10/wpdatautil/mgzip.py
@@ -0,0 +1,38 @@
+"""mgzip utilities."""
+import logging
+import math
+import multiprocessing
+
+import mgzip
+
+from wpdatautil.humanfriendly import format_size
+from wpdatautil.timeit import Timer
+
+_COMPRESS_LEVEL = 6
+_INVERSE_PI = 1 / math.pi
+_CPU_COUNT_SCALER = (2 ** 30) ** _INVERSE_PI
+_MAX_USABLE_CPU_COUNT = multiprocessing.cpu_count() * 0.95
+
+log = logging.getLogger(__name__)
+
+
+def _num_threads(num_bytes: int) -> int:
+    """Return the number of threads to use for compression or decompression."""
+    return max(1, math.floor(min(_CPU_COUNT_SCALER, num_bytes ** _INVERSE_PI) * _MAX_USABLE_CPU_COUNT / _CPU_COUNT_SCALER))
+
+
+def compress(uncompressed: bytes, description: str = "blob") -> bytes:
+    """Compress the given bytes."""
+    len_uncompressed = len(uncompressed)
+    len_uncompressed_formatted = format_size(len_uncompressed)
+    num_threads = _num_threads(len_uncompressed)
+    log.debug(f"Compressing {description} from {len_uncompressed_formatted} using {num_threads} threads.")
+    timer = Timer()
+    compressed = mgzip.compress(uncompressed, compresslevel=_COMPRESS_LEVEL, thread=num_threads)
+    log.debug(f"Compressed {description} to {format_size(len(compressed))} from {len_uncompressed_formatted} in {timer} using {num_threads} threads.")
+    return compressed
+
+
+def decompress(compressed: bytes) -> bytes:
+    """Decompress the given bytes."""
+    return mgzip.decompress(compressed, thread=_num_threads(len(compressed)))

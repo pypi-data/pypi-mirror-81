5418378c51fa2ffa4d4eb98eac4f1af2610eb1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:55 +0000

    Add package voxelbotutils/0.0.3/voxelbotutils-0.0.3.tar.gz

---
+++ packages/voxelbotutils/voxelbotutils-0.0.3.tar.gz/voxelbotutils-0.0.3/voxelbotutils/cogs/__init__.py
@@ -0,0 +1,11 @@
+all_packages = (
+    'analytics',
+    'bot_settings',
+    'bot_stats',
+    'command_event',
+    'connect_event',
+    'error_handler',
+    'help_command',
+    'misc_commands',
+    'owner_only',
+)

a691ed3f2260274aa37da6556c6c8fe389ad665d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:35 +0000

    Add package FormEncode/2.0.0/FormEncode-2.0.0.tar.gz

---
+++ packages/FormEncode/FormEncode-2.0.0.tar.gz/FormEncode-2.0.0/examples/WebwareExamples/index.py
@@ -0,0 +1,104 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from formencode import Invalid, htmlfill, Schema, validators
+
+from WebKit.Page import Page
+import six
+
+
+page_style = '''
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+ <head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>Tell me about yourself</title>
+  <style type="text/css">
+    .error {background-color: #ffdddd}
+    .error-message {border: 2px solid #f00}
+  </style>
+ </head>
+ <body>
+
+ <h1>Tell me about yourself</h1>
+ <p><i>A FormEncode example</i></p>
+
+ %s
+
+ </body></html>'''
+
+form_template = '''
+<form action="" method="POST">
+
+<p>Your name:<br>
+<form:error name="name">
+<input type="text" name="name"></p>
+
+<p>Your age:<br>
+<form:error name="age">
+<input type="text" name="age"></p>
+
+<p>Your favorite color:<br>
+<form:error name="color">
+<input type="checkbox" value="red" name="color"> Red<br>
+<input type="checkbox" value="blue" name="color"> Blue<br>
+<input type="checkbox" value="black" name="color"> Black<br>
+<input type="checkbox" value="green" name="color"> Green<br>
+<input type="checkbox" value="pink" name="color"> Pink</p>
+
+<input type="submit" name="_action_save" value="Submit">
+</form>'''
+
+response_template = '''
+<h2>Hello, %(name)s!</h2>
+<p>You are %(age)d years old
+and your favorite color is %(color)s.</p>'''
+
+
+class FormSchema(Schema):
+    name = validators.String(not_empty=True)
+    age = validators.Int(min=13, max=99)
+    color = validators.OneOf(['red', 'blue', 'black', 'green'])
+    filter_extra_fields = True
+    allow_extra_fields = True
+
+
+class index(Page):
+
+    def awake(self, trans):
+        Page.awake(self, trans)
+        self.rendered_form = None
+
+    def actions(self):
+        return ['save']
+
+    def save(self):
+        fields = self.request().fields()
+        try:
+            fields = FormSchema.to_python(fields, self)
+        except Invalid as e:
+            errors = dict((k, v.encode('utf-8'))
+                for k, v in six.iteritems(e.unpack_errors()))
+            print("Errors:", errors)
+            self.rendered_form = htmlfill.render(form_template,
+                defaults=fields, errors=errors)
+            self.writeHTML()
+        else:
+            self.doAction(fields)
+
+    def doAction(self, fields):
+        print("Fields:", fields)
+        self.rendered_form = response_template % fields
+        self.writeHTML()
+
+    def writeHTML(self):
+        if self.rendered_form is None:
+            self.rendered_form = htmlfill.render(form_template,
+                defaults=self.getDefaults())
+        self.write(page_style % self.rendered_form)
+
+    def getDefaults(self):
+        return dict(age='enter your age', color=['blue'])
+
+    def preAction(self, trans):
+        pass
+    postAction = preAction
---
+++ packages/FormEncode/FormEncode-2.0.0.tar.gz/FormEncode-2.0.0/setup.py
@@ -0,0 +1,55 @@
+"""FormEncode validates and converts nested structures.
+
+It allows for a declarative form of defining the validation,
+and decoupled processes for filling and generating forms.
+
+The official repo is at GitHub: https://github.com/formencode/formencode
+"""
+from __future__ import absolute_import
+
+import sys
+from setuptools import setup, find_packages
+
+if not '2.7' <= sys.version < '3.0' and not '3.6' <= sys.version:
+    raise ImportError('Python version not supported')
+
+tests_require = ['nose', 'dnspython==1.16.0' if sys.version < '3.0' else 'dnspython>=2.0.0',
+     'pycountry<19' if sys.version < '3.0' else 'pycountry']
+
+doctests = ['docs/htmlfill.txt', 'docs/Validator.txt',
+    'formencode/tests/non_empty.txt']
+
+setup(name='FormEncode',
+      # requires_python='>=2.7,!=3.0,!=3.1,!=3.2,!=3.3,!=3.4,' # PEP345
+      description="HTML form validation, generation, and conversion package",
+      long_description=__doc__,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+           "Intended Audience :: Developers",
+           "License :: OSI Approved :: MIT License",
+           "Programming Language :: Python",
+           "Programming Language :: Python :: 2",
+           "Programming Language :: Python :: 2.7",
+           "Programming Language :: Python :: 3",
+           "Programming Language :: Python :: 3.6",
+           "Programming Language :: Python :: 3.7",
+           "Programming Language :: Python :: 3.8",
+           "Topic :: Software Development :: Libraries :: Python Modules",
+           ],
+      author='Ian Bicking',
+      author_email='ianb@colorstudy.com',
+      url='http://formencode.org',
+      license='MIT',
+      data_files = [("", ["LICENSE.txt"])],
+      zip_safe=False,
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={'formencode': ['../docs/*.txt']},
+      test_suite='formencode.tests',
+      install_requires=['six'],
+      tests_require=tests_require,
+      use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
+
+      extras_require={'testing': tests_require},
+    )

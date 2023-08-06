c56b1239950b07eaf1740735bd782b0fd6d24a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:25 +0000

    Add package babachi/1.4.0/babachi-1.4.0.tar.gz

---
+++ packages/babachi/babachi-1.4.0.tar.gz/babachi-1.4.0/README.md
@@ -0,0 +1,43 @@
+# BABACHI: Backgroud Allelic Dosage Bayesian Checkpoint Identification
+[![DOI](https://zenodo.org/badge/255952669.svg)](https://zenodo.org/badge/latestdoi/255952669) <br>
+BABACHI is a tool for Background Allelic Dosage (BAD) genomic regions calling from
+non-phased heterozygous SNVs. It is aimed at estimation of BAD on low-coverage sequencing data, where
+the precise estimation of allelic copy numbers is not possible.
+
+BAD corresponds to the ratio of Major copy number to Minor copy number.
+
+BABACHI takes in a vcf-like .tsv file with heterozygous SNVs sorted by genome positions (ascending).
+The input file must contain the following first 7 columns:
+chromosome, position, ID, reference base, alternative base, reference read count, alternative read count
+All lines, starting with # are ignored.
+
+The output is a .bed file with BAD annotations.
+
+## Installation
+
+```
+pip3 install babachi 
+```
+
+## Requirements
+```
+python >= 3.6
+```
+
+## Usage
+```
+babachi <options>...
+```
+To get full usage description one can execute:
+```
+babachi --help
+```
+
+## Quick start
+To perform a test run:
+```
+babachi --test
+```
+
+
+The BABACHI tool is maintained by Sergey Abramov and Alexandr Boytsov.
---
+++ packages/babachi/babachi-1.4.0.tar.gz/babachi-1.4.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from io import open
+import os
+
+version_module = {}
+dir_name = os.path.dirname(__file__)
+with open(os.path.join(dir_name, "src", "babachi", "version.py")) as fp:
+    exec(fp.read(), version_module)
+    __version__ = version_module['__version__']
+
+with open(os.path.join(dir_name, "README.md"), encoding='utf8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='babachi',
+    version=__version__,
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_data={'babachi': ['tests/*.tsv']},
+    long_description=long_description,
+    entry_points={
+        'console_scripts': [
+            'babachi = babachi.BADEstimation:segmentation_start',
+        ],
+    },
+    author="Sergey Abramov, Alexandr Boytsov",
+    author_email='aswq22013@gmail.com',
+    package_dir={'': 'src'},
+    install_requires=[
+        'docopt>=0.6.2',
+        'numpy>=1.18.0',
+        'schema>=0.7.2',
+        'contextlib2>=0.5.5',
+        'pandas>=1.0.4',
+        'matplotlib>=3.2.1',
+        'seaborn>=0.10.1'
+    ],
+    extras_require={
+        'dev': ['wheel', 'twine', 'setuptools_scm'],
+    },
+    python_requires='>=3.6',
+    url="https://github.com/autosome-ru/BABACHI",
+)

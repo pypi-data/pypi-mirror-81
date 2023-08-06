169c5e98e02171cdbc4deaba0496f40dfe61febd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:29 +0000

    Add package Geode-SimplexRemesh/3.1.0/Geode_SimplexRemesh-3.1.0-cp37-cp37m-win_amd64.whl

---
+++ packages/Geode-SimplexRemesh/Geode_SimplexRemesh-3.1.0-cp37-cp37m-win_amd64.whl/geode_simplexremesh/surface.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2020 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+from geode_parameterization import surface
+
+from .geode_simplexremesh_py_surface import *
+

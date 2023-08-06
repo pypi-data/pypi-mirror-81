fa4c14bb171c041322bcb1ec0ce26e87c0565b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:46 +0000

    Add package Quart-Discord/2.0.0/Quart-Discord-2.0.0.tar.gz

---
+++ packages/Quart-Discord/Quart-Discord-2.0.0.tar.gz/Quart-Discord-2.0.0/quart_discord/__init__.py
@@ -0,0 +1,17 @@
+from .exceptions import *
+from .utils import *
+
+from .client import DiscordOAuth2Session
+
+
+__all__ = [
+    "DiscordOAuth2Session",
+    "requires_authorization",
+
+    "HttpException",
+    "RateLimited",
+    "Unauthorized",
+]
+
+
+__version__ = "2.0.0"

58770a7d2874bf11beb99bbdde0596ce6e422859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:47 +0000

    Add package pycolour/0.0.7/pycolour-0.0.7.tar.gz

---
+++ packages/pycolour/pycolour-0.0.7.tar.gz/pycolour-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycolour", # Replace with your own username
+    version="0.0.7",
+    author="Daniel Smith",
+    author_email="daniel@waiora.com.au",
+    description="A simple command line colour tool for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PythonEuropa/pycolour",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

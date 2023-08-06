cd7d888a0a657f0fae735a4436d7869e70e8dd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:54 +0000

    Add package asyncpgx/1.1.1/asyncpgx-1.1.1.tar.gz

---
+++ packages/asyncpgx/asyncpgx-1.1.1.tar.gz/asyncpgx-1.1.1/README.md
@@ -0,0 +1,43 @@
+# asyncpgx
+[![Build passed](https://img.shields.io/github/workflow/status/laukhin/asyncpgx/CI)](https://github.com/laukhin/asyncpgx/actions?query=workflow%3ACI)
+[![Test coverage](https://img.shields.io/codecov/c/github/laukhin/asyncpgx)](https://codecov.io/gh/laukhin/asyncpgx)
+[![Version](https://img.shields.io/pypi/v/asyncpgx)](https://pypi.org/project/asyncpgx/)
+
+Extensions for asyncpg.
+
+Based on the [asyncpg](https://github.com/MagicStack/asyncpg) and highly inspired by the [sqlx](https://github.com/jmoiron/sqlx) package
+
+This package supports 3.6, 3.7 and 3.8 python versions
+
+## Setup
+Use `pip install asyncpgx`
+
+## Purpose
+This is a thin wrapper on the `asyncpg` package.
+Our purpose is to provide convenient extensions to the original package.
+We're trying to delegate as much work as we can to the asyncpg (basically our extension methods are high-level proxies to the underlying ones)
+and make only converting job.
+Original asyncpg API stays the same, you can see it in the [asyncpg documentation](https://magicstack.github.io/asyncpg/current/).
+
+## Functionality
+* queries with named parameters, i.e.
+```python
+import asyncpgx
+
+connection = await asyncpgx.connect('postgresql://127.0.0.1:5432')
+await connection.named_fetch('''SELECT field FROM some_table WHERE id <= :id;''', {'id': 1})
+```
+* prepared statements with named parameters, i.e.
+```python
+import asyncpgx
+
+connection = await asyncpgx.connect('postgresql://127.0.0.1:5432')
+prepared_statement = await connection.named_prepare('''SELECT field FROM some_table WHERE id <= :id;''')
+await prepared_statement.named_fetch({'id': 1})
+```
+
+## Documentation
+You can find project documentation [here](https://laukhin.github.io/asyncpgx/index.html)
+
+## Changelog
+You can find all releases description [here](https://github.com/laukhin/asyncpgx/releases)
---
+++ packages/asyncpgx/asyncpgx-1.1.1.tar.gz/asyncpgx-1.1.1/asyncpgx/connection.py
@@ -0,0 +1,99 @@
+"""Module with extensions of asyncpg `Connection` class."""
+import functools
+import typing
+
+import asyncpg
+from asyncpg import cursor
+
+from asyncpgx import prepared_statement
+from asyncpgx import query as query_module
+
+
+class ConnectionX(asyncpg.connection.Connection):
+    """Extended version of asyncpg `Connection` class.
+
+    Provides various extension methods, but doesn't touches the original
+    ones
+    """
+
+    def _prepare_asyncpg_parameters(
+        self, query: str, args: typing.Any, converter: query_module.QueryParamsConverter
+    ) -> typing.Tuple[str, typing.List]:
+        """Prepare high-level query and arguments to underlying asyncpg
+        backend."""
+        converted_query, params_order_list = query_module.construct_asyncpg_query(query)
+        return converted_query, converter.prepare_asyncpg_args(args, params_order_list)
+
+    async def named_execute(self, query: str, args: typing.Dict, timeout: typing.Optional[float] = None) -> str:
+        """Extended versions of `execute` with support of the named
+        parameters."""
+        converted_query, asyncpg_args = self._prepare_asyncpg_parameters(
+            query, args, query_module.QueryParamsDictConverter()
+        )
+        return await super().execute(converted_query, *asyncpg_args, timeout=timeout)
+
+    async def named_executemany(self, query: str, args: typing.List, *, timeout: typing.Optional[float] = None) -> None:
+        """Extended versions of `executemany` with support of the named
+        parameters."""
+        converted_query, asyncpg_args = self._prepare_asyncpg_parameters(
+            query, args, query_module.QueryParamsListDictConverter()
+        )
+        return await super().executemany(converted_query, asyncpg_args, timeout=timeout)
+
+    async def named_fetch(
+        self, query: str, args: typing.Dict, timeout: typing.Optional[float] = None
+    ) -> typing.List[asyncpg.Record]:
+        """Extended versions of `fetch` with support of the named
+        parameters."""
+        converted_query, asyncpg_args = self._prepare_asyncpg_parameters(
+            query, args, query_module.QueryParamsDictConverter()
+        )
+        return await super().fetch(converted_query, *asyncpg_args, timeout=timeout)
+
+    async def named_fetchval(
+        self, query: str, args: typing.Dict, column: int = 0, timeout: typing.Optional[float] = None
+    ) -> typing.Optional[typing.Any]:
+        """Extended versions of `fetchval` with support of the named
+        parameters."""
+        converted_query, asyncpg_args = self._prepare_asyncpg_parameters(
+            query, args, query_module.QueryParamsDictConverter()
+        )
+        return await super().fetchval(converted_query, *asyncpg_args, column=column, timeout=timeout)
+
+    async def named_fetchrow(
+        self, query: str, args: typing.Dict, timeout: typing.Optional[float] = None
+    ) -> typing.Optional[asyncpg.Record]:
+        """Extended versions of `fetchrow` with support of the named
+        parameters."""
+        converted_query, asyncpg_args = self._prepare_asyncpg_parameters(
+            query, args, query_module.QueryParamsDictConverter()
+        )
+        return await super().fetchrow(converted_query, *asyncpg_args, timeout=timeout)
+
+    def named_cursor(
+        self,
+        query: str,
+        args: typing.Dict,
+        prefetch: typing.Optional[int] = None,
+        timeout: typing.Optional[float] = None,
+    ) -> cursor.CursorFactory:
+        """Extended version of `cursor` with support of the named
+        parameters."""
+        converted_query, asyncpg_args = self._prepare_asyncpg_parameters(
+            query, args, query_module.QueryParamsDictConverter()
+        )
+        return super().cursor(converted_query, *asyncpg_args, prefetch=prefetch, timeout=timeout)
+
+    async def named_prepare(
+        self, query: str, *, timeout: typing.Optional[float] = None
+    ) -> prepared_statement.PreparedStatementX:
+        """Extended version of `prepare` with support of the named
+        parameters."""
+        converted_query, params_order_list = query_module.construct_asyncpg_query(query)
+        self._check_open()
+        stmt = await self._get_statement(converted_query, timeout, named=True, use_cache=False)
+        return prepared_statement.PreparedStatementX(self, converted_query, stmt, query, params_order_list)
+
+
+create_pool = functools.partial(asyncpg.create_pool, connection_class=ConnectionX)
+connect = functools.partial(asyncpg.connect, connection_class=ConnectionX)

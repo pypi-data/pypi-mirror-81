87daa7b4088e944fd11c79751a076b0824588fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:56 +0000

    Add package boardwatch-models/0.5.0/boardwatch_models-0.5.0-py3-none-any.whl

---
+++ packages/boardwatch-models/boardwatch_models-0.5.0-py3-none-any.whl/boardwatch_models/platform_edition.py
@@ -0,0 +1,11 @@
+class PlatformEdition():
+	def __init__(self, id, name, official_color, has_matte, has_transparency, has_gloss, note, image_url):
+		self.id = id
+		self.name = name
+		self.official_color = official_color
+		self.has_matte = has_matte
+		self.has_transparency = has_transparency
+		self.has_gloss = has_gloss
+		self.note = note
+		self.image_url = image_url
+		self.colors = list()

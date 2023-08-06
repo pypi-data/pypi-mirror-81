06b199f14ad794096a6e55445069b4461691db08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:30 +0000

    Add package mitosheet/0.1.24/mitosheet-0.1.24.tar.gz

---
+++ packages/mitosheet/mitosheet-0.1.24.tar.gz/mitosheet-0.1.24/src/components/SheetTab.tsx
@@ -0,0 +1,28 @@
+// Copyright (c) Mito
+// Distributed under the terms of the Modified BSD License.
+
+import React from 'react';
+
+// import css
+import "../../css/sheet-tab.css"
+
+type SheetTabProps = {
+    sheetName: string;
+};
+
+
+export default function SheetTab(props : SheetTabProps) : JSX.Element {
+
+    const sheetName = props.sheetName ? props.sheetName : "sheet1";
+
+    return (
+        <div className="tab">
+            <div>
+                <p>
+                    {sheetName}
+                </p>
+                <hr className="selected-tab-indicator"></hr>
+            </div>  
+        </div>
+    );
+}

c61fed088365d43166dcf99029b693f94fc724e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:39 +0000

    Add package andrew-is-hungry-for-py/0.0.1/andrew_is_hungry_for_py-0.0.1-py3-none-any.whl

---
+++ packages/andrew-is-hungry-for-py/andrew_is_hungry_for_py-0.0.1-py3-none-any.whl/andrew_is_hungry_for_py/eat.py
@@ -0,0 +1,11 @@
+from dateutil.parser import parse
+
+def feedme(inputdate):
+    datetime = parse(inputdate).date()
+    day = datetime.day
+    month = datetime.month
+
+    if day == 31 and month == 10:
+        return "Birthday Cake"
+
+    return "Cake"

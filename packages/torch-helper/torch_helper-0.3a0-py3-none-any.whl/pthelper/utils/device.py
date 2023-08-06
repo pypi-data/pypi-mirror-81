4add54c6f88d89d61896d9be2cbe30f055b07671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:12 +0000

    Add package torch-helper/0.3a0/torch_helper-0.3a0-py3-none-any.whl

---
+++ packages/torch-helper/torch_helper-0.3a0-py3-none-any.whl/pthelper/utils/device.py
@@ -0,0 +1,14 @@
+import torch
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+def to_device(data, device=get_default_device()):
+    """Move data/model to GPU/CPU"""
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device)

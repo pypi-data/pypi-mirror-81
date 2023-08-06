682d362299b58fe70059df14444974c1780365c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:09 +0000

    Add package bitmovin-api-sdk/1.50.0/bitmovin-api-sdk-1.50.0.tar.gz

---
+++ packages/bitmovin-api-sdk/bitmovin-api-sdk-1.50.0.tar.gz/bitmovin-api-sdk-1.50.0/bitmovin_api_sdk/models/filter_type.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from enum import Enum
+from six import string_types, iteritems
+from bitmovin_api_sdk.common.poscheck import poscheck_model
+
+
+class FilterType(Enum):
+    CROP = "CROP"
+    CONFORM = "CONFORM"
+    WATERMARK = "WATERMARK"
+    ENHANCED_WATERMARK = "ENHANCED_WATERMARK"
+    ROTATE = "ROTATE"
+    DEINTERLACE = "DEINTERLACE"
+    AUDIO_MIX = "AUDIO_MIX"
+    DENOISE_HQDN3D = "DENOISE_HQDN3D"
+    TEXT = "TEXT"
+    UNSHARP = "UNSHARP"
+    SCALE = "SCALE"
+    INTERLACE = "INTERLACE"
+    AUDIO_VOLUME = "AUDIO_VOLUME"
+    EBU_R128_SINGLE_PASS = "EBU_R128_SINGLE_PASS"

ed731fb728b4bd6bb920fc042413f80ab3b9ba43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:30 +0000

    Add package python-ilorest-library/3.1.0/python-ilorest-library-3.1.0.zip

---
+++ packages/python-ilorest-library/python-ilorest-library-3.1.0.zip/python-ilorest-library-3.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+extras = {'socks': ['pysocks']}
+
+setup(name='python-ilorest-library',
+      version='3.1.0',
+      description='iLO Rest Python Library',
+	  author = 'Hewlett Packard Enterprise',
+	  author_email = 'kocurek@hpe.com',
+      extras_require = extras,
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Communications'
+      ],
+      keywords='HP Enterprise',
+      url='https://github.com/HewlettPackard/python-ilorest-library',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      install_requires=[
+          'jsonpatch',
+          'jsonpath_rw',
+          'jsonpointer',
+          'urllib3',
+          'six'
+      ])

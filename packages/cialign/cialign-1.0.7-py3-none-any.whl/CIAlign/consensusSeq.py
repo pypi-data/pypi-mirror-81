830e1da7b25114ab5d40fdfd9d1ae37af8414e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:39 +0000

    Add package cialign/1.0.7/cialign-1.0.7-py3-none-any.whl

---
+++ packages/cialign/cialign-1.0.7-py3-none-any.whl/CIAlign/consensusSeq.py
@@ -0,0 +1,569 @@
+#! /usr/bin/env python
+
+import matplotlib
+from math import log
+import numpy as np
+# import scipy.interpolate as interpolate
+import matplotlib.pyplot as plt
+import operator
+import matplotlib.patheffects
+import math
+from matplotlib import gridspec
+try:
+    import CIAlign.utilityFunctions as utilityFunctions
+except ImportError:
+    import utilityFunctions
+matplotlib.use('Agg')
+
+
+def getAxisUnits(subplot):
+    '''
+    Translates the height of one unit of y axis
+    and width of one unit of x axis into pixels
+    Translates height of y axis and width of x axis
+    to pixels
+
+    Parameters
+    ----------
+    subplot: matplotlib.pyplot.subplot
+        An open subplot
+
+    Returns
+    -------
+    D: dict
+        Keys are names of axis heights and widths, the values are in units
+        or pixels
+    '''
+    axis_dimensions = subplot.transData.transform(
+        [(subplot.get_xlim()[1],
+          subplot.get_ylim()[1]), (0, 0)]) - subplot.transData.transform((
+              0, 0))
+    D = dict()
+    # height of y in pixels
+    D['axis_height_px'] = axis_dimensions[0][1]
+    # height of x in pixels
+    D['axis_width_px'] = axis_dimensions[0][0]
+    D['axis_bottom'], D['axis_top'] = subplot.get_ylim()
+    D['axis_left'], D['axis_right'] = subplot.get_xlim()
+    # total number of units on x axis
+    D['axis_width_u'] = D['axis_right'] - D['axis_left']
+    # total number of units on y axis
+    D['axis_height_u'] = D['axis_top'] - D['axis_bottom']
+    # height of one y axis unit in pixels
+    D['u_height_px'] = D['axis_height_px'] / D['axis_height_u']
+    # width of one x axis unit in pixels
+    D['u_width_px'] = D['axis_width_px'] / D['axis_width_u']
+
+    return (D)
+
+
+def getFontSize(figure, subplot, height_u):
+    '''
+    Based on axes units, converts specified number of y axis units into
+    the font size in points
+
+    Parameters
+    ----------
+    figure: matplotlib.Figure
+        An open figure
+
+    subplot: matplotlib.pyplot.subplot
+        An open subplot
+
+    height_u: float
+        Height in units
+
+    Returns
+    -------
+    Height_points*1.8: float
+        adjusted height in points
+    '''
+
+    D = getAxisUnits(subplot)
+    perc_height = height_u / (D['axis_top'] - D['axis_bottom'])
+    height_pixels = D['axis_height_px'] * perc_height
+    dpi = figure.dpi
+    height_points = PixelsToPoints(height_pixels, dpi)
+
+    return (height_points * 1.8)
+
+
+def PixelsToPoints(pixels, dpi):
+    '''
+    Converts pixel values to point values
+
+    Parameters
+    ----------
+    pixels: float
+        pixel value
+
+    dpi: int
+        dpi value
+
+    Returns
+    -------
+    points: float
+        points value
+    '''
+    # one point is 1/72 of an inch
+    points = pixels * (72 / dpi)
+
+    return (points)
+
+
+def getLetters(typ='nt', fontname='monospace', dpi=500):
+    '''
+    Generates a temporary image file for every letter (4 nt or 20 aa)
+    Each letter extends to the full length of both axes
+
+    Parameters
+    ----------
+    typ: string
+        nt (default) or aa
+
+    fontname: string
+        name of a font (default: monospace)
+
+    dpi: int
+        DPI value (default: 500)
+
+    Returns
+    -------
+    none
+    '''
+    # obtain color scheme depending on nt or aa alignment
+    if typ == 'nt':
+        colours = utilityFunctions.getNtColours()
+    elif typ == 'aa':
+        colours = utilityFunctions.getAAColours()
+    # for each possible base/aa create temporary plot
+    for base in colours.keys():
+        f = plt.figure(figsize=(1, 1), dpi=dpi, edgecolor='black')
+        a = f.add_subplot(111)
+        a.set_xlim(0, 1)
+        a.set_ylim(0, 1)
+        fs = getFontSize(f, a, 1)
+        a.text(0.5, 0, base, fontsize=fs, fontdict={'family': 'monospace',
+                                                    'name': fontname},
+               color=colours[base], va='baseline', ha='center')
+        plt.gca().set_axis_off()
+        a.margins(0, 0)
+        f.subplots_adjust(top=1, bottom=0, right=1, left=0,
+                          wspace=None, hspace=None)
+        a.set_frame_on(False)
+        # temporarily safe plot in working directory
+        f.savefig("%s_temp.png" % base, dpi=500,
+                  pad_inches=0)
+        plt.close()
+
+
+def findConsensus(alignment, log, consensus_type="majority"):
+    '''
+    Calculates the consensus sequence and the coverage for the alignment
+    Utilises different types of the consensus
+
+    Parameters
+    ----------
+    alignment: np.array
+        The alignment stored as a numpy array
+
+    log: string
+        name of log file
+
+    consensus_type: string
+        majority (default) or majority_nongap
+
+    Returns
+    -------
+    consensus: list of strings
+        consensus sequence
+
+    coverage: list of floats
+        alignment coverage
+    '''
+
+    consensus = []
+    coverage = []
+    numberOfSequences = len(alignment[:, 0])
+
+    # need the reverse of array to access every column
+    for i in range(0, len(alignment[0, :])):
+        unique, counts = np.unique(alignment[:, i], return_counts=True)
+        count = dict(zip(unique, counts))
+        unique_ng = unique[unique != "-"]
+        counts_ng = counts[unique != "-"]
+        # deal with gap only columns
+        if counts_ng.size == 0:
+            count_ng = {"N": len(alignment[:, i])}
+            nonGapContent = 0
+        else:
+            count_ng = dict(zip(unique_ng, counts_ng))
+            if '-' in count:
+                nonGapContent = 1-(count['-']/numberOfSequences)
+            else:
+                nonGapContent = 1
+
+        # dealing with gap only collumns
+        maxChar, maxCount = max(count.items(), key=operator.itemgetter(1))
+        maxChar_ng, maxCount_ng = max(count_ng.items(),
+                                      key=operator.itemgetter(1))
+
+        # if there is an equal number of gap and non-gap characters at the
+        # site, keep the non-gap character
+        # if majoriy_nongap chosen, use the nongap
+        if maxCount_ng == maxCount or consensus_type == "majority_nongap":
+            maxChar = maxChar_ng
+        consensus.append(maxChar)
+        coverage.append(nonGapContent)
+
+    return consensus, coverage
+
+
+def makeCoveragePlot(coverage, dest, dpi=300, height=3, width=5,
+                     colour='#007bf5'):
+    '''
+    Creates a plot of the coverage
+
+    Parameters
+    ----------
+    coverage: list of strings
+        Coverage for alignment
+
+    dest: str
+        folder to store file
+
+    dpi: int
+        DPI value (default: 500)
+
+    height: int
+            height of plot, default: 3
+
+    width: int
+            height of plot, default: 5
+
+    colour: str
+            coverage colour (default: #007bf5)
+
+    Returns
+    -------
+    none
+    '''
+
+    fontsize = 1500 / dpi
+    x = np.arange(0, len(coverage), 1)
+    y = coverage
+
+    xmax = x.max()
+    # used for polynomial interpolation
+    # xmin = x.min()
+    # N = 100
+    # xx = np.linspace(xmin, xmax, N)
+
+    # plain plotting of the coverage
+    f = plt.figure(figsize=(width, height), dpi=dpi)
+    a = f.add_subplot('211')
+    a.plot(x, y, color=colour)
+    a.set_xlabel('Position', fontsize=fontsize)
+    a.set_ylabel('Coverage', fontsize=fontsize)
+    a.set_xticks([0, xmax])
+    a.set_xticklabels([0, xmax], fontsize=fontsize)
+    a.set_yticks(np.arange(0, 1.1, 0.5))
+    a.set_yticklabels(np.arange(0, 1.1, 0.5), fontsize=fontsize)
+
+    # b = f.add_subplot('212')
+
+    # polynomial interpolation leaving this in just in case
+    # c = f.add_subplot('313')
+    # z = np.polyfit(x, bla, 30)
+    # p = np.poly1d(z)
+    # c.plot(xx, p(xx))
+    # xnew = np.linspace(x.min(),x.max(),300)
+    # 300 represents number of points to make between T.min and T.max
+
+    # interpolating the coverage function to make it smooth
+    # t, c, k = interpolate.splrep(x, y, s=0, k=4)
+    # spline = interpolate.BSpline(t, c, k, extrapolate=False)
+    # b.plot(xx, spline(xx), color=colour)
+    # b.set_xlabel('Position', fontsize=fontsize)
+    # b.set_ylabel('Coverage (Smoothed)', fontsize=fontsize)
+    # b.set_xticks([0, xmax])
+    # b.set_xticklabels([0, xmax], fontsize=fontsize)
+    # b.set_yticks(np.arange(0, 1.1, 0.5))
+    # b.set_yticklabels(np.arange(0, 1.1, 0.5), fontsize=fontsize)
+    f.savefig(dest, dpi=dpi, bbox_inches='tight')
+
+
+def sequence_logo(alignment,
+                  figname,
+                  typ='nt',
+                  figfontname='Arial',
+                  figdpi=300,
+                  figrowlength=50):
+    '''
+    Creates a sequence logo based on an entropy calculation using letters
+    Scales the letters according to the information content of the alignment
+    Representations the consensus sequence of the alignment
+
+    Parameters
+    ----------
+    alignment: np.array
+        The alignment stored as a numpy array
+
+    figname: str
+        name of figure
+
+    typ: str
+        Either 'aa' - amino acid - or 'nt' - nucleotide
+
+    figfontname: str
+            Name of font, default: Arial
+
+    figdpi: int
+            DPI (default: 300)
+
+    figrowlength: int
+            clength of figure (default: 50)
+
+    Returns
+    -------
+    none
+    '''
+    alignment_width = len(alignment[0, :])
+    if alignment_width < figrowlength:
+        figrowlength = alignment_width
+    nsegs = math.ceil(alignment_width / figrowlength)
+    f = plt.figure(figsize=(figrowlength, nsegs*2), dpi=figdpi)
+    gs = gridspec.GridSpec(ncols=1, nrows=nsegs)
+    getLetters(typ=typ, fontname=figfontname, dpi=figdpi)
+    rstart = 0
+    rend = rstart + figrowlength
+    for n in range(nsegs):
+
+        if rend > alignment_width:
+            rend = alignment_width
+        a = plt.subplot(gs[n])
+        a.set_xlim(rstart, rstart+figrowlength)
+        a.set_ylim(0, 2.1)
+        limits = a.axis()
+
+        # for each column calculate heights via entropy
+        # and scale letters accordlingly
+        for i in range(rstart, rend):
+            unique, counts = np.unique(alignment[:, i],
+                                       return_counts=True)
+            count = dict(zip(unique, counts))
+            height_per_base, info_per_base = calc_entropy(count,
+                                                          len(alignment[:, 0]),
+                                                          typ=typ)
+            height_sum_higher = 0
+            for base, height in height_per_base.items():
+                if height > 0:
+                    L = plt.imread("%s_temp.png" % base)
+                    a.imshow(L, extent=(i, i+1, height_sum_higher,
+                                        height_sum_higher+height),
+                             filternorm=False)
+                    height_sum_higher += height
+        a.axis(limits)
+        a.set_xticks([rstart, rend])
+        a.set_xticklabels([rstart, rend])
+        a.set_yticks(np.arange(0, 2.1, 1))
+        a.spines['right'].set_visible(False)
+        a.spines['top'].set_visible(False)
+        if n == (nsegs - 1):
+            a.set_xlabel("Position")
+        a.set_ylabel("Bit Score")
+        rstart += figrowlength
+        rend += figrowlength
+    # obtain colours
+    #  if typ == 'nt':
+    #    allbases = utilityFunctions.getNtColours()
+    # elif typ == 'aa':
+    #    allbases = utilityFunctions.getAAColours()
+    # for base in allbases:
+    #    os.unlink("%s_temp.png" % base)
+    # save plot using figname
+    f.savefig(figname, dpi=figdpi, bbox_inches='tight')
+    plt.close()
+
+
+def sequence_bar_logo(alignment,
+                      figname,
+                      typ='nt',
+                      figdpi=300,
+                      figrowlength=50):
+    '''
+    Creates a sequence logo based on an entropy calculation using bars
+    Scales the bars according to the information content of the alignment
+    Representations the consensus sequence of the alignment
+
+    Parameters
+    ----------
+    alignment: np.array
+        The alignment stored as a numpy array
+
+    figname: str
+        name of figure
+
+    typ: str
+        Either 'aa' - amino acid - or 'nt' - nucleotide
+
+    figfontname: str
+            Name of font, default: Arial
+
+    figdpi: int
+            DPI (default: 300)
+
+    figrowlength: int
+            clength of figure (default: 50)
+
+    Returns
+    -------
+    none
+    '''
+    alignment_width = len(alignment[0, :])
+    if alignment_width < figrowlength:
+        figrowlength = alignment_width
+    nsegs = math.ceil(alignment_width / figrowlength)
+    f = plt.figure(figsize=(figrowlength/5, nsegs*2), dpi=figdpi)
+    gs = gridspec.GridSpec(ncols=1, nrows=nsegs)
+    rstart = 0
+    rend = rstart + figrowlength
+
+    for n in range(nsegs):
+        if rend > alignment_width:
+            rend = alignment_width
+        axes = f.add_subplot(gs[n])
+        axes.set_xlim(rstart-0.5, rend-0.5)
+        axes.set_ylim(0, 2.1)
+        seq_count = len(alignment[:, 0])
+        width = 0.75
+        ind = np.arange(rstart, rend)
+
+        if typ == "nt":
+            element_list = utilityFunctions.getNtColours()
+            colours = utilityFunctions.getNtColours()
+        elif typ == "aa":
+            element_list = utilityFunctions.getAAColours()
+            colours = utilityFunctions.getAAColours()
+        height_list = {}
+
+        for element in element_list:
+            height_list[element] = []
+
+        bottom_height = []
+        # for each column calculate heights via entropy
+        # and scale letters accordlingly
+        for i in range(rstart, rend):
+            unique, counts = np.unique(alignment[:, i], return_counts=True)
+            count = dict(zip(unique, counts))
+            height_per_base, info_per_base = calc_entropy(count, seq_count,
+                                                          typ)
+            bottom_height.append(0)
+
+            # need a list of each nt/aa separately to plot them as bars
+            for base, height in height_per_base.items():
+                height_list[base].append(height_per_base[base])
+
+        # stag bars on top of each other
+        for base, height in height_list.items():
+            plt.bar(ind, height, width, bottom=bottom_height,
+                    color=colours[base])
+            bottom_height = [i+j for i, j in zip(bottom_height, height)]
+
+        plt.xticks([rstart, rend-1], [rstart+1, rend])
+        plt.yticks(np.arange(0, 2.1, 1))
+        plt.xlabel("Position")
+        plt.ylabel("Bit Score")
+
+        axes.spines['right'].set_visible(False)
+        axes.spines['top'].set_visible(False)
+        rstart += figrowlength
+        rend += figrowlength
+    # save plot as figname
+    plt.savefig(figname, bbox_inches='tight', dpi=figdpi)
+    plt.close()
+
+
+def calc_entropy(count, seq_count, typ):
+    '''
+    Creates a sequence logo based on an entropy calculation using bars
+    Scales the bars according to the information content of the alignment
+    Representations the consensus sequence of the alignment
+
+    Parameters
+    ----------
+    count: dict
+        of nt/aa with counts
+
+    seq_count: int
+        number of sequences in alignment
+
+    typ: str
+        nt or aa
+
+    Returns
+    -------
+    height_per_base: dictionary
+        height for each nt/aa
+
+    info_per_base: dictionary
+        information content for each nt/aa
+
+    '''
+    # obtain nt/aa lists, use colour scheme for that w/o using colours here
+    # just because another list of nt/aa would be obsolete
+    if typ == "nt":
+        element_list = utilityFunctions.getNtColours()
+        s = 4
+        max_entropy = log(4, 2)
+    elif typ == "aa":
+        element_list = utilityFunctions.getAAColours()
+        s = 20
+        max_entropy = log(20, 2)
+
+    info_per_base = {}
+    freq_per_base = {}
+    height_per_base = {}
+    entropy_per_base = {}
+
+    for element in element_list:
+        info_per_base[element] = 0
+        freq_per_base[element] = 0
+        height_per_base[element] = 0
+        entropy_per_base[element] = 0
+
+    # correct for small sample sizes
+    sample_size_correction = (1/log(2)) * ((s-1)/(2*seq_count))
+    gap_correction = seq_count
+    if count.get("-"):
+        seq_count -= count.get("-")
+    # correct for gaps, since they lower the information content
+    gap_correction = seq_count/gap_correction
+
+    entropy = 0
+    if seq_count == 0:
+        return height_per_base, info_per_base
+
+    # caluclate entropy, from that information, from that height
+    for base, quantity in count.items():
+        if base != "-":
+            frequency = quantity/seq_count
+            freq_per_base[base] = frequency
+            entropy -= frequency*log(frequency, 2)
+            info_per_base[base] = max_entropy + frequency*log(frequency, 2)
+            entropy_per_base[base] = -frequency * log(frequency, 2)
+    information_per_column = max_entropy-entropy-sample_size_correction
+
+    # if the information content is constant throughout the column,
+    # these value will be negative. Since this does not add any information
+    # set them to 0
+    for base, quantity in info_per_base.items():
+        if freq_per_base[base]*information_per_column < 0:
+            height_per_base[base] = 0
+        else:
+            # scale to accomodate gaps
+            height_per_base[base] = (gap_correction *
+                                     freq_per_base[base] *
+                                     information_per_column)
+
+    return height_per_base, info_per_base
---
+++ packages/cialign/cialign-1.0.7-py3-none-any.whl/CIAlign/utilityFunctions.py
@@ -0,0 +1,355 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('Agg')
+
+
+def replaceUbyT(arr):
+    '''
+    Replaces all Us by Ts in the alignment.
+
+    Parameters
+    ----------
+    arr: np.array
+        2D numpu array with the alignment.
+
+    Returns
+    -------
+    arr: np.array
+        2D numpy array of sequences with Ts instead of Us.
+    '''
+
+    arr = np.where(arr == "U", "T", arr)
+    return (arr)
+
+
+def unAlign(arr):
+    '''
+    Removes all gaps from the alignment.
+
+    Parameters
+    ----------
+    arr: np.array
+        2D numpu array with the alignment.
+
+    Returns
+    -------
+    arr: np.array
+        2D numpy array of sequences without any gaps.
+    '''
+
+    arr = np.where(arr == "-", "", arr)
+    return (arr)
+
+
+def FastaToArray(infile, log, outfile_stem=None):
+    '''
+    Convert an alignment into a numpy array.
+
+    Parameters
+    ----------
+    fasta_dict: dict
+        dictionary based on a fasta file with sequence names as keys and
+        sequences as values
+
+    Returns
+    -------
+    arr: np.array
+        2D numpy array in the same order as fasta_dict where each row
+        represents a single column in the alignment and each column a
+        single sequence.
+    nams: list
+        List of sequence names in the same order as in the input file
+    '''
+
+    formatErrorMessage = "The MSA file needs to be in FASTA format."
+    nams = []
+    seqs = []
+    nam = ""
+    seq = ""
+    with open(infile) as input:
+        for line in input:
+            line = line.strip()
+            if len(line) == 0:
+                continue # todo: test!
+            if line[0] == ">":
+                seqs.append([s.upper() for s in seq])
+                nams.append(nam)
+                seq = []
+                nam = line.replace(">", "")
+            else:
+                if len(nams) == 0:
+                    log.error(formatErrorMessage)
+                    print(formatErrorMessage)
+                    exit()
+                seq += list(line)
+    seqs.append(np.array([s.upper() for s in seq]))
+    nams.append(nam)
+    arr = np.array(seqs[1:])
+    return (arr, nams[1:])
+
+
+def getAAColours():
+    '''
+    Generates a dictionary which assigns a colour to each amino acid.
+    Based on the "RasmMol" amino colour scheme and the table here:
+        http://acces.ens-lyon.fr/biotic/rastop/help/colour.htm
+    (plus grey for "X" and white for "-")
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    dict
+        Dictionary where keys are single letter amino acid codes and
+        values are hexadecimal codes for colours
+    '''
+    return {'D': '#E60A0A',
+            'E': '#E60A0A',
+            'C': '#E6E600',
+            'M': '#E6E600',
+            'K': '#145AFF',
+            'R': '#145AFF',
+            'S': '#FA9600',
+            'T': '#FA9600',
+            'F': '#3232AA',
+            'Y': '#3232AA',
+            'N': '#00DCDC',
+            'Q': '#00DCDC',
+            'G': '#EBEBEB',
+            'L': '#0F820F',
+            'V': '#0F820F',
+            'I': '#0F820F',
+            'A': '#C8C8C8',
+            'W': '#B45AB4',
+            'H': '#8282D2',
+            'P': '#DC9682',
+            'X': '#b2b2b2',
+            '-': '#FFFFFF00',
+            'B': '#b2b2b2',
+            'Z': '#b2b2b2',
+            'J': '#b2b2b2',
+            '*': '#FFFFFF00'
+            }
+
+
+def getNtColours():
+    '''
+    Generates a dictionary which assigns a colour to each nucleotide (plus grey
+    for "N" and white for "-")
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    dict
+        Dictionary where keys are single letter nucleotide codes and
+        values are hexadecimal codes for colours
+    '''
+    return {'A': '#1ed30f',
+            'G': '#f4d931',
+            'T': '#f43131',
+            'C': '#315af4',
+            'N': '#b2b2b2',
+            "-": '#FFFFFF',
+            "U": '#f43131',
+            "R": '#b2b2b2',
+            "Y": '#b2b2b2',
+            "S": '#b2b2b2',
+            "W": '#b2b2b2',
+            "K": '#b2b2b2',
+            "M": '#b2b2b2',
+            "B": '#b2b2b2',
+            "D": '#b2b2b2',
+            "H": '#b2b2b2',
+            "V": '#b2b2b2',
+            "X": '#b2b2b2'}
+
+
+def writeOutfile(outfile, arr, nams, removed, rmfile=None):
+    '''
+    Writes an alignment stored in a numpy array into a FASTA file.
+
+    Parameters
+    ----------
+    outfile: str
+        Path to FASTA file where the output should be stored
+    arr: np.array
+        Numpy array containing the cleaned alignment
+    nams: list
+        List of nams of sequences in the input alignment
+    removed: set
+        Set of names of sequences which have been removed
+    rmfile: str
+        Path to file used to log sequences and columns which have been removed
+
+    Returns
+    -------
+    None
+
+    '''
+    out = open(outfile, "w")
+    i = 0
+    for nam in nams:
+        if nam not in removed:
+            out.write(">%s\n%s\n" % (nam, "".join(list(arr[i]))))
+            i += 1
+    out.close()
+
+
+def seqType(arr):
+    '''
+    Detects if an alignment is of nucleotides or amino acids using pre-built
+    dictionarys of amino acid and nucleotide codes.
+    Checks if arr contains characters that are not in the dictionary (not IUPAC)
+
+    Parameters
+    ----------
+    arr: np.array
+        Numpy array containing the alignment
+
+    Returns
+    -------
+    str
+    'aa' for amino acid and 'nt for nucleotide
+    '''
+    nt_count = 0
+    aa_count = 0
+    for seq in arr:
+        nucs = set(list(getNtColours().keys()))
+        aas = set(list(getAAColours().keys()))
+        n = 0
+        a = 0
+        x = 0
+        for s in seq:
+            s = s.upper()
+            if s in nucs:
+                n += 1
+            if s in aas:
+                a += 1
+            if s not in aas and s not in nucs:
+                x += 1
+        counts = n, a, x
+        if n == len(seq):
+            nt_count += 1
+        elif a == len(seq):
+            aa_count += 1
+        else:
+            print("Unknown nucleotides or amino acids detected. Please fix your MSA.")
+            exit()
+    if nt_count == len(arr):
+        return "nt"
+    elif aa_count == len(arr):
+        return "aa"
+    else:
+        print("MSA type couldn't be established. Please fix your MSA.")
+        exit()
+
+
+def updateNams(nams, removed_seqs):
+    '''
+    Takes nams, a list of sequence names in the input file, and removed_seqs,
+    a set of sequences which have been removed from the file and subtracts
+    the removed sequences from nams while maintaining the order.
+
+    Parameters
+    ----------
+    nams: list
+        A list of sequence names in the input file
+    removed_seqs: set
+        Set of sequence names to remove
+
+    Returns
+    -------
+    nams2: list
+        A list of sequence names which have not been removed
+    '''
+    nams2 = []
+    for nam in nams:
+        if nam not in removed_seqs:
+            nams2.append(nam)
+    return (nams2)
+
+
+def checkArrLength(arr, log):
+    '''
+    Checks the shape of the array containing the alignment to ensure that it
+    all the sequences haven't been removed by parsing and that all the
+    sequences have the same number of columns
+
+    Parameters
+    -----------
+    arr: np.array
+        Numpy array containing the multiple sequence alignment
+    log: logging.Logger
+        An open log file object
+    Returns
+    -------
+    None
+    '''
+    emptyAlignmentMessage = """Error: Parsing your alignment with these \
+settings has removed all of the sequences."""
+    differentLengthsMessage = """Error: The sequences in your alignment are \
+not all the same length."""
+    if 0 in np.shape(arr):
+        log.error(emptyAlignmentMessage)
+        print(emptyAlignmentMessage)
+        exit()
+    if len(np.shape(arr)) == 1:
+        log.error(differentLengthsMessage)
+        print(differentLengthsMessage)
+        exit()
+
+
+def listFonts(outfile):
+    '''
+    Generates an image file containing a sample of all the fonts matplotlib
+    has access to on your system, which can be used to choose a font for a
+    text-based sequence logo.
+    Not parameterised - the same image will be generated every time.
+
+    Parameters
+    ----------
+    outfile: str
+        Path to the file in which to save the image
+
+    Returns
+    -------
+    None
+    '''
+    matplotlib.font_manager._rebuild()
+    flist = matplotlib.font_manager.get_fontconfig_fonts()
+    flist2 = set()
+    for fname in flist:
+        try:
+            F = matplotlib.font_manager.FontProperties(fname=fname)
+            font = matplotlib.font_manager.get_font(fname)
+            L = font.get_charmap()
+            # this tests if matplotlib can actually render "ACTG" in this
+            # font
+            if 108 in L:
+                g = F.get_name()
+                flist2.add(g)
+        except RuntimeError:
+            # Some of the fonts seem not to have a name? Ignore these.
+            pass
+
+    flist2 = sorted(list(flist2))[::-1]
+    f = plt.figure(figsize=(5, len(flist2) / 4), dpi=200)
+    a = f.add_subplot(111)
+    a.set_ylim(0, len(flist2))
+    a.set_xlim(0, 1)
+    a.text(-0.1, -1, "*Fonts shown as [] cannot be displayed with CIAlign")
+    for i, fname in enumerate(flist2):
+        a.text(0.7, i, "ACTG", fontdict={'name': fname, 'size': 14})
+        a.text(0, i, fname, fontsize=10)
+
+    a.text(0.7, i+1, "Sample", fontsize=10, fontweight='bold')
+    a.text(0, i+1, "Font Name", fontsize=10, fontweight='bold')
+    a.set_axis_off()
+    f.tight_layout()
+    f.savefig(outfile, dpi=200, bbox_inches='tight')

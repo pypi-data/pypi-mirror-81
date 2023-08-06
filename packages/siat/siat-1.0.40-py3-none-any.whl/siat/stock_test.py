82f322d0c4915485a87cf6f89a47287fbeca54ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:57 +0000

    Add package siat/1.0.40/siat-1.0.40-py3-none-any.whl

---
+++ packages/siat/siat-1.0.40-py3-none-any.whl/siat/stock_test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import os; os.chdir("S:/siat")
+from siat.stock import *
+
+market={'Market':('China','^HSI')}
+stocks={'0700.HK':3,'9618.HK':2,'9988.HK':1}
+portfolio=dict(market,**stocks)
+esg=portfolio_esg2(portfolio)
+
+
+

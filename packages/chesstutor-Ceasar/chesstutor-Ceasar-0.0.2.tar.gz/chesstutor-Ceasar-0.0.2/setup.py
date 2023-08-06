ca58b7487a105c28e1f3db540de89ed3b46fa5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:15 +0000

    Add package chesstutor-Ceasar/0.0.2/chesstutor-Ceasar-0.0.2.tar.gz

---
+++ packages/chesstutor-Ceasar/chesstutor-Ceasar-0.0.2.tar.gz/chesstutor-Ceasar-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chesstutor-Ceasar",
+    version="0.0.2",
+    author="Ceasar Bautista",
+    author_email="cbautista2010@gmail.com",
+    description="Play chess on the command line.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/Ceasar/chesstutor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

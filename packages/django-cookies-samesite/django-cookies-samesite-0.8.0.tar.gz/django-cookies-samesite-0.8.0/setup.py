922f4ffee37144fd64b140c4cdf7d603943e6d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:41 +0000

    Add package django-cookies-samesite/0.8.0/django-cookies-samesite-0.8.0.tar.gz

---
+++ packages/django-cookies-samesite/django-cookies-samesite-0.8.0.tar.gz/django-cookies-samesite-0.8.0/setup.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from io import open
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def get_version(*file_paths):
+    """Retrieves the version from django_cookies_samesite/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+version = get_version("django_cookies_samesite", "__init__.py")
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+
+    Returns:
+        list: Requirements file relative path strings
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path, encoding='utf-8') as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement.
+
+    Returns:
+        bool: True if the line is not blank, a comment, a URL, or an included file
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on git:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open('README.rst', encoding='utf-8').read()
+
+setup(
+    name='django-cookies-samesite',
+    version=version,
+    description="""Middleware which sets SameSite flag for session and csrf cookies in legacy versions of Django.""",
+    long_description=readme,
+    author='Jarek "jotes" Śmiejczak',
+    author_email='poke@jotes.work',
+    url='https://github.com/jotes/django-cookies-samesite',
+    packages=[
+        'django_cookies_samesite',
+    ],
+    include_package_data=True,
+    install_requires=load_requirements('requirements.txt'),
+    tests_require=load_requirements('requirements_test.txt'),
+    zip_safe=False,
+    keywords='django-cookies-samesite',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

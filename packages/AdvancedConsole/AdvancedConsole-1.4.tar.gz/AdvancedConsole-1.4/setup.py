8c0137423aedef9726b9506cea64ab8376736df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:22 +0000

    Add package AdvancedConsole/1.4/AdvancedConsole-1.4.tar.gz

---
+++ packages/AdvancedConsole/AdvancedConsole-1.4.tar.gz/AdvancedConsole-1.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='AdvancedConsole',
+      version='1.4',
+      py_modules=['Advancedconsole'],
+      install_requires=[
+          'keyboard'
+      ], classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],url="https://github.com/pypa/sampleproject",
+      )

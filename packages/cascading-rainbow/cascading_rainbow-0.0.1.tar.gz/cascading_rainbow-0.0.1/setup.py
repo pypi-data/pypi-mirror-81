d9df53227aeacdce220731f823d7ec2512b8a7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:04 +0000

    Add package cascading-rainbow/0.0.1/cascading_rainbow-0.0.1.tar.gz

---
+++ packages/cascading-rainbow/cascading_rainbow-0.0.1.tar.gz/cascading_rainbow-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+	name="cascading_rainbow",
+	version="0.0.1",
+	author="Mel Sawyer",
+	author_email="melanie.sawyer@columbia.edu",
+	description="Custom rainbow powerline segments!",
+	url="https://github.com/soybean/cascading-rainbow-powerline",
+	license="MIT",
+	packages=setuptools.find_packages(),
+)

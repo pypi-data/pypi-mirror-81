9f9ebac330f086f307fc9916dc5fd068523e60af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:59 +0000

    Add package bob.db.lfw/2.1.11/bob.db.lfw-2.1.11.zip

---
+++ packages/bob.db.lfw/bob.db.lfw-2.1.11.zip/bob.db.lfw-2.1.11/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# author: Manuel Guenther <manuel.guenther@idiap.ch>
+# date:   Wed Feb 13 12:35:29 CET 2013
+
+from setuptools import setup, find_packages, dist
+dist.Distribution(dict(setup_requires=['bob.extension']))
+
+from bob.extension.utils import load_requirements
+install_requires = load_requirements()
+
+# Define package version
+version = open("version.txt").read().rstrip()
+
+# The only thing we do in this file is to call the setup() function with all
+# parameters that define our package.
+setup(
+
+    name='bob.db.lfw',
+    version=version,
+    description='Labeled Faces in the Wild Database Access API for Bob',
+    url='http://gitlab.idiap.ch/bob/bob.db.lfw',
+    license='BSD',
+    author='Manuel Guenther',
+    author_email='manuel.guenther@idiap.ch',
+    long_description=open('README.rst').read(),
+
+    # This line is required for any distutils based packaging.
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires = install_requires,
+
+
+
+    entry_points = {
+      # bob database declaration
+      'bob.db': [
+        'lfw = bob.db.lfw.driver:Interface',
+      ],
+    },
+
+    classifiers = [
+      'Framework :: Bob',
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: BSD License',
+      'Natural Language :: English',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'Topic :: Database :: Front-Ends',
+    ],
+)

d65e878a29b18f193a4795eca54cc98ae9f622a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:05 +0000

    Add package NlpToolkit-Dictionary-Cy/1.0.4/NlpToolkit-Dictionary-Cy-1.0.4.tar.gz

---
+++ packages/NlpToolkit-Dictionary-Cy/NlpToolkit-Dictionary-Cy-1.0.4.tar.gz/NlpToolkit-Dictionary-Cy-1.0.4/Dictionary/ExceptionalWord.pyx
@@ -0,0 +1,49 @@
+from Dictionary.Word cimport Word
+from Dictionary.Pos cimport Pos
+
+
+cdef class ExceptionalWord(Word):
+
+    cdef str __root
+    cdef Pos __pos
+
+    def __init__(self, name: str, root: str, pos: Pos):
+        """
+        A constructor of ExceptionalWord class which takes a Pos as a  part of speech and two Strings; name
+        and root as inputs. Then, calls its super class Word with given name and initialises root and pos variables
+        with given inputs.
+
+        PARAMETERS
+        ----------
+        name : str
+            String input.
+        root : str
+            String input.
+        pos : Pos
+            Pos type input.
+        """
+        super().__init__(name)
+        self.__root = root
+        self.__pos = pos
+
+    cpdef str getRoot(self):
+        """
+        Getter for the root variable.
+
+        RETURNS
+        -------
+        str
+            root variable.
+        """
+        return self.__root
+
+    cpdef Pos getPos(self):
+        """
+        Getter for the pos variable.
+
+        RETURNS
+        -------
+        Pos
+            pos variable.
+        """
+        return self.__pos
---
+++ packages/NlpToolkit-Dictionary-Cy/NlpToolkit-Dictionary-Cy-1.0.4.tar.gz/NlpToolkit-Dictionary-Cy-1.0.4/Dictionary/Pos.pxd
@@ -0,0 +1,9 @@
+cpdef enum Pos:
+    ADJECTIVE = 0
+    NOUN = 1
+    VERB = 2
+    ADVERB = 3
+    CONJUNCTION = 4
+    INTERJECTION = 5
+    PREPOSITION = 6
+    PRONOUN = 7

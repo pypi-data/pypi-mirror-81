b75fd3ac1ad6d36ba4a8b202f9dd71003ae1a269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:05 +0000

    Add package sqlphile/0.6.5.33/sqlphile-0.6.5.33-py3-none-any.whl

---
+++ packages/sqlphile/sqlphile-0.6.5.33-py3-none-any.whl/sqlphile/model.py
@@ -0,0 +1,22 @@
+from abc import *
+
+class AbstractModel:
+    @abstractmethod
+    def get_table_name (self):
+        pass
+
+    @classmethod
+    def get_columns (self):
+        raise NotImplementedError ('return column list like [name, ...]')
+
+    @classmethod
+    def validate (self, data, create = False):
+        raise NotImplementedError
+
+    @classmethod
+    def get_pk (self):
+        raise NotImplementedError ('return pk column name like id')
+
+    @classmethod
+    def get_fks (self):
+        raise NotImplementedError ('return fks like {fk_alias: fk_model} or {fk_alias: (fk_column, fk_model)}')

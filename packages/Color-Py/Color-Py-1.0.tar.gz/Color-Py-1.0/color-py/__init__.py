0fe840b40a3943f6d28307b7527cdb8765512154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:07 +0000

    Add package Color-Py/1.0/Color-Py-1.0.tar.gz

---
+++ packages/Color-Py/Color-Py-1.0.tar.gz/Color-Py-1.0/color-py/__init__.py
@@ -0,0 +1,1 @@
+from color-py.Color import Color
---
+++ packages/Color-Py/Color-Py-1.0.tar.gz/Color-Py-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = "Color-Py",
+    version = "1.0",
+    description = "Color handling library written in Python.",
+    url = "https://github.com/IceCruelStuff/Color-Py",
+    author = "IceCruelStuff",
+    license = "Apache-2.0 License",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries'
+    ],
+    keywords = 'Color handling library written in Python.',
+    packages = find_packages(where='.')
+)

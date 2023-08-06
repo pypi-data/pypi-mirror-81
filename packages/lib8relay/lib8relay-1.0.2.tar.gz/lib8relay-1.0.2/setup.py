4a7d161f4e6f5fde97b62ad683340d8b806c155f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:48 +0000

    Add package lib8relay/1.0.2/lib8relay-1.0.2.tar.gz

---
+++ packages/lib8relay/lib8relay-1.0.2.tar.gz/lib8relay-1.0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'lib8relay',
+  packages = ['lib8relay'],
+  version = '1.0.2',
+  license='MIT',
+  description = 'Library to control Sequent Microsystems 8-RELAY Card',
+  author = 'Sequent Microsystems',
+  author_email = 'olcitu@gmail.com',
+  url = 'https://sequentmicrosystems.com',
+  download_url = 'https://github.com/alexburcea2877/lib8relay/archive/v_1_0_2.tar.gz',
+  keywords = ['relay', 'raspberry', 'power'],
+  install_requires=[
+          'smbus',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 2.7',      
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

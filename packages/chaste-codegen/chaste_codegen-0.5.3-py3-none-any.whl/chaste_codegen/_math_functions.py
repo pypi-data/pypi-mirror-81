4b40bc843f7f3d1a7c2fdff47cbdb19be79934d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:38 +0000

    Add package chaste-codegen/0.5.3/chaste_codegen-0.5.3-py3-none-any.whl

---
+++ packages/chaste-codegen/chaste_codegen-0.5.3-py3-none-any.whl/chaste_codegen/_math_functions.py
@@ -0,0 +1,80 @@
+from sympy import (
+    Abs,
+    Function,
+    acos,
+    cos,
+    exp,
+    sign,
+    sin,
+    sqrt,
+)
+
+
+class RealFunction(Function):
+    def _eval_is_real(self):
+        return self.args[0].is_real
+
+
+class exp_(RealFunction):
+
+    def fdiff(self, argindex=1):
+        """
+        Returns the first derivative of this function.
+        """
+        assert argindex == 1
+        return self
+
+
+class abs_(RealFunction):
+
+    def fdiff(self, argindex=1):
+        """
+        Returns the first derivative of this function.
+        """
+        assert argindex == 1
+        return sign(self.args[0])
+
+
+class acos_(RealFunction):
+
+    def fdiff(self, argindex=1):
+        """
+        Returns the first derivative of this function.
+        """
+        assert argindex == 1
+        return -1 / sqrt_(1 - self.args[0]**2)
+
+
+class cos_(RealFunction):
+
+    def fdiff(self, argindex=1):
+        """
+        Returns the first derivative of this function.
+        """
+        assert argindex == 1
+        return -sin_(self.args[0])
+
+
+class sqrt_(RealFunction):
+
+    def fdiff(self, argindex=1):
+        """
+        Returns the first derivative of this function.
+        """
+        assert argindex == 1
+        return 1 / (2 * sqrt_(self.args[0]))
+
+
+class sin_(RealFunction):
+
+    def fdiff(self, argindex=1):
+        """
+        Returns the first derivative of this function.
+        """
+        assert argindex == 1
+        return (cos_(self.args[0]))
+
+
+# MATH_FUNC_SYMPY_MAPPING provides a mapping from our specified math functions back to sympy versions.
+# This can be used to put sympy function into an expression or evaluation. e.g. `expr.subs(MATH_FUNC_SYMPY_MAPPING)`.
+MATH_FUNC_SYMPY_MAPPING = {abs_: Abs, acos_: acos, cos_: cos, exp_: exp, sin_: sin, sqrt_: sqrt}

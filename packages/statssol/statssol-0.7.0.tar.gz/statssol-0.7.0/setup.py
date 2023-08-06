c871f5429b80ad1531920b1b3fe84990ba2115d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:17 +0000

    Add package statssol/0.7.0/statssol-0.7.0.tar.gz

---
+++ packages/statssol/statssol-0.7.0.tar.gz/statssol-0.7.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='statssol',  
+     version='0.7.0',
+     py_modules=['statssol'] ,
+     author="**insert Author**",
+     author_email="some_email@mail.ru",
+     description="stats",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://some_website.nice",
+     install_requires=['PyTelegramBotApi', 'cloudinary', 'requests', 'numpy'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )

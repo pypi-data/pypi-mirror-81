4011fa6db1bc0507901302cfe174e9f96bda014a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:45 +0000

    Add package google-apis-oauth-django/0.1.0/google-apis-oauth-django-0.1.0.tar.gz

---
+++ packages/google-apis-oauth-django/google-apis-oauth-django-0.1.0.tar.gz/google-apis-oauth-django-0.1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="google-apis-oauth-django",
+    version="0.1.0",
+    author="Nishant Mittal",
+    author_email="admin@nishantwrp.com",
+    description="A library to help integrate Google OAuth 2.0 to your Django application.",
+    license = 'MIT License',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nishantwrp/google-apis-oauth-django",
+    install_requires=[
+        'google-auth-httplib2', 'google-auth-oauthlib'
+    ],
+    packages=setuptools.find_packages()
+)

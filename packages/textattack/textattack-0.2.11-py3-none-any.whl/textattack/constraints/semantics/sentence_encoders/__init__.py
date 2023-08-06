e299b563f4b794ba7a47141b33e0d7619e35ea92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:51 +0000

    Add package textattack/0.2.11/textattack-0.2.11-py3-none-any.whl

---
+++ packages/textattack/textattack-0.2.11-py3-none-any.whl/textattack/constraints/semantics/sentence_encoders/__init__.py
@@ -0,0 +1,6 @@
+from .sentence_encoder import SentenceEncoder
+
+from .bert import BERT
+from .infer_sent import InferSent
+from .thought_vector import ThoughtVector
+from .universal_sentence_encoder import UniversalSentenceEncoder

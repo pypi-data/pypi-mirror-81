02374406a4f976232f9b02b19c2e849b9da27ce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:37 +0000

    Add package country-viewport/0.1.9/country_viewport-0.1.9.tar.gz

---
+++ packages/country-viewport/country_viewport-0.1.9.tar.gz/country_viewport-0.1.9/country_viewport/__init__.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import os
+import sys
+
+csv.field_size_limit(sys.maxsize)
+
+
+def singleton(cls):
+    """Singleton pattern to avoid loading class multiple times
+    """
+    instances = {}
+
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+
+    return getinstance
+
+
+@singleton
+class CountryViewport:
+    def __init__(self, viewports_filename='viewports.csv'):
+        filepath = os.path.join(os.getcwd(), os.path.dirname(__file__), viewports_filename)
+        self.countries_viewports = self.extract(filepath)
+
+    def extract(self, local_filename):
+        """Extract the viewports from the csv file
+        """
+
+        if not os.path.exists(local_filename):
+            raise Exception("The file containing the viewports is missing. Expected at '{}'".format(local_filename))
+
+        with open(local_filename) as handler:
+            rows = csv.reader(handler)
+            viewports = {}
+            for country_code, min_latitude, min_longitude, max_latitude, max_longitude in rows:
+                viewports[country_code] = min_latitude, min_longitude, max_latitude, max_longitude
+
+        return viewports
+
+
+def get(country_code):
+    """Gets the approximate viewport for this country
+    """
+    viewports = CountryViewport()
+    coordinates = viewports.countries_viewports[country_code]
+    return {
+        'min_latitude': coordinates[0],
+        'min_longitude': coordinates[1],
+        'max_latitude': coordinates[2],
+        'max_longitude': coordinates[3],
+    }
+
+
+def countries_list():
+    """Gets the list of all known countries
+    """
+    viewports = CountryViewport()
+    return list(viewports.countries_viewports.keys())
---
+++ packages/country-viewport/country_viewport-0.1.9.tar.gz/country_viewport-0.1.9/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+import setuptools
+
+from distutils.core import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='country_viewport',
+    packages=['country_viewport'],
+    package_data={'country_viewport': ['viewports.csv']},
+    include_package_data=True,
+    version='0.1.9',
+    license='MIT',
+    description='Extract viewports from country codes, without the need of relying on Google APIs.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='YTEC',
+    author_email='alvin@ytec.nl',
+    url='https://ytec.nl/',
+    keywords=['viewport', 'country'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)

d2d2fc750e9fd5e99ede8b16a6870591816b838b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:23 +0000

    Add package graiax-nem/0.0.4.dev3/graiax-nem-0.0.4.dev3.tar.gz

---
+++ packages/graiax-nem/graiax-nem-0.0.4.dev3.tar.gz/graiax-nem-0.0.4.dev3/graiax/nem/elements.py
@@ -0,0 +1,100 @@
+from typing import List, Optional, Union
+
+from graia.application.message.elements.internal import At, Image
+
+
+class Command(object):
+    '''指令相关
+
+    `cmd` 为指令，
+    `args` 为参数，
+    `argsList` 为参数列表，
+    '''
+    cmd: Optional[str]
+    args: Optional[str]
+    argsList: Optional[List[str]]
+
+    #TODO 迁移指令解析逻辑
+
+
+class AtList(List[At]):
+    '''
+    At 的增强版，允许你更方便地操作 At 列表
+    AtList 本身也是一个列表，因此可以使用大部分的 list 特性
+    '''
+    ints: List[int]
+
+    @staticmethod
+    def __getTarget(at: Union[At, int]) -> Optional[int]:
+        if isinstance(at, At):
+            return at.target
+        elif isinstance(at, int):
+            return at
+        else:
+            raise TypeError('Must be Union[At, int]')
+
+    def __init__(self, atList: List[At]) -> None:
+        self.ints = [i.target for i in atList]
+        super().__init__(atList)
+
+    def __contains__(self, at: Union[At, int]) -> bool:
+        return self.__getTarget(at) in self.ints
+
+    def append(self, at: Union[At, int]) -> None:
+        if isinstance(at, At):
+            atObj = at
+        elif isinstance(at, int):
+            atObj =  At(at)
+        else:
+            raise TypeError('Must be Union[At, int]')
+        super().append(atObj)
+
+    def __set(self, l: List[At]) -> None:
+        '''
+        内部函数，用于更新 AtList
+        '''
+        self.__init__(l)
+
+    def get(self, at: Union[At, int]) -> At:
+        '''
+        根据 QQ 号或 `At` 对象获取 `At` 对象本身
+        '''
+        target = self.__getTarget(at)
+        ats: List[At] = self.copy()
+        for i in ats:
+            if i.target == target:
+                return i
+        else:
+            return None
+
+    def __del(self, at: Union[At, int]) -> None:
+        '''
+        内部函数，用于删除 `AtList` 中的某一项
+        '''
+        target = self.__getTarget(at)
+        ats = self.copy()
+        for i in range(len(ats)):
+            if ats[i].target == target:
+                del ats[i]
+                self.__set(ats)
+                break
+
+    def delete(self, at: Union[At, int]) -> None:
+        '''
+        根据传入的 QQ 号或 `At` 对象删除 `AtList` 中的所有匹配项
+        '''
+        while self.__contains__(at):
+            self.__del(at)
+
+
+class ImagePro(Image):
+    async def save2file(self, filepath: str) -> None:
+        '''
+        将图片保存至文件
+
+        Args:
+            filepath: 文件路径
+        '''
+        imageBytes = await self.http_to_bytes()
+        with open(filepath, 'wb') as f:
+            f.write(imageBytes)
---
+++ packages/graiax-nem/graiax-nem-0.0.4.dev3.tar.gz/graiax-nem-0.0.4.dev3/graiax/nem/filters.py
@@ -0,0 +1,182 @@
+import re
+from typing import Callable, List
+
+from graia.application.entry import Group, GroupMessage
+from graia.application.event.messages import FriendMessage
+from graia.broadcast.exceptions import ExecutionStop
+from graia.broadcast.builtin.decoraters import Depend
+
+from .nem import NEM
+
+
+class FrontFilters(object):
+    '''过滤器的前置修饰器，无法直接使用'''
+    @staticmethod
+    def requirePlain(func: Callable[[GroupMessage], Callable]):
+        def wrapper(gm: GroupMessage):
+            nem = NEM(gm)
+            if not nem.plain_message:
+                raise ExecutionStop()
+            return func(gm)
+        return wrapper
+
+    @staticmethod
+    def requireNotBanded(func: Callable[[GroupMessage], Callable]):
+        def wrapper(gm: GroupMessage):
+            nem = NEM(gm)
+            if nem.permission.isBlocked():
+                raise ExecutionStop()
+            return func(gm)
+        return wrapper
+
+    @staticmethod
+    def depend(func: Callable) -> Depend:
+        def wrapper(*args, **kwargs):
+            return Depend(func(*args, **kwargs))
+        return wrapper
+
+
+class GroupFilters(object):
+    '''
+    Group 消息过滤器，作为 `headless_decoraters` 使用
+
+    请查看源码以了解所有可用的过滤器
+    '''
+
+    # GroupMessage
+    @staticmethod
+    @FrontFilters.depend
+    def exceptGroups(group_list: List[int]) -> Callable:
+        '''在指定群聊内禁用'''
+        def wrapper(group: Group):
+            if group.id in group_list:
+                raise ExecutionStop()
+        return wrapper
+
+    @staticmethod
+    @FrontFilters.depend
+    def inGroups(group_list: List[int]) -> Depend:
+        '''仅在指定群聊内启用'''
+        def wrapper(group: Group):
+            if not group.id in group_list:
+                raise ExecutionStop()
+        return wrapper
+
+    @staticmethod
+    @FrontFilters.depend
+    def adminOnly() -> Depend:
+        '''仅管理员'''
+        def wrapper(gm: GroupMessage):
+            nem = NEM(gm)
+            if not nem.permission.isAdmin():
+                raise ExecutionStop()
+        return wrapper
+
+    @staticmethod
+    @FrontFilters.depend
+    def onCommand(command_name: str) -> Depend:
+        '''在特定指令时执行
+
+        Args:
+            command_name: 指令名称（不带标识符）'''
+        @FrontFilters.requirePlain
+        @FrontFilters.requireNotBanded
+        def wrapper(gm: GroupMessage):
+            nem = NEM(gm)
+            if nem.Command.cmd != command_name:
+                raise ExecutionStop()
+        return wrapper
+    
+    @staticmethod
+    @FrontFilters.depend
+    def onAt(qq: int) -> Depend:
+        '''当 `qq` 在群内被 At 时执行
+
+        Args:
+            qq: QQ 号'''
+        @FrontFilters.requireNotBanded
+        def wrapper(gm: GroupMessage):
+            nem = NEM(gm)
+            if not qq in nem.at:
+                raise ExecutionStop()
+        return wrapper
+
+    @staticmethod
+    @FrontFilters.depend
+    def onWord(word: str) -> Depend:
+        '''在消息中有特定文字时执行
+
+        Args:
+            word: 文字'''
+        @FrontFilters.requirePlain
+        @FrontFilters.requireNotBanded
+        def wrapper(gm: GroupMessage):
+            nem = NEM(gm)
+            if not word in nem.plain_message:
+                raise ExecutionStop()
+        return wrapper
+
+    @staticmethod
+    @FrontFilters.depend
+    def onWords(words_list: List[str]) -> Depend:
+        '''在消息中有特定文字时执行
+
+        Args:
+            words_list: 关键字列表'''
+        @FrontFilters.requirePlain
+        @FrontFilters.requireNotBanded
+        def wrapper(gm: GroupMessage):
+            nem = NEM(gm)
+            inList = False
+            for word in words_list:
+                if word in nem.plain_message:
+                    inList = True
+                    break
+            if not inList:
+                raise ExecutionStop()
+        return wrapper
+
+    @staticmethod
+    @FrontFilters.depend
+    def onMatch(pattern: str) -> Depend:
+        '''在消息匹配正则表达式时执行
+
+        Args:
+            pattern: 正则表达式（必须为 str）'''
+        @FrontFilters.requirePlain
+        @FrontFilters.requireNotBanded
+        def wrapper(gm: GroupMessage):
+            nem = NEM(gm)
+            if not re.match(pattern, nem.plain_message):
+                raise ExecutionStop()
+        return wrapper
+
+    @staticmethod
+    @FrontFilters.depend
+    def onMatchs(pattern_list: List[str]) -> Depend:
+        '''在消息匹配正则表达式列表中的任何一项时执行
+
+        Args:
+            pattern_list: 正则表达式列表'''
+        @FrontFilters.requirePlain
+        @FrontFilters.requireNotBanded
+        def wrapper(gm: GroupMessage):
+            nem = NEM(gm)
+            matched: bool = False
+            for p in pattern_list:
+                if re.match(p, nem.plain_message):
+                    matched = True
+                    break
+            if not matched:
+                raise ExecutionStop()
+        return wrapper
+
+
+class FriendFilters(object):
+    @staticmethod
+    @FrontFilters.depend
+    def onFriend(qq: int) -> Depend:
+        def wrapper(fm: FriendMessage):
+            if fm.sender.id != qq:
+                raise ExecutionStop()
+        return wrapper

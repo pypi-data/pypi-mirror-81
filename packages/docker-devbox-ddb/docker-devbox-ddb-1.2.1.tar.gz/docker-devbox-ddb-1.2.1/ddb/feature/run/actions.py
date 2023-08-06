97cc9e8d72539096970f50e66b308dfd788df59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:11 +0000

    Add package docker-devbox-ddb/1.2.1/docker-devbox-ddb-1.2.1.tar.gz

---
+++ packages/docker-devbox-ddb/docker-devbox-ddb-1.2.1.tar.gz/docker-devbox-ddb-1.2.1/ddb/feature/run/actions.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import subprocess
+
+from ...action import Action
+from ...action.action import EventBinding
+from ...binary import binaries
+from ...config import config
+from ...event import events
+
+
+class RunAction(Action):
+    """
+    Run a binary
+    """
+
+    @property
+    def event_bindings(self):
+        return (
+            events.phase.run,
+            EventBinding(events.run.run, self.run)
+        )
+
+    @property
+    def name(self) -> str:
+        return "run:run"
+
+    @staticmethod
+    def execute():
+        """
+        Execute the action defined by argument "name".
+        """
+        if hasattr(config.args, "name"):
+            name = config.args.name
+            events.run.run(name=name)
+
+    @staticmethod
+    def run(name: str, *args):
+        """
+        Execute the action
+        """
+        if not args:
+            args = config.unknown_args
+
+        binary = binaries.get(name)
+        print(subprocess.list2cmdline(binary.command(*args)))

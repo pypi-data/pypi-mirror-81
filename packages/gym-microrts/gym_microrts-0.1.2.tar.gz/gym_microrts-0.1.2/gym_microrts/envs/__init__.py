b1457780cf7e56f9cadeedee59049b1a8b19080c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:09 +0000

    Add package gym-microrts/0.1.2/gym_microrts-0.1.2.tar.gz

---
+++ packages/gym-microrts/gym_microrts-0.1.2.tar.gz/gym_microrts-0.1.2/gym_microrts/envs/__init__.py
@@ -0,0 +1,20 @@
+from gym_microrts.envs.global_agent_env import (
+    GlobalAgentEnv, 
+    GlobalAgentHRLAttackCloserToEnemyBaseEnv, 
+    GlobalAgentCloserToEnemyBaseRewardEnv, 
+    GlobalAgentHRLEnv,
+    GlobalAgentMiningEnv, 
+    GlobalAgentBinaryEnv, 
+    GlobalAgentAttackEnv, 
+    GlobalAgentProduceWorkerEnv, 
+    GlobalAgentProduceBuildingEnv, 
+    GlobalAgentProduceCombatUnitEnv,
+    GlobalAgentRandomEnemyEnv,
+    GlobalAgentCombinedRewardEnv,
+    GlobalAgentHRLProduceCombatUnitEnv,
+    GlobalAgentHRLProduceCombatUnitPerfectEnv,
+    GlobalAgentMultiActionsCombinedRewardEnv,
+    GlobalAgentCombinedRewardSelfPlayEnv,
+    GlobalAgentMultiActionsHRLEnv)
+from gym_microrts.envs.local_agent_env import LocalAgentEnv
+from gym_microrts.envs.param_op_env import ParamOpEnv
---
+++ packages/gym-microrts/gym_microrts-0.1.2.tar.gz/gym_microrts-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='gym_microrts',
+    include_package_data=True,
+    version='0.1.2',
+    install_requires=['gym', 'dacite', 'jPype1'],
+    packages=setuptools.find_packages(),
+)

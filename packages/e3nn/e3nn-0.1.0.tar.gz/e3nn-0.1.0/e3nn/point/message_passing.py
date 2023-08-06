eb4b50ac0656e8ff6fe7155e805264dfdb7a13ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:23 +0000

    Add package e3nn/0.1.0/e3nn-0.1.0.tar.gz

---
+++ packages/e3nn/e3nn-0.1.0.tar.gz/e3nn-0.1.0/e3nn/point/message_passing.py
@@ -0,0 +1,36 @@
+# pylint: disable=arguments-differ, redefined-builtin, missing-docstring, no-member, invalid-name, line-too-long, not-callable
+import torch
+import torch_geometric as tg
+
+
+class Convolution(tg.nn.MessagePassing):
+    def __init__(self, kernel):
+        super(Convolution, self).__init__(aggr='add', flow='target_to_source')
+        self.kernel = kernel
+
+    def forward(self, features, edge_index, edge_r, size=None, n_norm=1, groups=1):
+        """
+        :param features: Tensor of shape [n_target, dim(Rs_in)]
+        :param edge_index: LongTensor of shape [2, num_messages]
+                           edge_index[0] = sources (convolution centers)
+                           edge_index[1] = targets (neighbors)
+        :param edge_r: Tensor of shape [num_messages, 3]
+                       edge_r = position_target - position_source
+        :param size: (n_source, n_target) or None
+        :param n_norm: typical number of targets per source
+
+        :return: Tensor of shape [n_source, dim(Rs_out)]
+        """
+        k = self.kernel(edge_r)
+        k.div_(n_norm ** 0.5)
+        return self.propagate(edge_index, size=size, x=features, k=k, groups=groups)
+
+    def message(self, x_j, k, groups):
+        N = x_j.shape[0]
+        cout, cin = k.shape[-2:]
+        x_j = x_j.view(N, groups, cin)  # Rs_tp1
+        if k.shape[0] == 0:  # https://github.com/pytorch/pytorch/issues/37628
+            return torch.zeros(0, groups * cout)
+        if k.dim() == 4 and k.shape[1] == groups:  # kernel has group dimension
+           return torch.einsum('egij,egj->egi', k, x_j).reshape(N, groups * cout)
+        return torch.einsum('eij,egj->egi', k, x_j).reshape(N, groups * cout)

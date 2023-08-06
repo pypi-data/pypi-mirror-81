883e8a38df186077320f451de5c7550a819899c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:53 +0000

    Add package flask-util-job-runner/0.2.1/flask-util-job-runner-0.2.1.tar.gz

---
+++ packages/flask-util-job-runner/flask-util-job-runner-0.2.1.tar.gz/flask-util-job-runner-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['flask_util_job_runner']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Flask==1.1.2',
+ 'gunicorn==20.0.4',
+ 'minio==6.0.0',
+ 'redis==3.5.3',
+ 'requests==2.24.0']
+
+setup_kwargs = {
+    'name': 'flask-util-job-runner',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'eqqe',
+    'author_email': 'sfbrey@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

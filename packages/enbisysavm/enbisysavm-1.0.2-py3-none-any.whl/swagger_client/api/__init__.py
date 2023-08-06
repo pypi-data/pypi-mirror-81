f036b2ff540ee570b656e07fa20b8daf2c70c3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:26 +0000

    Add package enbisysavm/1.0.2/enbisysavm-1.0.2-py3-none-any.whl

---
+++ packages/enbisysavm/enbisysavm-1.0.2-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.addresses_api import AddressesApi
+from swagger_client.api.properties_api import PropertiesApi
+from swagger_client.api.valuation_api import ValuationApi

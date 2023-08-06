a68269dbc48cd2d5a67a5fc5554d969b65cfc2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:16 +0000

    Add package pyteller/0.0.0/pyteller-0.0.0.tar.gz

---
+++ packages/pyteller/pyteller-0.0.0.tar.gz/pyteller-0.0.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    author="MIT Data To AI Lab",
+    author_email='dailabmit@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+    ],
+    install_requires=[
+    ],
+    license="MIT license",
+    include_package_data=True,
+    name='pyteller',
+    version='0.0.0',
+    zip_safe=False,
+)

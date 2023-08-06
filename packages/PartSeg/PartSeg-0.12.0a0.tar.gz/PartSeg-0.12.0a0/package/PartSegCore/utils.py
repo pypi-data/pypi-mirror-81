dc85f91b8c639638ff6fbe8ac1bee302ddcbddd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:44 +0000

    Add package PartSeg/0.12.0a0/PartSeg-0.12.0a0.tar.gz

---
+++ packages/PartSeg/PartSeg-0.12.0a0.tar.gz/PartSeg-0.12.0a0/package/PartSegCore/utils.py
@@ -0,0 +1,59 @@
+import logging
+
+import numpy as np
+
+__author__ = "Grzegorz Bokota"
+
+
+def class_to_dict(obj, *args):
+    """
+    Create dict which contains values of given fields
+    :type obj: object
+    :type args: list[str]
+    :return:
+    """
+    res = dict()
+    for name in args:
+        res[name] = getattr(obj, name)
+    return res
+
+
+def dict_set_class(obj, dic, *args):
+    """
+    Set fields of given object based on values from dict.
+    If *args contains no names all values from dict are used
+    :type obj: object
+    :type dic: dict[str,object]
+    :param args: list[str]
+    :return:
+    """
+    if len(args) == 0:
+        li = dic.keys()
+    else:
+        li = args
+    for name in li:
+        try:
+            getattr(obj, name)
+            setattr(obj, name, dic[name])
+        except AttributeError as ae:
+            logging.warning(ae)
+
+
+def bisect(arr, val, comp):
+    left = -1
+    right = len(arr)
+    while right - left > 1:
+        mid = (left + right) >> 1
+        if comp(arr[mid], val):
+            left = mid
+        else:
+            right = mid
+    return right
+
+
+def numpy_repr(val: np.ndarray):
+    if val is None:
+        return repr(val)
+    if val.size < 20:
+        return repr(val)
+    return f"array(size={val.size}, shape={val.shape}, dtype={val.dtype}, min={val.min()}, max={val.max()})"

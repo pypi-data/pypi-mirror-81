e95d95621598f87fbfb4135159f6968db8b4a4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:47 +0000

    Add package cmudict/0.4.5/cmudict-0.4.5.tar.gz

---
+++ packages/cmudict/cmudict-0.4.5.tar.gz/cmudict-0.4.5/tests/test_compatability.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import cmudict
+
+
+def test_dict():
+    EXPECTED_SIZE = 126045
+    d = cmudict.dict()
+    SIZE = len(d)
+    if (EXPECTED_SIZE != SIZE):
+        raise AssertionError('cmudict.dict(): Expected {0} keys, got {1}.'.format(
+            EXPECTED_SIZE, SIZE))
+
+
+def test_entries():
+    EXPECTED = 135154
+    e = cmudict.entries()
+    COUNT = len(e)
+    if (COUNT != EXPECTED):
+        raise AssertionError('cmudict.entries(): Expected {0} entries, got {1}.'.format(
+            EXPECTED, COUNT))
+
+
+def test_raw():
+    EXPECTED = 3618063
+    r = cmudict.raw()
+    COUNT = len(r)
+    if (COUNT != EXPECTED):
+        raise AssertionError('cmudict.raw(): Expected {0} bytes, got {1}.'.format(
+            EXPECTED, COUNT))
+
+
+def test_words():
+    EXPECTED = 135154
+    w = cmudict.words()
+    COUNT = len(w)
+    if (COUNT != EXPECTED):
+        raise AssertionError('cmudict.raw(): Expected {0} bytes, got {1}.'.format(
+            EXPECTED, COUNT))

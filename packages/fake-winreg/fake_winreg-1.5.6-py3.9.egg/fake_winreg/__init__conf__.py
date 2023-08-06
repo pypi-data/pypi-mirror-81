8b44d0ff34963c780c9f346f7aaba2a55bf9e53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:22 +0000

    Add package fake-winreg/1.5.6/fake_winreg-1.5.6-py3.9.egg

---
+++ packages/fake-winreg/fake_winreg-1.5.6-py3.9.egg/fake_winreg/__init__conf__.py
@@ -0,0 +1,20 @@
+name = 'fake_winreg'
+title = 'fake winreg, in order to test registry related functions on linux'
+version = 'v1.5.6'
+url = 'https://github.com/bitranox/fake_winreg'
+author = 'Robert Nowotny'
+author_email = 'bitranox@gmail.com'
+shell_command = 'fake_winreg'
+
+
+def print_info() -> None:
+    print("""\
+
+Info for fake_winreg:
+
+    fake winreg, in order to test registry related functions on linux
+
+    Version : v1.5.6
+    Url     : https://github.com/bitranox/fake_winreg
+    Author  : Robert Nowotny
+    Email   : bitranox@gmail.com""")

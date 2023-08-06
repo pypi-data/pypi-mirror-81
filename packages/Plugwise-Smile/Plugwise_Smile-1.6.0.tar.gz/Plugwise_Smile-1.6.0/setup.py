389c52f6dcc260f313db9bb74420e5b11a06cf0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:57 +0000

    Add package Plugwise-Smile/1.6.0/Plugwise_Smile-1.6.0.tar.gz

---
+++ packages/Plugwise-Smile/Plugwise_Smile-1.6.0.tar.gz/Plugwise_Smile-1.6.0/setup.py
@@ -0,0 +1,43 @@
+import codecs
+import os.path
+from setuptools import setup
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="Plugwise_Smile",
+    version=get_version("Plugwise_Smile/__init__.py"),
+    description="Plugwise_Smile (Anna/Adam/P1/Stretch) API to use in conjunction with Home Assistant.",
+    long_description="Plugwise Smile API to use in conjunction with Home Assistant, but it can also be used without Home Assistant as a module.",
+    keywords="HomeAssistant HA Home Assistant Anna Adam P1 Smile Plugwise",
+    url="https://github.com/plugwise/Plugwise-Smile",
+    author="Plugwise",
+    author_email="info@compa.nl",
+    license="MIT",
+    packages=["Plugwise_Smile"],
+    install_requires=[
+        "aiohttp",
+        "async_timeout",
+        "lxml",
+        "pytz",
+        "python-dateutil",
+        "semver",
+    ],
+    zip_safe=False,
+)

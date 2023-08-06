5cc10ccea5288f7e3573f4cc3e97ace80b04fb8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:05 +0000

    Add package pcrglobwb-utils/0.1.4.4/pcrglobwb_utils-0.1.4.4-py2.py3-none-any.whl

---
+++ packages/pcrglobwb-utils/pcrglobwb_utils-0.1.4.4-py2.py3-none-any.whl/pcrglobwb_utils/obs_data.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+
+import xarray as xr
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import rasterio as rio
+import spotpy as sp
+import os, sys
+
+class grdc_data:
+    """Retrieve, re-work and visualize data from a GRDC-file.
+
+    Arguments:
+        fo (str): path to file with GRDC data
+    """
+
+    def __init__(self, fo):
+        """Initiates grdc_data object.
+        """
+
+        self.fo = fo
+
+    def get_grdc_station_properties(self):
+        """Retrieves GRDC station properties from txt-file. Creates and returns header from those properties as well as a dictionary containt station name, lat, and lon info.
+
+        Returns:
+            str: plot title containing station name and lat/lon info
+            dict: dictionary containing name, lat, and lon info
+        """
+        
+        # open file
+        fp = open(self.fo)
+        
+        # go through lines in file
+        for i, line in enumerate(fp):
+            
+            # station name normally in line 11 of GRDC file
+            if i == 10:
+                station_grdc = line
+                # check whether line contains station information
+                if 'Station'not in station_grdc.split(":")[0]:
+                    os.sys.exit('Station name should be in line 11 but not found - please check if input txt-file is original GRDC format!')
+                # split and strip string
+                station_grdc = station_grdc.split(":")[-1].strip()  
+            
+            # latitude normally in line 13 of GRDC file
+            if i == 12:
+                lat_grdc = line
+                if 'Latitude'not in lat_grdc.split(":")[0]:
+                    os.sys.exit('Latitude should be in line 13 but not found - please check if input txt-file is original GRDC format!')
+                lat_grdc = lat_grdc.split(":")[-1].strip()
+                
+            # longitude normally in line 14 of GRDC file
+            elif i == 13:
+                lon_grdc = line
+                if 'Longitude'not in lon_grdc.split(":")[0]:
+                    os.sys.exit('Latitude should be in line 13 but not found - please check if input txt-file is original GRDC format!')
+                lon_grdc = lon_grdc.split(":")[-1].strip()
+                
+            # break loop to save time    
+            elif i > 13:
+                break
+                
+        # close file        
+        fp.close()
+        
+        # write station name, latitude, and longitude to dic
+        self.props = dict(station=str(station_grdc), 
+                     latitude=float(lat_grdc), 
+                     longitude=float(lon_grdc))
+        
+        # create simple title for plots
+        plot_title = 'station ' + str(station_grdc) + ' at latitude/longitude ' + str(lat_grdc) + '/' + str(lon_grdc)
+
+        return plot_title, self.props
+
+    def get_grdc_station_values(self, var_name, remove_mv=True, mv_val=-999, print_head=False, plot=False, plot_title=None):
+        """Reads (discharge-)values of GRDC station from txt-file. Creates a pandas dataframe with a user-specified column header for values instead of default ' Values' header name. Also possible to remove possible missing values in the timeseries and plot the resulting series.
+
+        Arguments:
+            var_name (str): user-specified variable name to be given to time series
+
+        Keyword Arguments:
+            remove_mv (bool): whether or not remove missing values in timeseries (default: True)
+            mv_val (int): missing value in timeseries (default: -999)
+            print_head (bool): whether or not to print the pandas dataframe head (default: False)
+            plot (bool): whether or not to plot the timeseries (default: False)
+            plot_title (str): user-specified title for plot of timeseries (default: None)
+
+        Returns:
+            dataframe: dataframe containing datetime objects as index and observations as column values
+        """
+
+        #TODO: this function should also work if get_grdc_station_properties() was not executed before;
+        #TODO: because, if not executed before, self.props is no attribute yet and function exists with error
+
+        f = open(self.fo)
+
+        for i, line in enumerate(f):
+            if '#' in line:
+                pass
+            else:
+                stopline = i
+                break
+
+        df = pd.read_csv(self.fo, skiprows=stopline, sep=';')
+            
+        df[var_name] = df[' Original'].copy()
+        del df[' Original']
+        
+        df['date'] = pd.to_datetime(df['YYYY-MM-DD'])
+        df.set_index(df['date'], inplace=True)
+
+        df_out = pd.DataFrame(index=df.index,
+                            data=df[var_name])
+        
+        if remove_mv == True:
+            df_out.replace(mv_val, np.nan, inplace=True)
+        
+        if print_head == True:
+            print(df_out.head())
+            
+        if plot == True:
+            df_out.plot(title=plot_title, legend=True)
+
+        self.df = df_out
+
+        self.props['start_data_obs'] = self.df.index.strftime('%m/%d/%Y').values[0]
+        self.props['end_data_obs'] = self.df.index.strftime('%m/%d/%Y').values[-1]
+
+        return self.df, self.props
+        
+    def resample2monthly(self, stat_func='mean', suffix='_monthly'):
+        """Resampling values to monthly time scale.
+
+        Keyword Arguments:
+            stat_func (str): statistical descriptor to be used in resampling . currently supported is 'mean', 'max', and 'min' (default: mean)
+            suffix (str): suffix to be added to column name (default: _monthly)
+
+        Returns:
+            dataframe: dataframe containing monthly average values
+        """        
+
+        if stat_func == 'mean':
+            self.df_monthly = self.df.resample('M').mean()
+        elif stat_func == 'max':
+            self.df_monthly = self.df.resample('M').max()
+        elif stat_func == 'min':
+            self.df_monthly = self.df.resample('M').min()
+        else:
+            raise ValueError('no supported statistical function provided - choose between mean, max, and min')
+
+        self.df_monthly = self.df_monthly.rename(columns={self.df_monthly.columns.values[0]: self.df_monthly.columns.values[0] + suffix + '_' + stat_func})
+
+        return self.df_monthly
+
+    def resample2yearly(self, stat_func='mean', suffix='_yearly'):
+        """Resampling values to monthly time scale.
+
+        Keyword Arguments:
+            stat_func (str): statistical descriptor to be used in resampling . currently supported is 'mean', 'max', and 'min' (default: mean)
+            suffix (str): suffix to be added to column name (default: _monthly)
+
+        Returns:
+            dataframe: dataframe containing monthly average values
+        """        
+
+        if stat_func == 'mean':
+            self.df_yearly = self.df.resample('Y').mean()
+        elif stat_func == 'max':
+            self.df_yearly = self.df.resample('Y').max()
+        elif stat_func == 'min':
+            self.df_yearly = self.df.resample('Y').min()
+        else:
+            raise ValueError('no supported statistical function provided - choose between mean, max, and min')
+
+        self.df_yearly = self.df_yearly.rename(columns={self.df_yearly.columns.values[0]: self.df_yearly.columns.values[0] + suffix + '_' + stat_func})
+
+        return self.df_yearly
+
+class other_data:
+    """Retrieve, re-work and visualize data from other data sources than GRDC files
+
+    Arguments:
+        fo (str): path to text file containing data
+    """
+
+    def __init__(self, fo):
+        """Initializing class.
+        """        
+
+        self.fo = fo
+
+    def get_values_from_csv(self, var_name, remove_mv=True, mv_val=-999, print_head=False, plot=False, plot_title=None):
+        """Reads simple two-column csv file and returns dataframe for dates and selected column.
+
+        Arguments:
+            var_name (str): user-specified column name
+
+        Keyword Arguments:
+            remove_mv (bool): whether or not to remove missing values (default: True)
+            mv_val (int): placeholder value of missing values (default: -999)
+            print_head (bool): whether or not to print the header of dataframe (default: False)
+            plot (bool): whether or not to plot the timeseries (default: False)
+            plot_title (str): optional title for plot (default: None)
+
+        Returns:
+            dataframe: dataframe containing datetime objects as index and observations as column values
+        """
+        
+        df = pd.read_csv(self.fo, sep=None, engine='python', parse_dates=[0], index_col=0)
+
+        df = df.rename(columns={df.columns.values[0]: var_name})
+
+        if remove_mv == True:
+            df.replace(mv_val, np.nan, inplace=True)
+            
+        if print_head == True:
+            print(df.head())
+            
+        if plot == True:
+            df.plot(title=plot_title, legend=True)
+        
+        self.df = df
+
+        return self.df
+
+    def get_values_from_excel(self, var_name, remove_mv=True, mv_val=-999, plot=False, plot_title=None):
+
+        df = pd.read_excel(self.fo, index_col=0)
+
+        df = df.rename(columns={df.columns.values[0]: var_name})
+
+        if remove_mv == True:
+            df.replace(mv_val, np.nan, inplace=True)
+
+        if plot == True:
+            df.plot(title=plot_title, legend=True)
+
+        self.df = df
+
+        return self.df
+
+    def resample2monthly(self, stat_func='mean', suffix='_monthly'):
+        """Resampling values to monthly time scale.
+
+        Keyword Arguments:
+            stat_func (str): statistical descriptor to be used in resampling . currently supported is 'mean', 'max', and 'min' (default: mean)
+            suffix (str): suffix to be added to column name (default: _monthly)
+
+        Returns:
+            dataframe: dataframe containing monthly average values
+        """        
+
+        if stat_func == 'mean':
+            self.df_monthly = self.df.resample('M').mean()
+        elif stat_func == 'max':
+            self.df_monthly = self.df.resample('M').max()
+        elif stat_func == 'min':
+            self.df_monthly = self.df.resample('M').min()
+        else:
+            raise ValueError('no supported statistical function provided - choose between mean, max, and min')
+
+        self.df_monthly = self.df_monthly.rename(columns={self.df_monthly.columns.values[0]: self.df_monthly.columns.values[0] + suffix + '_' + stat_func})
+
+        return self.df_monthly
+
+    def resample2yearly(self, stat_func='mean', suffix='_yearly'):
+        """Resampling values to monthly time scale.
+
+        Keyword Arguments:
+            stat_func (str): statistical descriptor to be used in resampling . currently supported is 'mean', 'max', and 'min' (default: mean)
+            suffix (str): suffix to be added to column name (default: _monthly)
+
+        Returns:
+            dataframe: dataframe containing monthly average values
+        """        
+
+        if stat_func == 'mean':
+            self.df_yearly = self.df.resample('Y').mean()
+        elif stat_func == 'max':
+            self.df_yearly = self.df.resample('Y').max()
+        elif stat_func == 'min':
+            self.df_yearly = self.df.resample('Y').min()
+        else:
+            raise ValueError('no supported statistical function provided - choose between mean, max, and min')
+
+        self.df_yearly = self.df_yearly.rename(columns={self.df_yearly.columns.values[0]: self.df_yearly.columns.values[0] + suffix + '_' + stat_func})
+
+        return self.df_yearly
---
+++ packages/pcrglobwb-utils/pcrglobwb_utils-0.1.4.4-py2.py3-none-any.whl/pcrglobwb_utils/sim_data.py
@@ -0,0 +1,194 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import rasterio as rio
+import spotpy as sp
+import csv
+import os, sys
+
+class from_nc:
+    """Retrieving and working with timeseries data from a nc-file.
+
+    Arguments:
+        fo (str): path to nc-file
+    """
+
+    def __init__(self, fo):
+        """Initializing class.
+        """
+
+        self.ds = fo
+    
+    def read_values_at_indices(self, idx_row, idx_col, var_name='discharge', plot=False, plot_var_name=None, plot_title=None):
+        """Reading a nc-file and retrieving variable values at given column and row indices. Default setting is that discharge is extracted at this point. Resulting timeseries is stored as pandas timeframe and can be plotted with user-specified variable name and title.
+
+        Arguments:
+            idx_row (float): row index from which to read the data
+            idx_col (float): column index from which to read the data
+
+        Keyword Arguments:
+            var_name (str): variable in nc-file whose data is to be read (default: 'discharge')
+            plot (bool): whether or not to plot the timeseries (default: False)
+            plot_var_name (str): user-specified name to be used for plot legend (default: None)
+            plot_title (str): user-specified plot title (default: None)
+
+        Returns:
+            dataframe: dataframe containing values
+        """
+
+        # open file
+        ds = xr.open_dataset(self.ds)
+        
+        # read variable values at indices as xarray DataArray
+        try:
+            dsq = ds[var_name].isel(lat=idx_row, lon=idx_col)
+        except:
+            dsq = ds[var_name].isel(latitude=idx_row, longitude=idx_col)
+        
+        # change variable names if specified
+        if plot_var_name != None:
+            var_name = plot_var_name
+        
+        # convert DataArray to pandas dataframe
+        df = pd.DataFrame(data=dsq.to_pandas(), 
+                    columns=[var_name])
+        
+        # plot if specified
+        if plot == True:
+            df.plot(title=plot_title)
+        
+        # close file
+        ds.close()
+
+        self.df = df
+        
+        return self.df
+
+    def resample2monthly(self, stat_func='mean'):
+        """Resampling values to monthly time scale.
+
+        Keyword Arguments:
+            stat_func (str): statistical descriptor to be used in resampling . currently supported is 'mean', 'max', and 'min' (default: mean)
+
+        Returns:
+            dataframe: dataframe containing monthly average values
+        """        
+
+        if stat_func == 'mean':
+            self.df_monthly = self.df.resample('M').mean()
+        elif stat_func == 'max':
+            self.df_monthly = self.df.resample('M').max()
+        elif stat_func == 'min':
+            self.df_monthly = self.df.resample('M').min()
+        else:
+            raise ValueError('no supported statistical function provided - choose between mean, max, and min')
+
+        return self.df_monthly
+
+    def resample2yearly(self, stat_func='mean'):
+        """Resampling values to monthly time scale.
+
+        Keyword Arguments:
+            stat_func (str): statistical descriptor to be used in resampling . currently supported is 'mean', 'max', and 'min' (default: mean)
+
+        Returns:
+            dataframe: dataframe containing monthly average values
+        """        
+
+        if stat_func == 'mean':
+            self.df_yearly = self.df.resample('Y').mean()
+        elif stat_func == 'max':
+            self.df_yearly = self.df.resample('Y').max()
+        elif stat_func == 'min':
+            self.df_yearly = self.df.resample('Y').min()
+        else:
+            raise ValueError('no supported statistical function provided - choose between mean, max, and min')
+
+        return self.df_yearly
+
+    def validate_results(self, df_obs, out_dir, var_name_obs=None, var_name_sim=None, return_all_KGE=False):
+        """Validates simulated values with observations. Computes KGE, NSE, RMSE, and R^2. Concatenates the two dataframes and drops all NaNs to achieve dataframe with common time period.
+
+        Arguments:
+            df_obs (dataframe): pandas dataframe containing observed values
+            out_dir (str): user-specified output directory for validation output
+
+        Keyword Arguments:
+            var_name_obs (str): header name of column in df_obs whose values are to be used (default: None)
+            var_name_sim (str): header name of column in df_sim whose values are to be used (default: None)
+            return_all_KGE (bool): whether or not to return all KGE components (default: False)
+
+        Raises:
+            error: if df_obs and df_sim do not overlap in time, an error is thrown.
+
+        Returns:
+            dataframe: pandas dataframe containing evaluated values for overlapping time period
+            dict: dictionary containing results of objective functions KGE, RMSE, NSE and R2
+        """ 
+
+        # if variable name is not None, then pick values from specified column
+        if var_name_obs != None:
+            df_obs = df_obs[var_name_obs]
+        # else, just use the dataframe as is
+        else:
+            df_obs = df_obs
+            
+        # idem  
+        if var_name_sim != None:
+            df_sim = self.df[var_name_sim]
+        else:
+            df_sim = self.df
+        
+        # concatenate both dataframes
+        self.both = pd.concat([df_obs, df_sim], axis=1)
+        # drop all entries where any of the dataframes contains NaNs
+        # this yields a dataframe containing values only for common time period
+        both_noMV = self.both.dropna()
+
+        # raise error if there is no common time period
+        if self.both.empty:
+            os.sys.exit('no common time period of observed and simulated values found in dataframes!')
+        
+        # convert to np-arrays
+        obs = both_noMV[both_noMV.columns[0]].to_numpy()
+        sim = both_noMV[both_noMV.columns[1]].to_numpy()
+        
+        # apply objective functions
+        kge = sp.objectivefunctions.kge(obs, sim, return_all=return_all_KGE)
+        nse = sp.objectivefunctions.nashsutcliffe(obs, sim)
+        rmse = sp.objectivefunctions.rmse(obs, sim)
+        r2 = sp.objectivefunctions.rsquared(obs, sim)
+        
+        # fill dict
+        evaluation = {'KGE': kge,
+                    'NSE': nse,
+                    'RMSE': rmse,
+                    'R2': r2}
+
+        # save dict to csv
+        out_fo = os.path.join(out_dir, 'evaluation.csv')
+        w = csv.writer(open(out_fo, "w"))
+        for key, val in evaluation.items():
+            w.writerow([key, val])
+        
+        return self.both, evaluation
+
+    def calc_stats(self, out_dir, add_obs=False):
+        """Calculates statistics for both observed and simulated timeseries using the pandas describe function.
+
+        Keyword Arguments:
+            plot (bool): whether or not to plot the histogram (default: False)
+
+        Returns:
+            dataframe: dataframe containing statistical values
+        """        
+        if not add_obs:
+            stats = self.df.describe()
+        if add_obs:
+            stats = self.both.describe()
+
+        # save dict to csv
+        stats.to_csv(os.path.join(out_dir, 'stats.csv'))
+
+        return stats

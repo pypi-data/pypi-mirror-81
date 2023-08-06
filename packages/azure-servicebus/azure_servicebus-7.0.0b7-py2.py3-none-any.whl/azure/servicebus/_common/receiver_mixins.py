a25f60a1d69e69918651bd282f0979a378bd9f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:13 +0000

    Add package azure-servicebus/7.0.0b7/azure_servicebus-7.0.0b7-py2.py3-none-any.whl

---
+++ packages/azure-servicebus/azure_servicebus-7.0.0b7-py2.py3-none-any.whl/azure/servicebus/_common/receiver_mixins.py
@@ -0,0 +1,107 @@
+# ------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+import uuid
+from uamqp import Source
+from .message import ReceivedMessage
+from .constants import (
+    NEXT_AVAILABLE,
+    SESSION_FILTER,
+    SESSION_LOCKED_UNTIL,
+    DATETIMEOFFSET_EPOCH,
+    MGMT_REQUEST_SESSION_ID,
+    ReceiveMode
+)
+from ..exceptions import (
+    _ServiceBusErrorPolicy,
+    SessionLockExpired
+)
+from .utils import utc_from_timestamp, utc_now
+
+
+class ReceiverMixin(object):  # pylint: disable=too-many-instance-attributes
+    def _populate_attributes(self, **kwargs):
+        if kwargs.get("subscription_name"):
+            self._subscription_name = kwargs.get("subscription_name")
+            self._is_subscription = True
+            self.entity_path = self._entity_name + "/Subscriptions/" + self._subscription_name
+        else:
+            self.entity_path = self._entity_name
+
+        self._auth_uri = "sb://{}/{}".format(self.fully_qualified_namespace, self.entity_path)
+        self._entity_uri = "amqps://{}/{}".format(self.fully_qualified_namespace, self.entity_path)
+        self._receive_mode = kwargs.get("receive_mode", ReceiveMode.PeekLock)
+        # While we try to leave failures to the service, in this case the errors lower down the stack are less clear.
+        if not isinstance(self._receive_mode, ReceiveMode):
+            raise TypeError("Parameter 'receive_mode' must be of type ReceiveMode")
+
+        self._error_policy = _ServiceBusErrorPolicy(
+            max_retries=self._config.retry_total
+        )
+        self._name = "SBReceiver-{}".format(uuid.uuid4())
+        self._last_received_sequenced_number = None
+        self._message_iter = None
+        self._connection = kwargs.get("connection")
+        prefetch_count = kwargs.get("prefetch_count", 0)
+        if int(prefetch_count) < 0 or int(prefetch_count) > 50000:
+            raise ValueError("prefetch_count must be an integer between 0 and 50000 inclusive.")
+        self._prefetch_count = prefetch_count + 1
+        # The relationship between the amount can be received and the time interval is linear: amount ~= perf * interval
+        # In large max_message_count case, like 5000, the pull receive would always return hundreds of messages limited
+        # by the perf and time.
+        self._further_pull_receive_timeout_ms = 200
+        self._max_wait_time = kwargs.get("max_wait_time", None)
+
+    def _build_message(self, received, message_type=ReceivedMessage):
+        message = message_type(message=received, receive_mode=self._receive_mode, receiver=self)
+        self._last_received_sequenced_number = message.sequence_number
+        return message
+
+    def _check_live(self):
+        """check whether the receiver is alive"""
+
+    def _get_source(self):
+        return self._entity_uri
+
+    def _on_attach(self, source, target, properties, error):
+        pass
+
+    def _populate_message_properties(self, message):
+        pass
+
+
+class SessionReceiverMixin(ReceiverMixin):
+    def _get_source(self):
+        source = Source(self._entity_uri)
+        session_filter = None if self._session_id == NEXT_AVAILABLE else self._session_id
+        source.set_filter(session_filter, name=SESSION_FILTER, descriptor=None)
+        return source
+
+    def _on_attach(self, source, target, properties, error):  # pylint: disable=unused-argument
+        # pylint: disable=protected-access
+        if str(source) == self._entity_uri:
+            # This has to live on the session object so that autorenew has access to it.
+            self._session._session_start = utc_now()
+            expiry_in_seconds = properties.get(SESSION_LOCKED_UNTIL)
+            if expiry_in_seconds:
+                expiry_in_seconds = (expiry_in_seconds - DATETIMEOFFSET_EPOCH)/10000000
+                self._session._locked_until_utc = utc_from_timestamp(expiry_in_seconds)
+            session_filter = source.get_filter(name=SESSION_FILTER)
+            self._session_id = session_filter.decode(self._config.encoding)
+            self._session._session_id = self._session_id
+
+    def _check_live(self):
+        if self._session and self._session._lock_expired:  # pylint: disable=protected-access
+            raise SessionLockExpired(inner_exception=self._session.auto_renew_error)
+
+    def _populate_session_attributes(self, **kwargs):
+        self._session_id = kwargs.get("session_id") or NEXT_AVAILABLE
+        self._error_policy = _ServiceBusErrorPolicy(
+            max_retries=self._config.retry_total,
+            is_session=bool(self._session_id)
+        )
+
+    def _populate_message_properties(self, message):
+        message[MGMT_REQUEST_SESSION_ID] = self._session_id
---
+++ packages/azure-servicebus/azure_servicebus-7.0.0b7-py2.py3-none-any.whl/azure/servicebus/_common/utils.py
@@ -0,0 +1,217 @@
+# ------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
+
+import sys
+import datetime
+import logging
+import functools
+import platform
+import time
+from typing import Optional, Dict, Tuple
+from msrest.serialization import UTC
+
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse
+
+from uamqp import authentication, types
+
+from .._version import VERSION
+from .constants import (
+    JWT_TOKEN_SCOPE,
+    TOKEN_TYPE_JWT,
+    TOKEN_TYPE_SASTOKEN,
+    DEAD_LETTER_QUEUE_SUFFIX,
+    TRANSFER_DEAD_LETTER_QUEUE_SUFFIX,
+    USER_AGENT_PREFIX,
+)
+
+_log = logging.getLogger(__name__)
+
+
+def utc_from_timestamp(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp, tz=UTC())
+
+
+def utc_now():
+    return datetime.datetime.now(UTC())
+
+
+# This parse_conn_str is used for mgmt, the other in base_handler for handlers.  Should be unified.
+def parse_conn_str(conn_str):
+    # type: (str) -> Tuple[str, Optional[str], Optional[str], str, Optional[str], Optional[int]]
+    endpoint = ""
+    shared_access_key_name = None  # type: Optional[str]
+    shared_access_key = None  # type: Optional[str]
+    entity_path = ""
+    shared_access_signature = None  # type: Optional[str]
+    shared_access_signature_expiry = None  # type: Optional[int]
+    for element in conn_str.split(";"):
+        key, _, value = element.partition("=")
+        if key.lower() == "endpoint":
+            endpoint = value.rstrip("/")
+        elif key.lower() == "sharedaccesskeyname":
+            shared_access_key_name = value
+        elif key.lower() == "sharedaccesskey":
+            shared_access_key = value
+        elif key.lower() == "entitypath":
+            entity_path = value
+        elif key.lower() == "sharedaccesssignature":
+            shared_access_signature = value
+            try:
+                # Expiry can be stored in the "se=<timestamp>" clause of the token. ('&'-separated key-value pairs)
+                # type: ignore
+                shared_access_signature_expiry = int(
+                    shared_access_signature.split("se=")[1].split("&")[0]
+                )
+            except (
+                IndexError,
+                TypeError,
+                ValueError,
+            ):  # Fallback since technically expiry is optional.
+                # An arbitrary, absurdly large number, since you can't renew.
+                shared_access_signature_expiry = int(time.time() * 2)
+    if not (
+        all((endpoint, shared_access_key_name, shared_access_key))
+        or all((endpoint, shared_access_signature))
+    ) or all(
+        (shared_access_key_name, shared_access_signature)
+    ):  # this latter clause since we don't accept both
+        raise ValueError(
+            "Invalid connection string. Should be in the format: "
+            "Endpoint=sb://<FQDN>/;SharedAccessKeyName=<KeyName>;SharedAccessKey=<KeyValue>"
+            "\nWith alternate option of providing SharedAccessSignature instead of SharedAccessKeyName and Key"
+        )
+    return (
+        endpoint,
+        str(shared_access_key_name) if shared_access_key_name else None,
+        str(shared_access_key) if shared_access_key else None,
+        entity_path,
+        str(shared_access_signature) if shared_access_signature else None,
+        shared_access_signature_expiry,
+    )
+
+
+def build_uri(address, entity):
+    parsed = urlparse(address)
+    if parsed.path:
+        return address
+    if not entity:
+        raise ValueError("No Service Bus entity specified")
+    address += "/" + str(entity)
+    return address
+
+
+def create_properties(user_agent=None):
+    # type: (Optional[str]) -> Dict[types.AMQPSymbol, str]
+    """
+    Format the properties with which to instantiate the connection.
+    This acts like a user agent over HTTP.
+
+    :param str user_agent: If specified,
+    this will be added in front of the built-in user agent string.
+
+    :rtype: dict
+    """
+    properties = {}
+    properties[types.AMQPSymbol("product")] = USER_AGENT_PREFIX
+    properties[types.AMQPSymbol("version")] = VERSION
+    framework = "Python/{}.{}.{}".format(
+        sys.version_info[0], sys.version_info[1], sys.version_info[2]
+    )
+    properties[types.AMQPSymbol("framework")] = framework
+    platform_str = platform.platform()
+    properties[types.AMQPSymbol("platform")] = platform_str
+
+    final_user_agent = "{}/{} {} ({})".format(
+        USER_AGENT_PREFIX, VERSION, framework, platform_str
+    )
+    if user_agent:
+        final_user_agent = "{} {}".format(user_agent, final_user_agent)
+
+    properties[types.AMQPSymbol("user-agent")] = final_user_agent
+    return properties
+
+
+def renewable_start_time(renewable):
+    try:
+        return renewable._received_timestamp_utc  # pylint: disable=protected-access
+    except AttributeError:
+        pass
+    try:
+        return renewable._session_start  # pylint: disable=protected-access
+    except AttributeError:
+        raise TypeError("Registered object is not renewable.")
+
+
+def create_authentication(client):
+    # pylint: disable=protected-access
+    try:
+        # ignore mypy's warning because token_type is Optional
+        token_type = client._credential.token_type  # type: ignore
+    except AttributeError:
+        token_type = TOKEN_TYPE_JWT
+    if token_type == TOKEN_TYPE_SASTOKEN:
+        auth = authentication.JWTTokenAuth(
+            client._auth_uri,
+            client._auth_uri,
+            functools.partial(client._credential.get_token, client._auth_uri),
+            token_type=token_type,
+            timeout=client._config.auth_timeout,
+            http_proxy=client._config.http_proxy,
+            transport_type=client._config.transport_type,
+        )
+        auth.update_token()
+        return auth
+    return authentication.JWTTokenAuth(
+        client._auth_uri,
+        client._auth_uri,
+        functools.partial(client._credential.get_token, JWT_TOKEN_SCOPE),
+        token_type=token_type,
+        timeout=client._config.auth_timeout,
+        http_proxy=client._config.http_proxy,
+        transport_type=client._config.transport_type,
+    )
+
+
+def generate_dead_letter_entity_name(
+    queue_name=None, topic_name=None, subscription_name=None, transfer_deadletter=False
+):
+    entity_name = (
+        queue_name
+        if queue_name
+        else (topic_name + "/Subscriptions/" + subscription_name)
+    )
+    entity_name = "{}{}".format(
+        entity_name,
+        TRANSFER_DEAD_LETTER_QUEUE_SUFFIX
+        if transfer_deadletter
+        else DEAD_LETTER_QUEUE_SUFFIX,
+    )
+
+    return entity_name
+
+
+def transform_messages_to_sendable_if_needed(messages):
+    """
+    This method is to convert single/multiple received messages
+    to sendable messages to enable message resending.
+    """
+    # pylint: disable=protected-access
+    try:
+        msgs_to_return = []
+        for each in messages:
+            try:
+                msgs_to_return.append(each._to_outgoing_message())
+            except AttributeError:
+                msgs_to_return.append(each)
+        return msgs_to_return
+    except TypeError:
+        try:
+            return messages._to_outgoing_message()
+        except AttributeError:
+            return messages

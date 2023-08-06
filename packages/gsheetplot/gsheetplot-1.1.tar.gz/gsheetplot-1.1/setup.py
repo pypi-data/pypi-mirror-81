987c72d966b6bae03824557a0e6eb52928e1a79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:22 +0000

    Add package gsheetplot/1.1/gsheetplot-1.1.tar.gz

---
+++ packages/gsheetplot/gsheetplot-1.1.tar.gz/gsheetplot-1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='gsheetplot',
+      version='1.1',
+      description='Helps in Visualising Google Sheet Data',
+      long_description='Python Package Used to plot Bar Graph based on the data in Google Sheet',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+      ],
+      keywords='Python-Data-Visualization Bar-Graph Python-Package Google-Sheet-Visualisation Matplotlib',
+      url='https://github.com/ChaitanyaUndavalli/GSheetPlot',
+      author='Undavalli Lalkrishnachaitanya',
+      author_email='undavalli.14@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'Python3.7','Matplotlib','gspread'
+      ],
+      include_package_data=True,
+      zip_safe=False)

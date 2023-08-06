1d8111f624d94bad67c08ea5b601d10066904424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:27 +0000

    Add package FisherExactTest/1.0.1/FisherExactTest-1.0.1-py3-none-any.whl

---
+++ packages/FisherExactTest/FisherExactTest-1.0.1-py3-none-any.whl/FisherExactTest/__version__.py
@@ -0,0 +1,16 @@
+__title__ = "FisherExactTest"
+__version__ = "1.0.1"
+__author__ = "Ae-Mc"
+__author_email__ = "ae_mc@mail.ru"
+__description__ = "Two tailed Fisher's exact test wrote in pure Python"
+__url__ = "https://github.com/Ae-Mc/Fisher"
+__classifiers__ = [
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Topic :: Utilities"
+]

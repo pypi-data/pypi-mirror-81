5879c827800b9d5f886581227f2e26cf6b207abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:27 +0000

    Add package johann/0.2.0a0/johann-0.2.0a0-py3-none-any.whl

---
+++ packages/johann/johann-0.2.0a0-py3-none-any.whl/johann/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2019-present, The Johann Authors. All Rights Reserved.
+# Use of this source code is governed by a BSD-3-clause license that can
+# be found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+"""Johann, lightweight and flexible scenario orchestration"""
+
+__version__ = "0.2.0-alpha"

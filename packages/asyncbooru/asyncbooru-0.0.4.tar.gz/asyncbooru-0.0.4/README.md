d0caa25db62740d26cdf46a91825f9a6016cc438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:04 +0000

    Add package asyncbooru/0.0.4/asyncbooru-0.0.4.tar.gz

---
+++ packages/asyncbooru/asyncbooru-0.0.4.tar.gz/asyncbooru-0.0.4/README.md
@@ -0,0 +1,6 @@
+# asyncbooru
+
+This project exists simply because I want to learn new things.
+
+## To-do list
+* Maybe add support for auth
---
+++ packages/asyncbooru/asyncbooru-0.0.4.tar.gz/asyncbooru-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="asyncbooru",
+    version="0.0.4",
+    author="Nick80835",
+    author_email="nick80835@gmail.com",
+    description="An async library for interfacing with Danbooru and similar websites.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nick80835/asyncbooru",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["aiohttp"],
+    python_requires='>=3.6'
+)

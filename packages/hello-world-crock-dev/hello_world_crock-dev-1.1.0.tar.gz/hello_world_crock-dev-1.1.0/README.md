6fb15baaf682f58c73996fb5e9373da165b373a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:01 +0000

    Add package hello-world-crock-dev/1.1.0/hello_world_crock-dev-1.1.0.tar.gz

---
+++ packages/hello-world-crock-dev/hello_world_crock-dev-1.1.0.tar.gz/hello_world_crock-dev-1.1.0/README.md
@@ -0,0 +1,19 @@
+# Simple package PyPi
+
+This is a simple exercise to publish a package onto PyPi.
+
+## Installation
+
+Run the following to install
+
+```
+pip install hello-world-crock-dev
+```
+
+## Usage
+
+```python
+from hello_world.main import hello_world
+# Genarate "Hello world"
+hello_world()
+```
---
+++ packages/hello-world-crock-dev/hello_world_crock-dev-1.1.0.tar.gz/hello_world_crock-dev-1.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='hello_world_crock-dev',
+    version='1.1.0',
+    author='Crock-dev',
+    author_email='leiko.dima2013@yandex.by',
+    description='A Hello World package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/LeikoDmitry/hello-world',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests <= 2.24.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)

282523ceb0ef14369d4642705a32bfbb86739d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:31 +0000

    Add package treeschema/1.0.2/treeschema-1.0.2.tar.gz

---
+++ packages/treeschema/treeschema-1.0.2.tar.gz/treeschema-1.0.2/treeschema/exceptions.py
@@ -0,0 +1,19 @@
+
+class InvalidInputs(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class InvalidLinksException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class TreeSchemaApiError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class UsernameSecretRequired(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+

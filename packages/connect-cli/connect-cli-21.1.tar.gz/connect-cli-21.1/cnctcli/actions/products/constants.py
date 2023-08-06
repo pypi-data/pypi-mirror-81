d12375ff04321ebacb7a019c0918449d752e2964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:07 +0000

    Add package connect-cli/21.1/connect-cli-21.1.tar.gz

---
+++ packages/connect-cli/connect-cli-21.1.tar.gz/connect-cli-21.1/cnctcli/actions/products/constants.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of the Ingram Micro Cloud Blue Connect connect-cli.
+# Copyright (c) 2019-2020 Ingram Micro. All Rights Reserved.
+
+ITEMS_COLS_HEADERS = {
+    'A': 'ID',
+    'B': 'MPN',
+    'C': 'Name',
+    'D': 'Description',
+    'E': 'Type',
+    'F': 'Precision',
+    'G': 'Unit',
+    'H': 'Billing Period',
+    'I': 'Commitment',
+    'J': 'Status',
+    'K': 'Created',
+    'L': 'Modified',
+}

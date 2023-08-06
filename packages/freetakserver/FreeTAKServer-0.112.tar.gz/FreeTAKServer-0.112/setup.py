2b460342e3577d324e81f66d0e95d0c0e965641a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:15 +0000

    Add package freetakserver/0.112/FreeTAKServer-0.112.tar.gz

---
+++ packages/freetakserver/FreeTAKServer-0.112.tar.gz/FreeTAKServer-0.112/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='FreeTAKServer',
+    packages=find_packages(include = ['FreeTAKServer', 'FreeTAKServer.*']),
+    version='0.112',
+    license='MIT',
+    description='An open source server for the TAK family of applications.',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ghosty 1008',
+    author_email='your.email@domain.com',
+    url='https://github.com/Tapawingo/FreeTakServer',
+    download_url='https://github.com/Tapawingo/FreeTakServer/archive/v0.8.4-Beta.tar.gz',
+    keywords=['TAK', 'OPENSOURCE'],
+    install_requires=[
+        'flask',
+        'lxml',
+        'pathlib',
+        'tabulate',
+        'sqlalchemy',
+        'setuptools'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)

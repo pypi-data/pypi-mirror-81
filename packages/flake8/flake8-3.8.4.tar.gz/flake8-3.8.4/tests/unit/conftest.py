97b539a238ff33713d691431a5c4d208a819db43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:58 +0000

    Add package flake8/3.8.4/flake8-3.8.4.tar.gz

---
+++ packages/flake8/flake8-3.8.4.tar.gz/flake8-3.8.4/tests/unit/conftest.py
@@ -0,0 +1,21 @@
+"""Shared fixtures between unit tests."""
+import argparse
+
+import pytest
+
+
+def options_from(**kwargs):
+    """Generate a Values instances with our kwargs."""
+    kwargs.setdefault('hang_closing', True)
+    kwargs.setdefault('max_line_length', 79)
+    kwargs.setdefault('max_doc_length', None)
+    kwargs.setdefault('verbose', False)
+    kwargs.setdefault('stdin_display_name', 'stdin')
+    kwargs.setdefault('disable_noqa', False)
+    return argparse.Namespace(**kwargs)
+
+
+@pytest.fixture
+def default_options():
+    """Fixture returning the default options of flake8."""
+    return options_from()

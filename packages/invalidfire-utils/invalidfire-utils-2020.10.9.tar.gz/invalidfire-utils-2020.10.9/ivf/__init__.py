0160268fc780cdd694d377363e4819f0bdf48560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:10 +0000

    Add package invalidfire-utils/2020.10.9/invalidfire-utils-2020.10.9.tar.gz

---
+++ packages/invalidfire-utils/invalidfire-utils-2020.10.9.tar.gz/invalidfire-utils-2020.10.9/ivf/__init__.py
@@ -0,0 +1,2 @@
+from .config import save, load, reload
+from .files import get_program_dir, get_program_path, download, create_zip, extract_zip
---
+++ packages/invalidfire-utils/invalidfire-utils-2020.10.9.tar.gz/invalidfire-utils-2020.10.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='invalidfire-utils',
+    version='2020.10.9',
+    packages=['ivf'],
+    url='https://github.com/InValidFire/invalidfire-utils',
+    license='The Unlicense',
+    author='InValidFire',
+    author_email='',
+    description='Various Utilities for my projects.'
+)

b794b582abc958c12de68be3966cbffe48770a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:06 +0000

    Add package actarius/0.0.2/actarius-0.0.2.tar.gz

---
+++ packages/actarius/actarius-0.0.2.tar.gz/actarius-0.0.2/tests/temp_env_var.py
@@ -0,0 +1,9 @@
+TEMP_ENV_VARS = {
+    'DATABRICKS_HOST': 'https://community.cloud.databricks.com/',
+    'DATABRICKS_USERNAME': 'actarius.package@gmail.com',
+    'DATABRICKS_PASSWORD': 'fXbEO75mvE7BojNzosXrL3jB^6rNsbDTKhRP3wMqDvd2mxl6',
+}
+
+ENV_VARS_TO_SUSPEND = [
+    'DATABRICKS_TOKEN',
+]

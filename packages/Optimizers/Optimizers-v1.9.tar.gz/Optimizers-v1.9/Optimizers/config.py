8ac94f430694e4968663ade021b5ed4e34f4a441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:21 +0000

    Add package Optimizers/v1.9/Optimizers-v1.9.tar.gz

---
+++ packages/Optimizers/Optimizers-v1.9.tar.gz/Optimizers-v1.9/Optimizers/config.py
@@ -0,0 +1,8 @@
+class Server():
+    url = 'https://optimizer.davidswlai.com'
+    key = '39FA7035-B511-44CB-8B94-79E93A77648F'
+
+
+def usr_login(url=Server.url, key=Server.key):
+    Server.url = url
+    Server.key = key

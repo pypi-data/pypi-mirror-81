08a8cae12078e092da3e5bc939749b7d35e12f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:24 +0000

    Add package insightconnect-plugin-runtime/4.2.0/insightconnect-plugin-runtime-4.2.0.tar.gz

---
+++ packages/insightconnect-plugin-runtime/insightconnect-plugin-runtime-4.2.0.tar.gz/insightconnect-plugin-runtime-4.2.0/insightconnect_plugin_runtime/step.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from io import StringIO
+
+
+class Step(object):
+    """A action"""
+
+    def __init__(self, name, description, input, output, state=None):
+        self.name = name
+        self.description = description
+        self.input = input
+        self.output = output
+        self.state = state
+        self.connection = None
+        self.debug = False
+        self.stream = StringIO()
+        self.handler = logging.StreamHandler(self.stream)
+        self.logger = logging.getLogger(self.name)
+
+    def run(self, params={}):
+        """ Run a action, return output or raise error """
+        raise NotImplementedError
+
+    def test(self, params={}):
+        """
+        Test an action, return output or raise error
+        Deprecated in favor of using the test function in the plugin connection.py file
+        """
+        pass
---
+++ packages/insightconnect-plugin-runtime/insightconnect-plugin-runtime-4.2.0.tar.gz/insightconnect-plugin-runtime-4.2.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="insightconnect-plugin-runtime",
+    version="4.2.0",
+    description="InsightConnect Plugin Runtime",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Rapid7 Integrations Alliance",
+    author_email="integrationalliance@rapid7.com",
+    url="https://github.com/rapid7/komand-plugin-sdk-python",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.23.0",
+        "python_jsonschema_objects==0.3.12",
+        "jsonschema==3.2.0",
+        "certifi==2019.11.28",
+        "Flask==1.1.1",
+        "gunicorn==20.0.4",
+        "gevent==20.9.0",
+        "marshmallow==3.4.0",
+        "apispec==3.2.0",
+        "apispec-webframeworks==0.5.2",
+    ],
+    tests_require=[
+        "pytest",
+        "docker",
+        "dockerpty",
+        "swagger-spec-validator",
+        "pre-commit",
+    ],
+    test_suite="tests",
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Build Tools",
+    ],
+)

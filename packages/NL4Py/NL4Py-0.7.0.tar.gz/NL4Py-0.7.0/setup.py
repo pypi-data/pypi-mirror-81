ad7017ccece1b95d17473414d24aeb894629f9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:10 +0000

    Add package NL4Py/0.7.0/NL4Py-0.7.0.tar.gz

---
+++ packages/NL4Py/NL4Py-0.7.0.tar.gz/NL4Py-0.7.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools  import setup
+
+setup(
+    name='NL4Py',
+    version='0.7.0',
+    author='Chathika Gunaratne',
+    author_email='chathikagunaratne@gmail.com',
+    packages=['nl4py', 'nl4py.test'],
+    include_package_data=True,
+    url='https://github.com/chathika/NL4Py',
+    license='GPL',
+    description='A NetLogo connector for Python.',
+    long_description="""https://github.com/chathika/NL4Py""",
+    long_description_content_type='text/markdown',
+    project_urls={
+    'Source': 'https://github.com/chathika/NL4Py',
+    'Thanks!': 'https://github.com/dmasad/Py2NetLogo',
+    'Thanks!': 'http://complexity.cecs.ucf.edu/',
+    },
+    install_requires=[
+        'matplotlib >= 2.0.2',
+        'py4j >= 0.10.6',
+        'psutil >= 5.4.3',
+		'pandas >= 0.20.1',
+		'numpy >= 1.13.3',
+    ]
+)
+

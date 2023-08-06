819ada029880b454f23eace4c2a1e90f8de34abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:14 +0000

    Add package ndlearn/0.0.2/ndlearn-0.0.2-py2.py3-none-any.whl

---
+++ packages/ndlearn/ndlearn-0.0.2-py2.py3-none-any.whl/ndl/__init__.py
@@ -0,0 +1,17 @@
+
+"""Top-level package for Network-Dictionary-Learning."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.2"
+
+__author__ = "Joshua Vendrow"
+__email__ = "jvendrow@math.ucla.edu"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020, Joshua Vendrow"
+
+
+from .ndl import *
+from .onmf import *
+from .NNetwork import *

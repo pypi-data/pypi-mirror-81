112627cc07d4fe22e6bccbe5a74675a8ac287bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:27 +0000

    Add package konfik/0.1.5/konfik-0.1.5.tar.gz

---
+++ packages/konfik/konfik-0.1.5.tar.gz/konfik-0.1.5/konfik/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1.5"
+
+from .main import Konfik
---
+++ packages/konfik/konfik-0.1.5.tar.gz/konfik-0.1.5/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['konfik']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dotenv>=0.14.0,<0.15.0', 'rich>=8.0.0,<9.0.0', 'toml>=0.10.1,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['konfik = konfik.main:deploy_cli']}
+
+setup_kwargs = {
+    'name': 'konfik',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': None,
+    'author': 'rednafi',
+    'author_email': 'redowan.nafi@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

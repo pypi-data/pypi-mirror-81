a978778e8c06d92cab19e38100027fa7479ab38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:53 +0000

    Add package pyrameter/0.2.0.post1/pyrameter-0.2.0.post1.tar.gz

---
+++ packages/pyrameter/pyrameter-0.2.0.post1.tar.gz/pyrameter-0.2.0.post1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='pyrameter',
+    version='0.2.0post1',
+    description='Structure, sample, and savor hyperparameter searches',
+    url='https://github.com/jeffkinnison/pyrameter',
+    author='Jeff Kinnison',
+    author_email='jkinniso@nd.edu',
+    packages=['pyrameter',
+              'pyrameter.backend',
+              'pyrameter.domains',
+              'pyrameter.methods',],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+    ],
+    keywords='machine_learning hyperparameters',
+    install_requires=[
+        'dill',
+        'numpy',
+        'pandas',
+        'pymongo',
+        'scipy',
+        'scikit-learn',
+        'six',
+    ],
+)

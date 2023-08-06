2a75507447e9198ce71bd3b4b760bbbf021d37fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:20 +0000

    Add package iot-device/0.2.1/iot_device-0.2.1-py3-none-any.whl

---
+++ packages/iot-device/iot_device-0.2.1-py3-none-any.whl/iot_device/default_config.py
@@ -0,0 +1,22 @@
+# default config for IoT49
+
+import os
+
+default_config = {}
+default_docs   = {}
+
+def __register(name, value, doc):
+    default_config[name] = value
+    default_docs  [name] = doc
+
+__register('host_dir', 
+    os.path.expanduser(os.path.join(os.getenv('IOT49', '~/'), 'mcu')),
+    "Configuration and libraries. Default: $IOT49/mcu")
+
+__register('server_port', 
+    50001,
+    "Port on which DeviceServer listens for connections")
+
+__register('password', 
+    "replace with a strong password",
+    "Password protection for NetDevice & DeviceServer")

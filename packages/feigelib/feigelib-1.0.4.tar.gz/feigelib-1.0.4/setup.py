d20124f20c2f53f0864c8942335ad0ce16c8007a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:11 +0000

    Add package feigelib/1.0.4/feigelib-1.0.4.tar.gz

---
+++ packages/feigelib/feigelib-1.0.4.tar.gz/feigelib-1.0.4/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name="feigelib", #库的名称
+    version="1.0.4", #版本号
+    author="飞鸽工作室", #名字（化名）
+    author_email="3357135715@qq.com",
+    description="基本格式：print(左双引号 想说的话 右双引号 等待时间)", #介绍
+    url="https://github.com",
+)

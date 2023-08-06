ca2225d816a11a0111adb0c0a8012c6e3a991dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:16 +0000

    Add package aurflux/3.0.29/aurflux-3.0.29.tar.gz

---
+++ packages/aurflux/aurflux-3.0.29.tar.gz/aurflux-3.0.29/aurflux/__init__.py
@@ -0,0 +1,5 @@
+from .flux import FluxClient, FluxEvent, CommandEvent
+from .config import Config
+from .errors import *
+__package__ = "aurflux"
+__all__ = ["FluxClient", "FluxEvent"]
---
+++ packages/aurflux/aurflux-3.0.29.tar.gz/aurflux-3.0.29/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aurflux',
+ 'aurflux.cog',
+ 'aurflux.command',
+ 'aurflux.context',
+ 'aurflux.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aurcore', 'discord.py', 'loguru>=0.5.2,<0.6.0', 'pyyaml']
+
+setup_kwargs = {
+    'name': 'aurflux',
+    'version': '3.0.29',
+    'description': 'Aurflux!',
+    'long_description': None,
+    'author': 'Zenith',
+    'author_email': 'inbox@zenith.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)

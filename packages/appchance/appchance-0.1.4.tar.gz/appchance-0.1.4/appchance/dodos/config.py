50020e678dc6236b6281ed65184a91d5576dd4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:11 +0000

    Add package appchance/0.1.4/appchance-0.1.4.tar.gz

---
+++ packages/appchance/appchance-0.1.4.tar.gz/appchance-0.1.4/appchance/dodos/config.py
@@ -0,0 +1,17 @@
+# Project configuration
+
+# Specify command with path to your docker compose file.
+DOCKER_COMPOSE_CMD = "docker-compose -f docker-compose.yml"
+
+# Service name running django in docker-compose file.
+DJANGO_SERVICE_NAME = "django"
+DJANGO_APPS_DIR = "apps"
+
+# Active task modules.
+TASK_MODULES = [
+    "python",
+    "docker_django",
+]
+
+# Place to specify your custom tasks.
+CUSTOM_TASKS = []

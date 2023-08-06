f93b5f0a47b6b678ba3bbc303dd5b7771af5cbdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:20 +0000

    Add package peppy/0.31.0/peppy-0.31.0-py3-none-any.whl

---
+++ packages/peppy/peppy-0.31.0-py3-none-any.whl/peppy/exceptions.py
@@ -0,0 +1,57 @@
+""" Custom error types """
+
+from abc import ABCMeta
+try:
+    from collections.abc import Iterable
+except ImportError:
+    # for py2
+    from collections import Iterable
+
+__all__ = ["IllegalStateException", "InvalidSampleTableFileException",
+           "PeppyError", "MissingAmendmentError", "InvalidConfigFileException",
+           "SampleTableFileException"]
+
+
+class PeppyError(Exception, metaclass=ABCMeta):
+    """ Base error type for peppy custom errors. """
+
+    def __init__(self, msg):
+        super(PeppyError, self).__init__(msg)
+
+
+class IllegalStateException(PeppyError):
+    """ Occurrence of some illogical/prohibited state within an object. """
+    pass
+
+
+class SampleTableFileException(PeppyError):
+    """ Error type for invalid sample annotations file. """
+    pass
+
+
+class InvalidSampleTableFileException(SampleTableFileException):
+    """ Error type for invalid sample annotations file. """
+    pass
+
+
+class MissingAmendmentError(PeppyError):
+    """ Error when project config lacks a requested subproject. """
+
+    def __init__(self, amendment, defined=None):
+        """
+        Create exception with missing amendment request.
+
+        :param str amendment: the requested (and missing) amendment
+        :param Iterable[str] defined: collection of names of defined amendment
+        """
+        msg = "Amendment '{}' not found".format(amendment)
+        if isinstance(defined, Iterable):
+            ctx = "defined amendments(s): {}".\
+                format(", ".join(map(str, defined)))
+            msg = "{}; {}".format(msg, ctx)
+        super(MissingAmendmentError, self).__init__(msg)
+
+
+class InvalidConfigFileException(PeppyError):
+    """ Error type for invalid project config file """
+    pass
---
+++ packages/peppy/peppy-0.31.0-py3-none-any.whl/peppy/utils.py
@@ -0,0 +1,68 @@
+""" Helpers without an obvious logical home. """
+
+import logging
+from .const import CONFIG_KEY
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def copy(obj):
+    def copy(self):
+        """
+        Copy self to a new object.
+        """
+        from copy import deepcopy
+
+        return deepcopy(self)
+    obj.copy = copy
+    return obj
+
+
+def grab_project_data(prj):
+    """
+    From the given Project, grab Sample-independent data.
+
+    There are some aspects of a Project of which it's beneficial for a Sample
+    to be aware, particularly for post-hoc analysis. Since Sample objects
+    within a Project are mutually independent, though, each doesn't need to
+    know about any of the others. A Project manages its, Sample instances,
+    so for each Sample knowledge of Project data is limited. This method
+    facilitates adoption of that conceptual model.
+
+    :param Project prj: Project from which to grab data
+    :return Mapping: Sample-independent data sections from given Project
+    """
+    if not prj:
+        return {}
+
+    try:
+        return prj[CONFIG_KEY].to_dict()
+    except KeyError:
+        raise KeyError("Project lacks section '{}'".format(CONFIG_KEY))
+    return data
+
+
+def make_list(arg, obj_class):
+    """
+    Convert an object of predefined class to a list of objects of that class or
+    ensure a list is a list of objects of that class
+
+    :param list[obj] | obj arg: string or a list of strings to listify
+    :param str obj_class: name of the class of intrest
+    :return list: list of objects of the predefined class
+    :raise TypeError: if a faulty argument was provided
+    """
+    def _raise_faulty_arg():
+        raise TypeError("Provided argument has to be a list[{o}] or a {o}, "
+                        "got '{a}'".format(o=obj_class.__name__,
+                                           a=arg.__class__.__name__))
+
+    if isinstance(arg, obj_class):
+        return [arg]
+    elif isinstance(arg, list):
+        if not all(isinstance(i, obj_class) for i in arg):
+            _raise_faulty_arg()
+        else:
+            return arg
+    else:
+        _raise_faulty_arg()

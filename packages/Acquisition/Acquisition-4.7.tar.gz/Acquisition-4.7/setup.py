308efeb0cda8c2d30ab87902e2ec3569d77b8eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:39 +0000

    Add package Acquisition/4.7/Acquisition-4.7.tar.gz

---
+++ packages/Acquisition/Acquisition-4.7.tar.gz/Acquisition-4.7/setup.py
@@ -0,0 +1,79 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for the Acquisition distribution
+"""
+import os
+import platform
+from setuptools import setup, find_packages, Extension
+
+with open('README.rst') as f:
+    README = f.read()
+
+with open('CHANGES.rst') as f:
+    CHANGES = f.read()
+
+# PyPy won't build the extension.
+py_impl = getattr(platform, 'python_implementation', lambda: None)
+is_pypy = py_impl() == 'PyPy'
+is_pure = 'PURE_PYTHON' in os.environ
+if is_pypy or is_pure:
+    ext_modules = []
+else:
+    ext_modules = [
+        Extension("Acquisition._Acquisition",
+                  [os.path.join('src', 'Acquisition', '_Acquisition.c')],
+                  include_dirs=['include', 'src']),
+    ]
+
+version = '4.7'
+
+setup(
+    name='Acquisition',
+    version=version,
+    url='https://github.com/zopefoundation/Acquisition',
+    license='ZPL 2.1',
+    description="Acquisition is a mechanism that allows objects to obtain "
+    "attributes from the containment hierarchy they're in.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description='\n\n'.join([README, CHANGES]),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    ext_modules=ext_modules,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    install_requires=[
+        'ExtensionClass >= 4.2.0',
+        'zope.interface',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)

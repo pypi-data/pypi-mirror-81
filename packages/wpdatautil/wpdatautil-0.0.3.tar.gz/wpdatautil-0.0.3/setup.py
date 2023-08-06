6f4105b6271b7faa57d1ace8c6a623d32cac7c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:52 +0000

    Add package wpdatautil/0.0.3/wpdatautil-0.0.3.tar.gz

---
+++ packages/wpdatautil/wpdatautil-0.0.3.tar.gz/wpdatautil-0.0.3/setup.py
@@ -0,0 +1,27 @@
+"""Package installation setup."""
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+_DIR = Path(__file__).parent
+
+
+setup(
+    name="wpdatautil",
+    author="Data Util",
+    author_email="user@users.nomail.github.com",
+    version="0.0.3",
+    description="Utility functions for data processing",
+    keywords="utilities",
+    long_description=(_DIR / "README_PyPI.md").read_text().strip(),
+    long_description_content_type="text/markdown",
+    # url="https://pypi.org/project/wpdatautil/",  # Intentionally not enabled or linked to GitHub!
+    packages=find_packages(exclude=["scripts", "tests"]),
+    python_requires=">=3.8",
+    classifiers=[  # https://pypi.org/classifiers/
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+)

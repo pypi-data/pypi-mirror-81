56250f22a3a1335f1ef6d346af5d755fab9479a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:15 +0000

    Add package HBT-IP-Test/1.0.1/HBT_IP_Test-1.0.1.tar.gz

---
+++ packages/HBT-IP-Test/HBT_IP_Test-1.0.1.tar.gz/HBT_IP_Test-1.0.1/HBT_IP_Test/src/common/config.py
@@ -0,0 +1,7 @@
+class Config:    
+    spgproto             =       "spBv1.0"
+    groupId              =       "HBT Security" 
+    brokerAddr           =       "localhost"
+    brokerPort           =       1886
+    myUsername           =       "admin"
+    myPassword           =       "changeme"

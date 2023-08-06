cf813e95468389e052ba1f1f4a7650d66895bbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:20 +0000

    Add package pytapo/0.9/pytapo-0.9.tar.gz

---
+++ packages/pytapo/pytapo-0.9.tar.gz/pytapo-0.9/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytapo",
+    version="0.9",
+    author="Juraj Nyíri",
+    author_email="juraj.nyiri@gmail.com",
+    description="Python library for communication with Tapo Cameras",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JurajNyiri/pytapo",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'requests',
+          'urllib3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

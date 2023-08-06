558cf69b67ed521ac04072b985c8bc76a27cb95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:19 +0000

    Add package vulcan-athena/0.1.13/vulcan_athena-0.1.13.tar.gz

---
+++ packages/vulcan-athena/vulcan_athena-0.1.13.tar.gz/vulcan_athena-0.1.13/vulcan_athena/mirandareporting/setup.py
@@ -0,0 +1,75 @@
+'''
+This file is for setting up the global variables used throughout the project
+Customized folder paths and details to be initialized here
+'''
+
+# initialize data - reading from blob storage
+filepath = 'pass'
+sheetname = 'pass'
+
+STATUS = ['Delivered', 'Future', 'Prospective']
+BUSINESS_LINE = ['TEM Corporate', 'TEM Bluehalo', 'Qantas FP', 'Qantas FCN']
+
+TEST_MODE = False
+
+EXCEL_FILENAME = "MVP template - June 2020.xlsx"
+# "MVP_2.xlsx"
+EXCEL_PATH = "reports/bridge/"
+
+# column mapping
+COL_DICT = {'July': 'C', 'August': 'D', 'September': 'E', 'October': 'F', 'November': 'G', 'December': 'H',
+            'January': 'I',
+            'February': 'J', 'March': 'K', 'April': 'L', 'May': 'M', 'June': 'N'}
+COL_DICT_FUTURE = {'July': 'S', 'August': 'T', 'September': 'U', 'October': 'V', 'November': 'W', 'December': 'X',
+                   'January': 'Y',
+                   'February': 'Z', 'March': 'AA', 'April': 'AB', 'May': 'AC', 'June': 'AD'}
+
+SALES_LIST = ['Delivered TEM Corporate', 'Delivered TEM Bluehalo', 'Delivered Qantas FP', 'Delivered Qantas FCN',
+              'Future TEM Corporate', 'Future TEM Bluehalo', 'Future Qantas FP', 'Future Qantas FCN',
+              'Prospective TEM Corporate', 'Prospective TEM Bluehalo', 'Prospective Qantas FP',
+              'Prospective Qantas FCN']
+
+GEN_LIST = ['GSC-XXX-GS Generalised', 'KACCU-AUS-Generic HIR', 'KACCU-Generic', 'VCS-IND-Wind Generalised',
+            'VCS-IND-Generalised Biomass', 'KACCU-AUS-ALFA Generalised',
+            'NZU-NZL-Generalised', 'EUA-EUR-Generic European Allowance', "LGC-AUS-Generic LGC's"]
+
+FY_YEAR = [2020, 2021, 2022]
+FY_MAPPER = {'July': 1, 'August': 2, 'September': 3, 'October': 4, 'November': 5, 'December': 6,
+             'January': 7, 'February': 8, 'March': 9, 'April': 10, 'May': 11, 'June': 12}
+
+HIR = ['KACCU-AUS-Babinda GBR',
+       'KACCU-AUS-Barney Gumble HIR',
+       'KACCU-AUS-Berangabah HIR',
+       'KACCU-AUS-Bierbank & Lanherne HIR',
+       'KACCU-AUS-Blinky Forest HIR',
+       'KACCU-AUS-Boonora Downs HIR',
+       'KACCU-AUS-Byrock Station Regrowth Project',
+       'KACCU-AUS-Colodan GBR',
+       'KACCU-AUS-Colodan GBR #6',
+       'KACCU-AUS-Darling River Eco Corridor 3',
+       'KACCU-AUS-Generic HIR',
+       'KACCU-AUS-Hillview Park',
+       'KACCU-AUS-Kilcowera & Zenonie HIR',
+       'KACCU-AUS-Lindermans HIR',
+       'KACCU-AUS-Lynwood Human-Induced Regeneration Project',
+       'KACCU-AUS-Mullagalah HIR',
+       'KACCU-AUS-Mullagalah II HIR',
+       'KACCU-AUS-Myroolia HIR',
+       'KACCU-AUS-Paroo River HIR',
+       'KACCU-AUS-Paroowidgee HIR',
+       'KACCU-AUS-Quimby Forest HIR',
+       'KACCU-AUS-The Range HIR',
+       'KACCU-AUS-Tuncoona Forest',
+       'KACCU-AUS-Uteara Regeneration',
+       'KACCU-AUS-Wiralla Regeneration HIR',
+       'KACCU-AUS-Wongalee Mervyndale & Rundalua HIR',
+       'KACCU-AUS-Woodstock Regeneration Project',
+       'NKACCU-AUS-Inverness Human-Induced Regen',
+       'KACCU-AUS-Generic HIR'
+       ]
+
+# OpEx information
+client_id = 'A81BEC698C134BC88405217CA4AD3B0D'
+client_secret = 'DQBNeDAUV4BPdSf2-Dx_FJz1hNqH6d0AzImAZSh5rd372u0R'
+redirect_url = 'https://xero.com/'
+scope = 'offline_access payroll.payruns'

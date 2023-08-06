97255802fd0524c89fdcd98dc4563b4f49bc0d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:42 +0000

    Add package mautrix-signal/0.1.0b1/mautrix-signal-0.1.0b1.tar.gz

---
+++ packages/mautrix-signal/mautrix-signal-0.1.0b1.tar.gz/mautrix-signal-0.1.0b1/mautrix_signal/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = 'v0.1.0b1'
+git_revision = '4e8769f0'
+version = '0.1.0b1'
+linkified_version = '[0.1.0b1](https://github.com/tulir/mautrix-signal/releases/tag/v0.1.0b1)'

bf3c83c4d5f0a0c8bafb89a8fe13e4e1c0a6d91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:35 +0000

    Add package celery-logger/0.0.1/celery-logger-0.0.1.tar.gz

---
+++ packages/celery-logger/celery-logger-0.0.1.tar.gz/celery-logger-0.0.1/setup.py
@@ -0,0 +1,40 @@
+from os import path
+
+import setuptools
+
+requirements_file = path.join("requirements", "base.in")
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open(requirements_file) as rfh:
+    contents = rfh.read()
+    requirements = contents.strip().split("\n")
+
+setuptools.setup(
+    author="Thiago Ferreira",
+    author_email="th.thiago96@gmail.com",
+    license="BSD license",
+    name="celery-logger",
+    version="0.0.1",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Python library for logging celery events",
+    install_requires=requirements,
+    keywords="logging, celery",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["celerylogger"],
+    python_requires=">=3.7",
+    url="https://github.com/CraveFood/celery-logger",
+)

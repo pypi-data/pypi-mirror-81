feda5ba0d5ef639d7b56b0ed7d3ed6b0fae444eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:17 +0000

    Add package aiohttphelper/2.3.0/aiohttphelper-2.3.0.tar.gz

---
+++ packages/aiohttphelper/aiohttphelper-2.3.0.tar.gz/aiohttphelper-2.3.0/aiohttphelper/functions.py
@@ -0,0 +1,65 @@
+import asyncio
+import aiohttp
+from types import SimpleNamespace
+
+def __list_to_namespace(data):
+    obj = SimpleNamespace()
+    obj.text = data[0]
+    obj.status = data[1]
+    obj.url = str(data[2])
+    return obj
+
+def __transform_results(results):
+    new_results = []
+    for result in results:
+        new_results.append(__list_to_namespace(result))
+    return new_results
+
+
+
+async def __fetch(session, call, headers, **kwargs):
+    async with session.get(call, headers=headers, **kwargs) as response:
+        return await response.text(), response.status, response.url
+
+async def __update(session, call, headers, **kwargs):
+    async with session.put(call[0], headers=headers, data=call[1], **kwargs) as response:
+        return await response.text(), response.status, response.url
+        
+async def __create(session, call, headers, **kwargs):
+    async with session.post(call[0], headers=headers, data=call[1], **kwargs) as response:
+        return await response.text(), response.status, response.url
+
+async def __remove(session, call, headers, **kwargs):
+    async with session.get(call, headers=headers, **kwargs) as response:
+        return await response.text(), response.status, response.url
+
+async def __async_call(method, calls, headers, **kwargs):
+    async with aiohttp.ClientSession(raise_for_status=True) as session:
+        if not isinstance(calls, (list, tuple)):
+            calls = [calls]
+        tasks = [ asyncio.ensure_future(method(session, call, headers, **kwargs)) for call in calls]
+        return await asyncio.gather(*tasks)
+
+def get(calls, headers, **kwargs):
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(__async_call(__fetch, calls, headers, **kwargs))
+    results_objects = __transform_results(results)
+    return results_objects
+            
+def put(calls, headers, **kwargs):
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(__async_call(__update, calls, headers, **kwargs))
+    results_objects = __transform_results(results)
+    return results_objects
+
+def post(calls, headers, **kwargs):
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(__async_call(__create, calls, headers, **kwargs))
+    results_objects = __transform_results(results)
+    return results_objects
+
+def delete(calls, headers, **kwargs):
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(__async_call(__remove, calls, headers, **kwargs))
+    results_objects = __transform_results(results)
+    return results_objects
---
+++ packages/aiohttphelper/aiohttphelper-2.3.0.tar.gz/aiohttphelper-2.3.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aiohttphelper", # Replace with your own username
+    version="2.3.0",
+    author="Gregory Barillé",
+    author_email="contact@gregorybarille.io",
+    description="Simple wrapper for aiohttp. Designed for my own use but might be useful to others.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gregorybarille/aiocalls",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["aiohttp[speedups]==3.6.2"]
+)

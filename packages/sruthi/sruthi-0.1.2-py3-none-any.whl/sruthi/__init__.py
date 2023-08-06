7897cfbca6d98f12c68ce974ac3adbc6af0696c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:49 +0000

    Add package sruthi/0.1.2/sruthi-0.1.2-py3-none-any.whl

---
+++ packages/sruthi/sruthi-0.1.2-py3-none-any.whl/sruthi/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1.2'
+__all__ = ['client', 'errors', 'response', 'sru', 'xmlparse']
+
+from .errors import SruthiError, ServerIncompatibleError, SruError, NoMoreRecordsError  # noqa
+from .errors import SruthiWarning, WrongNamespaceWarning # noqa
+from .sru import searchretrieve, explain  # noqa
+from .client import Client # noqa

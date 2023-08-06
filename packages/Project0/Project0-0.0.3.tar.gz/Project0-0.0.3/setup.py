79e564a4ec736a25ae9c14690b394e6e35cc1ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:18 +0000

    Add package Project0/0.0.3/Project0-0.0.3.tar.gz

---
+++ packages/Project0/Project0-0.0.3.tar.gz/Project0-0.0.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+#def readme():
+#  with open('README.rst') as f:
+#    return f.read()
+
+setup(
+  name='Project0',
+  version='0.0.3',
+  description='Offline vocal assistant and tts',
+#  long_description=readme(),
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
+    'Topic :: Multimedia :: Sound/Audio :: Speech',
+    'Topic :: Text Processing',
+  ],
+  keywords='vocal assistant offline tts',
+  url='http://github.com/fenrir77/project0',
+  author='Fenrir77',
+  author_email='fenrir7377@gmail.com',
+  license='MIT',
+  packages=['Project0'],
+  install_requires=[
+    'markdown',
+    'PyAudio',
+    'pyttsx',
+    'SpeechRecognition',
+    'PocketSphinx'
+  ],
+  test_suite='nose.collectior',
+  tests_require=['nose', 'nose-cover3'],
+  entry_points={
+    'console_scripts': ['main=project0.command_line:main'],
+  },
+  include_package_data=True,
+  zip_safe=False
+)

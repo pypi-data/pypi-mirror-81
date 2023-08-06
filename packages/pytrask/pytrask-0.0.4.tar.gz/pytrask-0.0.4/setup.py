cee2165049787c8afc6dffbdadf1ee454660e4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:14 +0000

    Add package pytrask/0.0.4/pytrask-0.0.4.tar.gz

---
+++ packages/pytrask/pytrask-0.0.4.tar.gz/pytrask-0.0.4/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytrask", # Replace with your own username
+    version="0.0.4",
+    author="Nimelli",
+    author_email="",
+    description="Minimalist Kanban task management tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nimelli/pytrask",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'flask',
+        'flaskwebgui'
+    ],
+    package_data = {
+        'pytrask': ['templates/*']
+    }
+)
